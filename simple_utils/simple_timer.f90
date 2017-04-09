3dd7ff845704e6072740c1f2b455c7969b01d7ab
Michael Eager <mick.eager@gmail.com>
2017-04-10 09:39:25 +1000

    Simple_timer module complete and working
    Test examples included in cuda/timing/simple_timer_basic_test.f90

--- simple_utils/simple_timer.f90
+++ simple_utils/simple_timer.f90
@@ -1,164 +1,100 @@
-!= Module simple_timer
+!==Module simple_timer
 !
+!>\brief High resolution timer in fortran
 !
-! Michael Eager 2017-03-15
+!  64 bit INT implementation of system_clock
+!  present in gfortran and pgfortran
+!<------------------------------------
 
 module simple_timer
-  use precision_m
-#ifdef _CUDA
-  use cudafor
-#endif
-  implicit none
+!     use simple_jiffys ! singleton
+!     use simple_defs   ! singleton
+   use precision_m
+   implicit none
 
+!  private :: raise_sys_error
+   private
+   logical :: debug = .false.
+   logical :: warn = .false.
+   integer(dp), save :: clock_rate = INT(0, dp) !< Number of counts per second
+   integer(dp), save :: last_time_point = INT(0, dp) !< State timesamp
+   integer(dp), save :: end_point = INT(0, dp) !< End timestamp
 
-  public :: get_clock_rate,tic, toc, now, timer_setup, timer_kill
-  private
-  !type timer ! system or CUDA timer
+   public ::  tic, toc, tdiff, now, get_clock_rate, reset_timer, tocprint
 
-
-  ! contains
-  ! declare a static constructor        
-  ! initial  :: timer_setup  ! this will be evoked before any
-  ! calls to timer variables or
-  ! functions
-  !     final :: timer_destructor
-  ! methods
-  !    procedure :: kill
-  !    procedure :: tic
-  !    procedure :: toc
-  !   procedure :: now
-  !    procedure :: setup
-  !     procedure :: assign
-  !     procedure :: add
-  !     procedure :: sub
-  !    procedure :: writef
-  !     generic :: assignment(=) => assign
-  !     generic :: operator(+) => add
-  !     generic :: operator(-) => sub
-  !    generic :: write(formatted) => writef
-
-  !end type timer
-
-  ! interface timer
-  !       module procedure  constructor
-  !       procedure :: constructFromArray
-  !  end interface timer
-
-#ifdef CUDA
-  integer :: istat
-  type (cudaEvent) :: &
-#else
-       integer(fp_kind) :: &
-#endif
-       clock_start, clock_end, clock_rate
-  real(fp_kind) :: elapsed
-
+   public :: last_time_point, end_point, clock_rate
 contains
 
-  !  function constructor() result(self)
-  !    call setup
-  !  end function constructor
+   !< Force timestamps and clock rate to zero
+   subroutine reset_timer()
+      last_time_point = INT(0, dp)
+      end_point = INT(0, dp)
+      clock_rate = INT(0, dp)
+   end subroutine reset_timer
 
-
-  !< Timer constructor
-  subroutine timer_setup()
-    call timer_kill
-#ifdef CUDA
-    integer :: nDevices
-    istat = cudaGetDeviceCount(nDevices)
-    write(*,"('Number of CUDA-capable devices: ', i0,/)") &
-         nDevices 
-    istat = cudaEventCreate(clock_start)
-    istat = cudaEventCreate(clock_end)
-    ! output device info and transfer size
-    !  istat = cudaGetDeviceProperties(prop, 0)
-
-#else
-    intrinsic system_clock
-#ifdef GF_CLOCK_MONOTONIC
-    ! 2010 fortran intrinsic module ISO FORTRAN ENV  
-    ! build with -lrt to ensure high precision clock is used
-    call system_clock_8(count, count_rate, count_max)
-#else
-    call system_clock(count_rate=clock_rate) ! Find the rate
+   !< Get system_clock timestamp
+   integer(dp) function tic() ! result (timestamp)
+      call system_clock(count=tic)
+#ifdef DEBUG
+      write (*, '(A,1d20.10)') " TIC Time stamp", REAL(tic, dp)
 #endif
+      last_time_point = tic
+      end_point = INT(0, dp)
+   end function tic
 
+   !< Get the clock tick count per second
+   real(dp) function get_clock_rate()
+      if (clock_rate .eq. 0) call system_clock(count_rate=clock_rate)
+      get_clock_rate = REAL(clock_rate, dp)
+#ifdef DEBUG
+      write (*, '(A,1d20.10)') " CLOCK_RATE (ticks/sec) ", REAL(clock_rate, dp)
 #endif
-  end subroutine timer_setup
+   end function get_clock_rate
 
-  !< Get the clock_rate variable, make sure the timer is setup 
-  function get_clock_rate() result(rate)
-    real(fp_kind) :: rate
-    call timer_setup
-    rate = REAL(clock_rate,fp_kind)/1e9
-  end function get_clock_rate
+   !< Calculate the time from two timestamps
+   real(dp) function tdiff(tend, tstart)
+      integer(dp), intent(in), optional :: tend
+      integer(dp), intent(in), optional :: tstart
+      if (present(tend)) end_point = tend
+      if (present(tstart)) last_time_point = tstart
+      if (clock_rate .eq. 0) call system_clock(count_rate=clock_rate)
+      ! Calulate the time difference
+      tdiff = REAL(end_point - last_time_point, dp)/REAL(clock_rate, dp)
+   end function tdiff
 
-    function tic() result(itic)
-#ifdef CUDA
-      integer(fp_kind) :: itic
-      istat = cudaEventRecord(clock_start,0) ! start event 
-#else
-      integer(fp_kind) :: itic
-      intrinsic system_clock
-#ifdef GF_CLOCK_MONOTONIC
-      call system_clock_8(count=clock_start) ! Start timing
-#else
-      call system_clock(count=clock_start) ! Start timing
+   !< Complete the timing regime using a reference timestamp or the one
+   !  in last_time_point
+   real(dp) function toc(start_optional)
+      integer(dp), intent(in), optional ::  start_optional
+      if (present(start_optional)) last_time_point = start_optional
+      call system_clock(count=end_point)
+      toc = tdiff(end_point, last_time_point)
+      last_time_point = end_point
+   end function toc
+
+   !< Complete the timing regime using a reference timestamp or the one
+   !  in last_time_point
+   real(dp) function tocprint(start_optional)
+      integer(dp), intent(in), optional ::  start_optional
+      if (present(start_optional)) last_time_point = start_optional
+      call system_clock(count=end_point)
+#ifdef DEBUG
+      write (*, '(A,1d20.10)') " TOC Time stamp ", REAL(end_point, dp)
 #endif
-      itic=clock_start !REAL(clock_start,fp_kind)
-#endif
+      tocprint = tdiff(end_point, last_time_point)
+      last_time_point = end_point
+      write (*, '(A,1d20.10)') " Elapsed time ", tocprint
+   end function tocprint
 
-    end function tic
+   !> print current time and date
+   subroutine now()
+      character(len=8)  :: date
+      character(len=10) :: time
+      character(len=33) :: f_result
+      !***********************************************************************************
+      call date_and_time(date, time)
+      write (*, '(A,A,A,A,A,A,A)') 'Date: ', date(7:8), '-', date(5:6), '-', date(1:4), '\n'
+      write (*, '(A,A,A,A,A,A,A)') 'Time: ', time(1:2), ':', time(3:4), ':', time(5:10), '\n'
+   end subroutine now
 
-    function toc (arg) result (elapsed_time)
-      real(fp_kind) :: elapsed_time
-#ifdef CUDA
-      type (cudaEvent), optional, intent(in) :: arg
-      if( present(arg)) then
-         clock_start = arg
-      end if
-      istat = cudaEventRecord(clock_end,0)
-      istat = cudaEventSynchronize(clock_end)
-      istat = cudaEventElapsedTime(elapsed, &
-           clock_start, clock_end)
-#else
-      real(fp_kind) :: itoc
-      integer(8), optional, intent(in) :: arg
-      intrinsic system_clock
-      if( present(arg)) then
-         clock_start = arg
-      end if
-#ifdef GF_CLOCK_MONOTONIC
-      call system_clock_8(count=clock_end) ! End timing
-#else
-      call system_clock(count=clock_end) ! End timing
-#endif
-      itoc = REAL(clock_end,fp_kind)
-      elapsed=REAL(clock_end-clock_start,fp_kind)/REAL(clock_rate,fp_kind)
-#endif
-      elapsed_time=elapsed
-    end function toc
-
-    !< Timer destructor
-    subroutine timer_kill () !timer_destructor(this)
-#ifdef _CUDA
-      ! nothing to do here
-      istat = cudaEventDestroy(clock_start)
-      istat = cudaEventDestroy(clock_end)
-#else
-      ! nothing much to do here
-
-      clock_end=0.
-      clock_start=0.
-#endif
-
-    end subroutine timer_kill !timer_destructor
-
-    subroutine now()
-
-    end subroutine now
-
-  end module simple_timer
-
-
-
+end module simple_timer
