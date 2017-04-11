5743ed005e1a55ce8b7530a8cb8ff05f6e28ee45
Michael Eager <mick.eager@gmail.com>
2017-04-11 23:00:36 +1000

    Timer test program shows loop average example

--- test_code/cpu/simple_timer_basic_test.f90
+++ test_code/cpu/simple_timer_basic_test.f90
@@ -5,20 +5,43 @@
 #define DEV_NULL "/dev/null"
 #endif
 
-  
-#define TBLOCK(XX) \
-XX = tic();
-!  write(*,'(A)') "Begin TBLOCK"
+#define NREP_MAX 10000000
 
-#define TSTOP(XX) \
-  write(*,'(A,A,1i4,A,1d20.10)') __FILE__,":",__LINE__,":Elapsed time (s) ", toc()
+
+#define CAT(prefix, suffix)            prefix ## suffix
+#define _UNIQUE_LABEL(prefix, suffix)  CAT(prefix, suffix)
+#define UNIQUE_LABEL(prefix)           _UNIQUE_LABEL(prefix, __LINE__)
+
+
+#define TBLOCK() \
+  t1 = tic();
+
+#define TSTOP() \
+  write(*,'(A,A,1i4,A,1d20.10)') __FILE__,":",__LINE__,":Elapsed time (s) ", toc(); 
+
+#define TIMER_LOOP_START()    \
+  integer(dp) :: timestamp; \
+  real, dimension(3):: elapsed; \
+  integer :: ii \
+  do ii=1,3 \
+     timestamp = tic();
+
+
+#define TIMER_LOOP_END()           \
+     elapsed(ii) = toc(timestamp); \
+end do; \
+write(*,'(A,A,1i4,A)') __FILE__,":",__LINE__, ' *** Timed loop *** '; \
+write(*,'(A,1d20.10)') "  Average over 3 (sec):",  SUM(elapsed,DIM=1) / REAL(3.,dp); \
+write(*,'(A,1d20.10)') "  Min time (sec) ", MINVAL(elapsed);
+
 
 program simple_timer_basic_test
      use precision_m
      use simple_timer
      implicit none
-     integer(dp),parameter :: nrep = 500000000
-     real(dp) :: xx, c, cfac, b, etime
+     integer(dp),parameter :: nrep = NREP_MAX
+     real(dp)    :: xx, c, cfac, b
+     real(dp)    :: etime
      integer(dp) ::  t1, t2
      integer(dp) :: i
 
@@ -28,8 +51,6 @@ program simple_timer_basic_test
      xx = 12.0_dp
 
 
-
-    
 !tstart = call tic(timer)
 !do  i = 1,nrep
 !  c = cfac*c +b
@@ -39,9 +60,10 @@ program simple_timer_basic_test
 !print *, 'Time for local evaluation = ', etime, ' seconds'
 ! tstart = functic(timer)
 
-     write (*, '(A)') "Welcome to the HighRes Fortran Timer\n"
+     write (*, '(A)') "HighRes Fortran Timer"
      t1= get_clock_rate()
      print *, 'Clock rate: ', t1
+     print *,"   "
      write (*, '(A)') "1. Simple timestamp and diff "
      t1 = tic()
      write (*, '(A,1d20.10)') "   t1 = ", real(t1, dp)
@@ -53,7 +75,8 @@ program simple_timer_basic_test
      etime = tdiff( t2, t1)
      write (*, '(A,1d20.10)') 'Time for simple evaluation (s) = ', etime
 
-          call reset_timer()
+
+     call reset_timer()
      write(*,"(A)") "  "
      write (*, '(A)') "2. Simple tic toc usage "
      write (*, '(A)') "2. t1=TIC; etime=TOC(T1)"
@@ -65,7 +88,8 @@ program simple_timer_basic_test
         c = cfac*c + b
      end do
      etime = toc(t1)
-     write (*, '(A,1d20.10)') "2.   toc(t1) = ", etime
+     write (*, '(A,1d20.10)') '2.   toc(t1) = ', etime
+     write(*,"(A)") "  "
 
 
           call reset_timer()
@@ -77,7 +101,8 @@ program simple_timer_basic_test
         c = cfac*c + b
      end do
      etime = toc()
-     write (*, '(A,1d20.10)') "3.   toc() = ", etime
+     write (*, '(A,1d20.10)') '3.   toc() = ', etime
+     write(*,"(A)") ' '
 
 
      call reset_timer()
@@ -88,7 +113,8 @@ program simple_timer_basic_test
      do i = 1, nrep
         c = cfac*c + b
      end do
-     write (*, '(A,1d20.10)') "4.  toc in write ", toc(t1)
+     write (*, '(A,1d20.10)') '4.  toc in write ', toc(t1)
+     write(*,"(A)") "  "
 
          call reset_timer()
      write (*, '(A)') "5.  Testing empty tic() lhs "
@@ -98,18 +124,76 @@ program simple_timer_basic_test
      c=.1
      do i = 1, nrep
         c = cfac*c + b
-       
      end do
      write (*, '(A,1d20.10)') "5.   tic/toc in write ", toc()
 
      call reset_timer()
-     write (*, '(A)') "6.  Testing tic toc in preprocessor macro "
-
-     TBLOCK(t1)
+     write(*,"(A)") ' '
+     write (*, '(A)') '6.  Testing tic toc in preprocessor macro  '
+     TBLOCK()
      c=.1
      do i = 1, nrep
         c = cfac*c + b
      end do
-     TSTOP(t1)
+     TSTOP()
+     write(*,"(A)") ' '
+
+     call reset_timer()
+     write(*,"(A)") ' '
+     write (*, '(A)') '7.  Testing tic toc in preprocessor macro with subroutine  '
+     TBLOCK()
+     c = saxy(c)
+     TSTOP()
+     write(*,"(A)") ' '
+
+     call reset_timer()
+     write(*,"(A)") ' '
+     write (*, '(A)') '8.  Testing timed_loop macro - must be in own subroutine '
+     c = test_loop()
+
+
+
+
+   contains
+
+
+
+     function saxy(c_in) result(c)
+       real(dp), intent(in) :: c_in
+       integer(dp),parameter :: nrep = NREP_MAX
+       real(dp)              :: xx, c, cfac, b
+       integer(dp)           :: i
+
+       c = c_in
+       c=.1
+       do i = 1, nrep
+          c = cfac*c + b
+       end do
+     end function saxy
+
+     ! Loop macro has to declare an array and an int
+     ! -- this means it has to be at the top or in
+     !    its own function
+     function test_loop() result(cloop)
+       real(dp) :: cloop
+       integer(dp) :: timestamp
+       real(dp), dimension(3):: elapsed
+       integer :: ii
+       do ii=1,3 
+          timestamp = tic();
+
+          cloop= REAL(.1,dp)
+          cloop = saxy(cloop)
+
+
+          elapsed(ii) = toc(timestamp)
+       end do
+       write(*,'(A,A,1i4,A)') __FILE__,":",__LINE__, ' *** Timed loop *** '
+       write(*,'(A,1d20.10)') "  Average over 3 (sec):",  SUM(elapsed,DIM=1) / REAL(3.,dp)
+       write(*,'(A,1d20.10)') "  Min time (sec) ", MINVAL(elapsed)
+       write(*,"(A)") ' '
+
+     end function test_loop
+
 
   end program simple_timer_basic_test
