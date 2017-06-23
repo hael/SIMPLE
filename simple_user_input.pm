0f65f450c0db94c3d3005a76fdd66810e9564adf
Dominika Elmlund <dominika.elmlund@monash.edu>
2017-06-23 10:42:52 +1000

    fixed Relion conversion script

--- simple_user_input.pm
+++ simple_user_input.pm
@@ -17,14 +17,12 @@ use Exporter;
 @ISA = ("Exporter");
 use Config;
 @ISA = ('Exporter');
-@EXPORT = qw($ICOMPILE $SIMPLE_PATH $FCOMPILER $PLATFORM $CUDADIR $FFTW_LIB $FFTW_INC $OBJDIR $MODDIR $DEBUG $DEBUG_LEVEL $CC_COMPILER $GCC_COMPILER $DOPENMP $DCUDA $DBENCH $SET_OPTIMIZATION $op_sys $architecture getPlatform );
+@EXPORT = qw($ICOMPILE $FCOMPILER $PLATFORM $CUDADIR $FFTW_LIB $FFTW_INC $OBJDIR $MODDIR $DEBUG $DEBUG_LEVEL $CC_COMPILER $GCC_COMPILER $DOPENMP $DCUDA $DBENCH $SET_OPTIMIZATION $op_sys $architecture getPlatform );
 
 #####################################################################
 # User-defined variables controlling compilation                    #
 #####################################################################
 
-# enter the SIMPLE root path
-our$SIMPLE_PATH="/Users/creboul/Simple3";
 # specifying the compiling directives
 # with OpenMP:  -fopenmp, CUDA: -DCUDA
 # Benchmarking: -DBENCH
@@ -37,7 +35,7 @@ getPlatform();
 # would you like to compile the library from scracth?
 # make cleanall before make and link: 0   make only and generate scripts : 2
 # make clean before make and link   : 1   link, compile production only  : 3 
-our$ICOMPILE = 1;
+our$ICOMPILE = 2;
 # the name compiler default(linux): cc, gcc, gfortran
 # the name compiler default(MacOSX): /usr/local/bin/gcc, /usr/local/bin/gcc,
 #                                    /usr/local/bin/gfortran
@@ -48,9 +46,9 @@ our$FCOMPILER = "gfortran";
 our$CUDADIR="";
 # enter the fftw lib default: /usr/lib/x86_64-linux-gnu for [linux]
 #                             /usr/local/lib for [MacOSX]
-our$FFTW_LIB="/opt/local/lib/";
+our$FFTW_LIB="/usr/lib/x86_64-linux-gnu/";
 # on clusters we need extra path after module load fftw/3.3.4-gcc to identify the FFTW header
-our$FFTW_INC="/opt/local/include/";
+our$FFTW_INC="/usr/include";
 # Modules and objects directories. default: obj/SIMPLEOFILES
 our$OBJDIR="obj/SIMPLEOFILES";
 our$MODDIR="obj/SIMPLEOFILES";
