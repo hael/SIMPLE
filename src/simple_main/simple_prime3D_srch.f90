e576fb70866996b76f0c9d77870a60baeb5c67f1
Hans Elmlund <hans.elmlund@monash.edu>
2017-07-01 06:44:27 +1000

    put back greedy inplane for refine=no

--- src/simple_main/simple_prime3D_srch.f90
+++ src/simple_main/simple_prime3D_srch.f90
@@ -335,17 +335,8 @@ contains
                 if( self%nstates > 1 ) state = nint( self%o_refs%get(iref, 'state') )
                 if( self%state_exists(state) )then
                     corrs     = pftcc%gencorrs(iref, iptcl) ! In-plane correlations
-                    !!!!!!!!!!!!!!!
-                    inpl_ind = shcloc(self%nrots, corrs, self%prev_corr) ! first improving in-plane index
-                    if( inpl_ind > 0 )then
-                        ! improving correlation found
-                    else
-                        loc      = maxloc(corrs) ! greedy in-plane
-                        inpl_ind = loc(1)        ! in-plane angle index
-                    endif
-                    !!!!!!!!!!!!!!
-                    ! loc       = maxloc(corrs) ! greedy in-plane
-                    ! inpl_ind  = loc(1)        ! in-plane angle index
+                    loc       = maxloc(corrs)   ! greedy in-plane
+                    inpl_ind  = loc(1)          ! in-plane angle index
                     inpl_corr = corrs(inpl_ind) ! max in plane correlation
                     call self%store_solution(pftcc, iref, iref, inpl_ind, inpl_corr)
                     projspace_corrs( iref ) = inpl_corr ! stash in-plane correlation for sorting                   
