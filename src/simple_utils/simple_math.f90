77d13c9cca46b9226443441e980104800ea3b9b8
Michael Eager <mick.eager@gmail.com>
2017-08-01 18:03:17 +1000

    Applying cyril's fix to my kv2wl bug after merge

--- src/simple_utils/simple_math.f90
+++ src/simple_utils/simple_math.f90
@@ -195,12 +195,14 @@ contains
         deg = (rad/DPI)*180.d0
     end function
 
-    !>  \brief  Convert acceleration voltage in kV into electron wavelength in Angstroms
-    pure  real function kV2wl( kV )
-        real, intent(in):: kV
+    !>    Convert acceleration voltage in kV into electron wavelength in Angstroms
+    !! \f$ \lambda (\si{\angstrom}) =  \frac{12.26}{\sqrt{(eV+0.9784 eV^2) / 10^6 }} \f$
+    pure function kV2wl( kV ) result( kV_to_wl )
+        real, intent(in):: kV         !< acceleration voltage in \f$\si{\kilovolt}\f$
+        real :: kV_to_wl              !< electron wavelength in Angstroms \f$\si{\angstrom}\f$
         kV2wl = 12.26/sqrt(1000.0*kV+0.9784*(1000.0*kV)**2/(10.0**6.0))
-    end function
-    
+    end function kV2wl
+
     !>   converts from correlation to euclidean distance
     pure function corr2dist( corr ) result( dist )
         real, intent(in) :: corr   !< query correlation
