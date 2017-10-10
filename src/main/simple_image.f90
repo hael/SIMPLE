c8d0533f590a9ddc25f94ffbe52f713375457ed9
Hans Elmlund <hans.elmlund@monash.edu>
2017-10-10 20:36:09 +1100

    put back FFTW_ESTIMATE in image class; FFTW_PATIENT too slow on build

--- src/main/simple_image.f90
+++ src/main/simple_image.f90
@@ -344,11 +344,11 @@ contains
             call fftwf_plan_with_nthreads(nthr_glob)
         endif
         if(self%ldim(3) > 1)then
-            self%plan_fwd = fftwf_plan_dft_r2c_3d(self%ldim(3), self%ldim(2), self%ldim(1), self%rmat, self%cmat, FFTW_PATIENT)
-            self%plan_bwd = fftwf_plan_dft_c2r_3d(self%ldim(3), self%ldim(2), self%ldim(1), self%cmat, self%rmat, FFTW_PATIENT)
+            self%plan_fwd = fftwf_plan_dft_r2c_3d(self%ldim(3), self%ldim(2), self%ldim(1), self%rmat, self%cmat, FFTW_ESTIMATE)
+            self%plan_bwd = fftwf_plan_dft_c2r_3d(self%ldim(3), self%ldim(2), self%ldim(1), self%cmat, self%rmat, FFTW_ESTIMATE)
         else
-            self%plan_fwd = fftwf_plan_dft_r2c_2d(self%ldim(2), self%ldim(1), self%rmat, self%cmat, FFTW_PATIENT)
-            self%plan_bwd = fftwf_plan_dft_c2r_2d(self%ldim(2), self%ldim(1), self%cmat, self%rmat, FFTW_PATIENT)
+            self%plan_fwd = fftwf_plan_dft_r2c_2d(self%ldim(2), self%ldim(1), self%rmat, self%cmat, FFTW_ESTIMATE)
+            self%plan_bwd = fftwf_plan_dft_c2r_2d(self%ldim(2), self%ldim(1), self%cmat, self%rmat, FFTW_ESTIMATE)
         endif
         ! set shift constant (shconst)
         do i=1,3
