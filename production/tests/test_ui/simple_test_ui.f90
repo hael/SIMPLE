81fb9da6b2e2d36b9e8f8379fb532f2eb16dedb6
cyrilreboul <cyril.reboul@monash.edu>
2018-03-01 18:22:53 +1100

    JSON support

--- production/tests/test_ui/simple_test_ui.f90
+++ production/tests/test_ui/simple_test_ui.f90
@@ -3,5 +3,5 @@ use simple_user_interface ! use all in there
 implicit none
 call new_cluster2D
 call cluster2D%print_ui
-! call cluster2D%write2json()
+call cluster2D%write2json()
 end program simple_test_ui
