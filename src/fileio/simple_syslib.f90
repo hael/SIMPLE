a074603b733f70e03401b51f14557843f3e89993
Michael Eager <mick.eager@gmail.com>
2018-03-07 11:50:36 +1100

    memory usage function added in syslib -- simple_mem_usage
    gets the PID of the current process and returns the  vmRSS (resident set size) or RAM used by the process
    Optional return values include:
    VmPeak:	Peak virtual memory usage
    VmSize:	Current virtual memory usage

--- src/fileio/simple_syslib.f90
+++ src/fileio/simple_syslib.f90
@@ -964,5 +964,64 @@ contains
 
 #endif
 
+    ! Suggestion from https://stackoverflow.com/a/30241280
+    subroutine simple_mem_usage(valueRSS)
+        implicit none
+        integer, intent(out) :: valueRSS
+        integer, intent(out), optional :: valuePeak
+        integer, intent(out), optional :: valueSize
 
+        character(len=200):: filename=' '
+        character(len=80) :: line
+        character(len=8)  :: pid_char=' '
+        integer :: pid,unit
+        logical :: ifxst
+
+        valueRSS=-1    ! return negative number if not found
+
+        !--- get process ID
+
+        pid=getpid()
+        write(pid_char,'(I8)') pid
+        filename='/proc/'//trim(adjustl(pid_char))//'/status'
+
+        !--- read system file
+
+        inquire (file=filename,exist=ifxst)
+        if (.not.ifxst) then
+            write (*,*) 'system file does not exist'
+            return
+        endif
+
+        call fopen(unit, file=filename, action='read')
+        if(present(valuePeak))then
+            do
+                read (unit,'(a)',end=110) line
+                if (line(1:7).eq.'VmPeak:') then
+                    read (line(8:),*) valuePeak
+                endif
+            enddo
+110         continue
+        endif
+        if(present(valueSize))then
+            do
+                read (unit,'(a)',end=120) line
+                if (line(1:7).eq.'VmSize:') then
+                    read (line(8:),*) valueSize
+                endif
+            enddo
+120         continue
+        endif
+        do
+            read (unit,'(a)',end=130) line
+            if (line(1:6).eq.'VmRSS:') then
+                read (line(7:),*) valueRSS
+                exit
+            endif
+        enddo
+130     continue
+        call fclose(unit)
+
+        return
+    end subroutine simple_mem_usage
 end module simple_syslib
