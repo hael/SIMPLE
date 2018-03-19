268809513f27b0375798577e97dd7fc4c83c20e7
Michael Eager <mick.eager@gmail.com>
2018-03-20 10:30:20 +1100

    Simple_posix simplified with char* inputs reliant on trim(str) inputs

--- src/fileio/simple_posix.c
+++ src/fileio/simple_posix.c
@@ -3,24 +3,27 @@
 /* Author:  Michael Eager                                                    */
 /*                                                                           */
 /*****************************************************************************/
-
+#define _SVID_SOURCE
 #define _GNU_SOURCE             /* See feature_test_macros(7) */
 #include <sys/types.h>
 #include <sys/stat.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<string.h>
-#include<sys/wait.h>
-#include<errno.h>
-#include<pwd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <time.h>
+#include <glob.h>
 
 #ifdef __linux__
 #include<linux/limits.h>
 #endif
 
-#include <dirent.h>
 
+#define MAX_CHAR_FILENAME 256
 #define NORMAL_COLOR  "\x1B[0m"
 #define GREEN  "\x1B[32m"
 #define BLUE  "\x1B[34m"
@@ -29,6 +32,9 @@
 char *F90toCstring(char *str, int len)
 {
   char *res; /* C arrays are from 0:len-1 */
+  if (len > MAX_CHAR_FILENAME){
+    printf( "simple_posix.c possible strlen error in converting f90 string to c-string\n");
+  }
   if((res = (char*)malloc(len + 1))) {
     strncpy(res, str, len);
     res[len] = '\0';
@@ -63,17 +69,16 @@ void show_dir_content_recursive(const char * path)
   }
   closedir(d); // finally close the directory
 }
-#define MAX_CHAR_FILENAME 256
+
 
 #define GET_FILE_LIST get_file_list_
-int get_file_list(const char * path, int pathlen, const char * ext,int *count)
+int get_file_list(const char * path,  const char * ext,int *count)
 {
-  char *cpath = F90toCstring(path, pathlen);
+  char *cpath = F90toCstring(path, strlen(path));
   printf(" get_file_list %s\n", path);
-  char *buffer;
   extern int errno;
   DIR           *d;
-  int i;
+
   //  char ** results,
 
   d = opendir(cpath); free(cpath);
@@ -94,23 +99,23 @@ int get_file_list(const char * path, int pathlen, const char * ext,int *count)
     fcount = 0;
     char *cext = F90toCstring(ext, 3);
     if(strcmp(ext,"")){
-    while((elem = readdir(d)) != NULL) {
-      if(elem->d_type != DT_DIR) {
-        /*option 1*/
-        //        results[fcount] = malloc ( sizeof(char) * (elem->d_reclen+1));
-        //        strncpy(results[fcount], elem->d_name, elem->d_reclen );
-        /*option 2*/
-        fprintf(f, "%s\n", elem->d_name);
-        printf(" get_file_list elem %d %s\n", fcount, elem->d_name);
-        fcount++;
+      while((elem = readdir(d)) != NULL) {
+        if(elem->d_type != DT_DIR) {
+          /*option 1*/
+          //        results[fcount] = malloc ( sizeof(char) * (elem->d_reclen+1));
+          //        strncpy(results[fcount], elem->d_name, elem->d_reclen );
+          /*option 2*/
+          fprintf(f, "%s\n", elem->d_name);
+          // printf(" get_file_list elem %d %s\n", fcount, elem->d_name);
+          fcount++;
+        }
       }
-    }
     } else {
       while((elem = readdir(d)) != NULL) {
         if(elem->d_type != DT_DIR) {
           if (strstr(elem->d_name, ext)!=NULL){
             fprintf(f, "%s\n", elem->d_name);
-            printf(" get_file_list elem %d %s\n", fcount, elem->d_name);
+            //  printf(" get_file_list elem %d %s\n", fcount, elem->d_name);
             fcount++;
           }
         }
@@ -129,11 +134,10 @@ int get_file_list(const char * path, int pathlen, const char * ext,int *count)
   return 0;
 }
 
-  #define _SVID_SOURCE
-       /* print files in current directory in reverse order */
-       #include <dirent.h>
+
+/* print files in current directory in reverse order */
 #define GET_FILE_LIST_REVERSE_MODIFIED get_file_list_reverse_modified_
-int get_file_list_reverse_modified(const char * path, int pathlen, const char* ext, int* count)
+int get_file_list_reverse_modified(const char * path, const char* ext, int* count)
 {
   /*equivalent to 'ls -tr path' */
   struct dirent **namelist;
@@ -146,7 +150,7 @@ int get_file_list_reverse_modified(const char * path, int pathlen, const char* e
     perror("Failed : simple_posix.c::get_file_list ");
     return -1;
   }
-  char *cpath = F90toCstring(path, pathlen);
+  char *cpath = F90toCstring(path, strlen(path));
   n = scandir(cpath, &namelist, NULL, versionsort); free(cpath);
   if (n < 0)
     perror("scandir");
@@ -155,12 +159,12 @@ int get_file_list_reverse_modified(const char * path, int pathlen, const char* e
     char *cext = F90toCstring(ext, 3);
     if(strcmp(ext,"")){
       while (n--) {
-        fprintf(f, "%s\n", namelist[n]->d_name);
+        if(namelist[n]->d_type != DT_DIR) fprintf(f, "%s\n", namelist[n]->d_name);
       }
       free(namelist[n]);
     } else {
       while (n--) {
-        if (strstr(namelist[n]->d_name, ext)!=NULL)
+        if( (namelist[n]->d_type != DT_DIR) && (strstr(namelist[n]->d_name, ext)!=NULL) )
           fprintf(f, "%s\n", namelist[n]->d_name);
       }
       free(namelist[n]);
@@ -168,65 +172,70 @@ int get_file_list_reverse_modified(const char * path, int pathlen, const char* e
     *count = n;
     free(cext);
     free(namelist);
-    fclose(f);
   }
-}
-
-
-#define GET_SUBDIR_LIST get_subdir_list_
-int get_subdir_list(const char * path, int pathlen, int* count)
-{
-  char *cpath = F90toCstring(path, pathlen);
-  printf(" simple_posix.c:: get_subdir_list %s\n", cpath);
-  // char *buffer;
-  extern int errno;
-  /*option 2*/
-
-  //  char ** results;
-  DIR           *d;
-  int err=0;
-  d = opendir(cpath); free(cpath);
-  if(d) {
-
-    int fcount = 0;
-    struct dirent *dir;
-    while((dir = readdir(d)) != NULL) {
-      if(dir-> d_type == DT_DIR && strcmp(dir->d_name, ".") != 0 && strcmp(dir->d_name, "..") != 0) {
-        fcount++;
-      }
-    }
-    rewinddir(d);
-    //  results = malloc ( sizeof(char*) * fcount );
-    *count = fcount;
-    printf(" get_subdir_list size %d\n", fcount);
-    if (fcount > 0){
-      FILE* f = fopen("__simple_filelist__", "w");
-      fcount = 0;
-      while((dir = readdir(d)) != NULL) {
-        if(dir-> d_type == DT_DIR && strcmp(dir->d_name, ".") != 0 && strcmp(dir->d_name, "..") != 0) {
-          /*option 1*/
-          // results[fcount] = malloc ( sizeof(char) * (dir->d_reclen+1));
-          // strncpy(results[fcount], dir->d_name, (dir->d_reclen+1) );
-          /*option 2*/
-          fprintf(f,"%s\n", dir->d_name);
-          //              printf(" get_subdir_list elem %d %s\n", fcount, dir->d_name);
-          fcount++;
-        }
-      }
-      fclose(f);
-    }
-    closedir(d);
-
-  } else {
-    printf("%d %s\nget_subdir_list opendir failed to open %s\n", errno, strerror(errno), path);
-    perror("Failed : simple_posix.c::get_subdir_list ");
-    return -1;
-  }
+  fclose(f);
   return 0;
 }
 
 
-void fortran_rmdir_(char *buf, int *status, int buflength)
+#define LIST_DIRS list_dirs_
+int list_dirs(const char * path, int* count)
+{
+  char *cpath = F90toCstring(path, strlen(path));
+  //  printf(" simple_posix.c:: list_dirs %s\n", cpath);
+  extern int errno;
+  /*option 2*/
+  //  char ** results;
+  DIR           *d;
+  int fcount = 0;
+  d = opendir(cpath);
+  free(cpath);
+  if(d) {
+
+    struct dirent *dir;
+    //    while((dir = readdir(d)) != NULL) {
+    //  if(dir-> d_type == DT_DIR && strcmp(dir->d_name, ".") != 0 && strcmp(dir->d_name, "..") != 0) {
+    //    fcount++;
+    //  }
+    //}
+    // rewinddir(d);
+    //  results = malloc ( sizeof(char*) * fcount );
+    //printf(" list_dirs size %d\n", fcount);
+    //if (fcount > 0){
+    FILE* f = fopen("__simple_filelist__", "w");
+    if(f){
+    fcount = 0;
+    while((dir = readdir(d)) != NULL) {
+      if(dir-> d_type == DT_DIR && strcmp(dir->d_name, ".") != 0 && strcmp(dir->d_name, "..") != 0) {
+          /*option 1*/
+          // results[fcount] = malloc ( sizeof(char) * (dir->d_reclen+1));
+          // strncpy(results[fcount], dir->d_name, (dir->d_reclen+1) );
+          /*option 2*/
+        fprintf(f,"%s\n", dir->d_name);
+          //              printf(" list_dirs elem %d %s\n", fcount, dir->d_name);
+        fcount++;
+      }
+    }
+    fclose(f);
+    }else{
+      printf("%d %s\nlist_dirs opendir failed to open temp file\n", errno, strerror(errno));
+      perror("Failed : simple_posix.c::list_dirs ");
+      return -1;
+    }
+    //}
+    closedir(d);
+
+  } else {
+    printf("%d %s\nlist_dirs opendir failed to open %s\n", errno, strerror(errno), path);
+    perror("Failed : simple_posix.c::list_dirs ");
+    return -1;
+  }
+  *count = fcount;
+  return 0;
+}
+
+
+void c_rmdir_(char *buf, int *status, int buflength)
 {
   char *buffer;
   extern int errno;
@@ -250,56 +259,48 @@ void fortran_rmdir_(char *buf, int *status, int buflength)
   free(buffer);
 }
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <dirent.h>
-#include <time.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <glob.h>
 
 
 int qs_struct(struct dirent **namelist, int left, int right) {
 
-    register int i, j;
+  register int i, j;
 
-    struct dirent *temp;
-    struct stat ibuf;
-    struct stat jbuf;
-    struct stat xbuf;
+  struct dirent *temp;
+  struct stat ibuf;
+  struct stat jbuf;
+  struct stat xbuf;
 
-    i = left; j = right;
+  i = left; j = right;
 
-    stat(namelist[i]->d_name, &ibuf);
-    stat(namelist[j]->d_name, &jbuf);
-    stat(namelist[(left+right)/2]->d_name, &xbuf);
-
-
-    do {
-        while((ibuf.st_mtime < xbuf.st_mtime) && (i < right)) {
-            i++;
-            stat(namelist[i]->d_name, &ibuf);
-        }
-        while((jbuf.st_mtime > xbuf.st_mtime) && (j > left))  {
-            j--;
-            stat(namelist[j]->d_name, &jbuf);
-        }
-        if(i <= j) {
-            temp = namelist[i];
-            namelist[i] = namelist[j];
-            namelist[j] = temp;
+  stat(namelist[i]->d_name, &ibuf);
+  stat(namelist[j]->d_name, &jbuf);
+  stat(namelist[(left+right)/2]->d_name, &xbuf);
 
 
-            i++; j--;
-        }
+  do {
+    while((ibuf.st_mtime < xbuf.st_mtime) && (i < right)) {
+      i++;
+      stat(namelist[i]->d_name, &ibuf);
+    }
+    while((jbuf.st_mtime > xbuf.st_mtime) && (j > left))  {
+      j--;
+      stat(namelist[j]->d_name, &jbuf);
+    }
+    if(i <= j) {
+      temp = namelist[i];
+      namelist[i] = namelist[j];
+      namelist[j] = temp;
 
-    } while(i <= j);
 
-    if(left < j) qs_struct(namelist, left, j);
-    if(i < right) qs_struct(namelist, i, right);
+      i++; j--;
+    }
 
+  } while(i <= j);
 
+  if(left < j) qs_struct(namelist, left, j);
+  if(i < right) qs_struct(namelist, i, right);
+
+  return 0;
 }
 
 int qs_glob(glob_t*globlist, int left, int right)
@@ -342,6 +343,7 @@ int qs_glob(glob_t*globlist, int left, int right)
 
   if(left < j) qs_glob(globlist, left, j);
   if(i < right) qs_glob(globlist, i, right);
+  return 0;
 }
 
 
@@ -374,14 +376,14 @@ int glob_file_list(const char *match, int*len_match, int*count, int* sort_by_tim
     perror("Failed : simple_posix.c::glob_file_list ");
     return -1;
   }
-  int i, err;
+  int err;
   err = 0;
   if(!f) {
     fprintf(stderr, "simple_posix.c::glob_file_list cannot open tmp file \n");
     err = -3;
   } else {
-    char *cmatch = F90toCstring(match, *len_match);
-    int glob_val= glob(match, GLOB_PERIOD, NULL, &globlist);
+    char *cmatch = F90toCstring(match, strlen(match));
+    int glob_val= glob(match, GLOB_PERIOD, NULL, &globlist);free(cmatch);
     if(glob_val == GLOB_NOSPACE){
       fprintf(stderr, "simple_posix.c::glob_file_list NOSPACE no memory left \n");
       err = -2;
@@ -397,10 +399,10 @@ int glob_file_list(const char *match, int*len_match, int*count, int* sort_by_tim
       if(*count > 2) {
         if(*sort_by_time) quick_glob(&globlist, 2, *count);
         *count = 0;
-        i = 2;
+        int i = 2;
         while(globlist.gl_pathv[i]) {
 
-          fprintf(stderr, "GLOB>> %s\n", globlist.gl_pathv[i]);
+          //          fprintf(stderr, "GLOB>> %s\n", globlist.gl_pathv[i]);
           if(strstr(globlist.gl_pathv[i], "__simple_filelist__") == NULL) {
             /* if( strcmp(globlist.gl_pathv[i],".")!=0 && strcmp(globlist.gl_pathv[i],"..")!=0 ) */
             /* { */
@@ -429,82 +431,81 @@ int glob_file_list(const char *match, int*len_match, int*count, int* sort_by_tim
         /* printf(" glob_file_list freeing globlist struct \n"); */
         // #endif
       }
-      printf(" glob_file_list size after trimming %d \n", *count);
+      fprintf(stderr," glob_file_list size after trimming %d \n", *count);
     }
-    globfree(&globlist);free(cmatch);
-    if(f) fclose(f);
+    globfree(&globlist);
+    fclose(f);
   }
-    return err;
+  return err;
 }
 
 #define FREE_FILE_LIST free_file_list_
-  void free_file_list(char**ptr, int n)
-  {
-    for(int i = 0; i < n; i++) free(ptr[i]);
-    free(ptr);
-  }
+void free_file_list(char**ptr, int n)
+{
+  for(int i = 0; i < n; i++) free(ptr[i]);
+  free(ptr);
+}
 
 
 #define SUBPROCESS subprocess_
-  int subprocess(const char* cmd, char* const args)
-  {
+int subprocess(const char* cmd, char* const args)
+{
+  int pid = fork();
 
-    int pid = fork();
-
-    if(pid == 0) {
-      execvp(cmd , &args);
-    }
-    return pid;
-
+  if(pid == 0) {
+    execvp(cmd , &args);
   }
+  return pid;
+
+}
 
 #define FCOPY fcopy_
-  int fcopy(char* file1,  char* file2, int len1, int len2)
-  {
-    char *buffer1, *buffer2;
-    int err = 0, flag = 0;
-    if(!(buffer1 = F90toCstring(file1, len1))) {
-      perror("Failed : appendtostring (buf) in posixwrapper:fortranrmdir");
-      free(buffer1);
-       return 1;
-    }
-    if(!(buffer2 = F90toCstring(file2, len2))) {
-      perror("Failed : appendtostring (buf) in posixwrapper:fortranrmdir");
-      free(buffer2);
-      return 1;
-    }
-    FILE *f1, *f2;
-    f1 = fopen(buffer1, "r");
-    f2 = fopen(buffer2, "w");
-    free(buffer1); free(buffer2);
-    if(f1 && f2) {
-      char            buffer[MAX_CHAR_FILENAME];
-      size_t          n;
-
-      while((n = fread(buffer, sizeof(char), sizeof(buffer), f1)) > 0) {
-        if(fwrite(buffer, sizeof(char), n, f2) != n) {
-          flag = 1;
-          break;
-        }
-      }
-      fclose(f1); fclose(f2);
-
-    } else
-      flag = 1;
-
-    if(flag == 0) return 0;
-    else return -1;
+int fcopy(char* file1,  char* file2)
+{
+  char *buffer1, *buffer2;
+  int flag = 0;
+  if(!(buffer1 = F90toCstring(file1, strlen(file1)))) {
+    perror("Failed : appendtostring (buf) in posixwrapper:fortranrmdir");
+    free(buffer1);
+    return 1;
   }
+  if(!(buffer2 = F90toCstring(file2, strlen(file2)))) {
+    perror("Failed : appendtostring (buf) in posixwrapper:fortranrmdir");
+    free(buffer2);
+    return 1;
+  }
+  FILE *f1, *f2;
+  f1 = fopen(buffer1, "r");
+  f2 = fopen(buffer2, "w");
+  free(buffer1); free(buffer2);
+  if(f1 && f2) {
+    char            buffer[MAX_CHAR_FILENAME];
+    size_t          n;
+
+    while((n = fread(buffer, sizeof(char), sizeof(buffer), f1)) > 0) {
+      if(fwrite(buffer, sizeof(char), n, f2) != n) {
+        flag = 1;
+        break;
+      }
+    }
+    fclose(f1); fclose(f2);
+
+  } else
+    flag = 1;
+
+  if(flag == 0) return 0;
+  else return -1;
+}
 
 
-#include <linux/limits.h>
+
 #define GET_ABSOLUTE_PATHNAME get_absolute_pathname_
-int  get_absolute_pathname(const char* in, int* inlen, char out[], int* outlen)
+int  get_absolute_pathname(const char* in, char out[], int* outlen)
 {
   // realpath(in, resolved_path);
   //      printf("\n%s\n",resolved_path);
   //      return 0;
-  char *filein = F90toCstring(in, *inlen);
+  char *filein = F90toCstring(in, strlen(in));
   char *resolved = canonicalize_file_name(filein);free(filein);
   if(resolved ==NULL){
     printf("%d %s\nget_absolute_path failed to canonicalize  file %s\n", errno, strerror(errno), filein);
