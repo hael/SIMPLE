4346bf232d7b07c6c669c6401665df65b6dcf31d
Simon Kiesewetter <simon.kiesewetter@monash.edu>
2018-04-09 10:36:20 +1000

    replaced random seed generator in simplelib

--- src/utils/simple_rnd.f90
+++ src/utils/simple_rnd.f90
@@ -31,6 +31,69 @@ integer(long), save   :: idum
 contains
 
     !>  \brief  random seed
+    !>  solution from https://stackoverflow.com/questions/34797938/random-number-generator-in-pgi-fortran-not-so-random
+    !>  the old version was replace because the bug described in the thread was observed with GCC and PGI
+    subroutine seed_rnd
+        use iso_fortran_env, only: int64
+        integer, allocatable :: seed(:)
+        integer              :: i, n, istat, dt(8), pid
+        integer(int64)       :: t        
+        integer, parameter   :: un=703        
+        call random_seed(size = n)
+        allocate(seed(n))
+        ! First try if the OS provides a random number generator
+        open(unit=un, file="/dev/urandom", access="stream", &
+            form="unformatted", action="read", status="old", iostat=istat)
+        if (istat == 0) then
+            read(un) seed
+            close(un)
+        else
+            ! The PID is
+            ! useful in case one launches multiple instances of the same
+            ! program in parallel.
+            call system_clock(t)
+            if (t == 0) then
+                call date_and_time(values=dt)
+                t = (dt(1) - 1970) * 365_int64 * 24 * 60 * 60 * 1000 &
+                    + dt(2) * 31_int64 * 24 * 60 * 60 * 1000 &
+                    + dt(3) * 24_int64 * 60 * 60 * 1000 &
+                    + dt(5) * 60 * 60 * 1000 &
+                    + dt(6) * 60 * 1000 + dt(7) * 1000 &
+                    + dt(8)
+            end if
+            pid = my_getpid()
+            t = ieor( t, int(pid, kind(t)) )
+            do i = 1, n
+                seed(i) = lcg(t)
+            end do
+        end if
+        call random_seed(put=seed)
+        
+    contains
+        
+        function lcg(s) result(res)
+            integer                       :: res
+            integer(int64), intent(inout) :: s
+            if (s == 0) then
+                s = 104729
+            else
+                s = mod(s, 4294967296_int64)
+            end if
+            s = mod(s * 279470273_int64, 4294967291_int64)
+            res = int(mod(s, int(huge(0), 8)), kind(0))
+        end function lcg
+    
+        !this option is especially used for pgf90 to provide a getpid() function
+        !Returns the process ID of the current process
+        !todo: write the actual code, for now returns a fixed value
+        function my_getpid()result(pid)
+            integer :: pid
+            pid = 53 !just a prime number, no special meaning
+        end function my_getpid
+    end subroutine seed_rnd
+    
+    
+    !>  \brief  random seed
     subroutine seed_rnd
         integer :: i, n, clock
         integer, allocatable :: seed(:)
