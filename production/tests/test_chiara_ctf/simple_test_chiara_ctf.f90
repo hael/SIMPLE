513767bd80ebeff86d7bc9201ce14abe90f7cefc
Chiara Machello <chiara.machello@monash.edu>
2018-05-18 15:47:15 +1000

    Changing ctf

--- production/tests/test_chiara_ctf/simple_test_chiara_ctf.f90
+++ production/tests/test_chiara_ctf/simple_test_chiara_ctf.f90
@@ -3,7 +3,8 @@ use simple_ctf,   only: ctf
 use simple_image, only: image
 implicit none
 
-real,    parameter :: smpd=1.2, kV=300, Cs=2.7, amp_contr=0.1, dfx=1.54, dfy =1.72, angast=30.
+real,    parameter :: smpd=1.2, kV=300, Cs=2.7, amp_contr=0.1, dfx=60, dfy = 0, angast= 0
+! dfx=1.54, dfy =1.72, angast=30.
 integer, parameter :: box=256
 
 type(image) :: ctf_image, img4viz
