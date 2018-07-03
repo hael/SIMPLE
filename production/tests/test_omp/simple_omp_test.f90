26af11e73f8a93ac072d51b2f40076ffb1b67bba
Michael Eager <mick.eager@gmail.com>
2018-07-03 16:47:51 +1000

    Moving OpenMP main test file to appropriate name

--- production/tests/test_omp/simple_omp_test.f90
+++ production/tests/test_omp/simple_omp_test.f90
@@ -29,15 +29,40 @@ be_verbose = .true.
 
 write(*,'(a,i0)') 'OpenMP version: ', openmp_version
 
+call test_omp_basics(10000)
+print *, ' Test internal openMP '
+call test_internal_omp
+print *, ' Test parallel sections openMP '
+call test_parallel_sections_omp
+print *, ' Test parallel loops openMP '
+call test_parallel_loops_omp
+print *, ' Test shared race-conditions openMP '
+call test_shared_race_condition
+print *, ' Test OpenMP stand-alone '
+call test_standalone_ok
+print *, ' Test OpenMP collapse order '
+call test_collapse_order
+print *, ' Test OpenMP ordered example '
+call test_ordered_example
+print *, ' Test OpenMP firstprivate example '
+call test_first_private
+print *, ' Test OpenMP firstprivate example 2'
+call test_first_private2
+print *, ' Test OpenMP SIMD example 2'
+call test_simd_example2
+print *, ' Test OpenMP SIMD example 3'
+call test_simd_example3
+print *, ' Test OpenMP SIMD example 4'
+call test_simd_example4
+print *, ' Test OpenMP SIMD example 5'
+call test_simd_example5
+
+
 call exec_OpenMP_timer_test(be_verbose)
 call date_and_time(TIME=timestr)
 stoptime = str2real(timestr)
 write(*,'(a,1x,f9.2)') '<<< intrinsic date_and_time elapsed (s): ', stoptime - starttime
 
-call test_omp_basics(10000)
-call test_internal_omp
-call test_parallel_omp
-call test_shared_race_condition
 
 
 end program simple_omp_test
