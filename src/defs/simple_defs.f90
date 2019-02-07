aa18ae259a88d8147f792aef571f8c51fd3e349e
Hans Elmlund <hans.elmlund@monash.edu>
2019-02-07 13:02:52 +1100

    put back TAU_DEFAULT=0.01. Turned out 0.05 gave better result when running reconstruct3D on the orientation estimates obtained with 0.01 but when running it from the start with 0.05 gave worse resolution

--- src/defs/simple_defs.f90
+++ src/defs/simple_defs.f90
@@ -129,8 +129,8 @@ real, parameter    :: LP2SMPDFAC           = 0.4125    !< low-pass limit scaling
 real, parameter    :: LP2SMPDFAC2D         = 0.4       !< low-pass limit scaling constant
 real, parameter    :: NPEAKSATHRES         = 12.0      !< angular threshold for determining npeaks (PRIME3D)
 real, parameter    :: SHC_INPL_TRSHWDTH    = 2.0       !< shift search halfwidht (pixels)
-real, parameter    :: TAU_DEFAULT          = 0.05      !< controls the sharpeness of the orientation weight distribution when objfun .ne. euclid
-                                                       !! smaller number means sharper distribution, numbers BTW 0.03 and 0.5 make sense
+real, parameter    :: TAU_DEFAULT          = 0.01      !< controls the sharpeness of the orientation weight distribution when objfun .ne. euclid
+                                                       !! smaller number means sharper distribution, numbers BTW 0.01 and 0.5 make sense
 real, parameter    :: SIGMA2_FUDGE_DEFAULT = 100.      !< controls the sharpeness of the orientation weight distribution when objfun .eq. euclid
                                                        !! smaller number means sharper distribution, but with sigma2_fudge == 1 it turns into a delta
 integer, parameter :: MAX_EXTRLIM2D        = 15        !< maximum # of iterations for which 2D extremal opt is performed
