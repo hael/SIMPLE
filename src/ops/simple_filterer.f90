3cf14862b7add1427f8bbe693e02986a0b14fb97
cyrilreboul <cyril.reboul@monash.edu>
2019-06-14 16:33:08 +1000

    3-fold speed up anisotropic filter generation

--- src/ops/simple_filterer.f90
+++ src/ops/simple_filterer.f90
@@ -7,6 +7,7 @@ use simple_image, only: image
 use simple_oris,  only: oris
 use simple_ori,   only: ori
 use simple_error, only: simple_exception
+
 implicit none
 
 public :: gen_anisotropic_optlp
@@ -30,13 +31,14 @@ contains
         character(len=*),       intent(in)    :: pgrp
         integer,                intent(in)    :: hpind_fsc
         logical,                intent(in)    :: phaseplate
+        complex(kind=c_float_complex), pointer :: pcmat(:,:,:) => null()
         type(sym)         :: se
         type(ori)         :: orientation, o_sym
         integer           :: noris, iori, nprojs, ldim(3), lims(3,2), isym, nsym
         integer           :: sh, logi(3), phys(3), imatch, filtsz, h, k, l
         real, allocatable :: plane_normals(:,:,:), plane_normals_L2(:,:), filters2D(:,:)
         real, allocatable :: frc(:)
-        real              :: fwght, fwght_find0, fwght_find1, fwght_find2
+        real              :: loginorm(3),fwght, fwght_find0, fwght_find1, fwght_find2
         write(logfhandle,'(a)') '>>> GENERATING ANISOTROPIC OPTIMAL 3D LOW-PASS FILTER'
         ! sanity checking
         noris  = e_space%get_noris()
@@ -50,16 +52,17 @@ contains
         if( pgrp .eq. 'c1' )then
             nsym = 1
             ! extract plane normals and L2 norms from e_space & extract 2D filters from projfrcs
-            allocate( plane_normals(1,noris,3), plane_normals_L2(1,noris), filters2D(noris,filtsz) )
+            allocate( plane_normals(3,noris,1), plane_normals_L2(noris,1), filters2D(noris,filtsz) )
             plane_normals    = 0.0
             plane_normals_L2 = 0.0
             filters2D        = 0.0
             do iori=1,noris
-                ! plane normals & L2 norms
-                plane_normals(1,iori,:)  = e_space%get_normal(iori)
-                plane_normals_L2(1,iori) = sum(plane_normals(1,iori,:) * plane_normals(1,iori,:))
-                if( plane_normals_L2(1,iori) > TINY )then
-                    plane_normals_L2(1,iori) = sqrt(plane_normals_L2(1,iori))
+                ! normalized plane normals & L2 norms
+                plane_normals(:,iori,1)  = e_space%get_normal(iori)
+                plane_normals_L2(iori,1) = sum(plane_normals(:,iori,1) * plane_normals(:,iori,1))
+                if( plane_normals_L2(iori,1) > TINY )then
+                    plane_normals_L2(iori,1) = sqrt(plane_normals_L2(iori,1))
+                    plane_normals(:,iori,1)  = plane_normals(:,iori,1) / plane_normals_L2(iori,1)
                 endif
                 ! 2D filters
                 call projfrcs%frc_getter(iori, hpind_fsc, phaseplate, frc, state)
@@ -70,7 +73,7 @@ contains
             call se%new(pgrp)
             nsym = se%get_nsym()
             ! extract plane normals and L2 norms from e_space & extract 2D filters from projfrcs
-            allocate( plane_normals(nsym,noris,3), plane_normals_L2(nsym,noris), filters2D(noris,filtsz) )
+            allocate( plane_normals(3,noris,nsym), plane_normals_L2(noris,nsym), filters2D(noris,filtsz) )
             plane_normals    = 0.0
             plane_normals_L2 = 0.0
             filters2D        = 0.0
@@ -79,10 +82,11 @@ contains
                 do isym=1,nsym
                     call se%apply(orientation, isym, o_sym)
                     ! plane normals & L2 norms
-                    plane_normals(isym,iori,:) = o_sym%get_normal()
-                    plane_normals_L2(isym,iori) = sum(plane_normals(isym,iori,:) * plane_normals(isym,iori,:))
-                    if( plane_normals_L2(isym,iori) > TINY )then
-                        plane_normals_L2(isym,iori) = sqrt(plane_normals_L2(isym,iori))
+                    plane_normals(:,iori,isym)  = o_sym%get_normal()
+                    plane_normals_L2(iori,isym) = sum(plane_normals(:,iori,isym) * plane_normals(:,iori,isym))
+                    if( plane_normals_L2(iori,isym) > TINY )then
+                        plane_normals_L2(iori,isym) = sqrt(plane_normals_L2(iori,isym))
+                        plane_normals(:,iori,isym)  = plane_normals(:,iori,isym) / plane_normals_L2(iori,isym)
                     endif
                 end do
                 ! 2D filters
@@ -96,38 +100,41 @@ contains
         fwght_find1 = sum(filters2D(:,1)) / real(noris)
         fwght_find2 = sum(filters2D(:,2)) / real(noris)
         call vol_filter%zero_and_flag_ft
+        call vol_filter%get_cmat_ptr(pcmat)
         ! dynamic scheduling works better here because of the imbalanced amount of compute
         ! it cuts execution time by half as compared to static scheduling
-        !$omp parallel do collapse(3) default(shared) private(h,k,l,sh,logi,phys,imatch,fwght)&
+        !$omp parallel do collapse(3) default(shared) private(h,k,l,sh,logi,phys,imatch,fwght,loginorm)&
         !$omp schedule(dynamic) proc_bind(close)
         do h=lims(1,1),lims(1,2)
             do k=lims(2,1),lims(2,2)
                 do l=lims(3,1),lims(3,2)
                     ! shell
-                    sh   = nint(hyp(real(h),real(k),real(l)))
-                    ! logical index
-                    logi = [h, k, l]
-                    ! physical index
-                    phys = vol_filter%comp_addr_phys(logi)
-                    ! set 3D filter coeff
-                    select case(sh)
-                        case(0)
-                            call vol_filter%set_fcomp(logi, phys, cmplx(fwght_find0,0.0))
-                        case(1)
-                            call vol_filter%set_fcomp(logi, phys, cmplx(fwght_find1,0.0))
-                        case(2)
-                            call vol_filter%set_fcomp(logi, phys, cmplx(fwght_find2,0.0))
-                        case DEFAULT
-                            if( sh <= filtsz )then
-                                imatch = find2Dmatch(real([h,k,l]))
-                                fwght  = filters2D(imatch,sh) ! filter coeff
-                                call vol_filter%set_fcomp(logi, phys, cmplx(fwght,0.0))
-                            endif
-                    end select
+                    sh = nint(hyp(real(h),real(k),real(l)))
+                    if( sh <= filtsz )then
+                        ! logical index
+                        logi = [h, k, l]
+                        ! physical index
+                        phys = vol_filter%comp_addr_phys(logi)
+                        ! set 3D filter coeff
+                        select case(sh)
+                            case(0)
+                                pcmat(phys(1),phys(2),phys(3)) = cmplx(fwght_find0)
+                            case(1)
+                                pcmat(phys(1),phys(2),phys(3)) = cmplx(fwght_find1)
+                            case(2)
+                                pcmat(phys(1),phys(2),phys(3)) = cmplx(fwght_find2)
+                            case DEFAULT
+                                loginorm = real(logi) / sum(real(logi*logi)) ! normalize
+                                imatch   = find2Dmatch(loginorm)
+                                fwght    = filters2D(imatch,sh) ! filter coeff
+                                pcmat(phys(1),phys(2),phys(3)) = cmplx(fwght)
+                        end select
+                    endif
                 end do
             end do
         end do
         !$omp end parallel do
+        nullify(pcmat)
         call orientation%kill
         call o_sym%kill
         contains
@@ -135,33 +142,28 @@ contains
             ! index of matching 2D filter
             integer function find2Dmatch( vec )
                 real, intent(in) :: vec(3)
-                integer :: iori, loc(1)
-                real    :: angles(noris)
+                real    :: sines(noris)
+                integer :: iori
                 do iori=1,noris
-                    angles(iori) = angle_btw_vec_and_normal( vec, iori )
+                    sines(iori) = sine_btw_vec_and_normal(vec, iori)
                 end do
-                loc = minloc(angles)
-                find2Dmatch = loc(1)
+                find2Dmatch = minloc(sines,dim=1)
             end function find2Dmatch
 
-            ! angle minimised over symmetry group
-            real function angle_btw_vec_and_normal( vec, iori )
+            ! sine minimised over symmetry group (= angle minimized)
+            real function sine_btw_vec_and_normal( vec, iori )
                 real,    intent(in) :: vec(3)
                 integer, intent(in) :: iori
-                real    :: vec_L2, x, angle
+                real    :: sine
                 integer :: isym
-                vec_L2 = sum(vec * vec)
-                angle_btw_vec_and_normal = huge(x)
-                if( vec_L2 > TINY )then
-                    vec_L2 = sqrt(vec_L2)
-                    do isym=1,nsym
-                        if( plane_normals_L2(isym,iori) > TINY )then
-                            angle = asin( abs(sum(plane_normals(isym,iori,:) * vec(:))) / (vec_L2 * plane_normals_L2(isym,iori)) )
-                            if( angle < angle_btw_vec_and_normal ) angle_btw_vec_and_normal = angle
-                        endif
-                    end do
-                endif
-            end function angle_btw_vec_and_normal
+                sine_btw_vec_and_normal = huge(sine)
+                do isym=1,nsym
+                    if( plane_normals_L2(iori,isym) > TINY )then
+                        sine = abs( sum(plane_normals(:,iori,isym) * vec) )
+                        sine_btw_vec_and_normal = min(sine, sine_btw_vec_and_normal)
+                    endif
+                end do
+            end function sine_btw_vec_and_normal
 
     end subroutine gen_anisotropic_optlp
 
