2f2f4755431d24ac2f890ee175634cd5d46c7716
Hans Elmlund <hans.elmlund@monash.edu>
2019-12-10 16:03:11 +1100

    reverted to classic speccore (essentially the van Heel Q factor) which works well for HCN

--- src/main/simple_polarft_corrcalc.f90
+++ src/main/simple_polarft_corrcalc.f90
@@ -2258,7 +2258,7 @@ contains
         integer,                 intent(in)    :: iref, iptcl, irot
         real :: frc(params_glob%kfromto(1):params_glob%kstop)
         call self%genfrc(iref, iptcl, irot, frc)
-        specscore_1 = max(0.,min(real(count(frc>0.143)) / real(size(frc)),1.))
+        specscore_1 = sum(frc) / real(params_glob%kstop - params_glob%kfromto(1) + 1)
     end function specscore_1
 
     real function specscore_2( self, iref, iptcl, irot, shvec )
@@ -2267,7 +2267,7 @@ contains
         real,                    intent(in)    :: shvec(2)
         real :: frc(params_glob%kfromto(1):params_glob%kstop)
         call self%calc_frc(iref, iptcl, irot, shvec, frc )
-        specscore_2 = max(0.,min(real(count(frc>0.143)) / real(size(frc)),1.))
+        specscore_2 = sum(frc) / real(params_glob%kstop - params_glob%kfromto(1) + 1)
     end function specscore_2
 
     function calc_roinv_corrmat( self) result(corrmat )
