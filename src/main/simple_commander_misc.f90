70c497ce1878a142fc6b83a5f9d0d13df4b0e029
cyrilreboul <cyril.reboul@monash.edu>
2021-04-07 15:43:40 +1000

    Utilities for remoc

--- src/main/simple_commander_misc.f90
+++ src/main/simple_commander_misc.f90
@@ -1,6 +1,7 @@
 ! concrete commander: miscallenaous routines
 module simple_commander_misc
 include 'simple_lib.f08'
+include "starfile/starfile_enum.inc"
 use simple_cmdline,        only: cmdline
 use simple_commander_base, only: commander_base
 use simple_oris,           only: oris
@@ -22,6 +23,7 @@ public :: shift_commander
 public :: stk_corr_commander
 public :: kstest_commander
 public :: mkdir_commander
+public :: remoc_commander
 private
 #include "simple_local_flags.inc"
 
@@ -57,6 +59,10 @@ type, extends(commander_base) :: mkdir_commander
   contains
     procedure :: execute       => exec_mkdir
 end type mkdir_commander
+type, extends(commander_base) :: remoc_commander
+  contains
+    procedure :: execute       => exec_remoc
+end type remoc_commander
 
 contains
 
@@ -431,5 +437,278 @@ contains
         call cline%set('mkdir', 'yes')
         call params%new(cline)
     end subroutine exec_mkdir
+
+    subroutine exec_remoc( self, cline )
+        use simple_starfile_wrappers
+        use simple_projector
+        use simple_estimate_ssnr, only: fsc2optlp_sub
+        class(remoc_commander), intent(inout) :: self
+        class(cmdline),         intent(inout) :: cline
+        class(str4arr),   allocatable :: mics(:),mics_from_ptcls(:), star_movies(:)
+        character(len=:), allocatable :: fname, movie, vol_even_fname, vol_odd_fname, mask_fname, mic_fname
+        real,             allocatable :: fsc(:), optfilter(:)
+        type(parameters) :: params
+        type(ctfparams)  :: ctf_glob, ctf_mic
+        type(oris)       :: os
+        type(projector)  :: vol_even, vol_odd
+        type(image)      :: vol, vol_mask
+        real    :: mov_smpd
+        integer :: ldim(3), ldim_mov(3), nmics, nmovies, filtsz
+        logical :: l_scale
+        call cline%set('mkdir',       'yes')
+        if( .not. cline%defined('trs')           ) call cline%set('trs',           20.)
+        if( .not. cline%defined('lpstop')        ) call cline%set('lpstop',         5.)
+        if( .not. cline%defined('bfac')          ) call cline%set('bfac',          50.)
+        if( .not. cline%defined('lplim_crit')    ) call cline%set('lplim_crit',    0.5)
+        if( .not. cline%defined('wcrit')         ) call cline%set('wcrit',   'softmax')
+        if( .not. cline%defined('eer_fraction')  ) call cline%set('eer_fraction',  20.)
+        if( .not. cline%defined('eer_upsampling')) call cline%set('eer_upsampling', 1.)
+        call cline%set('oritype', 'mic')
+        call params%new(cline)
+        ! Particles
+        call parse_particles( params%star_ptcl, os, mics, ctf_glob, params%box, params%nptcls )
+        params%smpd = ctf_glob%smpd
+        ldim = params%box
+        ! Micrographs
+        call parse_mics( params%star_mic, mics, star_movies, ctf_mic, mov_smpd )
+        params%smpd = ctf_glob%smpd
+        nmics   = size(mics)
+        nmovies = size(star_movies)
+        if( abs(params%smpd-ctf_mic%smpd) > 0.01 ) THROW_HARD('Inconsistent pixel sizes between micrographs and particles!')
+        params%scale = mov_smpd / ctf_glob%smpd
+        l_scale = abs(params%scale-1.) > 0.01
+        if( l_scale )then
+            params%alpha = 1./params%scale
+            ldim_mov = round2even(real(ldim)/params%scale)
+        else
+            params%alpha = 1.
+            ldim_mov     = ldim
+        endif
+        ! Model
+        call parse_model(params%star_model, vol_even_fname, vol_odd_fname, mask_fname, fsc)
+        mask_fname = get_fpath(params%star_model)//'/'//trim(mask_fname)
+        filtsz = size(fsc)
+        allocate(optfilter(filtsz),source=0.)
+        call fsc2optlp_sub(filtsz, fsc, optfilter)
+        ! volumes prep
+        call vol%new(ldim,params%smpd)
+        call vol_mask%new(ldim,params%smpd)
+        call vol%read(vol_even_fname)
+        call vol_mask%read(mask_fname)
+        call vol%mul(vol_mask)
+        if( l_scale )then
+            call vol_even%new(ldim_mov,mov_smpd)
+            call vol%pad(vol_even)
+        else
+            vol_even = vol
+        endif
+        call vol%read(vol_odd_fname)
+        call vol%mul(vol_mask)
+        call vol_mask%kill
+        if( l_scale )then
+            call vol_odd%new(ldim_mov,mov_smpd)
+            call vol%pad(vol_odd)
+        else
+            vol_odd = vol
+        endif
+        call vol%kill
+        call vol_even%fft
+        call vol_odd%fft
+        call vol_even%apply_filter(optfilter)
+        call vol_odd%apply_filter(optfilter)
+        call vol_even%expand_cmat(1.,norm4proj=.true.)
+        call vol_odd%expand_cmat(1.,norm4proj=.true.)
+        ! end gracefully
+        call simple_end('**** SIMPLE_REMOC NORMAL STOP ****')
+        contains
+
+            integer function parse_int( table, emdl_id )
+                class(starfile_table_type) :: table
+                integer, intent(in)        :: emdl_id
+                logical :: l_ok
+                l_ok = starfile_table__getValue_int(table, emdl_id, parse_int)
+                if(.not.l_ok) THROW_HARD('Missing value in table!')
+            end function parse_int
+
+            real function parse_double( table, emdl_id )
+                class(starfile_table_type) :: table
+                integer, intent(in)        :: emdl_id
+                real(dp) :: v
+                logical  :: l_ok
+                l_ok = starfile_table__getValue_double(table, emdl_id, v)
+                if(.not.l_ok) THROW_HARD('Missing value in table!')
+                parse_double = real(v)
+            end function parse_double
+
+            subroutine parse_string( table, emdl_id, string )
+                class(starfile_table_type)                 :: table
+                integer,                       intent(in)  :: emdl_id
+                character(len=:), allocatable, intent(out) :: string
+                logical :: l_ok
+                l_ok = starfile_table__getValue_string(table, emdl_id, string)
+                if(.not.l_ok) THROW_HARD('Missing value in table!')
+            end subroutine parse_string
+
+            subroutine parse_particles( fname, os, mics, ctf, box, nptcls_out )
+                character(len=*),               intent(in)  :: fname
+                class(oris),                    intent(out) :: os
+                class(str4arr),    allocatable, intent(out) :: mics(:)
+                class(ctfparams),               intent(out) :: ctf
+                integer,                        intent(out) :: box, nptcls_out
+                type(str4arr),    allocatable :: names(:)
+                type(str4arr)    ::fname_here
+                type(starfile_table_type)     :: star_table
+                type(ctfparams)  :: ctfparms
+                real             :: euls(3),pos(2),shift(2)
+                integer          :: i,n,ind,eo
+                integer(C_long)  :: num_objs, object_id
+                fname_here%str = trim(fname)//C_NULL_CHAR
+                call starfile_table__new(star_table)
+                call starfile_table__getnames(star_table, fname_here%str, names)
+                n = size(names)
+                do i =1,n
+                    call starfile_table__read(star_table, fname_here%str, names(i)%str )
+                    select case(trim(names(i)%str))
+                    case('optics')
+                        ! ignoring multiple optics groups for now
+                        object_id = starfile_table__firstobject(star_table) ! base 0
+                        ! ctf
+                        ctf%fraca = parse_double(star_table, EMDL_CTF_Q0)
+                        ctf%cs    = parse_double(star_table, EMDL_CTF_CS)
+                        ctf%kv    = parse_double(star_table, EMDL_CTF_VOLTAGE)
+                        ctf%smpd  = parse_double(star_table, EMDL_IMAGE_PIXEL_SIZE)
+                        ! dimensions
+                        box = parse_int(star_table, EMDL_IMAGE_SIZE)
+                    case('particles')
+                        object_id  = starfile_table__firstobject(star_table) ! base 0
+                        num_objs   = starfile_table__numberofobjects(star_table)
+                        nptcls_out = int(num_objs - object_id)
+                        call os%new(nptcls_out)
+                        allocate(mics(nptcls_out))
+                        ind = 0
+                        do while( (object_id < num_objs) .and. (object_id >= 0) )
+                            ind = ind+1
+                            ! Micrograph name
+                            call parse_string(star_table, EMDL_MICROGRAPH_NAME, mics(ind)%str)
+                            ! picking coordinates
+                            pos(1) = parse_double(star_table, EMDL_IMAGE_COORD_X) - box/2 - 1
+                            pos(2) = parse_double(star_table, EMDL_IMAGE_COORD_Y) - box/2 - 1
+                            ! CTF
+                            ctfparms%dfx    = parse_double(star_table, EMDL_CTF_DEFOCUSU) / 10000.0 ! Microns
+                            ctfparms%dfy    = parse_double(star_table, EMDL_CTF_DEFOCUSU) / 10000.0 ! microns
+                            ctfparms%angast = parse_double(star_table, EMDL_CTF_DEFOCUS_ANGLE)
+                            ! e/o
+                            eo = parse_int(star_table, EMDL_PARTICLE_RANDOM_SUBSET) - 1
+                            ! Alignement parameters
+                            euls(1)  = parse_double(star_table, EMDL_ORIENT_ROT)
+                            euls(2)  = parse_double(star_table, EMDL_ORIENT_TILT)
+                            euls(3)  = parse_double(star_table, EMDL_ORIENT_PSI)
+                            shift(1) = parse_double(star_table, EMDL_ORIENT_ORIGIN_X_ANGSTROM) / ctf_glob%smpd ! in pixels
+                            shift(2) = parse_double(star_table, EMDL_ORIENT_ORIGIN_X_ANGSTROM) / ctf_glob%smpd ! in pixels
+                            ! transfer to oris object
+                            call os%set_euler(ind,   euls)
+                            call os%set_shift(ind,   shift)
+                            call os%set(ind,'dfx',   ctfparms%dfx)
+                            call os%set(ind,'dfy',   ctfparms%dfy)
+                            call os%set(ind,'angast',ctfparms%angast)
+                            call os%set(ind,'xpos',  pos(1))
+                            call os%set(ind,'ypos',  pos(2))
+                            call os%set(ind,'eo',    real(eo))
+                            ! done
+                            object_id = starfile_table__nextobject(star_table)
+                        end do
+                    case DEFAULT
+                        THROW_HARD('Invalid table: '//trim(names(i)%str))
+                    end select
+                enddo
+                call starfile_table__delete(star_table)
+            end subroutine parse_particles
+    
+            subroutine parse_mics( fname, mics, star_movies, ctf, mov_smpd )
+                character(len=*),                intent(in) :: fname
+                class(str4arr),    allocatable, intent(out) :: mics(:), star_movies(:)
+                class(ctfparams),               intent(out) :: ctf
+                real,                           intent(out) :: mov_smpd
+                type(str4arr),    allocatable :: names(:)
+                type(starfile_table_type)     :: star_table
+                integer          :: i,n,nmics,ind
+                integer(C_long)  :: num_objs, object_id
+                call starfile_table__new(star_table)
+                call starfile_table__getnames(star_table, trim(fname)//C_NULL_CHAR, names)
+                n = size(names)
+                do i =1,n
+                    call starfile_table__read(star_table, trim(fname)//C_NULL_CHAR, names(i)%str )
+                    select case(trim(names(i)%str))
+                    case('optics')
+                        ! ignoring multiple optics groups for now
+                        object_id = starfile_table__firstobject(star_table) ! base 0
+                        ! ctf
+                        ctf%fraca = parse_double(star_table, EMDL_CTF_Q0)
+                        ctf%cs    = parse_double(star_table, EMDL_CTF_CS)
+                        ctf%kv    = parse_double(star_table, EMDL_CTF_VOLTAGE)
+                        ctf%smpd  = parse_double(star_table, EMDL_MICROGRAPH_PIXEL_SIZE)
+                        mov_smpd  = parse_double(star_table, EMDL_MICROGRAPH_ORIGINAL_PIXEL_SIZE)
+                    case('micrographs')
+                        object_id = starfile_table__firstobject(star_table) ! base 0
+                        num_objs  = starfile_table__numberofobjects(star_table)
+                        nmics     = int(num_objs - object_id)
+                        allocate(mics(nmics),star_movies(nmics))
+                        ind = 0
+                        do while( (object_id < num_objs) .and. (object_id >= 0) )
+                            ind = ind+1
+                            call parse_string(star_table, EMDL_MICROGRAPH_NAME, mics(ind)%str)
+                            call parse_string(star_table, EMDL_MICROGRAPH_METADATA_NAME, star_movies(ind)%str)
+                            ! done
+                            object_id = starfile_table__nextobject(star_table)
+                        end do
+                    case DEFAULT
+                        THROW_HARD('Invalid table: '//trim(names(i)%str))
+                    end select
+                enddo
+                call starfile_table__delete(star_table)
+            end subroutine parse_mics
+
+            subroutine parse_model( fname, vol_even, vol_odd, mask, fsc )
+                character(len=*),              intent(in)  :: fname
+                character(len=:), allocatable, intent(out) :: vol_even, vol_odd, mask
+                real,             allocatable, intent(out) :: fsc(:)
+                type(str4arr),    allocatable :: names(:)
+                type(starfile_table_type)     :: star_table
+                integer          :: i,n,ind,nn
+                integer(C_long)  :: num_objs, object_id
+                call starfile_table__new(star_table)
+                call starfile_table__getnames(star_table, trim(fname)//C_NULL_CHAR, names)
+                n = size(names)
+                do i =1,n
+                    call starfile_table__read(star_table, trim(fname)//C_NULL_CHAR, names(i)%str )
+                    select case(trim(names(i)%str))
+                    case('general')
+                        object_id = starfile_table__firstobject(star_table)
+                        ! e/o volumes, mask
+                        call parse_string(star_table, EMDL_POSTPROCESS_UNFIL_HALFMAP1, vol_even)
+                        call parse_string(star_table, EMDL_POSTPROCESS_UNFIL_HALFMAP2, vol_odd)
+                        call parse_string(star_table, EMDL_MASK_NAME, mask)
+                    case('fsc')
+                        object_id = starfile_table__firstobject(star_table)
+                        num_objs  = starfile_table__numberofobjects(star_table)
+                        nn        = int(num_objs - object_id)
+                        if(nn /= params%box/2+1 ) THROW_HARD('Inconsistent image & fsc dimensions!')
+                        allocate(fsc(params%box/2),source=0.)
+                        do while( (object_id < num_objs) .and. (object_id >= 0) )
+                            ind = parse_int(star_table, EMDL_SPECTRAL_IDX)
+                            if( ind /= 0 ) fsc(ind) = parse_double(star_table, EMDL_POSTPROCESS_FSC_TRUE)
+                            ! done
+                            object_id = starfile_table__nextobject(star_table)
+                        end do
+                    case('guinier')
+                        ! Ignored for now
+                    case DEFAULT
+                        THROW_HARD('Invalid table: '//trim(names(i)%str))
+                    end select
+                enddo
+                call starfile_table__delete(star_table)
+            end subroutine parse_model
+
+    end subroutine exec_remoc
 
 end module simple_commander_misc
