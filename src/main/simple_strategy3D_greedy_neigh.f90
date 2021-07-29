ac00481fc0f46e310ac622168665265277efe7be
Hans Elmlund <hans.elmlund@monash.edu>
2021-07-30 09:02:40 +1000

    correspoding logic for fractional greedy search and fraction-dependent search switch for neigh refinement. Possibility to turn off automasking in postprocess when masked refinement has been run

--- src/main/simple_strategy3D_greedy_neigh.f90
+++ src/main/simple_strategy3D_greedy_neigh.f90
@@ -1,5 +1,5 @@
 ! concrete strategy3D: neighbourhood refinement
-module simple_strategy3D_neigh
+module simple_strategy3D_greedy_neigh
 include 'simple_lib.f08'
 use simple_strategy3D_alloc
 use simple_strategy3D_utils
@@ -10,35 +10,35 @@ use simple_strategy3D_srch,  only: strategy3D_srch, strategy3D_spec
 use simple_polarft_corrcalc, only: pftcc_glob
 implicit none
 
-public :: strategy3D_neigh
+public :: strategy3D_greedy_neigh
 private
 #include "simple_local_flags.inc"
 
-type, extends(strategy3D) :: strategy3D_neigh
+type, extends(strategy3D) :: strategy3D_greedy_neigh
     type(strategy3D_srch) :: s
     type(strategy3D_spec) :: spec
 contains
-    procedure          :: new         => new_neigh
-    procedure          :: srch        => srch_neigh
-    procedure          :: oris_assign => oris_assign_neigh
-    procedure          :: kill        => kill_neigh
-end type strategy3D_neigh
+    procedure          :: new         => new_greedy_neigh
+    procedure          :: srch        => srch_greedy_neigh
+    procedure          :: oris_assign => oris_assign_greedy_neigh
+    procedure          :: kill        => kill_greedy_neigh
+end type strategy3D_greedy_neigh
 
 contains
 
-    subroutine new_neigh( self, spec )
-        class(strategy3D_neigh), intent(inout) :: self
+    subroutine new_greedy_neigh( self, spec )
+        class(strategy3D_greedy_neigh), intent(inout) :: self
         class(strategy3D_spec),        intent(inout) :: spec
         call self%s%new(spec)
         self%spec = spec
-    end subroutine new_neigh
+    end subroutine new_greedy_neigh
 
-    subroutine srch_neigh( self, ithr )
+    subroutine srch_greedy_neigh( self, ithr )
         use simple_ori, only: ori
-        class(strategy3D_neigh), intent(inout) :: self
+        class(strategy3D_greedy_neigh), intent(inout) :: self
         integer,                       intent(in)    :: ithr
         type(ori) :: o
-        integer   :: iref, isample, iproj, minnrefs
+        integer   :: iref, iproj, minnrefs
         real      :: inpl_corrs(self%s%nrots)
         logical   :: lnns(params_glob%nspace)
         ! execute search
@@ -48,24 +48,21 @@ contains
             ! prep
             call self%s%prep4srch
             ! initialize, ctd
-            self%s%nbetter    = 0
-            self%s%nrefs_eval = 0
             call build_glob%spproj_field%get_ori(self%s%iptcl, o)
             call build_glob%pgrpsyms%nearest_proj_neighbors(build_glob%eulspace, o, params_glob%athres, lnns)
             self%s%nnn = count(lnns)
-            minnrefs   = ceiling(real(self%s%nnn) * NEIGH_MINFRAC)
             ! search
-            do isample=1,self%s%nrefs
-                iref  = s3D%srch_order(self%s%ithr,isample)  ! set the stochastic reference index
-                iproj = iref - (self%s%prev_state - 1) * params_glob%nspace
+            do iproj=1,params_glob%nspace
                 if( .not. lnns(iproj) ) cycle
+                iref = (self%s%prev_state - 1) * params_glob%nspace + iproj
                 call per_ref_srch
-                ! exit condition
-                if( self%s%nbetter > 0 .and. self%s%nrefs_eval >= minnrefs ) exit
             end do
+            ! in greedy mode, we evaluate all refs
+            self%s%nrefs_eval = self%s%nnn
             call sort_corrs(self%s) ! sort in correlation projection direction space
+            ! take care of the in-planes
             call self%s%inpl_srch   ! search shifts
-            ! prepare weights and orientations
+            ! prepare orientation
             call self%oris_assign
         else
             call build_glob%spproj_field%reject(self%s%iptcl)
@@ -80,23 +77,19 @@ contains
                 call pftcc_glob%gencorrs(iref, self%s%iptcl, inpl_corrs)
                 loc = maxloc(inpl_corrs)
                 call self%s%store_solution(iref, loc(1), inpl_corrs(loc(1)), .true.)
-                ! update nbetter to keep track of how many improving solutions we have identified
-                if( inpl_corrs(loc(1)) > self%s%prev_corr ) self%s%nbetter = self%s%nbetter + 1
-                ! keep track of how many references we are evaluating
-                self%s%nrefs_eval = self%s%nrefs_eval + 1
             endif
         end subroutine per_ref_srch
 
-    end subroutine srch_neigh
+    end subroutine srch_greedy_neigh
 
-    subroutine oris_assign_neigh( self )
-        class(strategy3D_neigh), intent(inout) :: self
+    subroutine oris_assign_greedy_neigh( self )
+        class(strategy3D_greedy_neigh), intent(inout) :: self
         call extract_peak_ori(self%s)
-    end subroutine oris_assign_neigh
+    end subroutine oris_assign_greedy_neigh
 
-    subroutine kill_neigh( self )
-        class(strategy3D_neigh),   intent(inout) :: self
+    subroutine kill_greedy_neigh( self )
+        class(strategy3D_greedy_neigh),   intent(inout) :: self
         call self%s%kill
-    end subroutine kill_neigh
+    end subroutine kill_greedy_neigh
 
-end module simple_strategy3D_neigh
+end module simple_strategy3D_greedy_neigh
