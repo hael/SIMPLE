1f99f43728aafe4e7af961af07ccbf39c5eb2221
hael <hans.elmlund@nih.gov>
2021-12-09 18:23:40 -0500

    changed convergence criterion for cluster2D when run without fractional update

--- src/defs/simple_defs_conv.f90
+++ src/defs/simple_defs_conv.f90
@@ -1,10 +1,10 @@
 module simple_defs_conv
 real, parameter :: MI_CLASS_LIM_3D      = 0.996
-real, parameter :: MI_CLASS_LIM_2D      = 0.98
-real, parameter :: MI_CLASS_LIM_2D_FRAC = 0.99
+real, parameter :: MI_CLASS_LIM_2D      = 0.80  ! based on Susan's observations on the small stuff, early stopping reduces overfitting
+real, parameter :: MI_CLASS_LIM_2D_FRAC = 0.99  ! this limit is for when fractional update is on (does it happen in cluster2D???)
 real, parameter :: MI_STATE_LIM         = 0.98
 real, parameter :: MI_STATE_JOINT_LIM   = 0.96
-real, parameter :: FRAC_LIM             = 98.0
+real, parameter :: FRAC_LIM             = 90.0  ! based on Susan's observations on the small stuff, early stopping reduces overfitting
 real, parameter :: FRAC_LIM_FRAC        = 99.0
 real, parameter :: MSK_FRAC             = 0.06
 real, parameter :: MINSHIFT             = 5.0
