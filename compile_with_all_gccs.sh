7b2f3525453d494440d199976d73b2a69a91343e
Hans Elmlund <hans.elmlund@nih.gov>
2021-12-13 09:54:25 -0500

    gcc 10 default compiler

--- compile_with_all_gccs.sh
+++ compile_with_all_gccs.sh
@@ -2,27 +2,27 @@
 module purge
 module load cmake/3.13.1
 
-echo "Compiling SIMPLE with gcc5.5.0"
-module load gcc/5.5.0
-rm -r build_gcc5.5.0
-mkdir build_gcc5.5.0
-cd build_gcc5.5.0
-cmake ..
-make -j 8 install
-cd ..
-chmod a+rx,o+rx -R build_gcc5.5.0/
-module unload gcc/5.5.0
+#echo "Compiling SIMPLE with gcc5.5.0"
+#module load gcc/5.5.0
+#rm -r build_gcc5.5.0
+#mkdir build_gcc5.5.0
+#cd build_gcc5.5.0
+#cmake ..
+#make -j 8 install
+#cd ..
+#chmod a+rx,o+rx -R build_gcc5.5.0/
+#module unload gcc/5.5.0
 
-echo "Compiling SIMPLE with gcc9.3.0"
-module load gcc/9.3.0
-rm -r build_gcc9.3.0
-mkdir build_gcc9.3.0
-cd build_gcc9.3.0
-cmake ..
-make -j 8 install
-cd ..
-chmod a+rx,o+rx -R build_gcc9.3.0/
-module unload gcc/9.3.0
+#echo "Compiling SIMPLE with gcc9.3.0"
+#module load gcc/9.3.0
+#rm -r build_gcc9.3.0
+#mkdir build_gcc9.3.0
+#cd build_gcc9.3.0
+#cmake ..
+#make -j 8 install
+#cd ..
+#chmod a+rx,o+rx -R build_gcc9.3.0/
+#module unload gcc/9.3.0
 
 echo "Compiling SIMPLE with gcc10.2.0"
 module load gcc/10.2.0
