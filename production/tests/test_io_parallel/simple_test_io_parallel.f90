b7c2f5299e1dc190b0f5f413cbebc287d255913e
hael <hans.elmlund@nih.gov>
2022-01-14 18:52:10 -0500

    parallel MRC vol IO test shows faster

---
+++ production/tests/test_io_parallel/simple_test_io_parallel.f90
@@ -0,0 +1,98 @@
+program simple_test_io_parallel
+include 'simple_lib.f08'
+use simple_image,    only: image
+use simple_stack_io, only: stack_io
+use simple_imgfile,  only: imgfile
+implicit none
+character(len=:), allocatable :: benchfname
+integer,          parameter   :: NVOLRWS   = 10
+integer,          parameter   :: NVOLS     = 4
+integer,          parameter   :: BOX       = 512
+integer,          parameter   :: ONE_M     = 1024**2
+integer(kind=8),  parameter   :: NVOLBYTES = NVOLRWS * BOX * BOX * BOX * 4
+real,             parameter   :: SMPD      = 1.0
+real(kind=c_float),            pointer :: rmat_ptr(:,:,:)=>null()  !< image pixels/voxels (in data)
+complex(kind=c_float_complex), pointer :: cmat_ptr(:,:,:)=>null()  !< Fourier components
+type(image)                   :: vols(NVOLS)
+type(imgfile)                 :: ioimg(NVOLS)
+integer(timer_int_kind)       ::  t_vol_w,  t_vol_w_para,  t_vol_r,  t_vol_r_para
+real(timer_int_kind)          :: rt_vol_w, rt_vol_w_para, rt_vol_r, rt_vol_r_para
+integer :: i, j, fnr
+
+print *, 'simulating volumes'
+do i = 1, NVOLS
+    call vols(i)%new([BOX,BOX,BOX], SMPD)
+    call vols(i)%ran()
+    call vols(i)%write('random_vol'//int2str(i)//'.mrc')
+end do
+
+print *, 'writing the volume '//int2str(NVOLRWS)//' times'
+t_vol_w = tic()
+do j = 1, NVOLRWS
+    do i = 1, NVOLS
+        call vols(i)%write('random_vol'//int2str(i)//'.mrc')
+    end do
+end do
+rt_vol_w = toc(t_vol_w)
+
+print *, 'writing the volume '//int2str(NVOLRWS)//' times in parallel'
+t_vol_w_para = tic()
+do i = 1, NVOLS
+    call ioimg(i)%open('random_vol'//int2str(i)//'.mrc', [BOX,BOX,BOX], SMPD, del_if_exists=.true., formatchar='M', readhead=.false., rwaction='write')
+end do
+do j = 1, NVOLRWS
+    !$omp parallel do default(shared) private(i,rmat_ptr) schedule(static) num_threads(NVOLS)
+    do i = 1, NVOLS
+        call vols(i)%get_rmat_ptr(rmat_ptr)
+        call ioimg(i)%wmrcSlices(1, BOX, rmat_ptr, [BOX,BOX,BOX], .false.) ! .false. is FT status
+    end do
+    !$omp end parallel do
+end do
+do i = 1, NVOLS
+    call ioimg(i)%close
+end do
+rt_vol_w_para = toc(t_vol_w_para)
+
+print *, 'reading the volume '//int2str(NVOLRWS)//' times'
+t_vol_r = tic()
+do j = 1, NVOLRWS
+    do i = 1, NVOLS
+        call vols(i)%read('random_vol'//int2str(i)//'.mrc')
+    end do
+end do
+rt_vol_r = toc(t_vol_r)
+
+print *, 'reading the volume '//int2str(NVOLRWS)//' times, in parallel'
+t_vol_r_para = tic()
+do i = 1, NVOLS
+    call ioimg(i)%open('random_vol'//int2str(i)//'.mrc', [BOX,BOX,BOX], SMPD, formatchar='M', readhead=.false., rwaction='read')
+end do
+do j = 1, NVOLRWS
+    !$omp parallel do default(shared) private(i,rmat_ptr) schedule(static) num_threads(NVOLS)
+    do i = 1, NVOLS
+        call vols(i)%get_rmat_ptr(rmat_ptr)
+        call ioimg(i)%rSlices(1,BOX,rmat_ptr,is_mrc=.true.)
+    end do
+    !$omp end parallel do
+end do
+rt_vol_r_para = toc(t_vol_r_para)
+
+! cleanup
+do i = 1, NVOLS
+    call del_file('random_vol'//int2str(i)//'.mrc')
+end do
+
+! write benchmark stats
+benchfname = 'SIMPLE_PARALLEL_IO_BENCH.txt'
+call fopen(fnr, FILE=trim(benchfname), STATUS='REPLACE', action='WRITE')
+write(fnr,'(a)') '*** TIMINGS (s) ***'
+write(fnr,'(a,1x,f9.2)') 'volume writes           : ', rt_vol_w
+write(fnr,'(a,1x,f9.2)') 'volume writes, parallel : ', rt_vol_w_para
+write(fnr,'(a,1x,f9.2)') 'volume reads            : ', rt_vol_r
+write(fnr,'(a,1x,f9.2)') 'volume reads, parallel  : ', rt_vol_r_para
+write(fnr,'(a)') '*** SPEEDUPS ***'
+write(fnr,'(a,1x,f9.2)') 'parallel write speedup  : ', rt_vol_w / rt_vol_w_para
+write(fnr,'(a,1x,f9.2)') 'parallel read  speedup  : ', rt_vol_r / rt_vol_r_para
+call fclose(fnr)
+
+end program simple_test_io_parallel
--- src/main/simple_image.f90
+++ src/main/simple_image.f90
@@ -986,17 +986,17 @@ contains
             endif
             self%ft = .false.
             select case(form)
-            case('M')
-                call ioimg%open(fname, self%ldim, self%smpd, formatchar=formatchar, readhead=readhead, rwaction=rwaction)
-                ! data type: 0 image: signed 8-bit bytes range -128 to 127
-                !            1 image: 16-bit halfwords
-                !            2 image: 32-bit reals (DEFAULT MODE)
-                !            3 transform: complex 16-bit integers
-                !            4 transform: complex 32-bit reals (THIS WOULD BE THE DEFAULT FT MODE)
-                mode = ioimg%getMode()
-                if( mode == 3 .or. mode == 4 ) self%ft = .true.
-            case('F','S','J','L')
-                call ioimg%open(fname, self%ldim, self%smpd, formatchar=formatchar, readhead=readhead, rwaction=rwaction)
+                case('M')
+                    call ioimg%open(fname, self%ldim, self%smpd, formatchar=formatchar, readhead=readhead, rwaction=rwaction)
+                    ! data type: 0 image: signed 8-bit bytes range -128 to 127
+                    !            1 image: 16-bit halfwords
+                    !            2 image: 32-bit reals (DEFAULT MODE)
+                    !            3 transform: complex 16-bit integers
+                    !            4 transform: complex 32-bit reals (THIS WOULD BE THE DEFAULT FT MODE)
+                    mode = ioimg%getMode()
+                    if( mode == 3 .or. mode == 4 ) self%ft = .true.
+                case('F','S','J','L')
+                    call ioimg%open(fname, self%ldim, self%smpd, formatchar=formatchar, readhead=readhead, rwaction=rwaction)
             end select
         else
             THROW_HARD('image need to be constructed before read/write; open')
