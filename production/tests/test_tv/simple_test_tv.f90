ff9ee97ca156cac2a679b1d9bbb593f41755f6fe
Van <vanc2@fnm-s105914.ncifcrf.gov>
2022-03-28 16:34:49 -0400

    Fix bugs in apply_filter_fsc, and add testing script for apply_filter_fsc. It seems to work on testing object.

--- production/tests/test_tv/simple_test_tv.f90
+++ production/tests/test_tv/simple_test_tv.f90
@@ -6,12 +6,13 @@ program simple_test_tv
     
     character(len=*), parameter :: test_in_img  = 'test.mrc'
     character(len=*), parameter :: test_out_img = 'test_out.mrc'
+    character(len=*), parameter :: test_out_fsc = 'test_out_fsc.mrc'
     integer,          parameter :: box    = 102
     real,             parameter :: smpd   = 1.275
     real,             parameter :: lambda = 0.5
     type(image)     :: img, img_in
     type(tvfilter)  :: tvfilter_obj
-    real            :: fsc_val(51)
+    real            :: fsc_val(51), n_voxel(51)
     
     call img_in%new([box,box,1], smpd)
     call img_in%read(test_in_img, 1)
@@ -22,6 +23,10 @@ program simple_test_tv
     call img%write(test_out_img, 1)
     call img%fsc(img_in, fsc_val)
     print *, 'fsc (noisy vs filtered) = ', fsc_val
+
+    n_voxel = 2.
+    call tvfilter_obj%apply_filter_fsc(img_in, fsc_val, n_voxel)
+    call img_in%write(test_out_fsc, 1)
 
     
 end program simple_test_tv
