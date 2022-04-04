5cae626ee868a6435f34f2a802580e2198c12e5f
Van <vanc2@nih.gov>
2022-04-04 10:33:29 -0400

    finish up the cost function of the butterworth kernel, still need testing

--- production/tests/test_math/simple_testfuns_butterworth.f90
+++ production/tests/test_math/simple_testfuns_butterworth.f90
@@ -2,7 +2,6 @@
 module simple_testfuns_butterworth
     use simple_defs
     implicit none
-    real :: a0, x0
     
     !>  \brief  defines the test function interface
     abstract interface
@@ -17,15 +16,26 @@ module simple_testfuns_butterworth
     contains
         function butterworth_cost( fun_self, x, d ) result( r )
             use simple_testfuns_constants, only: target_img, obj_img, ker_img
-            class(*), intent(inout) :: fun_self        
-            integer, intent(in) :: d
-            real, intent(in)    :: x(d)
-            integer :: i
-            real :: r
-            r = -2.
-            do i=1,d
-                r = r + (x(i)-3.)**2
-            end do
+            use simple_math,               only: butterworth, butterworth_kernel
+            use simple_image,              only: image
+            class(*), intent(inout) :: fun_self
+            integer,  intent(in)    :: d
+            real,     intent(in)    :: x(d)
+            real                    :: r
+            real,                          pointer :: rmat_target(:,:,:), rmat_ker(:,:,:)
+            complex(kind=c_float_complex), pointer :: cmat_conv(:,:,:)  , cmat_obj(:,:,:)
+
+            call ker_img%get_rmat_ptr(rmat_ker)
+            call butterworth_kernel(rmat_ker, 202, 8, x(1))  ! WARNING: fix the constants here
+            call ker_img%set_rmat(rmat_ker, .false.)
+            call target_img%get_rmat_ptr(rmat_target)
+            call obj_img%get_cmat_ptr(cmat_obj)
+            call ker_img%fft()
+            call ker_img%get_cmat_ptr(cmat_conv)
+            cmat_conv = cmat_conv*cmat_obj
+            call ker_img%ifft()
+            call ker_img%get_rmat_ptr(rmat_ker)
+            r = sum(abs(rmat_ker - rmat_target))
         end function
 end module
     
