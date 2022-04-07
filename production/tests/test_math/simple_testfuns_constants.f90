4ab0d746cab0779a68859ae9916b08dd3435514a
Van <vanc2@nih.gov>
2022-04-07 10:32:23 -0400

    more clean up before integrating butterworth stuffs

--- production/tests/test_math/simple_testfuns_constants.f90
+++
@@ -1,5 +0,0 @@
-module simple_testfuns_constants
-    use simple_image, only: image
-    implicit none
-    type(image) :: target_img, obj_img, ker_img, ker_der_img
-end module simple_testfuns_constants
