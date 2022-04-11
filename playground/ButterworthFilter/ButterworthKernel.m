27a66b852588b66c1058748de2a55821ba4ea554
Van <vanc2@nih.gov>
2022-04-11 13:55:43 -0400

    fully support of 3D butterworth optimization, need investigating the difference between fortran codes' results and matlab codes' results

--- playground/ButterworthFilter/ButterworthKernel.m
+++ playground/ButterworthFilter/ButterworthKernel.m
@@ -1,11 +1,13 @@
 function [ker, d_ker] = ButterworthKernel(width, n, fc)
 w_over_2 = width/2;
-ker      = zeros(width);
-d_ker    = zeros(width);
+ker      = zeros(width,width,width);
+d_ker    = zeros(width,width,width);
 for k = 1:width
     for l = 1:width
-        dist     = sqrt((k-w_over_2).^2 + (l - w_over_2).^2);
-        [ker(k,l), d_ker(k,l)] = ButterworthTransferShort(dist, n, fc);
+        for j = 1:width
+            dist     = sqrt((k-w_over_2).^2 + (l - w_over_2).^2 + (j - w_over_2).^2);
+            [ker(k,l,j), d_ker(k,l,j)] = ButterworthTransferShort(dist, n, fc);
+        end
     end
 end
 end
