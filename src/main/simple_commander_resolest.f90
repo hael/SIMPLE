b03a9b0dd5e2739aaee200f3f79dad5e156a7d37
cong <>
2022-04-25 12:22:31 -0400

    moving butterworth computation out of the commanders/into the butterworth_commander and tidying butterworth codes

--- src/main/simple_commander_resolest.f90
+++ src/main/simple_commander_resolest.f90
@@ -232,159 +232,75 @@ contains
         call simple_end('**** SIMPLE_NONUNIFORM_FILTER NORMAL STOP ****')
     end subroutine exec_nonuniform_filter
 
-    subroutine exec_butterworth(self, cline, dim3)
-        use simple_butterworth, only: butterworth_kernel
-        
-        class(commander_base), intent(inout) :: self
-        class(cmdline),        intent(inout) :: cline
-        integer,               intent(in)    :: dim3            ! dim3 is 1 for 2D case
-        integer,               parameter     :: CHUNKSZ=20
-        type(parameters) :: params
-        type(image)      :: even, odd, ker_odd_img
-        integer          :: k,l,m,n,k1,l1,m1,k_ind,l_ind,m_ind, max_sup
-        real             :: rad, cur_min_sum, ref_diff, sup, theta
-        real   , parameter   :: A = 47.27, B = -0.1781, C = 7.69, D = -0.02228  ! Fitting constants (constructed in MATLAB) of theta(FT_support) = a*exp(b*x) + c*exp(d*x)
-        real   , parameter   :: MIN_SUP = 0.5, RES_LB = 30.                     ! lower bound of resolution is 30 Angstrom, upper bound is nyquist, hence .5
-        integer, parameter   :: N_SUP = 20                                      ! number of intervals between MIN_SUP and MAX_SUP
-        integer, parameter   :: SPA_SUP = 2, MID = 1+SPA_SUP                    ! support of the window function
-        real   , allocatable :: weights(:,:,:)                                  ! weights of the neighboring differences
-        ! optimization variables
-        ! these are pointers to avoi excessive memory allocation
+    subroutine exec_butterworth_3D(self, cline)
+        use simple_butterworth, only: find_butterworth_opt
+        class(butterworth_3D_commander), intent(inout) :: self
+        class(cmdline),                  intent(inout) :: cline
+        ! these are pointers to avoid excessive memory allocation
         real,                          pointer :: rmat_odd(:,:,:)=>null(), rmat_even(:,:,:)=>null()
-        complex(kind=c_float_complex), pointer :: cmat_odd(:,:,:)=>null(), cmat_conv(:,:,:)=>null()
-        ! these should be allocatables for improved performance
-        real, allocatable :: rmat_ker(:,:,:), orig_ker(:,:,:), orig_ker_der(:,:,:), prev_diff(:,:,:), cur_diff(:,:,:), cur_mat(:,:,:)
+        complex(kind=c_float_complex), pointer :: cmat_odd(:,:,:)=>null()
+        type(parameters)  :: params
+        type(image)       :: even, odd
+        real, allocatable :: cur_mat(:,:,:)
+        integer :: ldim(3)
         if( .not. cline%defined('mkdir') ) call cline%set('mkdir', 'yes')
         call params%new(cline) 
-        call odd %new([params%box,params%box,dim3], params%smpd)
-        call even%new([params%box,params%box,dim3], params%smpd)
+        call odd %new([params%box,params%box,params%box], params%smpd)
+        call even%new([params%box,params%box,params%box], params%smpd)
         call odd %read(params%vols(1))
         call even%read(params%vols(2))
-        call ker_odd_img%new([params%box,params%box,dim3], params%smpd)
-
-
-
-        allocate(rmat_ker(params%box,params%box,dim3), orig_ker(params%box,params%box,dim3), orig_ker_der( params%box,params%box,dim3),&
-        &prev_diff(params%box,params%box,dim3), cur_diff(params%box,params%box,dim3), cur_mat(params%box,params%box,dim3), source=0.)
-
-
-
+        ldim = odd%get_ldim()
         call odd%get_rmat_ptr(rmat_odd)
         call even%get_rmat_ptr(rmat_even)
         ! real-space normalisation needed for correct cost function evaluation
-        rmat_odd     = rmat_odd /sum(rmat_odd)      ! Normalize to energy of 1, so B*odd is comparible with even in the cost function
+        rmat_odd     = rmat_odd /sum(rmat_odd)
         rmat_even    = rmat_even/sum(rmat_even)
-
         call odd%fft
         call odd%get_cmat_ptr(cmat_odd)
-
-        
-        max_sup      = int(RES_LB/params%smpd)*2    ! multiplication factor depending on the definition of support, set to 2 for now
-        prev_diff    = huge(rad)
-        cur_min_sum  = huge(rad)
-        
-        ! assign the weights of the neighboring voxels
-        allocate(weights(SPA_SUP*2+1, SPA_SUP*2+1, SPA_SUP*2+1))
-        do k = 1, 2*SPA_SUP+1
-            do l = 1, 2*SPA_SUP+1
-                do m = 1, 2*SPA_SUP+1
-                    rad = hyp(real(k-MID), real(l-MID), real(m-MID))
-                    weights(k,l,m) = -rad/(SPA_SUP + 1) + 1.  ! linear function: 1 at rad = 0 and 0 at rad = SPA_SUP + 1
-                    if (weights(k,l,m) < 0.) then
-                        weights(k,l,m) = 0.
-                    endif
-                enddo
-            enddo
-        enddo
-        weights = weights/sum(weights) ! weights has energy of 1
-
-        do n = 1, N_SUP
-            sup   = MIN_SUP + (n-1.)*(max_sup-MIN_SUP)/(N_SUP-1.)
-            theta = A*exp(B*sup) + C*exp(D*sup)
-            write(*, *) 'support = ', sup, '; theta = ', theta
-            call butterworth_kernel(orig_ker, orig_ker_der, params%box, 8, theta)  ! WARNING: fix the constants here
-
-            ! computing B_kernel 'convolve' odd
-            call ker_odd_img%set_rmat(orig_ker, .false.)
-            call ker_odd_img%fft()
-            call ker_odd_img%get_cmat_ptr(cmat_conv)
-            cmat_conv = cmat_conv * cmat_odd
-            call ker_odd_img%ifft()
-            call ker_odd_img%get_rmat_sub(rmat_ker)
-
-            ! Normalize to energy of 1, so B*odd is comparible with even in the cost function
-            rmat_ker = rmat_ker/sum(rmat_ker)
-
-            cur_diff = (rmat_ker - rmat_even)**2
-
-            ! do the non-uniform, i.e. optimizing at each voxel
-            if (params%is_uniform == 'no') then
-                !$omp parallel do collapse(3) default(shared) private(k,l,m,k1,l1,m1,k_ind,l_ind,m_ind,ref_diff) schedule(dynamic,CHUNKSZ) proc_bind(close)
-                do k = 1,params%box
-                    do l = 1,params%box
-                        do m = 1,dim3
-                            ref_diff = 0.
-                            ! applying an average window to each diff (eq 7 in the nonuniform paper)
-                            do k_ind = 1, 2*SPA_SUP+1
-                                k1 = k - SPA_SUP + k_ind - 1
-                                do l_ind = 1, 2*SPA_SUP+1
-                                    l1 = l - SPA_SUP + l_ind - 1
-
-                                    ! 2D vs 3D cases
-                                    if (dim3 == 1) then
-                                        if ((k1 >= 1 .and. k1 <= params%box) .and. (l1 >= 1 .and. l1 <= params%box)) then
-                                            ref_diff = ref_diff + cur_diff(k1,l1,m)
-                                        endif
-                                    else
-                                        do m_ind = 1, 2*SPA_SUP+1
-                                            m1 = m - SPA_SUP + m_ind - 1
-                                            if ((k1 >= 1 .and. k1 <= params%box) .and. (l1 >= 1 .and. l1 <= params%box) .and. (m1 >= 1 .and. m1 <= dim3)) then
-                                                ref_diff = ref_diff + cur_diff(k1,l1,m1)*weights(k_ind,l_ind,m_ind)
-                                            endif
-                                        enddo
-                                    endif
-                                enddo
-                            enddo
-
-                            ! prev_diff keeps the lowest cost value at each voxel of the search
-                            ! cur_mat   keeps the best voxel of the form B*odd
-                            if (ref_diff < prev_diff(k,l,m)) then
-                                cur_mat(k,l,m)   = rmat_ker(k,l,m)
-                                prev_diff(k,l,m) = ref_diff
-                            endif
-                        enddo
-                    enddo
-                enddo
-                !$omp end parallel do
-            else
-                ! keep the theta which gives the lowest cost (over all voxels)
-                if (sum(cur_diff) < cur_min_sum) then
-                    cur_mat     = rmat_ker
-                    cur_min_sum = sum(cur_diff)
-                endif
-            endif
-            
-            call odd%set_rmat(cur_mat, .false.)
-            call odd %write(params%is_uniform // '_uniform_butterworth_filter_iter' // int2str(n) // '.mrc')
-        enddo
-        
+        allocate(cur_mat(ldim(1),ldim(2),ldim(3)), source=0.)
+        call find_butterworth_opt(cmat_odd, rmat_even, cur_mat, ldim, params%smpd, params%is_uniform)
+        call odd%set_rmat(cur_mat, .false.)
+        call odd %write(trim(params%is_uniform)//'_uniform_butterworth_filter_odd.mrc')
         ! end gracefully
         call simple_end('**** SIMPLE_BUTTERWORTH_FILTER NORMAL STOP ****')
-    end subroutine exec_butterworth
-
+    end subroutine exec_butterworth_3D
 
     subroutine exec_butterworth_2D( self, cline )
+        use simple_butterworth, only: find_butterworth_opt
         class(butterworth_2D_commander), intent(inout) :: self
         class(cmdline),                  intent(inout) :: cline
-        call exec_butterworth(self, cline, 1)
+        ! these are pointers to avoid excessive memory allocation
+        real,                          pointer :: rmat_odd(:,:,:)=>null(), rmat_even(:,:,:)=>null()
+        complex(kind=c_float_complex), pointer :: cmat_odd(:,:,:)=>null()
+        type(parameters)  :: params
+        type(image)       :: even, odd
+        real, allocatable :: cur_mat(:,:,:)
+        integer           :: iptcl
+        if( .not. cline%defined('mkdir') ) call cline%set('mkdir', 'yes')
+        call params%new(cline) 
+        call find_ldim_nptcls(params%stk, params%ldim, params%nptcls)
+        params%ldim(3) = 1 ! because we operate on stacks
+        allocate(cur_mat(params%ldim(1),params%ldim(2),params%ldim(3)))
+        do iptcl = 1, params%nptcls
+            call odd%new(params%ldim, params%smpd)
+            call even%new(params%ldim, params%smpd)
+            call odd%read(params%stk2, iptcl)
+            call even%read(params%stk,  iptcl)
+            call odd%get_rmat_ptr(rmat_odd)
+            call even%get_rmat_ptr(rmat_even)
+            call odd%get_rmat_ptr(rmat_odd)
+            call even%get_rmat_ptr(rmat_even)
+            call odd%fft
+            call odd%get_cmat_ptr(cmat_odd)
+            cur_mat = 0.
+            call find_butterworth_opt(cmat_odd, rmat_even, cur_mat, params%ldim, params%smpd, params%is_uniform)
+            call odd%set_rmat(cur_mat, .false.)
+            call odd%write(trim(params%is_uniform)//'_uniform_butterworth_filter_odd.mrc', iptcl)
+            call odd%zero_and_unflag_ft
+            call even%zero_and_unflag_ft
+        end do
+        ! end gracefully
+        call simple_end('**** SIMPLE_BUTTERWORTH_FILTER NORMAL STOP ****')
     end subroutine exec_butterworth_2D
-
-    subroutine exec_butterworth_3D( self, cline )
-        class(butterworth_3D_commander), intent(inout) :: self
-        class(cmdline),                  intent(inout) :: cline
-        type(parameters) :: params
-        call params%new(cline)
-        call exec_butterworth(self, cline, params%box)
-    end subroutine exec_butterworth_3D
 
 end module simple_commander_resolest
