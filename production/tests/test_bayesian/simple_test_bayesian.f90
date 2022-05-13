1570966659277101cd5a0f12ff92fb00a34ab974
cong <>
2022-05-13 12:20:42 -0500

    adding Bayesian's Dirichlet metric and passing its unit test

--- production/tests/test_bayesian/simple_test_bayesian.f90
+++ production/tests/test_bayesian/simple_test_bayesian.f90
@@ -4,13 +4,17 @@ program simple_test_bayesian
     implicit none
     
     ! unit tests for 'compute_count_for_edges'
-    integer :: pop(3, 4), indexes(3), counts(2**3)
+    integer :: pop(3, 4), indexes(3), counts(2**3), candidates(1)
+    real    :: total
     indexes  = (/ 0, 2, 3 /)
-    pop(1,:) = (/ 1, 0, 1, 1 /)
-    pop(2,:) = (/ 1, 0, 1, 0 /)
-    pop(3,:) = (/ 1, 0, 0, 0 /)
+    pop(1,:) = (/ 0, 0, 1, 1 /)
+    pop(2,:) = (/ 0, 0, 1, 1 /)
+    pop(3,:) = (/ 0, 0, 0, 1 /)
     counts   = 0
-    call compute_count_for_edges(3, pop, 2, indexes, 3, counts)
-
+    call compute_count_for_edges(pop, indexes, counts)
     write(*, *) 'counts = ', counts
+    candidates = (/ 1 /)
+    total = k2equation(0, candidates, pop)
+    write(*, *) 'fact(3) = ', fact(3)
+    write(*, *) 'total = ', total
 end program simple_test_bayesian
--- src/main/simple_opt_bayesian.f90
+++ src/main/simple_opt_bayesian.f90
@@ -5,14 +5,11 @@ implicit none
 
 contains
     ! simple translation of rb's compute_count_for_edges
-    subroutine compute_count_for_edges(str_len, pop, pop_size, indexes, ind_size, counts)
-        integer, intent(in)    :: str_len, pop_size, ind_size
+    subroutine compute_count_for_edges(pop, indexes, counts)
         integer, intent(in)    :: indexes(:)
         integer, intent(in)    :: pop(:,:)
         integer, intent(inout) :: counts(:)
-
         integer :: k, l, index, val
-
         counts = 0
         do k = 1, size(pop, 1)
             index = 1
@@ -25,4 +22,37 @@ contains
             counts(index) = counts(index) + 1
         enddo
     end subroutine compute_count_for_edges
+
+    function fact(n) result(ret)
+        integer, intent(in) :: n
+        integer :: ret
+        ret = 1
+        if( n > 1 ) ret = n*fact(n-1)
+    end function fact
+
+    function k2equation(node, candidates, pop) result(total)
+        integer, intent(in)  :: node
+        integer, intent(in)  :: candidates(:)
+        integer, intent(in)  :: pop(:,:)
+        integer, allocatable :: indexes(:)
+        integer, allocatable :: counts(:)
+        real    :: total, rs
+        integer :: k, a1, a2
+        allocate(indexes(size(candidates)+1), counts(int(2**(size(candidates)+1))), source=0)
+        indexes(1)                    = node
+        indexes(2:size(candidates)+1) = candidates
+        call compute_count_for_edges(pop, indexes, counts)
+        write(*, *) "counts = ", counts
+        total = -1.
+        do k = 0, int(size(counts)/2)-1
+            a1 = counts(k*2 + 1)
+            a2 = counts(k*2 + 2)
+            rs = 1.0*fact(a1)*fact(a2)/fact((a1+a2)+1)
+            if( total < 0. )then
+                total = rs
+            else
+                total = total*rs
+            endif
+        enddo
+    end function k2equation
 end module simple_opt_bayesian
