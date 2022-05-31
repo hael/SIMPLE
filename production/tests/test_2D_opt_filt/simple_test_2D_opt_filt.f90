7e87bf1a7d89154b792f409d740c4f17909cf985
cong <>
2022-05-31 11:22:25 -0400

    tidying opt_filter unit tests and adding phase-randomization to 2D unit test of opt_filter

--- production/tests/test_2D_opt_filt/simple_test_2D_opt_filt.f90
+++ production/tests/test_2D_opt_filt/simple_test_2D_opt_filt.f90
@@ -13,11 +13,12 @@ type(image)                   :: even, odd, noise, res_map
 type(opt_2D_filter_commander) :: xopt_2D_filter
 type(reproject_commander)     :: xreproject
 integer                       :: j, nyq, nptcls, smooth_ext, rc, iptcl
-real                          :: res_fsc05, res_fsc0143, ave, sdev, maxv, minv, med, lp_lb
+real                          :: res_fsc05, res_fsc0143, ave, sdev, maxv, minv, med
 real, allocatable             :: res(:), corrs(:)
 character(len=20)             :: filter
 character(len=:), allocatable :: cmd
 logical                       :: mrc_exists
+real, parameter               :: LP_LB_PHASE = 7.
 if( command_argument_count() < 4 )then
     write(logfhandle,'(a)') 'Usage: simple_test_2D_opt_filt smpd=xx nthr=yy stk=stk.mrc, mskdiam=zz'
     write(logfhandle,'(a)') 'Example: projections of https://www.rcsb.org/structure/1jyx with smpd=1. mskdiam=180'
@@ -62,7 +63,7 @@ call noise%new(    p%ldim, p%smpd)
 do iptcl = 1, p%nptcls
     write(*, *) 'Particle # ', iptcl
     call even%read(p%stk, iptcl)
-    call odd%copy      (even)
+    call odd%copy(even)
     ! spherical masking
     call even%mask(p%msk, 'soft')
     call odd%mask( p%msk, 'soft')
@@ -85,8 +86,8 @@ do iptcl = 1, p%nptcls
     call noise%gauran(0., 5. * sdev)
     call noise%mask(0.4 * p%msk, 'soft')
     call even%add(noise)
-    call even%write('vol_noisy.mrc', iptcl)
-    call odd%write('vol_clean.mrc', iptcl)
+    call even%write('stk_noisy.mrc', iptcl)
+    call odd%write('stk_clean.mrc', iptcl)
     call even%fft
     call odd%fft
     call even%fsc(odd, corrs)
@@ -94,6 +95,18 @@ do iptcl = 1, p%nptcls
     call odd%ifft
     call get_resolution(corrs, res, res_fsc05, res_fsc0143)
     write(*, *) 'Comparing clean particle vs noisy particle...'
+    write(logfhandle,'(A,1X,F6.2)') '>>> RESOLUTION AT FSC=0.500 DETERMINED TO:', res_fsc05
+    write(logfhandle,'(A,1X,F6.2)') '>>> RESOLUTION AT FSC=0.143 DETERMINED TO:', res_fsc0143
+    call even%phase_rand(LP_LB_PHASE)
+    call even%write('stk_phase_rand.mrc', iptcl)
+    call odd%write('stk_clean.mrc', iptcl)
+    call even%fft
+    call odd%fft
+    call even%fsc(odd, corrs)
+    call even%ifft
+    call odd%ifft
+    call get_resolution(corrs, res, res_fsc05, res_fsc0143)
+    write(*, *) 'Comparing clean particle vs phase-randomized (beyond '//trim(int2str(int((LP_LB_PHASE))))//' A) (noisy) particle...'
     write(logfhandle,'(A,1X,F6.2)') '>>> RESOLUTION AT FSC=0.500 DETERMINED TO:', res_fsc05
     write(logfhandle,'(A,1X,F6.2)') '>>> RESOLUTION AT FSC=0.143 DETERMINED TO:', res_fsc0143
     call odd%zero_and_unflag_ft
@@ -108,8 +121,8 @@ smooth_ext     = 1
 if( cline%defined('smooth_ext') ) smooth_ext = p%smooth_ext
 if( cline%defined('filter') )     filter     = p%filter
 if( .not. cline%defined('match_filt') ) call cline_opt_filt%set('match_filt', 'no')
-call cline_opt_filt%set('stk'       , 'vol_noisy.mrc')
-call cline_opt_filt%set('stk2'      , 'vol_clean.mrc')
+call cline_opt_filt%set('stk'       , 'stk_noisy.mrc')
+call cline_opt_filt%set('stk2'      , 'stk_clean.mrc')
 call cline_opt_filt%set('filter'    , trim(filter))
 call cline_opt_filt%set('mkdir'     , 'no')
 call cline_opt_filt%set('smooth_ext', real(smooth_ext))
@@ -134,6 +147,46 @@ do iptcl = 1, p%nptcls
     call odd%ifft
     call get_resolution(corrs, res, res_fsc05, res_fsc0143)
     write(*, *) 'Comparing clean particle vs FILTERED noisy particle...'
+    write(logfhandle,'(A,1X,F6.2)') '>>> RESOLUTION AT FSC=0.500 DETERMINED TO:', res_fsc05
+    write(logfhandle,'(A,1X,F6.2)') '>>> RESOLUTION AT FSC=0.143 DETERMINED TO:', res_fsc0143
+    call odd%zero_and_unflag_ft
+    call even%zero_and_unflag_ft
+    call sleep(1)
+enddo
+! calculate optimal filter
+write(*, *) 'Filtering phase-randomized stack in progress...'
+cline_opt_filt = cline
+filter         = 'lp'
+smooth_ext     = 1
+if( cline%defined('smooth_ext') ) smooth_ext = p%smooth_ext
+if( cline%defined('filter') )     filter     = p%filter
+if( .not. cline%defined('match_filt') ) call cline_opt_filt%set('match_filt', 'no')
+call cline_opt_filt%set('stk'       , 'stk_phase_rand.mrc')
+call cline_opt_filt%set('stk2'      , 'stk_clean.mrc')
+call cline_opt_filt%set('filter'    , trim(filter))
+call cline_opt_filt%set('mkdir'     , 'no')
+call cline_opt_filt%set('smooth_ext', real(smooth_ext))
+call p%new(cline_opt_filt)
+call xopt_2D_filter%execute(cline_opt_filt)
+do iptcl = 1, p%nptcls
+    write(*, *) 'Particle # ', iptcl
+    ! comparing the nonuniform result with the original data
+    call even%read(p%stk2,  iptcl)
+    call odd%read('nonuniform_opt_2D_filter_'//trim(filter)//'_ext_'//int2str(smooth_ext)//'_odd.mrc', iptcl)
+    ! spherical masking
+    call even%mask(p%msk, 'soft')
+    call odd%mask( p%msk, 'soft')
+    ! forward FT
+    call even%fft()
+    call odd%fft()
+    ! calculate FSC
+    res = even%get_res()
+    nyq = even%get_filtsz()
+    call even%fsc(odd, corrs)
+    call even%ifft
+    call odd%ifft
+    call get_resolution(corrs, res, res_fsc05, res_fsc0143)
+    write(*, *) 'Comparing clean particle vs FILTERED phase-randomized particle...'
     write(logfhandle,'(A,1X,F6.2)') '>>> RESOLUTION AT FSC=0.500 DETERMINED TO:', res_fsc05
     write(logfhandle,'(A,1X,F6.2)') '>>> RESOLUTION AT FSC=0.143 DETERMINED TO:', res_fsc0143
     call odd%zero_and_unflag_ft
