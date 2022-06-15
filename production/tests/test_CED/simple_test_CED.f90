c6a00ecbd65f38ff4d71a939d214ae114f245c7b
cong <>
2022-06-15 09:58:07 -0400

    finishing up solving the diffusion eq in CED, need testing and debugging

--- production/tests/test_CED/simple_test_CED.f90
+++ production/tests/test_CED/simple_test_CED.f90
@@ -10,9 +10,10 @@ program simple_test_CED
     type(image)                   :: img, cur_img, ker, J11, J12, J22
     type(reproject_commander)     :: xreproject
     integer                       :: k, l, nptcls, iptcl, rc, gaussian_ext, sh
+    real                          :: t
     character(len=:), allocatable :: cmd
     logical                       :: mrc_exists
-    real,    parameter            :: SIGMA = 0.7, RHO = 4., C1 = 0.001, C2 = 1.
+    real,    parameter            :: SIGMA = 0.7, RHO = 4., C1 = 0.001, C2 = 1., DT = 0.15, T_MAX = 15.
     integer, allocatable          :: pos_ind_1(:), pos_ind_2(:), neg_ind_1(:), neg_ind_2(:)
     real,    allocatable          :: grad(:,:,:), D1(:,:,:), D2(:,:,:), eig_val(:,:,:,:), eig_vec_1(:,:,:,:),&
                                     &lambda(:,:,:,:), a(:,:,:), b(:,:,:), c(:,:,:), discrete_table(:,:,:,:,:)
@@ -68,85 +69,102 @@ program simple_test_CED
               &a(p%ldim(1),p%ldim(2),1), b(p%ldim(1),p%ldim(2),1), c(p%ldim(1),p%ldim(2),1),&
               &discrete_table(p%ldim(1),p%ldim(2),1,3,3), source=0.)
     allocate(neg_ind_1(p%ldim(1)), pos_ind_1(p%ldim(1)), neg_ind_2(p%ldim(2)), pos_ind_2(p%ldim(2)))
-    !neg_ind_1 = [  p%ldim(1), 1:p%ldim(1)-1]
-    !neg_ind_2 = [  p%ldim(2), 1:p%ldim(2)-1]
-    !pos_ind_1 = [2:p%ldim(1), 1]
-    !pos_ind_2 = [2:p%ldim(2), 1]
+    neg_ind_1 = [  p%ldim(1), (k, k = 1,p%ldim(1)-1)]
+    neg_ind_2 = [  p%ldim(2), (k, k = 1,p%ldim(2)-1)]
+    pos_ind_1 = [(k, k = 2,p%ldim(1)), 1]
+    pos_ind_2 = [(k, k = 2,p%ldim(2)), 1]
     do iptcl = 1, p%nptcls
         write(*, *) 'Particle # ', iptcl
         call img%read(p%stk, iptcl)
         call cur_img%copy(img)
-        ! build the Gaussian kernel with sigma
-        gaussian_ext = ceiling(2*SIGMA)
-        do k = -gaussian_ext, gaussian_ext
-        do l = -gaussian_ext, gaussian_ext
-            sh = nint(hyp(real(k),real(l)))
-            call ker%set_rmat_at(p%ldim(1)/2 + k, p%ldim(2)/2 + l, 1, exp(-(sh**2/(2*SIGMA**2)))) 
+        t = 0.
+        do while( t < (T_MAX - 0.001) )
+            call img%copy_fast(cur_img)
+            call ker%zero_and_unflag_ft()
+            t = t + DT
+            ! build the Gaussian kernel with sigma
+            gaussian_ext = ceiling(2*SIGMA)
+            do k = -gaussian_ext, gaussian_ext
+            do l = -gaussian_ext, gaussian_ext
+                sh = nint(hyp(real(k),real(l)))
+                call ker%set_rmat_at(p%ldim(1)/2 + k, p%ldim(2)/2 + l, 1, exp(-(sh**2/(2*SIGMA**2)))) 
+            enddo
+            enddo
+            ! convolving img with the kernel
+            call img%fft()
+            call ker%fft()
+            call img%get_cmat_ptr(img_cmat)
+            call ker%get_cmat_ptr(ker_cmat)
+            img_cmat = img_cmat*ker_cmat
+            call img%ifft()
+            call img%calc_gradient(grad, D1, D2)
+            ! build the Gaussian kernel with rho
+            gaussian_ext = ceiling(3*RHO)
+            call ker%zero_and_unflag_ft()
+            do k = -gaussian_ext, gaussian_ext
+            do l = -gaussian_ext, gaussian_ext
+                sh = nint(hyp(real(k),real(l)))
+                call ker%set_rmat_at(p%ldim(1)/2 + k, p%ldim(2)/2 + l, 1, exp(-(sh**2/(2*RHO**2)))) 
+            enddo
+            enddo
+            ! construct the structure tensor
+            call J11%new(p%ldim, p%smpd)
+            call J12%new(p%ldim, p%smpd)
+            call J22%new(p%ldim, p%smpd)
+            call J11%set_rmat(D1**2, .false.)
+            call J12%set_rmat(D1*D2, .false.)
+            call J22%set_rmat(D2**2, .false.)
+            call J11%fft()
+            call J12%fft()
+            call J22%fft()
+            call ker%fft()
+            call ker%get_cmat_ptr(ker_cmat)
+            call J11%get_cmat_ptr(img_cmat)
+            img_cmat = img_cmat*ker_cmat
+            call J11%ifft()
+            call J11%get_rmat_ptr(J11_rmat)
+            call J12%get_cmat_ptr(img_cmat)
+            img_cmat = img_cmat*ker_cmat
+            call J12%ifft()
+            call J12%get_rmat_ptr(J12_rmat)
+            call J22%get_cmat_ptr(img_cmat)
+            img_cmat = img_cmat*ker_cmat
+            call J22%ifft()
+            call J22%get_rmat_ptr(J22_rmat)
+            ! computing eigenvalues/eigenvectors of the structure tensor
+            eig_val(  :,:,:,1) = (J11_rmat + J22_rmat + sqrt((J11_rmat - J22_rmat)**2 + 4*J12_rmat**2))/2
+            eig_val(  :,:,:,2) = (J11_rmat + J22_rmat - sqrt((J11_rmat - J22_rmat)**2 + 4*J12_rmat**2))/2
+            eig_vec_1(:,:,:,1) = 2*J12_rmat
+            eig_vec_1(:,:,:,2) = J22_rmat - J11_rmat - sqrt((J11_rmat - J22_rmat)**2 + 4*J12_rmat**2)
+            lambda(   :,:,:,1) = C1
+            lambda(   :,:,:,2) = C1 + (1-C1)*exp(-C2/(eig_val(:,:,:,1) - eig_val(:,:,:,2))**2)
+            a = lambda(:,:,:,1)*eig_vec_1(:,:,:,1)**2 + lambda(:,:,:,2)*eig_vec_1(:,:,:,2)**2
+            c = lambda(:,:,:,1)*eig_vec_1(:,:,:,2)**2 + lambda(:,:,:,2)*eig_vec_1(:,:,:,1)**2
+            b = (lambda(:,:,:,1) - lambda(:,:,:,2))*eig_vec_1(:,:,:,1)*eig_vec_1(:,:,:,2)
+            ! solving the diffusion equations
+            call cur_img%get_rmat_ptr(cur_img_rmat)
+            discrete_table(:,:,1,1,1) = (cur_img_rmat(neg_ind_1, pos_ind_2, 1) - cur_img_rmat(:,:,1))*&
+                                    &( abs(b(neg_ind_1, pos_ind_2, 1)) - b(neg_ind_1, pos_ind_2, 1)+&
+                                    &  abs(b(:,:,1)) - b(:,:,1) )/4.
+            discrete_table(:,:,1,1,2) = (cur_img_rmat(:, pos_ind_2, 1) - cur_img_rmat(:,:,1))*&
+                                    &( c(:, pos_ind_2, 1) + c(:,:,1) - abs(b(:,pos_ind_2,1)) - abs(b(:,:,1)) )/2.
+            discrete_table(:,:,1,1,3) = (cur_img_rmat(pos_ind_1, pos_ind_2, 1) - cur_img_rmat(:,:,1))*&
+                                    &( abs(b(pos_ind_1, pos_ind_2, 1)) + b(pos_ind_1, pos_ind_2, 1)+&
+                                    &  abs(b(:,:,1)) - b(:,:,1) )/4.
+            discrete_table(:,:,1,2,1) = (cur_img_rmat(neg_ind_1, :, 1) - cur_img_rmat(:,:,1))*&
+                                    &( a(neg_ind_1, :, 1) + a(:,:,1) - abs(b(neg_ind_1,:,1)) - abs(b(:,:,1)) )/2.
+            discrete_table(:,:,1,2,3) = (cur_img_rmat(pos_ind_1, :, 1) - cur_img_rmat(:,:,1))*&
+                                    &( a(pos_ind_1, :, 1) + a(:,:,1) - abs(b(pos_ind_1,:,1)) - abs(b(:,:,1)) )/2.
+            discrete_table(:,:,1,3,1) = (cur_img_rmat(neg_ind_1, neg_ind_2, 1) - cur_img_rmat(:,:,1))*&
+                                    &( abs(b(neg_ind_1, neg_ind_2, 1)) + b(neg_ind_1, neg_ind_2, 1)+&
+                                    &  abs(b(:,:,1)) + b(:,:,1) )/4.
+            discrete_table(:,:,1,3,2) = (cur_img_rmat(:, neg_ind_2, 1) - cur_img_rmat(:,:,1))*&
+                                    &( c(:, neg_ind_2, 1) + c(:,:,1) - abs(b(:,neg_ind_2,1)) - abs(b(:,:,1)) )/2.
+            discrete_table(:,:,1,3,3) = (cur_img_rmat(pos_ind_1, neg_ind_2, 1) - cur_img_rmat(:,:,1))*&
+                                    &( abs(b(pos_ind_1, neg_ind_2, 1)) - b(pos_ind_1, neg_ind_2, 1)+&
+                                    &  abs(b(:,:,1)) - b(:,:,1) )/4.
+            cur_img_rmat = cur_img_rmat + DT*sum(sum(discrete_table,5),4)
         enddo
-        enddo
-        ! convolving img with the kernel
-        call img%fft()
-        call ker%fft()
-        call img%get_cmat_ptr(img_cmat)
-        call ker%get_cmat_ptr(ker_cmat)
-        img_cmat = img_cmat*ker_cmat
-        call img%ifft()
-        call img%write('CED_temp_out.mrc', iptcl)
-        call img%calc_gradient(grad, D1, D2)
-        ! build the Gaussian kernel with rho
-        gaussian_ext = ceiling(3*RHO)
-        call ker%zero_and_unflag_ft()
-        do k = -gaussian_ext, gaussian_ext
-        do l = -gaussian_ext, gaussian_ext
-            sh = nint(hyp(real(k),real(l)))
-            call ker%set_rmat_at(p%ldim(1)/2 + k, p%ldim(2)/2 + l, 1, exp(-(sh**2/(2*RHO**2)))) 
-        enddo
-        enddo
-        ! construct the structure tensor
-        call J11%new(p%ldim, p%smpd)
-        call J12%new(p%ldim, p%smpd)
-        call J22%new(p%ldim, p%smpd)
-        call J11%set_rmat(D1**2, .false.)
-        call J12%set_rmat(D1*D2, .false.)
-        call J22%set_rmat(D2**2, .false.)
-        call J11%fft()
-        call J12%fft()
-        call J22%fft()
-        call ker%fft()
-        call ker%get_cmat_ptr(ker_cmat)
-        call J11%get_cmat_ptr(img_cmat)
-        img_cmat = img_cmat*ker_cmat
-        call J11%ifft()
-        call J11%get_rmat_ptr(J11_rmat)
-        call J12%get_cmat_ptr(img_cmat)
-        img_cmat = img_cmat*ker_cmat
-        call J12%ifft()
-        call J12%get_rmat_ptr(J12_rmat)
-        call J22%get_cmat_ptr(img_cmat)
-        img_cmat = img_cmat*ker_cmat
-        call J22%ifft()
-        call J22%get_rmat_ptr(J22_rmat)
-        ! computing eigenvalues/eigenvectors of the structure tensor
-        eig_val(  :,:,:,1) = (J11_rmat + J22_rmat + sqrt((J11_rmat - J22_rmat)**2 + 4*J12_rmat**2))/2
-        eig_val(  :,:,:,2) = (J11_rmat + J22_rmat - sqrt((J11_rmat - J22_rmat)**2 + 4*J12_rmat**2))/2
-        eig_vec_1(:,:,:,1) = 2*J12_rmat
-        eig_vec_1(:,:,:,2) = J22_rmat - J11_rmat - sqrt((J11_rmat - J22_rmat)**2 + 4*J12_rmat**2)
-        lambda(   :,:,:,1) = C1
-        lambda(   :,:,:,2) = C1 + (1-C1)*exp(-C2/(eig_val(:,:,:,1) - eig_val(:,:,:,2))**2)
-        a = lambda(:,:,:,1)*eig_vec_1(:,:,:,1)**2 + lambda(:,:,:,2)*eig_vec_1(:,:,:,2)**2
-        c = lambda(:,:,:,1)*eig_vec_1(:,:,:,2)**2 + lambda(:,:,:,2)*eig_vec_1(:,:,:,1)**2
-        b = (lambda(:,:,:,1) - lambda(:,:,:,2))*eig_vec_1(:,:,:,1)*eig_vec_1(:,:,:,2)
-        ! solving the diffusion equations
-        call cur_img%get_rmat_ptr(cur_img_rmat)
-        discrete_table(:,:,1,1,1) = (cur_img_rmat(neg_ind_1, pos_ind_2, 1) - cur_img_rmat(:,:,1))*&
-                                   &( abs(b(neg_ind_1, pos_ind_2, 1)) - b(neg_ind_1, pos_ind_2, 1)+&
-                                   &  abs(b(:,:,1)) - b(:,:,1) )/4.
-        discrete_table(:,:,1,1,2) = (cur_img_rmat(:, pos_ind_2, 1) - cur_img_rmat(:,:,1))*&
-                                   &( c(:, pos_ind_2, 1) + c(:,:,1) - abs(b(:,pos_ind_2,1)) - b(:,:,1) )/2.
-        discrete_table(:,:,1,1,3) = (cur_img_rmat(pos_ind_1, pos_ind_2, 1) - cur_img_rmat(:,:,1))*&
-                                   &( abs(b(pos_ind_1, pos_ind_2, 1)) + b(pos_ind_1, pos_ind_2, 1)+&
-                                   &  abs(b(:,:,1)) - b(:,:,1) )/4.
-        discrete_table(:,:,1,2,1) = (cur_img_rmat(neg_ind_1, :, 1) - cur_img_rmat(:,:,1))*&
-                                   &( a(neg_ind_1, :, 1) + a(:,:,1) - abs(b(neg_ind_1,:,1)) - b(:,:,1) )/2.
+        call cur_img%write('test_CED_output.mrc', iptcl)
     enddo
 end program simple_test_CED
