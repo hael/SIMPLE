10bf129476bd6fcedebe29a65379f113f4d270fc
cong <>
2022-06-22 12:38:10 -0400

    fixing array dimensions in CED when working with padding in simple_image

--- production/tests/test_CED/simple_test_CED.f90
+++ production/tests/test_CED/simple_test_CED.f90
@@ -13,9 +13,9 @@ program simple_test_CED
     integer                       :: nptcls, iptcl, rc
     character(len=:), allocatable :: cmd
     logical                       :: mrc_exists
-    if( command_argument_count() < 4 )then
-        write(logfhandle,'(a)') 'Usage: simple_test_CED smpd=xx nthr=yy stk=stk.mrc, mskdiam=zz'
-        write(logfhandle,'(a)') 'Example: projections of https://www.rcsb.org/structure/1jyx with smpd=1. mskdiam=180'
+    if( command_argument_count() < 5 )then
+        write(logfhandle,'(a)') 'Usage: simple_test_CED smpd=xx nthr=yy stk=stk.mrc mskdiam=zz sigma=tt'
+        write(logfhandle,'(a)') 'Example: projections of https://www.rcsb.org/structure/1jyx with smpd=1. mskdiam=180 sigma=0.7'
         write(logfhandle,'(a)') 'DEFAULT TEST (example above) is running now...'
         inquire(file="1JYX.mrc", exist=mrc_exists)
         if( .not. mrc_exists )then
@@ -40,6 +40,7 @@ program simple_test_CED
         call cline%set('nthr'   , 16.)
         call cline%set('stk'    , 'reprojs.mrcs')
         call cline%set('mskdiam', 180.)
+        call cline%set('sigma'  , 0.7)
     else
         call cline%parse_oldschool
     endif
@@ -47,16 +48,16 @@ program simple_test_CED
     call cline%checkvar('nthr',    2)
     call cline%checkvar('stk' ,    3)
     call cline%checkvar('mskdiam', 4)
+    call cline%checkvar('sigma',   5)
     call cline%check
     call p%new(cline)
     call find_ldim_nptcls(p%stk, p%ldim, nptcls)
     p%ldim(3) = 1 ! because we operate on stacks
-    write(*, *) p%ldim, p%nptcls
     call img%new(p%ldim, p%smpd)
     do iptcl = 1, p%nptcls
         write(*, *) 'Particle # ', iptcl
         call img%read(p%stk, iptcl)
-        call ced_filter_2D(img, 0.7)
+        call ced_filter_2D(img, p%sigma)
         call img%write('test_CED_output.mrc', iptcl)
     enddo
 end program simple_test_CED
