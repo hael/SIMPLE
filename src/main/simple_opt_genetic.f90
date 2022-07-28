9cc1b58273e21eba79ee8726643335616d0869ee
cong <>
2022-07-28 09:59:48 -0400

    improving performance of GA and supporting any cost function in GA

--- src/main/simple_opt_genetic.f90
+++ src/main/simple_opt_genetic.f90
@@ -1,23 +1,24 @@
 ! simple probabilistic genetic algorithm
 module simple_opt_genetic
-include 'simple_lib.f08'
-implicit none
+    include 'simple_lib.f08'
+    implicit none
 
+    abstract interface
+        function objective_func(bitstring) result(val)
+            integer, intent(in)  :: bitstring(:)
+            real                 :: val
+        end function objective_func
+    end interface
 contains
-    subroutine crossover(parent1, parent2, cross_rate, child1, child2)
-        integer, intent(in)    :: parent1(:), parent2(:)
+    subroutine crossover(bitstr1, bitstr2, cross_rate)
+        integer, intent(inout) :: bitstr1(:), bitstr2(:)
         real   , intent(in)    :: cross_rate
-        integer, intent(inout) :: child1(:), child2(:)
         integer :: cross_pt, N
-        child1 = parent1
-        child2 = parent2
-        N      = size(parent1)
+        N = size(bitstr1)
         if( rand() < cross_rate )then
             cross_pt = 1 + floor(rand()*(N-1))
-            child1(         1:cross_pt) = parent1(         1:cross_pt)
-            child1(cross_pt+1:N)        = parent2(cross_pt+1:N)
-            child2(         1:cross_pt) = parent2(         1:cross_pt)
-            child2(cross_pt+1:N)        = parent1(cross_pt+1:N)
+            bitstr2(         1:cross_pt) = bitstr1(         1:cross_pt)
+            bitstr1(cross_pt+1:N)        = bitstr2(cross_pt+1:N)
         endif
     end subroutine crossover
 
@@ -43,7 +44,8 @@ contains
         enddo
     end function select_pop
 
-    subroutine genetic_opt(num_bits, max_iter, pop_size, cross_rate, mut_rate, best, best_val)
+    subroutine genetic_opt(obj_func, num_bits, max_iter, pop_size, cross_rate, mut_rate, best, best_val)
+        procedure(objective_func), pointer :: obj_func
         integer, intent(in)    :: num_bits
         integer, intent(in)    :: max_iter
         integer, intent(in)    :: pop_size
@@ -51,12 +53,12 @@ contains
         real   , intent(in)    :: mut_rate
         integer, intent(inout) :: best(:)
         real   , intent(inout) :: best_val
-        integer, allocatable :: pop(:,:), children(:,:), selected(:,:), p1(:), p2(:), c1(:), c2(:)
+        integer, allocatable :: pop(:,:), selected(:,:)
         real   , allocatable :: costs(:)
         integer :: k,l
+        logical :: got_new_best
         call srand(time())
-        allocate(pop(pop_size, num_bits), children(pop_size, num_bits), selected(pop_size, num_bits),&
-                &p1(num_bits), p2(num_bits), c1(num_bits), c2(num_bits), source = 0)
+        allocate(pop(pop_size, num_bits), selected(pop_size, num_bits), source = 0)
         allocate(costs(pop_size), source = 0.)
         do k = 1, pop_size
             do l = 1, num_bits
@@ -64,41 +66,29 @@ contains
             enddo
         enddo
         best     = pop(1,:)
-        best_val = objective(best)
+        best_val = obj_func(best)
         do k = 1, max_iter
+            got_new_best = .false.
             do l = 1, pop_size
-                costs(l) = objective(pop(l,:))
+                costs(l) = obj_func(pop(l,:))
                 if( costs(l) > best_val )then
-                    best     = pop(l,:)
-                    best_val = costs(l)
-                    print *, 'new best = ', best
-                    print *, 'current cost = ', best_val
+                    best         = pop(l,:)
+                    best_val     = costs(l)
+                    got_new_best = .true.
                 endif
             enddo
-            do l = 1, pop_size
-                selected(l,:) = pop(select_pop(pop, costs, times = 3), :)
-            enddo
+            if( got_new_best )then
+                print *, 'new best = ', best
+                print *, 'current cost = ', best_val
+            endif
             do l = 1, pop_size, 2
-                p1 = selected(l  , :)
-                p2 = selected(l+1, :)
-                call crossover(p1, p2, cross_rate, c1, c2)
-                call mutation(c1, mut_rate)
-                call mutation(c2, mut_rate)
-                children(l  ,:) = c1
-                children(l+1,:) = c2
+                selected(l  ,:) = pop(select_pop(pop, costs, times = 3), :)
+                selected(l+1,:) = pop(select_pop(pop, costs, times = 3), :)
+                call crossover(selected(l,:), selected(l+1,:), cross_rate)
+                call mutation(selected(l  ,:), mut_rate)
+                call mutation(selected(l+1,:), mut_rate)
             enddo
-            pop = children
+            pop = selected
         enddo
     end subroutine genetic_opt
-
-    function objective(bitstring) result(val)
-        integer, intent(in)  :: bitstring(:)
-        real :: val
-        integer, parameter :: GOAL(20) = [0,1,1,1,1,0,1,1,1,0,0,0,1,1,1,1,1,1,1,0]
-        integer :: k
-        val = 0
-        do k = 1, size(GOAL)
-            if( bitstring(k) == GOAL(k) ) val = val + 1
-        enddo
-    end function objective
 end module simple_opt_genetic
