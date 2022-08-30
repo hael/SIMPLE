64525ddb2dfc53b2001f430737eda2ab69143a7a
cyrilr <cyril.reboul@monash.edu>
2022-08-30 14:24:09 +1000

    Minor update 2d streaming

--- src/main/simple_commander_preprocess_stream.f90
+++ src/main/simple_commander_preprocess_stream.f90
@@ -284,7 +284,7 @@ contains
                 call update_pool_status
                 call update_pool
                 call reject_from_pool
-                ! call reject_from_pool_online
+                ! call reject_from_pool_user
                 call write_project_stream2D(.true.)
                 call import_chunks_into_pool(.false., nchunks_imported)
                 call classify_pool
