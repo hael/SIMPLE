ac262f6fd11f841269ab6eb482d565fe7f8d1137
hael <hans.elmlund@nih.gov>
2022-09-13 16:51:23 -0400

    correct application of nonuniform filter in postprocess

--- src/main/simple_commander_resolest.f90
+++ src/main/simple_commander_resolest.f90
@@ -12,8 +12,6 @@ use simple_masker,         only: masker
 implicit none
 
 public :: fsc_commander
-! public :: local_res_commander
-! public :: nonuniform_filter_commander
 public :: opt_3D_filter_commander
 public :: opt_2D_filter_commander
 private
@@ -23,16 +21,6 @@ type, extends(commander_base) :: fsc_commander
   contains
     procedure :: execute      => exec_fsc
 end type fsc_commander
-
-! type, extends(commander_base) :: local_res_commander
-!   contains
-!     procedure :: execute      => exec_local_res
-! end type local_res_commander
-
-! type, extends(commander_base) :: nonuniform_filter_commander
-!   contains
-!     procedure :: execute      => exec_nonuniform_filter
-! end type nonuniform_filter_commander
 
 type, extends(commander_base) :: opt_2D_filter_commander
   contains
@@ -104,133 +92,6 @@ contains
         ! end gracefully
         call simple_end('**** SIMPLE_FSC NORMAL STOP ****')
     end subroutine exec_fsc
-
-    !> calculates local resolution from Even/Odd Volume pairs
-    ! subroutine exec_local_res( self, cline )
-    !     use simple_estimate_ssnr, only: local_res, local_res_lp
-    !     class(local_res_commander), intent(inout) :: self
-    !     class(cmdline),             intent(inout) :: cline
-    !     type(parameters)     :: params
-    !     type(image)          :: even, odd, vol2filter
-    !     type(masker)         :: mskvol
-    !     real                 :: res_fsc05, res_fsc0143
-    !     logical              :: have_mask_file
-    !     integer, allocatable :: locres_finds(:,:,:)
-    !     real,    allocatable :: res(:), corrs(:)
-    !     if( .not. cline%defined('mkdir') )      call cline%set('mkdir', 'yes')
-    !     call params%new(cline)
-    !     ! read even/odd pair
-    !     call even%new([params%box,params%box,params%box], params%smpd)
-    !     call odd%new([params%box,params%box,params%box], params%smpd)
-    !     call odd%read(params%vols(1))
-    !     call even%read(params%vols(2))
-    !     have_mask_file = .false.
-    !     if( cline%defined('mskfile') )then
-    !         if( file_exists(params%mskfile) )then
-    !             call mskvol%new([params%box,params%box,params%box], params%smpd)
-    !             call mskvol%read(params%mskfile)
-    !             call even%zero_background
-    !             call odd%zero_background
-    !             call even%mul(mskvol)
-    !             call odd%mul(mskvol)
-    !             have_mask_file = .true.
-    !         else
-    !             THROW_HARD('mskfile: '//trim(params%mskfile)//' does not exist in cwd; exec_local_res')
-    !         endif
-    !     else
-    !         ! spherical masking
-    !         call even%mask(params%msk, 'soft')
-    !         call odd%mask(params%msk, 'soft')
-    !     endif
-    !     ! forward FT
-    !     call even%fft()
-    !     call odd%fft()
-    !     if( cline%defined('mskfile') )then
-    !         call mskvol%read(params%mskfile)
-    !         call mskvol%remove_edge
-    !     else
-    !         call mskvol%disc([params%box,params%box,params%box], params%smpd, params%msk)
-    !     endif
-    !     call local_res(even, odd, mskvol, params%lplim_crit, locres_finds)
-    !     ! destruct
-    !     call even%kill
-    !     call odd%kill
-    !     ! filter inputted vol3
-    !     if( cline%defined('vol3') )then
-    !         call vol2filter%new([params%box,params%box,params%box], params%smpd)
-    !         call vol2filter%read(params%vols(1))
-    !         call local_res_lp(locres_finds, vol2filter)
-    !         call vol2filter%write('map_locres_lp.mrc')
-    !         call vol2filter%kill
-    !     endif
-    !     ! end gracefully
-    !     call simple_end('**** SIMPLE_LOCAL_RES NORMAL STOP ****')
-    ! end subroutine exec_local_res
-
-    ! subroutine exec_nonuniform_filter( self, cline )
-    !     use simple_estimate_ssnr, only: nonuniform_fsc_filt
-    !     class(nonuniform_filter_commander), intent(inout) :: self
-    !     class(cmdline),                     intent(inout) :: cline
-    !     type(parameters) :: params
-    !     type(image)      :: even, odd, map2filt
-    !     type(masker)     :: mskvol
-    !     logical          :: have_mask_file, map2filt_present
-    !     if( .not. cline%defined('mkdir') ) call cline%set('mkdir', 'yes')
-    !     call params%new(cline)
-    !     ! read even/odd pair
-    !     call even%new([params%box,params%box,params%box], params%smpd)
-    !     call odd%new([params%box,params%box,params%box],  params%smpd)
-    !     call odd%read(params%vols(1))
-    !     call even%read(params%vols(2))
-    !     map2filt_present = cline%defined('vol3')
-    !     if( map2filt_present )then
-    !         call map2filt%new([params%box,params%box,params%box],  params%smpd)
-    !         call map2filt%read(params%vols(3))
-    !     endif
-    !     have_mask_file = .false.
-    !     if( cline%defined('mskfile') )then
-    !         if( file_exists(params%mskfile) )then
-    !             call mskvol%new([params%box,params%box,params%box], params%smpd)
-    !             call mskvol%read(params%mskfile)
-    !             have_mask_file = .true.
-    !         else
-    !             THROW_HARD('mskfile: '//trim(params%mskfile)//' does not exist in cwd; exec_nonuniform_filter')
-    !         endif
-    !     else
-    !         ! spherical masking
-    !         call even%mask(params%msk, 'soft')
-    !         call odd%mask(params%msk, 'soft')
-    !     endif
-    !     if( have_mask_file )then
-    !         call mskvol%one_at_edge ! to expand before masking of reference internally
-    !     else
-    !         call mskvol%disc([params%box,params%box,params%box], params%smpd, params%msk)
-    !     endif
-    !     if( map2filt_present )then
-    !         call nonuniform_fsc_filt(even, odd, mskvol, .false., map2filt, phran=trim(params%phrand).eq.'yes')
-    !     else
-    !         call nonuniform_fsc_filt(even, odd, mskvol, .false., phran=trim(params%phrand).eq.'yes')
-    !     endif
-    !     if( have_mask_file )then
-    !         call mskvol%read(params%mskfile)
-    !         call even%mul(mskvol)
-    !         call odd%mul(mskvol)
-    !         if( map2filt_present ) call map2filt%mul(mskvol)
-    !         call mskvol%kill
-    !     endif
-    !     if( map2filt_present )then
-    !         call map2filt%write('nonuniformly_filtered.mrc')
-    !     else
-    !         call even%write('nonuniformly_filtered_even.mrc')
-    !         call odd%write('nonuniformly_filtered_odd.mrc')
-    !     endif
-    !     ! destruct
-    !     call even%kill
-    !     call odd%kill
-    !     if( map2filt_present ) call map2filt%kill
-    !     ! end gracefully
-    !     call simple_end('**** SIMPLE_NONUNIFORM_FILTER NORMAL STOP ****')
-    ! end subroutine exec_nonuniform_filter
 
     subroutine exec_opt_3D_filter(self, cline)
         use simple_opt_filter, only: opt_filter_3D
