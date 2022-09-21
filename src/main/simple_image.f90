facd679a27598051845f1d08930f41076e74a44c
cong <>
2022-09-20 21:55:29 -0400

    using autodiff in cartesian shift search and passing the unit test

--- src/main/simple_image.f90
+++ src/main/simple_image.f90
@@ -4589,41 +4589,44 @@ contains
         real                                :: cc, eps, d0(2), sumsq_ref
         complex(dp)                         :: numer_der, denom_der, numer, temp
         integer                             :: ind, lims(3,2), h, k, hphys,kphys
-        type(dnS_t)                         :: shvec_ad(2), cc_ad, numer_ad, denom_ad, shconst_vec(2), arg_a, arg_k, arg_h
-        shvec_ad(1) = Variable( Val=real(shvec(1), kind=Rkind), nVar=2, iVar=1, nderiv=1 )
-        shvec_ad(2) = Variable( Val=real(shvec(2), kind=Rkind), nVar=2, iVar=2, nderiv=1 )
-        lims        = self_ref%fit%loop_lims(2)
-        numer_ad    = ZERO
-        denom_ad    = ZERO
+        type(dnS_t)                         :: shvec_ad(2), cc_ad, numer_ad, denom_ad, arg_a, arg_k, arg_h, A, B
+        shvec_ad(1)   = Variable( Val=real(shvec(1), kind=Rkind), nVar=2, iVar=1,  nderiv=1 )
+        shvec_ad(2)   = Variable( Val=real(shvec(2), kind=Rkind), nVar=2, iVar=2,  nderiv=1 )
+        numer_ad      = ZERO
+        denom_ad      = ZERO
+        cc_ad         = ZERO
+        lims          = self_ref%fit%loop_lims(2)
         do k=lims(2,1),lims(2,2)
             kphys = k + 1 + merge(self_ref%ldim(2),0,k<0)
-            arg_k = k*shvec_ad(2)
-            !arg_k = arg_k*self_ref%shconst(2)
+            arg_k = k*shvec_ad(2)*real(self_ref%shconst(2), kind=Rkind)
             do h=lims(1,1),lims(1,2)
-                arg_h = h*shvec_ad(1)
-                !arg_h = arg_h*self_ref%shconst(1)
+                arg_h = h*shvec_ad(1)*real(self_ref%shconst(1), kind=Rkind)
                 arg_a = arg_h + arg_k
                 hphys = h + 1
                 if( resmsk(hphys, kphys, 1) )then
-                    !numer_ad = numer_ad + self_ref%cmat(hphys,kphys,1) * cmplx(cos(arg_a), sin(arg_a))*conjg(self_ptcl%cmat)
-                    !denom_ad = numer_ad + self_ref%cmat(hphys,kphys,1) * cmplx(cos(arg_a), sin(arg_a))
+                    A        = cos(arg_a)*real(realpart(self_ref%cmat(hphys,kphys,1)), kind=Rkind) - sin(arg_a)*real(imagpart(self_ref%cmat(hphys,kphys,1)), kind=Rkind)
+                    B        = cos(arg_a)*real(imagpart(self_ref%cmat(hphys,kphys,1)), kind=Rkind) + sin(arg_a)*real(realpart(self_ref%cmat(hphys,kphys,1)), kind=Rkind)
+                    numer_ad = numer_ad +  A*real(realpart(self_ptcl%cmat(hphys,kphys,1)), kind=Rkind)&
+                                       &+  B*real(imagpart(self_ptcl%cmat(hphys,kphys,1)), kind=Rkind)
+                    denom_ad = denom_ad + A**2 + B**2
                 endif
             end do
         end do
         d0        = get_d0(denom_ad)
         sumsq_ref = d0(1)
-        !denom_ad  = sqrt(denom_ad * sumsq_ptcl)
-        !cc_ad     = real(numer_ad/denom_ad)
-        d0        = get_d0(cc_ad)
-        cc        = d0(1)
         if( cc < eps .and. sumsq_ref < eps .and. sumsq_ptcl < eps )then
-            cc = 1.
+            cc   = 1.
+            grad = 0.
         elseif( sqrt(sumsq_ref * sumsq_ptcl) < eps )then
-            cc = 0.
+            cc   = 0.
+            grad = 0.
         else
-            cc = cc / sqrt(sumsq_ref * sumsq_ptcl)
+            denom_ad  = sqrt(denom_ad * real(sumsq_ptcl, kind=Rkind))
+            cc_ad     = numer_ad/denom_ad
+            d0        = get_d0(cc_ad)
+            grad      = get_d1(cc_ad)
+            cc        = d0(1)
         endif
-        grad = get_d1(cc_ad)
     end function corr_grad_ad
 
     function corr_shifted( self_ref, self_ptcl, shvec, lp_dyn, hp_dyn ) result( r )
