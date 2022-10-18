51bcfc7587359265693633f75a7706404731a007
Hans Elmlund <hans.elmlund@nih.gov>
2022-10-18 10:33:20 -0400

    bugfix refine=greedyc

--- src/main/strategies/simple_strategy3D_shcc.f90
+++ src/main/strategies/simple_strategy3D_shcc.f90
@@ -38,9 +38,9 @@ contains
         class(strategy3D_shcc), intent(inout) :: self
         integer,                 intent(in)   :: ithr
         real, parameter :: MINFRAC = 0.1
-        integer   :: isample
+        integer   :: isample, irot
         type(ori) :: o, osym, obest
-        real      :: corr, euldist, dist_inpl, corr_best, frac
+        real      :: corr, euldist, dist_inpl, corr_best, frac, corr_inpl, e3
         ! continuous sochastic search
         if( build_glob%spproj_field%get_state(self%s%iptcl) > 0 )then
             ! set thread index
@@ -58,12 +58,23 @@ contains
             do isample=1,self%s%nsample
                 ! make a random rotation matrix within the assymetric unit
                 call build_glob%pgrpsyms%rnd_euler(o)
-                ! calculate Cartesian corr
-                call cartftcc_glob%project_and_correlate(self%s%iptcl, o, corr)
+                ! greedy optimization over in-plane angle
+                corr_inpl = -1.
+                do irot = 1,self%s%nrots
+                    call o%e3set(build_glob%inpl_rots(irot))
+                    ! calculate Cartesian corr
+                    call cartftcc_glob%project_and_correlate(self%s%iptcl, o, corr)
+                    if( corr > corr_inpl )then
+                        corr_inpl = corr
+                        e3 = build_glob%inpl_rots(irot)
+                    endif
+                end do
+                call o%e3set(e3)
+                corr = corr_inpl
                 ! keep track of how many references we are evaluating
-                self%s%nrefs_eval = self%s%nrefs_eval + 1
+                self%s%nrefs_eval = self%s%nrefs_eval + self%s%nrots
                 ! fraction of search space scanned
-                frac = real(isample) / real(self%s%nsample)
+                frac = real(isample) / real(self%s%nsample * self%s%nrots)
                 if( corr > corr_best )then
                     call build_glob%pgrpsyms%sym_dists(self%s%o_prev, o, osym, euldist, dist_inpl)
                     call o%set('dist',      euldist)
@@ -79,30 +90,30 @@ contains
                 endif
             end do
             ! local refinement step
-            do isample=1,self%s%nsample
-                ! make a random rotation matrix neighboring the previous best within the assymetric unit
-                call build_glob%pgrpsyms%rnd_euler(obest, self%s%athres, o)
-                ! calculate Cartesian corr
-                call cartftcc_glob%project_and_correlate(self%s%iptcl, o, corr)
-                ! keep track of how many references we are evaluating
-                self%s%nrefs_eval = self%s%nrefs_eval + 1
-                ! fraction of search space scanned
-                frac = real(isample) / real(self%s%nsample)
-                ! exit condition
-                if( corr > corr_best )then
-                    call build_glob%pgrpsyms%sym_dists(self%s%o_prev, o, osym, euldist, dist_inpl)
-                    call o%set('dist',      euldist)
-                    call o%set('dist_inpl', dist_inpl)
-                    call o%set('corr',      corr)
-                    call o%set('frac', 100.0 * (real(self%s%nrefs_eval) / real(2. * self%s%nsample)))
-                    call build_glob%spproj_field%set_ori(self%s%iptcl, o)
-                    corr_best = corr
-                    obest     = o
-                    ! cycle condition
-                    if( frac < MINFRAC ) cycle
-                    exit
-                endif
-            end do
+            ! do isample=1,self%s%nsample
+            !     ! make a random rotation matrix neighboring the previous best within the assymetric unit
+            !     call build_glob%pgrpsyms%rnd_euler(obest, self%s%athres, o)
+            !     ! calculate Cartesian corr
+            !     call cartftcc_glob%project_and_correlate(self%s%iptcl, o, corr)
+            !     ! keep track of how many references we are evaluating
+            !     self%s%nrefs_eval = self%s%nrefs_eval + 1
+            !     ! fraction of search space scanned
+            !     frac = real(isample) / real(self%s%nsample)
+            !     ! exit condition
+            !     if( corr > corr_best )then
+            !         call build_glob%pgrpsyms%sym_dists(self%s%o_prev, o, osym, euldist, dist_inpl)
+            !         call o%set('dist',      euldist)
+            !         call o%set('dist_inpl', dist_inpl)
+            !         call o%set('corr',      corr)
+            !         call o%set('frac', 100.0 * (real(self%s%nrefs_eval) / real(2. * self%s%nsample)))
+            !         call build_glob%spproj_field%set_ori(self%s%iptcl, o)
+            !         corr_best = corr
+            !         obest     = o
+            !         ! cycle condition
+            !         if( frac < MINFRAC ) cycle
+            !         exit
+            !     endif
+            ! end do
 
             !!!!!!!!!!!!!!!!!!! CARTESIAN SHIFT SRCH TO BE IMPLEMENTED
 
