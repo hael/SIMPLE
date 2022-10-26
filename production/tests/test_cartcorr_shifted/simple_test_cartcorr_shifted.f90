f442f78a47f349bc6995247d3296b9f58eb67534
cong <vanc2@nih.gov>
2022-10-26 12:11:27 -0400

    optimizing corr_shifted_ad, still way too slow compared to corr_shifted

--- production/tests/test_cartcorr_shifted/simple_test_cartcorr_shifted.f90
+++ production/tests/test_cartcorr_shifted/simple_test_cartcorr_shifted.f90
@@ -6,13 +6,14 @@ use simple_image,           only: image
 use simple_parameters,      only: parameters
 use simple_cmdline,         only: cmdline
 implicit none
-type(cmdline)         :: cline
-type(builder)         :: b
-type(parameters)      :: p
-type(cartft_corrcalc) :: cftcc
-real, parameter       :: SHMAG=5.0
-integer               :: i
-real                  :: grad(2)
+type(cmdline)           :: cline
+type(builder)           :: b
+type(parameters)        :: p
+type(cartft_corrcalc)   :: cftcc
+real, parameter         :: SHMAG=5.0
+integer                 :: i
+real                    :: grad(2)
+integer(timer_int_kind) :: t_tot
 if( command_argument_count() < 3 )then
     write(logfhandle,'(a)',advance='no') 'simple_test_cartcorr_shifted stk=<particles.ext>'
     write(logfhandle,'(a)') ' smpd=<sampling distance(in A)> [nthr=<number of threads{1}>] [verbose=<yes|no{no}>]'
@@ -32,6 +33,7 @@ do i = 1,p%nptcls
     call b%img%fft
     call cftcc%set_ptcl(i, b%img)
 end do
+t_tot = tic()
 do i = 1,1
     call b%img%read(p%stk, i)
     call b%img%fft
@@ -39,10 +41,20 @@ do i = 1,1
     call cftcc%set_ref(b%img)
     call srch_shifts(0.5, i)
 end do
+print *, 'corr_shifted timing = ', toc(t_tot)
+t_tot = tic()
+do i = 1,1
+    call b%img%read(p%stk, i)
+    call b%img%fft
+    call b%img%shift2Dserial([-0.5,-0.5]) 
+    call cftcc%set_ref(b%img)
+    call srch_shifts_ad(0.5, i)
+end do
+print *, 'corr_shifted_ad timing = ', toc(t_tot)
 
 contains
 
-    subroutine srch_shifts( shstep, iptcl )
+    subroutine srch_shifts_ad( shstep, iptcl )
         real,    intent(in) :: shstep
         integer, intent(in) :: iptcl
         real, allocatable   :: srch_space(:,:) 
@@ -54,7 +66,6 @@ contains
             y = -SHMAG
             do while( y <= SHMAG )
                 cnt = cnt + 1
-                print *, x, y
                 y = y + shstep
             end do
             x = x + shstep
@@ -73,7 +84,41 @@ contains
             x = x + shstep
         end do
         do i = 1,cnt
-            ! corr = cftcc%corr_shifted_grad(iptcl, srch_space(i,:), grad)
+            corr = cftcc%corr_shifted_ad(iptcl, srch_space(i,:), grad)
+            print *, srch_space(i,1), srch_space(i,2), corr
+        end do
+    end subroutine srch_shifts_ad
+
+    subroutine srch_shifts( shstep, iptcl )
+        real,    intent(in) :: shstep
+        integer, intent(in) :: iptcl
+        real, allocatable   :: srch_space(:,:) 
+        integer :: cnt, i
+        real    :: x, y, corr
+        cnt = 0
+        x   = -SHMAG
+        do while( x <= SHMAG )
+            y = -SHMAG
+            do while( y <= SHMAG )
+                cnt = cnt + 1
+                y = y + shstep
+            end do
+            x = x + shstep
+        end do
+        allocate(srch_space(cnt,2), source=0.)
+        cnt = 0
+        x   = -SHMAG; 
+        do while( x <= SHMAG )
+            y = -SHMAG
+            do while( y <= SHMAG )
+                cnt = cnt + 1
+                srch_space(cnt,1) = x
+                srch_space(cnt,2) = y
+                y = y + shstep
+            end do
+            x = x + shstep
+        end do
+        do i = 1,cnt
             corr = cftcc%corr_shifted(iptcl, srch_space(i,:))
             print *, srch_space(i,1), srch_space(i,2), corr
         end do
