3cad54e63617177d798e09b6ba5ad74f40f8cde1
cong <vanc2@nih.gov>
2022-10-31 18:23:04 -0400

    forcing NLOpt to install stuffs into build/lib for peace

--- cmake/SimpleFortranOptions.cmake
+++ cmake/SimpleFortranOptions.cmake
@@ -545,16 +545,15 @@ endif()
 if(USE_NLOPT)
   include(ExternalProject)
   set(NLOPT_VERSION 2.7.1)
-  set(NLOPT_INSTALL_PREFIX ${CMAKE_BINARY_DIR}/nlopt_install)
-  set(NLOPT_INSTALL_DIR    ${NLOPT_INSTALL_PREFIX}/${CMAKE_INSTALL_PREFIX})
+  set(NLOPT_INSTALL_DIR ${CMAKE_BINARY_DIR})
   ExternalProject_Add(nlopt_src
     DOWNLOAD_COMMAND wget https://github.com/stevengj/nlopt/archive/v${NLOPT_VERSION}.tar.gz
     PATCH_COMMAND   tar -xzf ../v${NLOPT_VERSION}.tar.gz && rm -rf ../nlopt_src-build/nlopt-${NLOPT_VERSION} && mv nlopt-${NLOPT_VERSION} ../nlopt_src-build/
-    CMAKE_COMMAND   cmake -DCMAKE_INSTALL_PREFIX=${NLOPT_INSTALL_DIR} nlopt-${NLOPT_VERSION}/
+    CMAKE_COMMAND   cmake -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_PREFIX=${NLOPT_INSTALL_DIR} nlopt-${NLOPT_VERSION}/
     BUILD_COMMAND   make
     INSTALL_COMMAND make install
   )
-  set(EXTRA_LIBS ${EXTRA_LIBS} -I${NLOPT_INSTALL_DIR}/include -L${NLOPT_INSTALL_DIR}/lib -L${NLOPT_INSTALL_DIR}/lib64 -lnlopt -lm )
+  set(EXTRA_LIBS ${EXTRA_LIBS} -I${NLOPT_INSTALL_DIR}/include -L${NLOPT_INSTALL_DIR}/lib -lnlopt -lm )
 endif()
 
 #############################################
