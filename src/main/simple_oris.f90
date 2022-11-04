0dfb028e59eff4dd8dacdb73d4780f58e63cd063
hael <hans.elmlund@nih.gov>
2022-11-03 22:23:32 -0400

    bugfix

--- src/main/simple_oris.f90
+++ src/main/simple_oris.f90
@@ -2790,21 +2790,27 @@ contains
         do i = 1,self%n
             if( i /= nnmat(i,1) ) THROW_HARD('self is not set to the first entry of the 2nd dimension')
         end do
-        ! search for peaks
         peaks(:) = .false.
-        do i = 1,self%n
-            if( corrs(nnmat(i,1)) > corr_t )then
-                if( corrs(nnmat(i,1)) > corrs(nnmat(i,2)) .and.&
-                &corrs(nnmat(i,1)) > corrs(nnmat(i,3)) .and.&
-                &corrs(nnmat(i,1)) > corrs(nnmat(i,4)) ) peaks(i) = .true.
-            endif
-        end do
+        ! good/bad binning with Otsu's algorithm
+        corrs_packed = pack(corrs, mask=corrs > 0.)
+        call otsu(corrs_packed, corr_t)
+        deallocate(corrs_packed)
+        where( corrs > corr_t )
+            peaks = .true.
+        elsewhere
+            peaks = .false.
+        end where
         if( count(peaks) > 0 )then
-            ! good/bad binning with Otsu's algorithm
-            corrs_packed = pack(corrs, mask=peaks)
-            call otsu(corrs_packed, corr_t)
-            deallocate(corrs_packed)
-            where( corrs <= corr_t ) peaks = .false.
+            ! peak detection
+            do i = 1,self%n
+                if( peaks(i) )then
+                    if( corrs(nnmat(i,1)) > corr_t )then
+                        if( corrs(nnmat(i,1)) > corrs(nnmat(i,2)) .and.&
+                        &corrs(nnmat(i,1)) > corrs(nnmat(i,3)) .and.&
+                        &corrs(nnmat(i,1)) > corrs(nnmat(i,4)) ) peaks(i) = .true.
+                    endif
+                endif
+            end do
         endif
     end subroutine detect_peaks
 
