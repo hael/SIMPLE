c5ff8aec58ba7d7b1a6ee73a4cec7d616c88632a
hael <hans.elmlund@nih.gov>
2022-11-05 09:37:00 -0400

    put back shc in ini3D for the time being

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -369,7 +369,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('3fcf6df')
+call simple_print_git_version('af378df')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
