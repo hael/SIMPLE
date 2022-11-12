6f073e7679765e8e91f452c01c146a6c4d57c1ee
hael <hans.elmlund@nih.gov>
2022-11-12 17:58:16 -0500

    parameter tuning refine=shcc

--- src/main/simple_parameters.f90
+++ src/main/simple_parameters.f90
@@ -1421,7 +1421,7 @@ contains
                     if( .not. cline%defined('nsample')       ) self%nsample       = 2000
                 case('neighc')
                     if( .not. cline%defined('nsample')       ) self%nsample       = 2000
-                    if( .not. cline%defined('nsample_neigh') ) self%nsample_neigh = 200
+                    if( .not. cline%defined('nsample_neigh') ) self%nsample_neigh = 500
                     if( .not. cline%defined('nsample_trs')   ) self%nsample_trs   = 50
                 case('greedyc')
                     if( .not. cline%defined('nspace')        ) self%nspace        = 500
