b26b43598e6a540c27f9cdc35383d03439fe03a5
cong <vanc2@nih.gov>
2022-11-14 15:09:22 -0500

    testing whether better cost implies better orientation

--- production/tests/test_project_shift/simple_test_project_shift.f90
+++ production/tests/test_project_shift/simple_test_project_shift.f90
@@ -10,18 +10,18 @@ use simple_sym
 use simple_ori
 implicit none
 character(len=:),   allocatable :: cmd
-integer,            parameter   :: N_PTCLS = 10
+integer,            parameter   :: N_PTCLS = 100, N_SAMPLES = 100, MAX_ANGERR = 15
 type(cmdline)           :: cline
 type(parameters)        :: p
-integer                 :: ifoo, rc, i, nevals(2)
+integer                 :: ifoo, rc, i, j, nevals(2), success_ang, success_norm
 type(projector)         :: vol_proj
 type(sym)               :: pgrpsyms
-type(ori)               :: o, o_cur
+type(ori)               :: o, o_cur, o1, o2
 type(image)             :: o_proj
 type(cartft_corrcalc)   :: cftcc
 type(cftcc_shsrch_grad) :: cftcc_shsrch
 logical                 :: mrc_exists, l_match_filt
-real                    :: corr, euls(3), lims(2,2), cxy(3)
+real                    :: corr, corr1, corr2, euls(3), lims(2,2), cxy(3), ran_ang, norm1, norm2
 if( command_argument_count() < 4 )then
     write(logfhandle,'(a)') 'Usage: simple_test_project_shift smpd=xx nthr=yy vol1=volume.mrc mskdiam=zz'
     write(logfhandle,'(a)') 'Example: https://www.rcsb.org/structure/1jyx with smpd=1. mskdiam=180'
@@ -59,14 +59,62 @@ call vol_proj%expand_cmat(1.)
 call pgrpsyms%new('c1')
 call o%new(.true.)
 l_match_filt = (p%match_filt .eq. 'yes')
-call cftcc%new(vol_proj, vol_proj, [1, N_PTCLS], l_match_filt=l_match_filt) ! 2 particles for now
+call cftcc%new(vol_proj, vol_proj, [1, N_PTCLS], l_match_filt=l_match_filt)
+success_ang  = 0
+success_norm = 0
 do i = 1, N_PTCLS
     call pgrpsyms%rnd_euler(o)
     call vol_proj%fproject(o, o_proj)
     call cftcc%set_ptcl(i, o_proj)
-    call o_proj%ifft()
-    ! call o_proj%write('TEST.mrc', i)
+    do j = 1, N_SAMPLES
+        norm1   = 0.
+        norm2   = 0.
+        ! first angle error and corr
+        ran_ang = (rand()*2 - 1)*MAX_ANGERR
+        euls(1) = o%e1get() + ran_ang
+        norm1   = norm1 + ran_ang**2
+        ran_ang = (rand()*2 - 1)*MAX_ANGERR
+        euls(2) = o%e2get() + ran_ang
+        norm1   = norm1 + ran_ang**2
+        ran_ang = (rand()*2 - 1)*MAX_ANGERR
+        euls(3) = o%e3get() + ran_ang
+        norm1   = norm1 + ran_ang**2
+        o1      = o
+        call o1%set_euler(euls)
+        corr1 = cftcc%project_and_correlate(i, o1)
+        ! second angle error and corr
+        ran_ang = (rand()*2 - 1)*MAX_ANGERR
+        euls(1) = o%e1get() + ran_ang
+        norm2   = norm2 + ran_ang**2
+        ran_ang = (rand()*2 - 1)*MAX_ANGERR
+        euls(2) = o%e2get() + ran_ang
+        norm2   = norm2 + ran_ang**2
+        ran_ang = (rand()*2 - 1)*MAX_ANGERR
+        euls(3) = o%e3get() + ran_ang
+        norm2   = norm2 + ran_ang**2
+        o2      = o
+        call o2%set_euler(euls)
+        corr2 = cftcc%project_and_correlate(i, o2)
+        if( (rot_angle(o%get_mat(), o1%get_mat()) > rot_angle(o%get_mat(), o2%get_mat()) .and. corr1 < corr2) .or. &
+           &(rot_angle(o%get_mat(), o1%get_mat()) < rot_angle(o%get_mat(), o2%get_mat()) .and. corr1 > corr2) )then
+            success_ang = success_ang + 1
+        ! else
+        !     print *, 'o  = ',  o%get_euler()
+        !     print *, 'o1 = ', o1%get_euler(), '; corr1 = ', corr1
+        !     print *, 'o2 = ', o2%get_euler(), '; corr2 = ', corr2
+        endif
+        if( (norm1 > norm2 .and. corr1 < corr2) .or. &
+           &(norm1 < norm2 .and. corr1 > corr2) )then
+            success_norm = success_norm + 1
+        ! else
+        !     print *, 'o  = ',  o%get_euler()
+        !     print *, 'o1 = ', o1%get_euler(), '; corr1 = ', corr1
+        !     print *, 'o2 = ', o2%get_euler(), '; corr2 = ', corr2
+        endif
+    enddo
 enddo
+print *, 'stats ( better rotation angle => better corr ) = ', success_ang *100./(N_PTCLS * N_SAMPLES)
+print *, 'stats ( better norm           => better corr ) = ', success_norm*100./(N_PTCLS * N_SAMPLES)
 o_cur = o
 corr  = cftcc%project_and_correlate(N_PTCLS, o_cur)
 print *, 'corr comparing projection against itself: ', corr
@@ -92,7 +140,6 @@ lims(2,1) = -6.
 lims(2,2) =  6.
 call cftcc_shsrch%new(lims)
 call cftcc_shsrch%set_pind(N_PTCLS)
-call o_proj%fft()
 call o_proj%shift2Dserial([-2., 2.]) 
 call cftcc%set_ref(o_proj)
 cxy = cftcc_shsrch%minimize(nevals)
