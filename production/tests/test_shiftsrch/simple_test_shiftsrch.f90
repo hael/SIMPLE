0787fec064fe3b1c9123da0f4356395ebd0c9462
cong <vanc2@nih.gov>
2022-11-22 14:29:19 -0500

    fix the shiftsrch test

--- production/tests/test_shiftsrch/simple_test_shiftsrch.f90
+++ production/tests/test_shiftsrch/simple_test_shiftsrch.f90
@@ -6,18 +6,20 @@ use simple_builder,          only: builder
 use simple_image,            only: image
 use simple_parameters,       only: parameters
 use simple_polarizer,        only: polarizer
+use simple_pftcc_shsrch_grad,only: pftcc_shsrch_grad  ! gradient-based in-plane angle and shift search
 implicit none
 type(cmdline)          :: cline
 type(builder)          :: b
 type(parameters)       :: p
 type(polarft_corrcalc) :: pftcc
 type(polarizer)        :: img_copy
+type(pftcc_shsrch_grad):: grad_shsrch_obj           !< origin shift search object, L-BFGS with gradient
 logical                :: be_verbose=.false.
-real,    parameter     :: SHMAG=1.0
+real,    parameter     :: SHMAG=3.0
 integer, parameter     :: N_PTCLS = 9
 real,    allocatable   :: corrs(:), norm_const(:, :)
-real                   :: corrmax, corr
-integer                :: xsh, ysh, xbest, ybest, i
+real                   :: corrmax, corr, cxy(3), lims(2,2)
+integer                :: xsh, ysh, xbest, ybest, i, irot
 real, allocatable      :: sigma2_noise(:,:)      !< the sigmas for alignment & reconstruction (from groups)
 if( command_argument_count() < 3 )then
     write(logfhandle,'(a)',advance='no') 'simple_test_shiftsrch stk=<particles.ext> mskdiam=<mask radius(in pixels)>'
@@ -48,72 +50,81 @@ call b%img%read(p%stk, 1)
 img_copy = b%img
 call img_copy%fft()
 call img_copy%polarize(pftcc, 1, isptcl=.false., iseven=.true., mask=b%l_resmsk)
-call img_copy%shift([SHMAG,0.,0.])   ! left
-call img_copy%polarize(pftcc, 1, isptcl=.true., iseven=.true., mask=b%l_resmsk)
+call img_copy%polarize(pftcc, 1, isptcl=.true.,  iseven=.true., mask=b%l_resmsk)
+call pftcc%shift_ptcl(1, [SHMAG,0.,0.]) ! left
 call img_copy%ifft()
 call img_copy%write('shifted.mrc', 1)
 img_copy = b%img
 call img_copy%fft()
 call img_copy%polarize(pftcc, 2, isptcl=.false., iseven=.true., mask=b%l_resmsk)
-call img_copy%shift([0.,SHMAG,0.])   ! down
-call img_copy%polarize(pftcc, 2, isptcl=.true., iseven=.true., mask=b%l_resmsk)
+call img_copy%polarize(pftcc, 2, isptcl=.true.,  iseven=.true., mask=b%l_resmsk)
+call pftcc%shift_ptcl(2, [0.,SHMAG,0.]) ! down
 call img_copy%ifft()
 call img_copy%write('shifted.mrc', 2)
 img_copy = b%img
 call img_copy%fft()
 call img_copy%polarize(pftcc, 3, isptcl=.false., iseven=.true., mask=b%l_resmsk)
-call img_copy%shift([-SHMAG,0.,0.])   ! right
-call img_copy%polarize(pftcc, 3, isptcl=.true., iseven=.true., mask=b%l_resmsk)
+call img_copy%polarize(pftcc, 3, isptcl=.true.,  iseven=.true., mask=b%l_resmsk)
+call pftcc%shift_ptcl(3, [-SHMAG,0.,0.]) ! right
 call img_copy%ifft()
 call img_copy%write('shifted.mrc', 3)
 img_copy = b%img
 call img_copy%fft()
 call img_copy%polarize(pftcc, 4, isptcl=.false., iseven=.true., mask=b%l_resmsk)
-call img_copy%shift([0.,-SHMAG,0.])   ! up
-call img_copy%polarize(pftcc, 4, isptcl=.true., iseven=.true., mask=b%l_resmsk)
+call img_copy%polarize(pftcc, 4, isptcl=.true.,  iseven=.true., mask=b%l_resmsk)
+call pftcc%shift_ptcl(4, [0.,SHMAG,0.]) ! up
 call img_copy%ifft()
 call img_copy%write('shifted.mrc', 4)
 img_copy = b%img
 call img_copy%fft()
 call img_copy%polarize(pftcc, 5, isptcl=.false., iseven=.true., mask=b%l_resmsk)
-call img_copy%shift([SHMAG,SHMAG,0.])   ! left + down
-call img_copy%polarize(pftcc, 5, isptcl=.true., iseven=.true., mask=b%l_resmsk)
+call img_copy%polarize(pftcc, 5, isptcl=.true.,  iseven=.true., mask=b%l_resmsk)
+call pftcc%shift_ptcl(5, [SHMAG,SHMAG,0.]) ! left + down
 call img_copy%ifft()
 call img_copy%write('shifted.mrc', 5)
 img_copy = b%img
 call img_copy%fft()
 call img_copy%polarize(pftcc, 6, isptcl=.false., iseven=.true., mask=b%l_resmsk)
-call img_copy%shift([-SHMAG,-SHMAG,0.]) ! right + up
-call img_copy%polarize(pftcc, 6, isptcl=.true., iseven=.true., mask=b%l_resmsk)
+call img_copy%polarize(pftcc, 6, isptcl=.true.,  iseven=.true., mask=b%l_resmsk)
+call pftcc%shift_ptcl(6, [-SHMAG,-SHMAG,0.]) ! right + up
 call img_copy%ifft()
 call img_copy%write('shifted.mrc', 6)
 img_copy = b%img
 call img_copy%fft()
 call img_copy%polarize(pftcc, 7, isptcl=.false., iseven=.true., mask=b%l_resmsk)
-call img_copy%shift([-SHMAG,SHMAG,0.])  ! right + down
-call img_copy%polarize(pftcc, 7, isptcl=.true., iseven=.true., mask=b%l_resmsk)
+call img_copy%polarize(pftcc, 7, isptcl=.true.,  iseven=.true., mask=b%l_resmsk)
+call pftcc%shift_ptcl(7, [-SHMAG,SHMAG,0.]) ! right + down
 call img_copy%ifft()
 call img_copy%write('shifted.mrc', 7)
 img_copy = b%img
 call img_copy%fft()
 call img_copy%polarize(pftcc, 8, isptcl=.false., iseven=.true., mask=b%l_resmsk)
-call img_copy%shift([SHMAG,-SHMAG,0.])  ! left + up
-call img_copy%polarize(pftcc, 8, isptcl=.true., iseven=.true., mask=b%l_resmsk)
+call img_copy%polarize(pftcc, 8, isptcl=.true.,  iseven=.true., mask=b%l_resmsk)
+call pftcc%shift_ptcl(8, [SHMAG,-SHMAG,0.]) ! left + up
 call img_copy%ifft()
 call img_copy%write('shifted.mrc', 8)
 img_copy = b%img
 call img_copy%fft()
 call img_copy%polarize(pftcc, 9, isptcl=.false., iseven=.true., mask=b%l_resmsk)
-call img_copy%shift([0.,0.,0.])   ! nothing
-call img_copy%polarize(pftcc, 9, isptcl=.true., iseven=.true., mask=b%l_resmsk)
+call img_copy%polarize(pftcc, 9, isptcl=.true.,  iseven=.true., mask=b%l_resmsk)
+call pftcc%shift_ptcl(9, [0.,0.,0.]) ! no shift
 call img_copy%ifft()
 call img_copy%write('shifted.mrc', 9)
 call pftcc%memoize_ffts
+lims(1,1) = -6.
+lims(1,2) =  6.
+lims(2,1) = -6.
+lims(2,2) =  6.
+call grad_shsrch_obj%new(lims, opt_angle=.true.)
+call grad_shsrch_obj%set_indices(8, 8)
+irot = 0
+cxy  = grad_shsrch_obj%minimize(irot)
+print *, cxy(1), cxy(2:3), irot
 do i=1,N_PTCLS
     corrmax = -1.
     do xsh=-2,2
         do ysh=-2,2
-            call pftcc%gencorrs(i, i, real([xsh,ysh]), corrs, l_norm = .true.)
+            call pftcc%gencorrs(i, i, real([xsh,ysh]), corrs)
             corr  = maxval(corrs)
 
             print *, 'corr: ', corr, xsh, ysh
