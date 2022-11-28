15eff671db463117a4710031230922686624fb4c
Hans Elmlund <hans.elmlund@nih.gov>
2022-11-28 15:56:35 -0500

    generation of class stats for comparison of objfun=cc vs. objfun=euclid

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -369,7 +369,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('7c88a96')
+call simple_print_git_version('29b6db8')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
