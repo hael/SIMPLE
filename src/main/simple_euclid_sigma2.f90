409da891849be7f5dda981967454c7e3ec9d0c55
cyrilr <cyril.reboul@monash.edu>
2022-12-01 22:57:13 +1100

    Minor changes singma2 format + minor edits

--- src/main/simple_euclid_sigma2.f90
+++ src/main/simple_euclid_sigma2.f90
@@ -97,16 +97,10 @@ contains
         class(euclid_sigma2), intent(inout) :: self
         class(oris),          intent(inout) :: os
         logical,              intent(in)    :: ptcl_mask(params_glob%fromp:params_glob%top)
-        integer                             :: iptcl,igroup,tom,eo
+        integer                             :: iptcl, igroup, ngroups, eo
         if( associated(pftcc_glob) ) call pftcc_glob%assign_pinds(self%pinds)
         if( associated(cftcc_glob) ) call cftcc_glob%assign_pinds(self%pinds)
-        ! determine number of groups
-        tom   = 0
-        do iptcl = 1, params_glob%nptcls
-            igroup = nint(os%get(iptcl, 'stkind'))
-            tom    = max(tom,   igroup)
-        end do
-        call self%read_groups_starfile( params_glob%which_iter, self%sigma2_groups, tom )
+        call self%read_groups_starfile( params_glob%which_iter, self%sigma2_groups, ngroups )
         ! copy group sigmas to particles
         do iptcl = params_glob%fromp, params_glob%top
             igroup  = nint(os%get(iptcl, 'stkind'))
@@ -183,9 +177,16 @@ contains
         integer,                       intent(in) :: ngroups
         character(len=:), allocatable :: stmp
         integer                       :: eo, igroup, idx
-        type(starfile_table_type)     :: ostarfile
-        call starfile_table__new(ostarfile)
-        call starfile_table__open_ofile(ostarfile, fname)
+        type(starfile_table_type)     :: ostar
+        call starfile_table__new(ostar)
+        call starfile_table__open_ofile(ostar, fname)
+        ! global fields
+        call starfile_table__addObject(ostar)
+        call starfile_table__setIsList(ostar, .false.)
+        call starfile_table__setname(ostar, "general")
+        call starfile_table__setValue_int(ostar, EMDL_MLMODEL_NR_GROUPS, ngroups)
+        call starfile_table__write_ofile(ostar)
+        ! values
         do eo = 1, 2
             if( eo == 1 )then
                 stmp = 'even'
@@ -193,42 +194,48 @@ contains
                 stmp = 'odd'
             end if
             do igroup = 1, ngroups
-                call starfile_table__clear(ostarfile)
-                call starfile_table__setComment(ostarfile, stmp // ', group ' // trim(int2str(igroup)) )
-                call starfile_table__setName(ostarfile, trim(int2str(eo)) // '_group_' // trim(int2str(igroup)) )
-                call starfile_table__setIsList(ostarfile, .false.)
+                call starfile_table__clear(ostar)
+                call starfile_table__setComment(ostar, stmp // ', group ' // trim(int2str(igroup)) )
+                call starfile_table__setName(ostar, trim(int2str(eo)) // '_group_' // trim(int2str(igroup)) )
+                call starfile_table__setIsList(ostar, .false.)
                 do idx = lbound(group_pspecs,3), ubound(group_pspecs, 3)
-                    call starfile_table__addObject(ostarfile)
-                    call starfile_table__setValue_int(ostarfile,    EMDL_SPECTRAL_IDX, idx)
-                    call starfile_table__setValue_double(ostarfile, EMDL_MLMODEL_SIGMA2_NOISE,&
+                    call starfile_table__addObject(ostar)
+                    call starfile_table__setValue_int(ostar,    EMDL_SPECTRAL_IDX, idx)
+                    call starfile_table__setValue_double(ostar, EMDL_MLMODEL_SIGMA2_NOISE,&
                         real(group_pspecs(eo,igroup,idx),dp) )
                 end do
-                call starfile_table__write_ofile(ostarfile)
+                call starfile_table__write_ofile(ostar)
             end do
         end do
-        call starfile_table__close_ofile(ostarfile)
-        call starfile_table__delete(ostarfile)
+        call starfile_table__close_ofile(ostar)
+        call starfile_table__delete(ostar)
     end subroutine write_groups_starfile
 
     subroutine read_groups_starfile( self, iter, group_pspecs, ngroups )
         class(euclid_sigma2),          intent(inout) :: self
         integer,                       intent(in)    :: iter
         real,             allocatable, intent(out)   :: group_pspecs(:,:,:)
-        integer,                       intent(in)    :: ngroups
+        integer,                       intent(out)    :: ngroups
         type(str4arr),    allocatable :: names(:)
         type(starfile_table_type)     :: istarfile
-        character                     :: eo_char
-        integer                       :: stat, spec_idx, eo, igroup, idx
-        real(dp)                      :: val
-        logical                       :: ares
-        integer(C_long)               :: num_objs, object_id
         character(len=:), allocatable :: starfile_fname
+        character                     :: eo_char
+        real(dp)                      :: val
+        integer(C_long)               :: num_objs, object_id
+        integer                       :: stat, spec_idx, eo, igroup, idx
+        logical                       :: l
         starfile_fname = trim(SIGMA2_GROUP_FBODY) // trim(int2str(iter)) // '.star'
-        allocate(group_pspecs(2,ngroups,self%kfromto(1):self%kfromto(2)))
         call starfile_table__new(istarfile)
         if (.not. file_exists(starfile_fname)) then
             THROW_HARD('euclid_sigma2_starfile: read_groups_pspecs; file does not exists: ' // starfile_fname)
         end if
+        ! read header
+        call starfile_table__getnames(istarfile, starfile_fname, names)
+        call starfile_table__read( istarfile, starfile_fname, names(1)%str )
+        l         = starfile_table__getValue_int(istarfile, EMDL_MLMODEL_NR_GROUPS, ngroups)
+        object_id = starfile_table__nextobject(istarfile)
+        ! read values
+        allocate(group_pspecs(2,ngroups,self%kfromto(1):self%kfromto(2)))
         call starfile_table__getnames(istarfile, starfile_fname, names)
         do idx = 1, size(names)
             if( len(names(idx)%str) < len('1_group_')+1 )cycle
@@ -244,10 +251,10 @@ contains
             object_id = starfile_table__firstobject(istarfile)
             num_objs  = starfile_table__numberofobjects(istarfile)
             do while( (object_id < num_objs) .and. (object_id >= 0) )
-                ares = starfile_table__getValue_int   (istarfile, EMDL_SPECTRAL_IDX, spec_idx)
-                if( ares ) then
-                    ares = starfile_table__getValue_double(istarfile, EMDL_MLMODEL_SIGMA2_NOISE, val)
-                    if( ares ) then
+                l = starfile_table__getValue_int(istarfile, EMDL_SPECTRAL_IDX, spec_idx)
+                if( l ) then
+                    l = starfile_table__getValue_double(istarfile, EMDL_MLMODEL_SIGMA2_NOISE, val)
+                    if( l ) then
                         if( (spec_idx >= self%kfromto(1)).and.(spec_idx <= self%kfromto(2)) ) then
                             group_pspecs(eo,igroup,spec_idx) = real(val)
                         end if
