d8be097b100de247fea6296a21281fdac24251d0
hael <hans.elmlund@nih.gov>
2022-12-02 07:24:13 -0500

    prep_ref4euclid in polarft_corrcalc, jus for clarity

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -158,7 +158,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('6f1c5bb')
+call simple_print_git_version('6a7d123')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
