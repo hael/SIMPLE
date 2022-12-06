c68d2d351ab88c1bf02d1d45918a4ec35aae00cc
Hans Elmlund <hans.elmlund@nih.gov>
2022-12-06 14:16:56 -0500

    ini3D bugfix

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -158,7 +158,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('201d99b')
+call simple_print_git_version('33a4f72')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
