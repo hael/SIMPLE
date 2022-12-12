e705511b4714260099348b79afa9af656bbf197a
cong <>
2022-12-12 16:04:23 -0500

    modifying cartft_corrcalc to be like polarft_corrcalc, in progress

--- production/tests/test_cartcorr_shifted/simple_test_cartcorr_shifted.f90
+++ production/tests/test_cartcorr_shifted/simple_test_cartcorr_shifted.f90
@@ -19,6 +19,7 @@ real                    :: grad(2), cxy(3), lims(2,2)
 integer(timer_int_kind) :: t_tot
 real,    parameter      :: NOISE_MIN = .3, NOISE_MAX = .7, NOISE_DEL = 0.1
 real                    :: ave, sdev, maxv, minv, noise_lvl, correct_sh(2)
+real, allocatable       :: sigma2_noise(:,:)      !< the sigmas for alignment & reconstruction (from groups)
 if( command_argument_count() < 4 )then
     write(logfhandle,'(a)',advance='no') 'simple_test_cartcorr_shifted stk=<particles.ext>'
     write(logfhandle,'(a)') ' smpd=<sampling distance(in A)> [nthr=<number of threads{1}>] mskdiam=zz [verbose=<yes|no{no}>]'
@@ -34,29 +35,20 @@ p%kfromto(1) = 1
 p%kfromto(2) = p%box/2 - 5
 call b%build_general_tbox(p, cline)
 call cftcc%new_dev([1,p%nptcls])
+allocate( sigma2_noise(p%kfromto(1):p%kfromto(2), 1:p%nptcls), source=1. )
+call cftcc%assign_sigma2_noise(sigma2_noise)
 do i = 1,p%nptcls
     call b%img%read(p%stk, i)
     call b%img%fft
     call cftcc%set_ptcl(i, b%img)
 end do
-! t_tot = tic()
-! do i = 1,1
-!     call b%img%read(p%stk, i)
-!     call b%img%fft
-!     call b%img%shift2Dserial([-0.5,-0.5]) 
-!     call cftcc%set_ref(b%img)
-!     call srch_shifts(0.5, i)
-! end do
-! print *, 'corr_shifted timing = ', toc(t_tot)
-! t_tot = tic()
-! do i = 1,1
-!     call b%img%read(p%stk, i)
-!     call b%img%fft
-!     call b%img%shift2Dserial([-0.5,-0.5]) 
-!     call cftcc%set_ref(b%img)
-!     call srch_shifts_ad(0.5, i)
-! end do
-! print *, 'corr_shifted_ad timing = ', toc(t_tot)
+do i = 1,1
+    call b%img%read(p%stk, i)
+    call b%img%fft
+    call b%img%shift2Dserial([-0.5,-0.5]) 
+    call cftcc%set_ref(b%img)
+    call srch_shifts(0.5, i)
+end do
 ! lbfgsb shift search
 lims(1,1) = -6.
 lims(1,2) =  6.
@@ -88,48 +80,12 @@ do noise_i = 1, noise_n
 enddo
 
 contains
-
-    subroutine srch_shifts_ad( shstep, iptcl )
-        real,    intent(in) :: shstep
-        integer, intent(in) :: iptcl
-        real, allocatable   :: srch_space(:,:) 
-        integer :: cnt, i
-        real    :: x, y, corr
-        cnt = 0
-        x   = -SHMAG
-        do while( x <= SHMAG )
-            y = -SHMAG
-            do while( y <= SHMAG )
-                cnt = cnt + 1
-                y = y + shstep
-            end do
-            x = x + shstep
-        end do
-        allocate(srch_space(cnt,2), source=0.)
-        cnt = 0
-        x   = -SHMAG; 
-        do while( x <= SHMAG )
-            y = -SHMAG
-            do while( y <= SHMAG )
-                cnt = cnt + 1
-                srch_space(cnt,1) = x
-                srch_space(cnt,2) = y
-                y = y + shstep
-            end do
-            x = x + shstep
-        end do
-        do i = 1,cnt
-            corr = cftcc%corr_shifted_ad(iptcl, srch_space(i,:), grad)
-            print *, srch_space(i,1), srch_space(i,2), corr, grad(1), grad(2)
-        end do
-    end subroutine srch_shifts_ad
-
     subroutine srch_shifts( shstep, iptcl )
         real,    intent(in) :: shstep
         integer, intent(in) :: iptcl
         real, allocatable   :: srch_space(:,:) 
         integer :: cnt, i
-        real    :: x, y, corr
+        real    :: x, y, corr, max_corr, max_x, max_y
         cnt = 0
         x   = -SHMAG
         do while( x <= SHMAG )
@@ -153,10 +109,16 @@ contains
             end do
             x = x + shstep
         end do
+        max_corr = 0.
         do i = 1,cnt
-            call cftcc%corr_shifted(iptcl, srch_space(i,:), corr, grad)
-            print *, srch_space(i,1), srch_space(i,2), corr, grad(1), grad(2)
+            call cftcc%corr_shifted(iptcl, srch_space(i,:), corr)
+            if( corr > max_corr )then
+                max_corr = corr
+                max_x    = srch_space(i,1)
+                max_y    = srch_space(i,2)
+            endif
         end do
+        print *, max_x, max_y, max_corr
     end subroutine srch_shifts
 
 end program simple_test_cartcorr_shifted
