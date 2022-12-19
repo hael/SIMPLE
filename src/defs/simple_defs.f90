b934545781ca9237c15b29b84ce3156ab0062fe9
cong <vanc2@nih.gov>
2022-12-19 09:51:40 -0500

    bugfix

--- src/defs/simple_defs.f90
+++ src/defs/simple_defs.f90
@@ -44,7 +44,7 @@ integer                       :: nthr_glob = 1                 !< number of thre
 logical                       :: l_distr_exec_glob             !< global distributed execution flag
 integer                       :: part_glob                     !< global part index
 character(len=:), allocatable :: cmdline_glob                  !< global command line string
-logical,          parameter   :: L_BENCH_GLOB       = .true.   !< global benchmarking flag
+logical,          parameter   :: L_BENCH_GLOB       = .false.  !< global benchmarking flag
 logical,          parameter   :: L_DO_GRIDCORR_GLOB = .false.  !< global gridding correction flag
 logical,          parameter   :: L_USE_SLURM_ARR    = .false.  !< use SLURM arrays for jobs where we know nparts
 logical,          parameter   :: L_DEV_GLOB         = .false.  !< global development flag
