3b8d9ddf68f91c35d5ca42750a39d758281b14e3
Wietfeldt <wietfeldthc@fnm-s106326.ncifcrf.gov>
2023-01-06 11:26:58 -0500

    Added more comments for readability

--- production/tests/test_matinv/simple_test_matinv.f90
+++ production/tests/test_matinv/simple_test_matinv.f90
@@ -5,38 +5,41 @@ program simple_test_matinv
     real        :: a(3,3), b(3,3)
     integer     :: errflg
 
-    print *, "First Test"
+    print *, "First Test. Produces correct result"
     a = 0.
     a(1,1) = 1.
     a(2,2) = 1.
     a(3,3) = 1.
     b = 0.
     call matinv(a, b, 3, errflg)
-    print *, errflg
-    print *, a
-    print *, b
+    print *, "erflg", errflg
+    print *, "a", a
+    print *, "b", b
 
-    print *, "Second Test"
+    print *, ""
+    print *, "Second Test.  Produces correct result"
     a = 0.
     b = 0.
     a(1,2) = 1.
     a(2,1) = 1.
     a(3,3) = 1.
     call matinv(a, b, 3, errflg)
-    print *, errflg
-    print *, a
-    print *, b
+    print *, "erflg", errflg
+    print *, "a", a
+    print *, "b", b
 
-    print *, "Third Test"
+    print *, ""
+    print *, "Third Test. Produces incorrect result"
     a = 0.
     b = 0.
+    ! Adding the following lines will produce a correct result
     !a(1,1) = 0.0001
     a(1,3) = 1.
     a(2,2) = 1.
     a(3,1) = 1.
     call matinv(a, b, 3, errflg)
-    print *, errflg
-    print *, a
-    print *, b
+    print *, "erflg", errflg
+    print *, "a", a
+    print *, "b", b
 
 end program simple_test_matinv
