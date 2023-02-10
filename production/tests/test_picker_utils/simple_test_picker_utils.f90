a6326977a4555cbffff19f377004caa567f2730a
hael <hans.elmlund@nih.gov>
2023-02-10 17:01:10 -0500

    massive speedup of gaupicker through parallel optimization

--- production/tests/test_picker_utils/simple_test_picker_utils.f90
+++ production/tests/test_picker_utils/simple_test_picker_utils.f90
@@ -6,7 +6,7 @@ use simple_picker_utils, only: picker_utils
 use simple_image,        only: image
 implicit none
 
-character(len=*), parameter :: micname = '/home/elmlundho/cache/relion_tut/2_motion_correct/20170629_00021_frameImage_intg.mrc'
+character(len=*), parameter :: micname = '/Users/elmlundho/Processing/relion_tut/20170629_00021_frameImage_intg.mrc'
 ! character(len=*), parameter :: micname = '/home/elmlundho/cache/pick_bench/optimal_movie_average.mrc'
 real,             parameter :: SMPD    = 0.885, MOLDIAM = 180., MAXDIAM = MOLDIAM + 20.
 ! real,             parameter :: SMPD    = 2.0, MOLDIAM = 200., MAXDIAM = MOLDIAM + 20.
@@ -16,6 +16,7 @@ integer            :: ldim(3), ifoo, nthr
 
 !$ nthr = omp_get_max_threads()
 !$ call omp_set_num_threads(nthr)
+nthr_glob = nthr
 
 call find_ldim_nptcls(micname, ldim, ifoo)
 call micimg%new(ldim, SMPD)
