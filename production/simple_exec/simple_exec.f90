a27dcf4b3ea166a43dd44ec9fd8f0ebde127be73
Hans Elmlund <hans.elmlund@nih.gov>
2023-02-13 14:22:29 -0500

    outlier removal in picker_utils based on average local variance in box

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -369,7 +369,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('f86b169')
+call simple_print_git_version('c1f90df')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
