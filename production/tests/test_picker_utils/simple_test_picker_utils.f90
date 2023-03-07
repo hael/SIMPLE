29c3b8ee6e8260f26727f8d5134235155cc17ed8
Hans Elmlund <hans.elmlund@nih.gov>
2023-03-07 10:15:43 -0500

    hybrid Gaussian/reference picking mode

--- production/tests/test_picker_utils/simple_test_picker_utils.f90
+++ production/tests/test_picker_utils/simple_test_picker_utils.f90
@@ -6,10 +6,11 @@ use simple_picker_utils, only: picker_utils
 use simple_image,        only: image
 implicit none
 
-character(len=*), parameter :: micname = '/home/elmlundho/cache/relion_tut/20170629_00021_frameImage_intg.mrc'
+character(len=*), parameter :: micname  = '/home/elmlundho/cache/relion_tut/20170629_00021_frameImage_intg.mrc'
 ! character(len=*), parameter :: micname = '/Users/elmlundho/Processing/relion_tut/20170629_00021_frameImage_intg.mrc'
-character(len=*), parameter :: boxrefs = '/home/elmlundho/cache/relion_tut/boxrefs.mrc'
-real,             parameter :: SMPD    = 0.885, MOLDIAM = 180., MSKDIAM = 200.
+character(len=*), parameter :: boxrefs  = '/home/elmlundho/cache/relion_tut/boxrefs.mrc'
+real,             parameter :: SMPD     = 0.885, MOLDIAM = 180., MSKDIAM = 200.
+logical,          parameter :: L_HYBRID = .false.
 type(image)                 :: micimg
 type(image),    allocatable :: refs(:)
 type(picker_utils)          :: putils
@@ -23,17 +24,17 @@ call simple_getcwd(cwd)
 allocate(CWD_GLOB, source=trim(cwd))
 
 call find_ldim_nptcls(micname, ldim, ifoo)
-! call find_ldim_nptcls(boxrefs, ldim_refs, nrefs)
-! ldim_refs(3) = 1
-! allocate(refs(nrefs))
-! do iref = 1,nrefs
-!     call refs(iref)%new(ldim_refs, SMPD)
-!     call refs(iref)%read(boxrefs, iref)
-! end do
+call find_ldim_nptcls(boxrefs, ldim_refs, nrefs)
+ldim_refs(3) = 1
+allocate(refs(nrefs))
+do iref = 1,nrefs
+    call refs(iref)%new(ldim_refs, SMPD)
+    call refs(iref)%read(boxrefs, iref)
+end do
 call micimg%new(ldim, SMPD)
 call micimg%read(micname)
 call putils%new(micname, 'black', SMPD, MOLDIAM)
-! call putils%set_refs(refs, MSKDIAM)
+call putils%set_refs(refs, MSKDIAM, L_HYBRID)
 call putils%exec_picker(boxname_out, nptcls)
 ! print *, 'boxname_out ', trim(boxname_out)
 ! print *, 'nptcls      ', nptcls
--- src/utils/simple_picker_utils.f90
+++ src/utils/simple_picker_utils.f90
@@ -31,6 +31,7 @@ type picker_utils
     character(len=LONGSTRLEN)     :: boxname
     character(len=:), allocatable :: fbody
     logical                       :: refpick = .false.
+    logical                       :: hybrid  = .false.
     logical                       :: exists  = .false.
   contains
     procedure          :: new
@@ -144,19 +145,22 @@ contains
         self%exists = .true.
     end subroutine new
 
-    subroutine set_refs( self, imgs, mskdiam )
+    subroutine set_refs( self, imgs, mskdiam, hybrid )
         class(picker_utils), intent(inout) :: self
         class(image),        intent(inout) :: imgs(:)
         real,                intent(in)    :: mskdiam
+        logical, optional,   intent(in)    :: hybrid
         type(image) :: img_rot
         integer     :: ldim(3), iimg, nimgs, irot, nrots, cnt
         real        :: scale1, scale2, mskrad1, mskrad2, pixrad_shrink1, pixrad_shrink2, smpd, ang, rot
-        smpd       = imgs(1)%get_smpd()
-        ldim       = imgs(1)%get_ldim()
+        self%hybrid = .false.
+        if( present(hybrid) ) self%hybrid = hybrid
+        smpd        = imgs(1)%get_smpd()
+        ldim        = imgs(1)%get_ldim()
         if( ldim(3) /= 1 ) THROW_HARD('box references must be 2D')
-        nimgs      = size(imgs)
-        nrots      = nint(real(MAXNREFS) / real(nimgs))
-        self%nrefs = nimgs * nrots
+        nimgs       = size(imgs)
+        nrots       = nint(real(MAXNREFS) / real(nimgs))
+        self%nrefs  = nimgs * nrots
         ! set shrunken logical dimensions of boxes
         scale1               = smpd / SMPD_SHRINK1
         scale2               = smpd / SMPD_SHRINK2
@@ -438,7 +442,7 @@ contains
         do ithr = 1,nthr_glob
             call boximgs_heap(ithr)%new(self%ldim_box1, SMPD_SHRINK1)
         end do
-        if( self%refpick )then
+        if( self%refpick .and. .not. self%hybrid )then
             !$omp parallel do schedule(static) collapse(2) default(shared) private(ioff,joff,ithr,outside,iref,scores) proc_bind(close)
             do ioff = 1,self%nx_offset
                 do joff = 1,self%ny_offset
