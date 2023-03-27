95a57b0543f11d7a0c2b6d3e0e24891675a48f7e
cyrilr <cyril.reboul@gmail.com>
2023-03-27 15:23:30 +1100

    Up to date with master commit '9e0f53f'

--- src/main/commanders/simple_commander_preprocess_stream.f90
+++ src/main/commanders/simple_commander_preprocess_stream.f90
@@ -90,15 +90,17 @@ contains
         ! extraction
         if( .not. cline%defined('pcontrast')       ) call cline%set('pcontrast',    'black')
         ! 2D classification
-        if( .not. cline%defined('lpthres')    ) call cline%set('lpthres',        30.0)
+        if( .not. cline%defined('lpthres')     ) call cline%set('lpthres',       30.0)
         if( .not. cline%defined('ndev2D')      ) call cline%set('ndev2D',         1.5)
         if( .not. cline%defined('wiener')      ) call cline%set('wiener',   'partial')
         if( .not. cline%defined('autoscale')   ) call cline%set('autoscale',    'yes')
         if( .not. cline%defined('nonuniform')  ) call cline%set('nonuniform',    'no')
         if( .not. cline%defined('nparts_chunk')) call cline%set('nparts_chunk',   1.0)
-        if( .not. cline%defined('nchunks'))      call cline%set('nchunks',        2.0)
-        if( .not. cline%defined('prune'))        call cline%set('prune',         'no')
-        if( .not. cline%defined('reject_cls')   ) call cline%set('reject_cls',  'yes')
+        if( .not. cline%defined('nchunks')     ) call cline%set('nchunks',        2.0)
+        if( .not. cline%defined('prune')       ) call cline%set('prune',         'no')
+        if( .not. cline%defined('reject_cls')  ) call cline%set('reject_cls',   'yes')
+        if( .not.cline%defined('objfun')       ) call cline%set('objfun',        'cc')
+        if( .not.cline%defined('ml_reg')       ) call cline%set('ml_reg',        'no')
         ncls_in = 0
         if( cline%defined('ncls') )then
             ! to circumvent parameters class stringency
