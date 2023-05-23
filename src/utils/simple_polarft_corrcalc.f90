e2c9f33ebd0cb631e67d1764ce62b319086a88de
cong <vanc2@nih.gov>
2023-05-23 15:58:10 -0400

    testing reg scheme using neighboring cavgs

--- src/utils/simple_polarft_corrcalc.f90
+++ src/utils/simple_polarft_corrcalc.f90
@@ -176,6 +176,7 @@ type :: polarft_corrcalc
     procedure          :: ref_reg_cc_neigh
     procedure          :: ref_reg_cc_dev
     procedure          :: regularize_refs
+    procedure          :: change_refs
     procedure          :: reset_regs
     procedure, private :: setup_npix_per_shell
     ! CALCULATORS
@@ -920,35 +921,21 @@ contains
         type(oris),              intent(in)    :: eulspace
         type(oris),              intent(in)    :: ptcl_eulspace
         integer,                 intent(in)    :: glob_pinds(self%nptcls)
-        integer  :: i, iref, iptcl, loc
-        real     :: inpl_corrs(self%nrots), ptcl_ref_dist, ptcl_ctf(self%pftsz,self%kfromto(1):self%kfromto(2),self%nptcls)
-        real     :: euls(3), euls_ref(3), shvec(2)
-        real(dp) :: ctf_rot(self%pftsz,self%kfromto(1):self%kfromto(2)), ptcl_ctf_rot(self%pftsz,self%kfromto(1):self%kfromto(2))
+        integer  :: i, iref, iptcl
+        real     :: ptcl_ctf(self%pftsz,self%kfromto(1):self%kfromto(2),self%nptcls)
+        real     :: euls(3), euls_ref(3), theta
         ptcl_ctf = real(self%pfts_ptcls * self%ctfmats)
-        !$omp parallel do collapse(2) default(shared) private(i,iref,euls_ref,euls,ptcl_ref_dist,iptcl,inpl_corrs,loc,ptcl_ctf_rot, ctf_rot, shvec) proc_bind(close) schedule(static)
+        !$omp parallel do collapse(2) default(shared) private(i,iref,euls_ref,euls,iptcl,theta) proc_bind(close) schedule(static)
         do iref = 1, self%nrefs
             do i = 1, self%nptcls
                 iptcl    = glob_pinds(i)
-                euls_ref = eulspace%get_euler(iref)
-                euls     = ptcl_eulspace%get_euler(iptcl)
-                ! projection direction distance, euler_dist could be used instead
-                euls_ref(3)   = 0.
-                euls(3)       = 0.
-                ptcl_ref_dist = geodesic_frobdev(euls_ref,euls)
-                ! find best irot for this pair of iref, iptcl
-                shvec         = ptcl_eulspace%get_2Dshift(iptcl)
-                call self%gencorrs( iref, iptcl, -shvec , inpl_corrs )
-                loc = maxloc(inpl_corrs, dim=1)
-                if( inpl_corrs(loc) < TINY ) cycle
-                ! distance & correlation weighing
-                ptcl_ref_dist = inpl_corrs(loc) / ( 1. + ptcl_ref_dist )
-                ! computing the reg terms as the gradients w.r.t 2D references of the probability
-                loc = (self%nrots+1)-(loc-1)
-                if( loc > self%nrots ) loc = loc - self%nrots
-                call self%rotate_polar(    ptcl_ctf(:,:,i), ptcl_ctf_rot, loc, shvec)
-                call self%rotate_polar(self%ctfmats(:,:,i),      ctf_rot, loc, shvec)
-                self%refs_reg(  :,:,iref) = self%refs_reg(  :,:,iref) + ptcl_ctf_rot * real(ptcl_ref_dist, dp)
-                self%regs_denom(:,:,iref) = self%regs_denom(:,:,iref) +      ctf_rot**2
+                euls_ref =      eulspace%get_euler(iref)  * pi / 180.
+                euls     = ptcl_eulspace%get_euler(iptcl) * pi / 180.
+                theta    = acos(cos(euls_ref(2))*cos(euls(2)) + sin(euls_ref(2))*sin(euls(2))*cos(euls_ref(1) - euls(1)))
+                if( theta <= params_glob%arc_thres*pi/180.  .and. theta >= 0. )then
+                    self%refs_reg(  :,:,iref) = self%refs_reg(  :,:,iref) +     ptcl_ctf(:,:,i)
+                    self%regs_denom(:,:,iref) = self%regs_denom(:,:,iref) + self%ctfmats(:,:,i)**2
+                endif
             enddo
         enddo
         !$omp end parallel do
@@ -974,6 +961,27 @@ contains
         !$omp end parallel do
         call self%memoize_refs
     end subroutine regularize_refs
+
+    subroutine change_refs( self )
+        class(polarft_corrcalc), intent(inout) :: self
+        integer  :: iref, k
+        !$omp parallel do default(shared) private(k) proc_bind(close) schedule(static)
+        do k = self%kfromto(1),self%kfromto(2)
+            where( abs(self%regs_denom(:,k,:)) < TINY )
+                self%refs_reg(:,k,:) = self%refs_reg(:,k,:)
+            elsewhere
+                self%refs_reg(:,k,:) = self%refs_reg(:,k,:) / self%regs_denom(:,k,:)
+            endwhere
+        enddo
+        !$omp end parallel do
+        !$omp parallel do default(shared) private(iref) proc_bind(close) schedule(static)
+        do iref = 1, self%nrefs
+            self%pfts_refs_even(:,:,iref) = real(self%refs_reg(:,:,iref))
+            self%pfts_refs_odd( :,:,iref) = real(self%refs_reg(:,:,iref))
+        enddo
+        !$omp end parallel do
+        call self%memoize_refs
+    end subroutine change_refs
 
     subroutine reset_regs( self )
         class(polarft_corrcalc), intent(inout) :: self
