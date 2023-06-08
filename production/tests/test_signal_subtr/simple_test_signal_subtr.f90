fad49a13560f30ec9a3612da816173ac778130d0
Wietfeldt <wietfeldthc@fnm-s106326.ncifcrf.gov>
2023-06-08 17:04:22 -0400

    signal subtraction with any class averages (including from 3D alignment); bug fix to 2D core finder

--- production/tests/test_signal_subtr/simple_test_signal_subtr.f90
+++ production/tests/test_signal_subtr/simple_test_signal_subtr.f90
@@ -7,36 +7,47 @@ program simple_test_signal_subtr
     implicit none
     
     type(image),    allocatable :: cavgs(:), reprojs(:), out(:)
-    real        :: smpd=0.358
-    integer     :: iref, nrefs, ldim(3), ldim_refs(3), ifoo
-    character(*), parameter    :: stks='cavgs_vs_reprojections_rec_and_sim.mrc'
+    real        :: smpd=0.358, nprad_A=15, nprad_vox
+    integer     :: iref, nrefs, ldim1(3), ldim2(3), ldim_refs(3), ifoo
+    character(*), parameter    :: fn_cavgs='cavgs.mrc', fn_reprojs='reprojs.mrcs'
     character(*), parameter    :: fn_out='cavgs_minus_reprojections.mrc'
     character(*), parameter    :: cavgs_out='cavgs_out.mrc', reprojs_out='reprojs_out.mrc'
 
     ! Read in stacks
-    call find_ldim_nptcls(stks, ldim, ifoo)
-    ldim_refs = [ldim(1), ldim(2), 1]
-    nrefs = ldim(3) / 3
+    call find_ldim_nptcls(fn_cavgs, ldim1, ifoo)
+    call find_ldim_nptcls(fn_reprojs, ldim2, ifoo)
+    if (ldim1(1) /= ldim2(1) .or. ldim1(2) /= ldim2(2)) then
+        print *, "CAVGS AND REPROJS OF DIFFERENT DIMENSIONS"
+        stop
+    else if (ldim1(3) /= ldim2(3)) then
+        print *, "CAVGS AND REPROJS OF DIFFERENT NUMBER"
+        stop
+    end if
+    ldim_refs = [ldim1(1), ldim1(2), 1]
+    nrefs = ldim1(3)
     allocate(cavgs(nrefs), reprojs(nrefs), out(nrefs))
     do iref = 1,nrefs
         call cavgs(iref)%new(ldim_refs, SMPD)
         call reprojs(iref)%new(ldim_refs, SMPD)
-        call cavgs(iref)%read(stks, 3*iref-2)
-        call reprojs(iref)%read(stks, 3*iref-1)
+        call cavgs(iref)%read(fn_cavgs, iref)
+        call reprojs(iref)%read(fn_reprojs, iref)
     end do
 
-    ! Ensure proper normalizations
+    ! Ensure proper normalizations of cavgs
     ! This is wrong
     do iref=1, nrefs
-        call cavgs(iref)%norm()
-        call reprojs(iref)%norm()
-    end do
+         call cavgs(iref)%norm()
+         call reprojs(iref)%norm()
+     end do
 
     ! Subtract reprojs from cavgs
     do iref=1,nrefs
         call out(iref)%new(ldim_refs, SMPD)
         out(iref) = cavgs(iref) - reprojs(iref)
     end do
+
+    ! Take averages of shells out to the NP radius
+    !--INSERT HERE---
 
     ! Write output
     do iref=1, nrefs
