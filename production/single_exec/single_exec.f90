aefd2f8aaee542be24934d5fe1323d3b1a3432dd
Hans Elmlund <hans.elmlund@nih.gov>
2023-07-10 13:26:34 -0400

    parallel IO cavg assembly

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -161,7 +161,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('62b07556')
+call simple_print_git_version('1d47e2cc')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
