a7692690d4ba2843edeb5be679e1da3e54509ff3
hael <hans.elmlund@nih.gov>
2023-07-17 16:07:11 -0400

    utility functions for 2D nano analysis

--- src/main/simple_parameters.f90
+++ src/main/simple_parameters.f90
@@ -41,6 +41,7 @@ type :: parameters
     character(len=3)          :: groupframes='no'     !< Whether to perform weighted frames averaging during motion correction(yes|no){no}
     character(len=3)          :: incrreslim='yes'     !< Whether to add ten shells to the FSC resolution limit
     character(len=3)          :: keepvol='no'         !< dev flag for preserving iterative volumes in refine3d
+    character(len=3)          :: loc_sdev='no'        !< Whether to calculate local standard deviations(yes|no){no}
     character(len=3)          :: makemovie='no'
     character(len=3)          :: masscen='no'         !< center to center of gravity(yes|no){no}
     character(len=3)          :: mcpatch='yes'        !< whether to perform patch-based alignment during motion correction
@@ -509,6 +510,7 @@ contains
         call check_carg('incrreslim',     self%incrreslim)
         call check_carg('interpfun',      self%interpfun)
         call check_carg('keepvol',        self%keepvol)
+        call check_carg('loc_sdev',       self%loc_sdev)
         call check_carg('kweight',        self%kweight)
         call check_carg('kweight_chunk',  self%kweight_chunk)
         call check_carg('kweight_pool',   self%kweight_pool)
