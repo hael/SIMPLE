3daae55d5a0c91f047d562a2812ab59930297c8a
Hans Elmlund <hans.elmlund@nih.gov>
2023-07-19 11:42:05 -0400

    being exlicit about kweight in refine3D_nano

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -375,7 +375,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('b19df7e0')
+call simple_print_git_version('9d1564f7')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
