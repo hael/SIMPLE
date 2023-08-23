4ab0dfa5c28ccbcc7a27c9240a2bd4e4377a7e66
cong <vanc2@nih.gov>
2023-08-23 15:05:40 -0400

    adding a global ref/ptcl 2D prob table in reg

--- src/main/strategies/simple_strategy3D_matcher.f90
+++ src/main/strategies/simple_strategy3D_matcher.f90
@@ -189,6 +189,15 @@ contains
             if( trim(params_glob%reg_mode) .eq. 'sto' ) reg_eps = real(which_iter)/real(params_glob%reg_iters)
             if( reg_eps < 1. )then
                 call reg_obj%reset_regs
+                call reg_obj%init_tab(pinds)
+                ! Batch loop
+                do ibatch=1,nbatches
+                    batch_start = batches(ibatch,1)
+                    batch_end   = batches(ibatch,2)
+                    batchsz     = batch_end - batch_start + 1
+                    call prob_batch_particles(batchsz, pinds(batch_start:batch_end))
+                enddo
+                call reg_obj%sort_tab
                 ! Batch loop
                 do ibatch=1,nbatches
                     batch_start = batches(ibatch,1)
@@ -531,8 +540,34 @@ contains
         ! Memoize particles FFT parameters
         call pftcc%memoize_ptcls
         ! compute regularization terms
-        ! call reg_obj%ref_reg_cc(build_glob%eulspace, build_glob%spproj_field, pinds_here)
-        call reg_obj%ref_reg_cc_test(pinds_here)
+        call reg_obj%ref_reg_cc_tab(pinds_here)
     end subroutine reg_batch_particles
+
+    subroutine prob_batch_particles( nptcls_here, pinds_here )
+        use simple_strategy2D3D_common, only: read_imgbatch, prepimg4align
+        integer, intent(in) :: nptcls_here
+        integer, intent(in) :: pinds_here(nptcls_here)
+        integer :: iptcl_batch, iptcl
+        call read_imgbatch( nptcls_here, pinds_here, [1,nptcls_here] )
+        ! reassign particles indices & associated variables
+        call pftcc%reallocate_ptcls(nptcls_here, pinds_here)
+        !$omp parallel do default(shared) private(iptcl,iptcl_batch) schedule(static) proc_bind(close)
+        do iptcl_batch = 1,nptcls_here
+            iptcl = pinds_here(iptcl_batch)
+            ! prep
+            call prepimg4align(iptcl, build_glob%imgbatch(iptcl_batch))
+            ! transfer to polar coordinates
+            call build_glob%img_match%polarize(pftcc, build_glob%imgbatch(iptcl_batch), iptcl, .true., .true., mask=build_glob%l_resmsk)
+            ! e/o flags
+            call pftcc%set_eo(iptcl, nint(build_glob%spproj_field%get(iptcl,'eo'))<=0 )
+        end do
+        !$omp end parallel do
+        ! make CTFs
+        call pftcc%create_polar_absctfmats(build_glob%spproj, 'ptcl3D')
+        ! Memoize particles FFT parameters
+        call pftcc%memoize_ptcls
+        ! compute regularization terms
+        call reg_obj%fill_tab(pinds_here)
+    end subroutine prob_batch_particles
 
 end module simple_strategy3D_matcher
--- src/utils/simple_regularizer.f90
+++ src/utils/simple_regularizer.f90
@@ -21,13 +21,21 @@ type :: regularizer
     real(dp), allocatable :: regs(:,:,:)             !< -"-, reg terms
     real(dp), allocatable :: regs_denom(:,:,:)       !< -"-, reg denom
     real,     allocatable :: ref_corr(:)             !< total ref corr sum
+    real,     allocatable :: ref_ptcl_corr(:,:)      !< 2D corr/prob table
+    integer,  allocatable :: ref_ptcl_loc(:,:)       !< 2D in-plane table
+    real,     allocatable :: ref_ptcl_sh(:,:,:)      !< 2D sh table
+    integer,  allocatable :: ref_ptcl_ind(:,:)       !< 2D index table
     class(polarft_corrcalc), pointer     :: pftcc => null()
     type(pftcc_shsrch_grad), allocatable :: grad_shsrch_obj(:)
     contains
     ! CONSTRUCTOR
     procedure          :: new
     ! PROCEDURES
+    procedure          :: init_tab
+    procedure          :: fill_tab
+    procedure          :: sort_tab
     procedure          :: ref_reg_cc
+    procedure          :: ref_reg_cc_tab
     procedure          :: ref_reg_cc_test
     procedure          :: ref_reg_cc_noshift
     procedure          :: regularize_refs
@@ -69,6 +77,95 @@ contains
             &shbarrier=params_glob%shbarrier, maxits=MAXITS, opt_angle=params_glob%l_reg_opt_ang)
         enddo
     end subroutine new
+
+    subroutine init_tab( self, pinds )
+        class(regularizer), intent(inout) :: self
+        integer,            intent(in)    :: pinds(params_glob%fromp,params_glob%top)
+        allocate(self%ref_ptcl_corr(params_glob%fromp:params_glob%top,self%nrefs),self%ref_ptcl_sh(2,params_glob%fromp:params_glob%top,self%nrefs), source=0.)
+        allocate(self%ref_ptcl_loc( params_glob%fromp:params_glob%top,self%nrefs), self%ref_ptcl_ind(params_glob%fromp:params_glob%top,self%nrefs), source=0)
+    end subroutine init_tab
+
+    ! filling prob/corr 2D table
+    subroutine fill_tab( self, glob_pinds )
+        class(regularizer), intent(inout) :: self
+        integer,            intent(in)    :: glob_pinds(self%pftcc%nptcls)
+        integer  :: i, iref, iptcl, ithr
+        real     :: inpl_corrs(self%nrots), cxy(3)
+        !$omp parallel do collapse(2) default(shared) private(i,iref,ithr,iptcl,inpl_corrs,cxy) proc_bind(close) schedule(static)
+        do iref = 1, self%nrefs
+            do i = 1, self%pftcc%nptcls
+                ithr  = omp_get_thread_num() + 1
+                iptcl = glob_pinds(i)
+                ! find best irot/shift for this pair of iref, iptcl
+                call self%pftcc%gencorrs( iref, iptcl, inpl_corrs )
+                self%ref_ptcl_loc(iptcl,iref) = maxloc(inpl_corrs, dim=1)
+                call self%grad_shsrch_obj(ithr)%set_indices(iref, iptcl)
+                cxy = self%grad_shsrch_obj(ithr)%minimize(irot=self%ref_ptcl_loc(iptcl,iref))
+                if( self%ref_ptcl_loc(iptcl,iref) > 0 )then
+                    self%ref_ptcl_corr(iptcl,iref) = cxy(1)
+                    self%ref_ptcl_sh(:,iptcl,iref) = cxy(2:3)
+                else
+                    self%ref_ptcl_loc(iptcl,iref)  = maxloc(inpl_corrs, dim=1)
+                    self%ref_ptcl_corr(iptcl,iref) = inpl_corrs(self%ref_ptcl_loc(iptcl,iref))
+                    self%ref_ptcl_sh(:,iptcl,iref) = 0.
+                endif
+            enddo
+        enddo
+        !$omp end parallel do
+    end subroutine fill_tab
+
+    subroutine sort_tab( self )
+        class(regularizer), intent(inout) :: self
+        integer :: iref, j
+        !$omp parallel do default(shared) proc_bind(close) schedule(static) private(iref,j)
+        do iref = 1, self%nrefs
+            ! finding the sample space, based on the sorted correlations
+            self%ref_ptcl_ind(:,iref) = (/(j,j=params_glob%fromp,params_glob%top)/)
+            call hpsort(self%ref_ptcl_corr(:,iref), self%ref_ptcl_ind(:,iref))
+            call reverse(self%ref_ptcl_ind( :,iref))
+            call reverse(self%ref_ptcl_corr(:,iref))
+        enddo
+        !$omp end parallel do
+    end subroutine sort_tab
+
+    ! accumulating reference reg terms for each batch of particles, with cc-based global objfunc
+    subroutine ref_reg_cc_tab( self, glob_pinds )
+        class(regularizer), intent(inout) :: self
+        integer,            intent(in)    :: glob_pinds(self%pftcc%nptcls)
+        complex(sp),        pointer       :: shmat(:,:)
+        integer  :: i, iptcl, iref, ithr, ninds, loc, pind_here
+        real     :: ptcl_ctf(self%pftsz,self%kfromto(1):self%kfromto(2),self%pftcc%nptcls), weight
+        real(dp) :: ctf_rot(self%pftsz,self%kfromto(1):self%kfromto(2)),&
+              &ptcl_ctf_rot(self%pftsz,self%kfromto(1):self%kfromto(2))
+        ptcl_ctf = real(self%pftcc%pfts_ptcls * self%pftcc%ctfmats)
+        ninds    = size(self%ref_ptcl_corr, 1)
+        !$omp parallel do default(shared) proc_bind(close) schedule(static) collapse(2)&
+        !$omp private(iref,ithr,i,iptcl,loc,ptcl_ctf_rot,ctf_rot,shmat,pind_here,weight)
+        do iref = 1, self%nrefs
+            ! taking top sorted corrs
+            do i = params_glob%fromp,(params_glob%fromp + int(ninds / self%nrefs))
+                ithr  = omp_get_thread_num() + 1
+                iptcl = self%ref_ptcl_ind(i, iref)
+                if( iptcl >= self%pftcc%pfromto(1) .and. iptcl <= self%pftcc%pfromto(2))then
+                    pind_here = self%pftcc%pinds(iptcl)
+                    if( self%ref_ptcl_corr(i, iref) < TINY ) cycle
+                    ! computing the reg terms as the gradients w.r.t 2D references of the probability
+                    loc = self%ref_ptcl_loc(iptcl, iref)
+                    loc = (self%nrots+1)-(loc-1)
+                    if( loc > self%nrots ) loc = loc - self%nrots
+                    shmat => self%pftcc%heap_vars(ithr)%shmat
+                    call self%pftcc%gen_shmat(ithr, real(self%ref_ptcl_sh(:,iptcl,iref)), shmat)
+                    call self%rotate_polar(real(ptcl_ctf(:,:,pind_here) * shmat), ptcl_ctf_rot, loc)
+                    call self%rotate_polar(self%pftcc%ctfmats(:,:,pind_here),          ctf_rot, loc)
+                    weight = self%ref_ptcl_corr(i, iref)
+                    self%regs(:,:,iref)       = self%regs(:,:,iref)       + weight * ptcl_ctf_rot
+                    self%regs_denom(:,:,iref) = self%regs_denom(:,:,iref) + weight * ctf_rot**2
+                    self%ref_corr(iref)       = self%ref_corr(iref)       + self%ref_ptcl_corr(i, iref)
+                endif
+            enddo
+        enddo
+        !$omp end parallel do
+    end subroutine ref_reg_cc_tab
 
     ! accumulating reference reg terms for each batch of particles, with cc-based global objfunc
     subroutine ref_reg_cc( self, eulspace, ptcl_eulspace, glob_pinds )
@@ -137,14 +234,14 @@ contains
         real,               parameter     :: SAMPLE_FRAC = 1.   ! frac of sample space used for the reg term
         integer  :: i, j, iind, iref, iptcl, n_samples, inpl_ind(self%nrots), cnt, ithr
         real     :: inpl_corrs(self%nrots), ptcl_ctf(self%pftsz,self%kfromto(1):self%kfromto(2),self%pftcc%nptcls)
-        real     :: cxy(3), weight, min_w, max_w
+        real     :: cxy(3), weight
         real(dp) :: ctf_rot(self%pftsz,self%kfromto(1):self%kfromto(2)),&
               &ptcl_ctf_rot(self%pftsz,self%kfromto(1):self%kfromto(2))
         n_samples = self%pftcc%nptcls * N_INPLS
         allocate(ptcl_corr(n_samples),loc(n_samples),ptcl_ind(n_samples),init_xy(2,n_samples))
         ptcl_ctf = real(self%pftcc%pfts_ptcls * self%pftcc%ctfmats)
         !$omp parallel do default(shared) proc_bind(close) schedule(static)&
-        !$omp private(i,j,iind,iref,iptcl,inpl_corrs,loc,ptcl_ctf_rot,ctf_rot,min_w,max_w,&
+        !$omp private(i,j,iind,iref,iptcl,inpl_corrs,loc,ptcl_ctf_rot,ctf_rot,&
         !$omp         ptcl_corr,init_xy,ithr,shmat,cxy,sample_ind,inpl_ind,ptcl_ind,cnt,weight)
         do iref = 1, self%nrefs
             ithr = omp_get_thread_num() + 1
@@ -157,6 +254,7 @@ contains
                 inpl_ind = (/(j,j=1,self%nrots)/)
                 call hpsort(inpl_corrs, inpl_ind)
                 call reverse(inpl_ind)
+                call reverse(inpl_corrs)
                 do j = 1, N_INPLS
                     ptcl_ind(cnt) = i
                     loc(cnt)      = inpl_ind(j)
@@ -167,7 +265,7 @@ contains
                         init_xy(:,cnt) = cxy(2:3)
                     else
                         loc(cnt)       = inpl_ind(j)
-                        ptcl_corr(cnt) = inpl_corrs(loc(cnt))
+                        ptcl_corr(cnt) = inpl_corrs(j)
                         init_xy(:,cnt) = 0.
                     endif
                     cnt = cnt + 1
@@ -177,26 +275,22 @@ contains
             sample_ind = (/(j,j=1,n_samples)/)
             call hpsort(ptcl_corr, sample_ind)
             call reverse(sample_ind)
-            min_w = minval(ptcl_corr)
-            max_w = maxval(ptcl_corr)
+            call reverse(ptcl_corr)
             ! constructing the cavgs/2D-gradient
-            do i = 1, int(n_samples * SAMPLE_FRAC)
+            do i = 1, int(self%pftcc%nptcls / self%nrefs)
                 iind = sample_ind(i)
-                if( ptcl_corr(iind) < TINY ) cycle
-                weight = (ptcl_corr(iind) - min_w)/(max_w - min_w)
-                weight = 0.5 + real(params_glob%which_iter)/real(params_glob%reg_iters)*(weight - 0.5)
-                if( ran3() < weight )then
-                    ! computing the reg terms as the gradients w.r.t 2D references of the probability
-                    loc(iind) = (self%nrots+1)-(loc(iind)-1)
-                    if( loc(iind) > self%nrots ) loc(iind) = loc(iind) - self%nrots
-                    shmat => self%pftcc%heap_vars(ithr)%shmat
-                    call self%pftcc%gen_shmat(ithr, real(init_xy(:,iind)), shmat)
-                    call self%rotate_polar(real(ptcl_ctf(:,:,ptcl_ind(iind)) * shmat), ptcl_ctf_rot, loc(iind))
-                    call self%rotate_polar(self%pftcc%ctfmats(:,:,ptcl_ind(iind)),          ctf_rot, loc(iind))
-                    self%regs(:,:,iref)       = self%regs(:,:,iref)       + weight * ptcl_ctf_rot
-                    self%regs_denom(:,:,iref) = self%regs_denom(:,:,iref) + weight * ctf_rot**2
-                    self%ref_corr(iref)       = self%ref_corr(iref)       + ptcl_corr(iind)
-                endif
+                if( ptcl_corr(i) < TINY ) cycle
+                ! computing the reg terms as the gradients w.r.t 2D references of the probability
+                loc(iind) = (self%nrots+1)-(loc(iind)-1)
+                if( loc(iind) > self%nrots ) loc(iind) = loc(iind) - self%nrots
+                shmat => self%pftcc%heap_vars(ithr)%shmat
+                call self%pftcc%gen_shmat(ithr, real(init_xy(:,iind)), shmat)
+                call self%rotate_polar(real(ptcl_ctf(:,:,ptcl_ind(iind)) * shmat), ptcl_ctf_rot, loc(iind))
+                call self%rotate_polar(self%pftcc%ctfmats(:,:,ptcl_ind(iind)),          ctf_rot, loc(iind))
+                weight = ptcl_corr(i)
+                self%regs(:,:,iref)       = self%regs(:,:,iref)       + weight * ptcl_ctf_rot
+                self%regs_denom(:,:,iref) = self%regs_denom(:,:,iref) + weight * ctf_rot**2
+                self%ref_corr(iref)       = self%ref_corr(iref)       + ptcl_corr(i)
             enddo
         enddo
         !$omp end parallel do
@@ -209,17 +303,17 @@ contains
         integer,            intent(in)    :: glob_pinds(self%pftcc%nptcls)
         integer,            allocatable   :: loc(:), sample_ind(:), ptcl_ind(:)
         real,               allocatable   :: ptcl_corr(:)
-        integer,            parameter     :: N_INPLS     = 3    ! number of inpl samples
-        real,               parameter     :: SAMPLE_FRAC = 0.2  ! frac of sample space used for the reg term
+        integer,            parameter     :: N_INPLS     = 1  ! number of inpl samples
+        real,               parameter     :: SAMPLE_FRAC = 1  ! frac of sample space used for the reg term
         integer  :: i, j, iind, iref, iptcl, n_samples, inpl_ind(self%nrots), cnt
-        real     :: inpl_corrs(self%nrots), ptcl_ctf(self%pftsz,self%kfromto(1):self%kfromto(2),self%pftcc%nptcls)
+        real     :: inpl_corrs(self%nrots), ptcl_ctf(self%pftsz,self%kfromto(1):self%kfromto(2),self%pftcc%nptcls),weight,min_w,max_w
         real(dp) :: ctf_rot(self%pftsz,self%kfromto(1):self%kfromto(2)),&
               &ptcl_ctf_rot(self%pftsz,self%kfromto(1):self%kfromto(2))
         n_samples = self%pftcc%nptcls * N_INPLS
         allocate(ptcl_corr(n_samples),loc(n_samples),ptcl_ind(n_samples))
         ptcl_ctf = real(self%pftcc%pfts_ptcls * self%pftcc%ctfmats)
         !$omp parallel do default(shared) proc_bind(close) schedule(static)&
-        !$omp private(i,j,iind,iref,iptcl,inpl_corrs,loc,ptcl_ctf_rot,ctf_rot,ptcl_corr,sample_ind,inpl_ind,ptcl_ind,cnt)
+        !$omp private(i,j,iind,iref,iptcl,inpl_corrs,loc,ptcl_ctf_rot,ctf_rot,ptcl_corr,sample_ind,inpl_ind,ptcl_ind,cnt,weight,min_w,max_w)
         do iref = 1, self%nrefs
             ! computing correlations
             cnt = 1
@@ -230,10 +324,11 @@ contains
                 inpl_ind = (/(j,j=1,self%nrots)/)
                 call hpsort(inpl_corrs, inpl_ind)
                 call reverse(inpl_ind)
+                call reverse(inpl_corrs)
                 do j = 1, N_INPLS
-                    ptcl_ind(cnt) = i
-                    loc(cnt)      = inpl_ind(j)
-                    ptcl_corr(cnt) = inpl_corrs(loc(cnt))
+                    ptcl_ind(cnt)  = i
+                    loc(cnt)       = inpl_ind(j)
+                    ptcl_corr(cnt) = inpl_corrs(j)
                     cnt = cnt + 1
                 enddo
             enddo
@@ -241,18 +336,25 @@ contains
             sample_ind = (/(j,j=1,n_samples)/)
             call hpsort(ptcl_corr, sample_ind)
             call reverse(sample_ind)
+            call reverse(ptcl_corr)
+            min_w = minval(ptcl_corr)
+            max_w = maxval(ptcl_corr)
             ! constructing the cavgs/2D-gradient
             do i = 1, int(n_samples * SAMPLE_FRAC)
                 iind = sample_ind(i)
-                if( ptcl_corr(iind) < TINY ) cycle
-                ! computing the reg terms as the gradients w.r.t 2D references of the probability
-                loc(iind) = (self%nrots+1)-(loc(iind)-1)
-                if( loc(iind) > self%nrots ) loc(iind) = loc(iind) - self%nrots
-                call self%rotate_polar(real(ptcl_ctf(     :,:,ptcl_ind(iind))), ptcl_ctf_rot, loc(iind))
-                call self%rotate_polar(self%pftcc%ctfmats(:,:,ptcl_ind(iind)),       ctf_rot, loc(iind))
-                self%regs(:,:,iref)       = self%regs(:,:,iref)       + ptcl_corr(iind) * ptcl_ctf_rot
-                self%regs_denom(:,:,iref) = self%regs_denom(:,:,iref) + ptcl_corr(iind) * ctf_rot**2
-                self%ref_corr(iref)       = self%ref_corr(iref)       + ptcl_corr(iind)
+                if( ptcl_corr(i) < TINY ) cycle
+                weight = (ptcl_corr(i) - min_w)/(max_w - min_w)
+                weight = 0.5 + real(params_glob%which_iter)/real(params_glob%reg_iters)*(weight - 0.5)
+                if( ran3() < weight )then
+                    ! computing the reg terms as the gradients w.r.t 2D references of the probability
+                    loc(iind) = (self%nrots+1)-(loc(iind)-1)
+                    if( loc(iind) > self%nrots ) loc(iind) = loc(iind) - self%nrots
+                    call self%rotate_polar(real(ptcl_ctf(     :,:,ptcl_ind(iind))), ptcl_ctf_rot, loc(iind))
+                    call self%rotate_polar(self%pftcc%ctfmats(:,:,ptcl_ind(iind)),       ctf_rot, loc(iind))
+                    self%regs(:,:,iref)       = self%regs(:,:,iref)       + ptcl_ctf_rot
+                    self%regs_denom(:,:,iref) = self%regs_denom(:,:,iref) + ctf_rot**2
+                    self%ref_corr(iref)       = self%ref_corr(iref)       + ptcl_corr(i)
+                endif
             enddo
         enddo
         !$omp end parallel do
@@ -428,5 +530,6 @@ contains
     subroutine kill( self )
         class(regularizer), intent(inout) :: self
         deallocate(self%regs,self%regs_denom,self%grad_shsrch_obj,self%ref_corr)
+        if(allocated(self%ref_ptcl_corr)) deallocate(self%ref_ptcl_corr,self%ref_ptcl_loc,self%ref_ptcl_sh,self%ref_ptcl_ind)
     end subroutine kill
 end module simple_regularizer
