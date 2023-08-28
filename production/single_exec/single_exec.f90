4bca60721193111ade44e4f2d4d5680ffa659bb8
cong <vanc2@nih.gov>
2023-08-28 13:18:59 -0400

    adding a commander to check the 3D alignment given a ground truth

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -161,7 +161,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('834df0cf')
+call simple_print_git_version('02648750')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
