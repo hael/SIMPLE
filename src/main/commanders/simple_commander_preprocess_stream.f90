72d5e1df7375ad160ef226aa21bea0b20faae03b
cyrilr <cyril.reboul@gmail.com>
2023-11-14 17:07:01 +1100

    Edits to streaming class rejection

--- src/main/commanders/simple_commander_preprocess_stream.f90
+++ src/main/commanders/simple_commander_preprocess_stream.f90
@@ -95,7 +95,7 @@ contains
         ! 2D classification
         call cline%set('wiener', 'full')
         if( .not. cline%defined('lpthres')     ) call cline%set('lpthres',       30.0)
-        if( .not. cline%defined('ndev2D')      ) call cline%set('ndev2D',         1.5)
+        if( .not. cline%defined('ndev2D')      ) call cline%set('ndev2D', CLS_REJECT_STD)
         if( .not. cline%defined('autoscale')   ) call cline%set('autoscale',    'yes')
         if( .not. cline%defined('nonuniform')  ) call cline%set('nonuniform',    'no')
         if( .not. cline%defined('nparts_chunk')) call cline%set('nparts_chunk',   1.0)
