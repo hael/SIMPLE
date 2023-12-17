b3a1b8dc16dca4ccdc57ef1b68fbc0a21c779d9d
Hans Elmlund <hans.elmlund@nih.gov>
2023-12-17 18:11:52 -0500

    support for reference-based picking in pickgau

--- src/utils/simple_pickgau.f90
+++ src/utils/simple_pickgau.f90
@@ -6,15 +6,14 @@ use simple_parameters, only: params_glob
 use simple_image,      only: image
 implicit none
 
-public :: pickgau
+public :: read_mic_raw, pickgau
 private
 #include "simple_local_flags.inc"
 
 ! class constants
-real,    parameter :: GAUSIG      = 5., BOX_EXP_FAC = 0.111
-integer, parameter :: OFFSET      = 3 , OFFSET_UB   = 2 * OFFSET
-real,    parameter :: DIST_THRES1 = real(OFFSET), DIST_THRES2 = real(4*OFFSET)
-logical, parameter :: L_WRITE     = .false.
+real,    parameter :: GAUSIG   = 5., BOX_EXP_FAC = 0.111
+integer, parameter :: OFFSET   = 3 , OFFSET_UB   = 2 * OFFSET, MAXNREFS = 100
+logical, parameter :: L_WRITE  = .false.
 
 ! class variables
 integer     :: ldim_raw(3), ldim_raw_box(3)
@@ -25,23 +24,32 @@ type(image) :: mic_raw
 type pickgau
     private
     real                     :: smpd_shrink = 0., maxdiam = 0., sig = 0., sxx = 0., t = 0.
-    integer                  :: ldim(3), ldim_box(3), nboxes = 0, nboxes_ub = 0
-    integer                  :: nx = 0, ny = 0, nx_offset = 0, ny_offset = 0, npeaks = 0
+    integer                  :: ldim(3), ldim_box(3), nboxes = 0, nboxes_ub = 0, nx = 0, ny = 0
+    integer                  :: nx_offset = 0, ny_offset = 0, npeaks = 0, nrefs = 0
     type(image)              :: mic_shrink, gauref
-    type(image), allocatable :: boximgs(:)
-    logical,     allocatable :: l_mic_mask(:,:)
+    type(image), allocatable :: boximgs(:), boxrefs(:)
+    logical,     allocatable :: l_mic_mask(:,:), l_err_refs(:)
     integer,     allocatable :: positions(:,:), inds_offset(:,:)
     real,        allocatable :: box_scores(:,:)
-    logical                  :: exists = .false.
+    logical                  :: refpick = .false.
+    logical                  :: exists  = .false.
 contains
-    procedure :: new
-    procedure :: extract_boximgs
-    procedure :: destruct_boximgs
-    procedure :: gaumatch_boximgs
-    procedure :: detect_peaks
-    procedure :: center_filter
-    procedure :: distance_filter
-    procedure :: kill
+    procedure          :: new_gaupicker
+    procedure          :: new_refpicker
+    procedure, private :: new
+    procedure, private :: set_refs
+    procedure, private :: setup_iterators
+    ! procedure          :: gaupick
+    ! procedure          :: refpick
+    procedure, private :: extract_boximgs
+    procedure, private :: destruct_boximgs
+    procedure, private :: gaumatch_boximgs
+    procedure, private :: refmatch_boximgs
+    procedure, private :: detect_peaks
+    procedure, private :: center_filter
+    procedure, private :: distance_filter
+    procedure          :: scale_positions
+    procedure          :: kill
 end type
 
 contains
@@ -57,14 +65,33 @@ contains
         call mic_raw%read(micname)
     end subroutine read_mic_raw
 
-    subroutine new( self, pcontrast, smpd_shrink, moldiam, moldiam_max )
+    subroutine new_gaupicker( self, pcontrast, smpd_shrink, moldiam, moldiam_max )
         class(pickgau),   intent(inout) :: self
         character(len=*), intent(in)    :: pcontrast
         real,             intent(in)    :: smpd_shrink, moldiam, moldiam_max
+        call self%new( pcontrast, smpd_shrink, moldiam, moldiam_max )
+    end subroutine new_gaupicker
+
+    subroutine new_refpicker( self, pcontrast, smpd_shrink, mskdiam, imgs )
+        class(pickgau),   intent(inout) :: self
+        character(len=*), intent(in)    :: pcontrast
+        real,             intent(in)    :: smpd_shrink, mskdiam
+        class(image),     intent(inout) :: imgs(:)
+        call self%new( pcontrast, smpd_shrink, mskdiam, mskdiam, mskdiam )
+        call self%set_refs( imgs, mskdiam )
+        call self%setup_iterators
+    end subroutine new_refpicker
+
+    subroutine new( self, pcontrast, smpd_shrink, moldiam, moldiam_max, mskdiam )
+        class(pickgau),   intent(inout) :: self
+        character(len=*), intent(in)    :: pcontrast
+        real,             intent(in)    :: smpd_shrink, moldiam, moldiam_max
+        real, optional,   intent(in)    :: mskdiam ! reference-based picking if present
         character(len=:), allocatable   :: numstr
-        integer     :: ldim_pd(3), box_max, xind, yind
+        integer     :: ldim_pd(3), box_max
         type(image) :: mic_pad, gauimg
         real        :: hpfreq, scale, maxdiam_max
+        if( self%exists ) call self%kill
         self%smpd_shrink = smpd_shrink
         if( trim(params_glob%pick_roi).eq.'yes' )then
             ! high-pass micrograph
@@ -88,7 +115,11 @@ contains
         self%ldim(2)     = round2even(real(ldim_raw(2)) * scale)
         self%ldim(3)     = 1
         ! set logical dimensions of boxes
-        self%maxdiam     = moldiam + moldiam * BOX_EXP_FAC
+        if( present(mskdiam) )then ! reference-based picking
+            self%maxdiam = mskdiam
+        else
+            self%maxdiam = moldiam + moldiam * BOX_EXP_FAC
+        endif
         ldim_raw_box(1)  = round2even(self%maxdiam / smpd_raw)
         ldim_raw_box(2)  = ldim_raw_box(1)
         ldim_raw_box(3)  = 1
@@ -96,7 +127,11 @@ contains
         self%ldim_box(2) = self%ldim_box(1)
         self%ldim_box(3) = 1
         ! set # pixels in x/y based on maximum expected box size
-        maxdiam_max      = moldiam_max + moldiam_max * BOX_EXP_FAC
+        if( present(mskdiam) )then ! reference-based picking
+            maxdiam_max  = mskdiam
+        else
+            maxdiam_max  = moldiam_max + moldiam_max * BOX_EXP_FAC
+        endif
         box_max          = round2even(maxdiam_max / self%smpd_shrink)
         self%nx          = self%ldim(1) - box_max
         self%ny          = self%ldim(2) - box_max
@@ -124,12 +159,122 @@ contains
         call mic_raw%ifft
         call self%mic_shrink%ifft
         if( L_WRITE )then
-            numstr = int2str(nint(moldiam))
-            call self%mic_shrink%write('mic_shrink_moldiam'//numstr//'.mrc')
-            call self%gauref%write('gauref_moldiam'//numstr//'.mrc')
+            if( present(mskdiam) )then ! reference-based picking
+                numstr = int2str(nint(mskdiam))
+                call self%mic_shrink%write('mic_shrink_mskdiam'//numstr//'.mrc')
+                call self%gauref%write('gauref_mskdiam'//numstr//'.mrc')
+            else
+                numstr = int2str(nint(moldiam))
+                call self%mic_shrink%write('mic_shrink_moldiam'//numstr//'.mrc')
+                call self%gauref%write('gauref_moldiam'//numstr//'.mrc')
+            endif
         endif
+        ! denoise mic_shrink
+        call gauimg%new(self%ldim, self%smpd_shrink)
+        call gauimg%gauimg2D(self%sig,self%sig)
+        call gauimg%fft
+        call self%mic_shrink%fft
+        call self%mic_shrink%mul(gauimg)
+        call self%mic_shrink%ifft
+        call gauimg%kill
+        if( L_WRITE )then
+            if( present(mskdiam) )then ! reference-based picking
+                call self%mic_shrink%write('gauconv_mic_shrink_mskdiam'//numstr//'.mrc')
+            else
+                call self%mic_shrink%write('gauconv_mic_shrink_moldiam'//numstr//'.mrc')
+            endif
+        endif
+        if( present(mskdiam) )then ! reference-based picking
+            ! iterators are established after calling set_refs because ldim_box, nx & ny are updated
+        else
+            ! establish iterators
+            call self%setup_iterators
+        endif
+        ! flag existence
+        self%exists = .true.
+    end subroutine new
+
+    subroutine set_refs( self, imgs, mskdiam )
+        class(pickgau), intent(inout) :: self
+        class(image),   intent(inout) :: imgs(:)
+        real,           intent(in)    :: mskdiam
+        character(len=:), allocatable :: numstr
+        type(image) :: img_rot
+        integer     :: ldim(3), iimg, nimgs, irot, nrots, cnt
+        real        :: scale, mskrad, pixrad_shrink1, pixrad_shrink2, smpd, ang, rot
+        smpd       = imgs(1)%get_smpd()
+        if( abs(smpd - smpd_raw) > 0.01 ) THROW_HARD('raw micrograph and picking references of different scale')
+        ldim       = imgs(1)%get_ldim()
+        if( ldim(3) /= 1 ) THROW_HARD('box references must be 2D')
+        nimgs      = size(imgs)
+        nrots      = nint(real(MAXNREFS) / real(nimgs))
+        self%nrefs = nimgs * nrots
+        ! set shrunken logical dimensions of boxes
+        scale            = smpd / self%smpd_shrink
+        self%ldim_box(1) = round2even(real(ldim(1)) * scale)
+        self%ldim_box(2) = round2even(real(ldim(2)) * scale)
+        self%ldim_box(3) = 1
+        ! set # pixels in x/y for both box sizes
+        self%nx          = self%ldim(1) - self%ldim_box(1)
+        self%ny          = self%ldim(2) - self%ldim_box(2)
+        mskrad           = (mskdiam / self%smpd_shrink) / 2.
+        ! set Gaussian
+        call self%gauref%new(self%ldim_box, self%smpd_shrink)
+        call self%gauref%gauimg2D(self%sig, self%sig)
+        call self%gauref%prenorm4real_corr(self%sxx)
+        if( L_WRITE )then
+            numstr = int2str(nint(mskdiam))
+            call self%gauref%write('gauref_mskdiam'//numstr//'.mrc')
+        endif
+        if( allocated(self%boxrefs) )then
+            do iimg = 1,size(self%boxrefs)
+                call self%boxrefs(iimg)%kill
+            end do
+            deallocate(self%boxrefs)
+        endif
+        if( allocated(self%l_err_refs) ) deallocate(self%l_err_refs)
+        allocate(self%l_err_refs(self%nrefs))
+        call img_rot%new(ldim, smpd)
+        ang = 360./real(nrots)
+        cnt = 0
+        do iimg = 1,nimgs
+            rot = 0.
+            do irot = 1,nrots
+                cnt = cnt + 1
+                call imgs(iimg)%rtsq(rot, 0., 0., img_rot)
+                call img_rot%fft
+                call self%boxrefs(cnt)%new(self%ldim_box, self%smpd_shrink, wthreads=.false.)
+                call self%boxrefs(cnt)%set_ft(.true.)
+                call img_rot%clip(self%boxrefs(cnt))
+                rot = rot + ang
+            end do
+        end do
+        !$omp parallel do schedule(static) default(shared) private(cnt) proc_bind(close)
+        do cnt = 1,self%nrefs
+            ! convolve with Gaussian
+            call self%boxrefs(cnt)%mul(self%gauref)
+            ! back to real-space
+            call self%boxrefs(cnt)%ifft
+            call self%boxrefs(cnt)%mask(mskrad, 'hard')
+            call self%boxrefs(cnt)%prenorm4real_corr(self%l_err_refs(cnt))
+        end do
+        !$omp end parallel do
+        if( L_WRITE )then
+            do cnt = 1,self%nrefs
+                call self%boxrefs(cnt)%write('boxrefs_shrink.mrc', cnt)
+            enddo
+        endif
+        call self%gauref%ifft
+        call img_rot%kill
+        self%refpick = .true.
+    end subroutine set_refs
+
+    subroutine setup_iterators( self )
+        class(pickgau), intent(inout) :: self
+        integer :: xind, yind
         ! allocate and set l_mic_mask
-        allocate(self%l_mic_mask(self%ldim(1),self%ldim(2)), source = .true.)
+        if( allocated(self%l_mic_mask) ) deallocate(self%l_mic_mask)
+        allocate(self%l_mic_mask(self%ldim(1),self%ldim(2)), source=.true.)
         self%nboxes    = 0
         self%nx_offset = 0
         do xind = 0,self%nx,OFFSET
@@ -149,6 +294,8 @@ contains
             end do
         end do
         ! allocate and set positions and inds_offset
+        if( allocated(self%positions)   ) deallocate(self%positions)
+        if( allocated(self%inds_offset) ) deallocate(self%inds_offset)
         allocate(self%positions(self%nboxes,2), self%inds_offset(self%nx_offset,self%ny_offset), source=0)
         ! calculate total # boxes
         self%nboxes    = 0
@@ -166,20 +313,9 @@ contains
             end do
         end do
         ! allocate box_scores
+        if( allocated(self%box_scores) ) deallocate(self%box_scores)
         allocate(self%box_scores(self%nx_offset,self%ny_offset), source = -1.)
-        ! denoise mic_shrink
-        call gauimg%new(self%ldim, self%smpd_shrink)
-        call gauimg%gauimg2D(self%sig,self%sig)
-        call gauimg%fft
-        call self%mic_shrink%fft
-        call self%mic_shrink%mul(gauimg)
-        call self%mic_shrink%ifft
-        if( L_WRITE )then
-            call self%mic_shrink%write('gauconv_mic_shrink_moldiam'//numstr//'.mrc')
-        endif
-        call gauimg%kill
-        self%exists = .true.
-    end subroutine new
+    end subroutine setup_iterators
 
     subroutine extract_boximgs( self )
         class(pickgau), intent(inout) :: self
@@ -245,6 +381,54 @@ contains
         end do
     end subroutine gaumatch_boximgs
 
+    subroutine refmatch_boximgs( self )
+        class(pickgau), intent(inout) :: self
+        logical     :: outside, l_err
+        integer     :: pos(2), ioff, joff, ithr, iref
+        real        :: scores(self%nrefs)
+        type(image) :: boximgs_heap(nthr_glob)
+        ! construct heap
+        do ithr = 1,nthr_glob
+            call boximgs_heap(ithr)%new(self%ldim_box, self%smpd_shrink)
+        end do
+        ! calculate box_scores
+        if( self%refpick )then
+            !$omp parallel do schedule(static) collapse(2) default(shared) private(ioff,joff,ithr,outside,iref,scores,pos,l_err) proc_bind(close)
+            do ioff = 1,self%nx_offset
+                do joff = 1,self%ny_offset
+                    if( self%inds_offset(ioff,joff) == 0 )then
+                        self%box_scores(ioff,joff) = -1.
+                    else
+                        pos = self%positions(self%inds_offset(ioff,joff),:)
+                        if( self%l_mic_mask(pos(1)+1,pos(2)+1) )then
+                            ithr = omp_get_thread_num() + 1
+                            pos  = self%positions(self%inds_offset(ioff,joff),:)
+                            call self%mic_shrink%window_slim(pos, self%ldim_box(1), boximgs_heap(ithr), outside)
+                            call boximgs_heap(ithr)%prenorm4real_corr(l_err)
+                            if( l_err )then
+                                self%box_scores(ioff,joff) = 0.
+                            else
+                                do iref = 1,self%nrefs
+                                    if( self%l_err_refs(iref) )then
+                                        scores(iref) = 0.
+                                    else
+                                        scores(iref) = self%boxrefs(iref)%real_corr_prenorm(boximgs_heap(ithr))
+                                    endif
+                                end do
+                                self%box_scores(ioff,joff) = maxval(scores)
+                            endif
+                        else
+                            self%box_scores(ioff,joff) = -1.
+                        endif
+                    endif
+                end do
+            end do
+            !$omp end parallel do
+        else
+            THROW_HARD('Instance not setup for reference-based picking')
+        endif
+    end subroutine refmatch_boximgs
+
     subroutine detect_peaks( self )
         class(pickgau), intent(inout) :: self
         real, allocatable :: tmp(:)
@@ -259,7 +443,7 @@ contains
             self%box_scores = -1.
         end where
         self%npeaks = count(self%box_scores >= self%t)
-        write(logfhandle,'(a,1x,I5)') '# peaks detected:                    ', self%npeaks
+        write(logfhandle,'(a,1x,I5)') '# peaks detected:                      ', self%npeaks
     end subroutine detect_peaks
 
      subroutine center_filter( self )
@@ -292,7 +476,7 @@ contains
             self%box_scores = -1.
         endwhere
         self%npeaks = count(self%box_scores >= self%t)
-        write(logfhandle,'(a,1x,I5)') '# npeaks    after    center filtering: ', self%npeaks
+        write(logfhandle,'(a,1x,I5)') '# npeaks    after    center filtering: ', self%npeaks 
     end subroutine center_filter
 
     subroutine distance_filter( self, dist_thres )
@@ -350,17 +534,41 @@ contains
         write(logfhandle,'(a,1x,I5)') '# positions after updating box_scores: ', npeaks
     end subroutine distance_filter
 
+    subroutine scale_positions( self, smpd_new, positions )
+        class(pickgau),       intent(inout) :: self
+        real,                 intent(in)    :: smpd_new
+        integer, allocatable, intent(inout) :: positions(:,:)
+        integer, allocatable :: pos_inds(:)
+        integer :: nbox, ibox
+        pos_inds = pack(self%inds_offset(:,:), mask=self%box_scores(:,:) >= self%t)
+        nbox     = size(pos_inds)
+        if( allocated(positions) ) deallocate(positions)
+        allocate( positions(nbox,2), source=0 )
+        do ibox = 1,nbox
+            positions(ibox,:) = nint((self%smpd_shrink/smpd_new) * real(self%positions(pos_inds(ibox),:)))
+        end do
+    end subroutine scale_positions
+
     subroutine kill( self )
         class(pickgau), intent(inout) :: self
+        integer :: iimg
         if( self%exists )then
             call self%mic_shrink%kill
             call self%gauref%kill
             call self%destruct_boximgs
             if( allocated(self%l_mic_mask)  ) deallocate(self%l_mic_mask)
+            if( allocated(self%l_err_refs)  ) deallocate(self%l_err_refs)
             if( allocated(self%positions)   ) deallocate(self%positions)
             if( allocated(self%inds_offset) ) deallocate(self%inds_offset)
             if( allocated(self%box_scores)  ) deallocate(self%box_scores)
-            self%exists = .false.
+            if( allocated(self%boxrefs) )then
+                do iimg = 1,size(self%boxrefs)
+                    call self%boxrefs(iimg)%kill
+                end do
+                deallocate(self%boxrefs)
+            endif
+            self%refpick = .false.
+            self%exists  = .false.
         endif
     end subroutine kill
 
