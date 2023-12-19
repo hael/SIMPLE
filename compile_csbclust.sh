51d9eda08156d68862ab25529ddff0cde7583b89
cyrilr <13562668+cyrilreboul@users.noreply.github.com>
2023-12-20 09:58:23 +1100

    moved csb compilation to gcc12

--- compile_csbclust.sh
+++ compile_csbclust.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 module purge
 module load cmake/3.25.2
-echo "Compiling SIMPLE with gcc11.2.0"
-module load gcc/11.2.0
-rm -r build_gcc11.2.0
-mkdir build_gcc11.2.0
-cd build_gcc11.2.0
+echo "Compiling SIMPLE with gcc12.2.0"
+module load gcc/12.2.0
+rm -r build_gcc12.2.0
+mkdir build_gcc12.2.0
+cd build_gcc12.2.0
 cmake ..
 make -j install
 cd ..
-chmod a+rx,o+rx -R build_gcc11.2.0
-module unload gcc/11.2.0
+chmod a+rx,o+rx -R build_gcc12.2.0
+module unload gcc/12.2.0
 exit
 
