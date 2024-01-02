8d8d4427e6ef9d7942f718e3c2e36ccfaa7dc3e7
Hans Elmlund <hans.elmlund@nih.gov>
2024-01-02 13:02:51 -0500

    work on pickgau

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -382,7 +382,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('12c46d2d')
+call simple_print_git_version('c3350279')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
