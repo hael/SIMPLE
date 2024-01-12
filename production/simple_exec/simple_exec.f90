61511817264c884d8000109c2239c1a86aa2908a
Hans Elmlund <hans.elmlund@nih.gov>
2024-01-12 15:18:54 -0500

    reference-based picker appears to be working

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -382,7 +382,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('382c5a63')
+call simple_print_git_version('3495a0c6')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
