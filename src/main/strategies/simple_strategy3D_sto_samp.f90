c327a65bc44ee143aba33b7bd24c7d6bce89e614
cong <vanc2@nih.gov>
2024-01-26 21:49:20 -0500

    bugfix in sto_samp

--- src/main/strategies/simple_strategy3D_sto_samp.f90
+++ src/main/strategies/simple_strategy3D_sto_samp.f90
@@ -36,7 +36,7 @@ contains
     subroutine srch_sto_samp( self, ithr )
         class(strategy3D_sto_samp), intent(inout) :: self
         integer,                    intent(in)    :: ithr
-        integer :: iref, loc
+        integer :: iref, locs(self%s%nrefs)
         real    :: inpl_corrs(self%s%nrots), ref_corrs(self%s%nrefs)
         ! execute search
         if( build_glob%spproj_field%get_state(self%s%iptcl) > 0 )then
@@ -48,13 +48,13 @@ contains
                 if( s3D%state_exists( s3D%proj_space_state(iref) ) )then
                     ! identify the top scoring in-plane angle
                     call pftcc_glob%gencorrs(iref, self%s%iptcl, inpl_corrs)
-                    loc             = reverse_multinomal(inpl_corrs, int(params_glob%reg_athres * self%s%nrots / 180.))
-                    ref_corrs(iref) = inpl_corrs(loc)
+                    locs(iref)      = reverse_multinomal(inpl_corrs, int(params_glob%reg_athres * self%s%nrots / 180.))
+                    ref_corrs(iref) = inpl_corrs(locs(iref))
                 endif
             enddo
             iref              = reverse_multinomal(ref_corrs, int(params_glob%reg_athres * self%s%nrefs / 180.))
             self%s%nrefs_eval = self%s%nrefs
-            call assign_ori(self%s, iref, loc, ref_corrs(iref))
+            call assign_ori(self%s, iref, locs(iref), ref_corrs(iref))
         else
             call build_glob%spproj_field%reject(self%s%iptcl)
         endif
