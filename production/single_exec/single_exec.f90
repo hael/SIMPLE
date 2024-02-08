d749267f6b90dfb38713c65bc5d445267b884a4b
Hans Elmlund <hans.elmlund@nih.gov>
2024-02-08 08:53:02 -0500

    segpicker tester code

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -164,7 +164,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('e8bc5ff5')
+call simple_print_git_version('52aa2510')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
