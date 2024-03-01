7a6db7eaab92861e6ef6fbd4f8df710536cce978
cong <vanc2@nih.gov>
2024-03-01 14:52:01 -0500

    critical bugfix of latest push

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -398,7 +398,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('07858c00')
+call simple_print_git_version('443074ff')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
