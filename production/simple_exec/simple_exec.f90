2e62db7751855605db6c06e0edab1b5e93225542
Hans Elmlund <hans.elmlund@nih.gov>
2024-03-02 15:13:29 -0500

    subspace sampling refinement mode: smpl_sub

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -387,7 +387,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('7f5a1e8d')
+call simple_print_git_version('0bb8661f')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
