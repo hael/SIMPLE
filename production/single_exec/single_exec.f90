554fbc7dde9a917b4888eb9425d40b4cb30e11ba
hael <hans.elmlund@nih.gov>
2024-03-02 10:00:11 -0500

    simga_est=global by default in abinitio workflow

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -174,7 +174,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('eb4157dd')
+call simple_print_git_version('c0ccb9c5')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
