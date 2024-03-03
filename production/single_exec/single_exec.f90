52d1325d9ff0b1648b16867a6d7378f9fa34a1b2
hael <hans.elmlund@nih.gov>
2024-03-02 23:40:55 -0500

    bugfix

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -174,7 +174,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('0bb8661f')
+call simple_print_git_version('a5742bc5')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
