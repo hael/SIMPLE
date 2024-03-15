fb3fa4eda90cfe0c534fe32297ef3c5ca28c01db
cong <vanc2@nih.gov>
2024-03-15 02:59:16 -0400

    bugfix of latest push

--- src/utils/simple_eul_prob_tab.f90
+++ src/utils/simple_eul_prob_tab.f90
@@ -46,8 +46,10 @@ contains
         real    :: x
         call self%kill
         self%nptcls = size(pinds)
-        print *, 'nptcls = ', self%nptcls
-        print *, 'pinds  = ', pinds
+        if( DEBUG )then
+            print *, 'nptcls = ', self%nptcls
+            print *, 'pinds  = ', pinds
+        endif
         allocate(self%pinds(self%nptcls), source=pinds)
         allocate(self%loc_tab(params_glob%nspace,self%nptcls), self%assgn_map(self%nptcls))
         !$omp parallel do default(shared) private(i,iptcl,iref) proc_bind(close) schedule(static)
