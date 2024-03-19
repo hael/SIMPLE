fbb4382c6eca4d4017ba1c5d6d63ec9b0cde344d
Hans Elmlund <hans.elmlund@nih.gov>
2024-03-19 15:08:01 -0400

    removed reduntant code

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -174,7 +174,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('170b669c')
+call simple_print_git_version('a72a3b0f')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
