6ed5085b7cdf6a5325f56ef3e73c9c02de1bc290
Hans Elmlund <hans.elmlund@nih.gov>
2024-03-19 16:38:58 -0400

    test of inverse decay function for update_frac

--- production/tests/test_extr_frac/simple_test_extr_frac.f90
+++ production/tests/test_extr_frac/simple_test_extr_frac.f90
@@ -2,15 +2,39 @@ program simple_test_extr_frac
 include 'simple_lib.f08'
 implicit none
 
-integer, parameter :: NPTCLS=50000, MAXITS=40
-real,    parameter :: UPDATE_FRAC = 0.1, INITHRESH = 0.8
-integer :: iextr_lim, i
-real    :: anneal_ratio, extr_thresh
+integer, parameter :: NPTCLS    = 2000000 ! two million
+integer, parameter :: NSAMPL_LB = 50000   ! minimum 50k
+integer, parameter :: NSAMPL_UB = 200000  ! maximum 200k
+integer, parameter :: MAXITS    = 40      ! upper iteration bound
+real    :: update_frac
+integer :: nsampl, i
 
-iextr_lim = ceiling(2. * log(real(NPTCLS)) * (2. - UPDATE_FRAC))
-do i=1,MAXITS
-    anneal_ratio      = max(0., cos(PI / 2. * real(i - 1) / real(iextr_lim))) ! cosine decay
-    extr_thresh       = INITHRESH * anneal_ratio                              ! fraction of particles
-    print *, i, extr_thresh
+do i = 1,MAXITS
+    update_frac = inv_frac_decay(i, MAXITS, [NSAMPL_LB,NSAMPL_UB], NPTCLS)
+    nsampl      = nint(update_frac * real(NPTCLS))
+    print *, inv_nsampl_decay(i, MAXITS, [NSAMPL_LB,NSAMPL_UB]), nsampl
 end do
+
+contains
+
+    function inv_frac_decay( i, maxits, nsampl_fromto, nptcls ) result( frac )
+        integer, intent(in) :: i, maxits, nsampl_fromto(2), nptcls
+        real :: frac
+        frac = cos_decay(maxits - i, maxits, real(nsampl_fromto)) / real(nptcls)
+    end function inv_frac_decay
+
+    function inv_nsampl_decay( i, maxits, nsampl_fromto ) result( nsampl )
+        integer, intent(in) :: i, maxits, nsampl_fromto(2)
+        integer :: nsampl
+        nsampl = nint(cos_decay(maxits - i, maxits, real(nsampl_fromto)))
+    end function inv_nsampl_decay
+
+    function cos_decay( i, maxits, eps_fromto ) result( eps )
+        integer, intent(in) :: i, maxits
+        real,    intent(in) :: eps_fromto(2)
+        real :: delta, eps
+        delta = 0.5 * (eps_fromto(2) - eps_fromto(1))
+        eps   = eps_fromto(1) + delta + delta * cos((real(i) * PI) / real(MAXITS))
+    end function cos_decay
+
 end program simple_test_extr_frac
