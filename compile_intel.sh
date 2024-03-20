6f83ccbc8e07a7129acecc2b2c9d52c734bda513
Ruben Meana-Paneda <rubenmepa@gmail.com>
2024-03-20 14:04:11 -0400

    back to the previous build Intel script recipe

--- compile_intel.sh
+++ compile_intel.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-cmake . -B ./build -DGUI=off -DUSE_LIBTIFF=off -DCMAKE_C_COMPILER=icx -DCMAKE_Fortran_COMPILER=ifx -DCMAKE_CXX_COMPILER=icpx
-cmake --build 
+rm -rf build
+cd build
+cmake .. -DGUI=off -DUSE_LIBTIFF=off -DCMAKE_C_COMPILER=icx -DCMAKE_Fortran_COMPILER=ifx -DCMAKE_CXX_COMPILER=icpx
+make -j install
+#cmake . -B ./build -DGUI=off -DUSE_LIBTIFF=off -DCMAKE_C_COMPILER=icx -DCMAKE_Fortran_COMPILER=ifx -DCMAKE_CXX_COMPILER=icpx
+#cmake --build 
