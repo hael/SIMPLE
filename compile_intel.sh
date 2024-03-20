66318d1cb29ff1dccca3bb74dc14414b0e9b67a2
Ruben Meana-Paneda <rubenmepa@gmail.com>
2024-03-20 11:32:00 -0400

    update Intel-OneAPI compilation script

--- compile_intel.sh
+++ compile_intel.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-rm -rf build
-mkdir build
-cd build
-cmake .. -DGUI=off -DUSE_LIBTIFF=off -DCMAKE_C_COMPILER=icx -DCMAKE_Fortran_COMPILER=ifx -DCMAKE_CXX_COMPILER=icpx
-make -j install
-
+cmake .. -B ./build -DGUI=off -DUSE_LIBTIFF=off -DCMAKE_C_COMPILER=icx -DCMAKE_Fortran_COMPILER=ifx -DCMAKE_CXX_COMPILER=icpx
+cmake --build 
