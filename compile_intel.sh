c7c4796074e107082f8e9418b91bffb053e19ead
Ruben Meana-Paneda <rubenmepa@gmail.com>
2024-03-20 12:31:00 -0400

    bugfix MD5 hash for nodejs on Darwin Arm64 Apple

--- compile_intel.sh
+++ compile_intel.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cmake .. -B ./build -DGUI=off -DUSE_LIBTIFF=off -DCMAKE_C_COMPILER=icx -DCMAKE_Fortran_COMPILER=ifx -DCMAKE_CXX_COMPILER=icpx
+cmake . -B ./build -DGUI=off -DUSE_LIBTIFF=off -DCMAKE_C_COMPILER=icx -DCMAKE_Fortran_COMPILER=ifx -DCMAKE_CXX_COMPILER=icpx
 cmake --build 
