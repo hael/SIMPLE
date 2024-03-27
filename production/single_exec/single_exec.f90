f06d24be5cce2032e12f463456246b0a8cf5e4e0
Joseph Caesar <joseph.caesar@path.ox.ac.uk>
2024-03-27 22:33:26 +0000

    Updated NICE development version in repo to 2.0.2

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -174,7 +174,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('8a4676f1')
+call simple_print_git_version('73754fec')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
