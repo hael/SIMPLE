bad295a16bccc45193a5fb6a9e2bf280031db0dd
Hans Elmlund <hans.elmlund@nih.gov>
2024-03-30 10:10:37 -0400

    integration of automasking within distributed refine3D workflow

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -174,7 +174,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('4b96d620')
+call simple_print_git_version('e24d60eb')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
