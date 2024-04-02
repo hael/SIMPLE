e249684ec479a285e83a7b01fb990f9c7b7e20ce
cong <vanc2@nih.gov>
2024-04-02 10:33:04 -0400

    faster ppca by changing matrix slicing

--- src/main/simple_ppca_inmem.f90
+++ src/main/simple_ppca_inmem.f90
@@ -53,20 +53,20 @@ contains
      !>  \brief  is a constructor
     function constructor( N, D, Q ) result( self )
         integer, intent(in) :: N, D, Q
-        type(ppca_inmem)          :: self
+        type(ppca_inmem)    :: self
         call self%new( N, D, Q )
     end function constructor
 
     !>  \brief  is a constructor
     subroutine new( self, N, D, Q )
         class(ppca_inmem), intent(inout) :: self
-        integer,     intent(in)    :: N, D, Q
+        integer,           intent(in)    :: N, D, Q
         call self%kill
         self%N = N
         self%D = D
         self%Q = Q
         ! allocate principal subspace and feature vectors
-        allocate( self%W(self%D,self%Q), self%E_zn(self%N,self%Q,1), self%W_1(self%D,self%Q),&
+        allocate( self%W(self%D,self%Q), self%E_zn(1,self%Q,self%N), self%W_1(self%D,self%Q),&
         self%W_2(self%Q,self%Q), self%W_3(self%Q,self%Q), self%Wt(self%Q,self%D),&
         self%M(self%Q,self%Q), self%Minv(self%Q,self%Q), self%MinvWt(self%Q,self%D),&
         self%X(self%D,1), self%E_znzn(self%Q,self%Q), source=0.)
@@ -95,24 +95,24 @@ contains
         class(ppca_inmem), intent(inout) :: self
         integer, intent(in)        :: i
         real, allocatable          :: feat(:)
-        allocate(feat(self%Q), source=self%E_zn(i,:,1))
+        allocate(feat(self%Q), source=self%E_zn(1,:,i))
     end function get_feat
 
     !>  \brief  is for getting a pointer to the features (that become exposed)
     function get_feats_ptr( self ) result( ptr )
         class(ppca_inmem), intent(inout), target :: self
         real, pointer :: ptr(:,:)
-        ptr => self%E_zn(:,:,1)
+        ptr => self%E_zn(1,:,:)
     end function get_feats_ptr
 
     !>  \brief  is for sampling the generative model at a given image index
     function generate( self, i, avg ) result( dat )
         class(ppca_inmem), intent(inout) :: self
-        integer,     intent(in)    :: i
-        real,        intent(in)    :: avg(self%D)
+        integer,           intent(in)    :: i
+        real,              intent(in)    :: avg(self%D)
         real, allocatable :: dat(:)
         real              :: tmp(self%D,1)
-        tmp = matmul(self%W,self%E_zn(i,:,:))
+        tmp = matmul(self%W,transpose(self%E_zn(:,:,i)))
         allocate(dat(self%D), source=tmp(:,1))
         dat = dat + avg
     end function generate
@@ -120,19 +120,19 @@ contains
     !>  \brief  is for sampling the generative model at a given image index
     subroutine generate_1( self, i, avg, dat )
         class(ppca_inmem), intent(inout) :: self
-        integer,     intent(in)    :: i
-        real,        intent(in)    :: avg(self%D)
-        real,        intent(inout) :: dat(self%D)
-        dat = avg + [matmul(self%W,self%E_zn(i,:,:))]
+        integer,           intent(in)    :: i
+        real,              intent(in)    :: avg(self%D)
+        real,              intent(inout) :: dat(self%D)
+        dat = avg + [matmul(self%W,transpose(self%E_zn(:,:,i)))]
     end subroutine generate_1
 
     !>  \brief  is for sampling the generative model at a given image index
     subroutine generate_2( self, i, avg, dat, var )
         class(ppca_inmem), intent(inout) :: self
-        integer,     intent(in)    :: i
-        real,        intent(in)    :: avg(self%D)
-        real,        intent(inout) :: dat(self%D), var
-        dat = [matmul(self%W,self%E_zn(i,:,:))]
+        integer,           intent(in)    :: i
+        real,              intent(in)    :: avg(self%D)
+        real,              intent(inout) :: dat(self%D), var
+        dat = [matmul(self%W,transpose(self%E_zn(:,:,i)))]
         var = sum(dat**2) / real(self%D)
         dat = dat + avg
     end subroutine generate_2
@@ -141,8 +141,8 @@ contains
 
     subroutine master( self, pcavecs, maxpcaits )
         class(ppca_inmem), intent(inout) :: self
-        real,        intent(in)    :: pcavecs(self%N,self%D)
-        integer,     intent(in)    :: maxpcaits
+        real,              intent(in)    :: pcavecs(self%D,self%N)
+        integer,           intent(in)    :: maxpcaits
         integer :: k, file_stat, err
         real    :: p, p_prev
         p = 0.
@@ -176,7 +176,7 @@ contains
         meanv = 0.
         ! initialize latent variables by zero mean gaussians with unit variance
         do i=1,self%N
-            self%E_zn(i,:,1) = mnorm_smp(Imat, self%Q, meanv)
+            self%E_zn(1,:,i) = mnorm_smp(Imat, self%Q, meanv)
         end do
         ! initialize weight matrix with uniform random nrs, normalize over j
         do i=1,self%D
@@ -186,7 +186,7 @@ contains
             self%W(i,:) = self%W(i,:)/sum(self%W(i,:))
         end do
         ! transpose W
-        self%Wt = transpose(self%W)
+        self%Wt  = transpose(self%W)
         ! set W_2 to WtW
         self%W_2 = matmul(self%Wt,self%W)
     end subroutine init
@@ -195,9 +195,9 @@ contains
     subroutine em_opt( self, pcavecs, p, err )
         use simple_math, only: matinv
         class(ppca_inmem), intent(inout) :: self
-        real,        intent(in)    :: pcavecs(self%N,self%D)
-        integer,     intent(out)   :: err
-        real,        intent(out)   :: p
+        real,              intent(in)    :: pcavecs(self%D,self%N)
+        integer,           intent(out)   :: err
+        real,              intent(out)   :: p
         integer :: i
         real    :: tmp(self%D,1)
         ! E-STEP
@@ -209,30 +209,30 @@ contains
         self%W_2 = 0.
         do i=1,self%N
             ! set data vec
-            self%X(:,1) = pcavecs(i,:)
+            self%X(:,1) = pcavecs(:,i)
             ! Expectation step (calculate expectations using the old W)
-            self%E_zn(i,:,:) = matmul(self%MinvWt,self%X)
-            self%E_znzn = matmul(self%E_zn(i,:,:),transpose(self%E_zn(i,:,:)))
+            self%E_zn(:,:,i) = matmul(self%MinvWt,self%X)
+            self%E_znzn = matmul(transpose(self%E_zn(:,:,i)),self%E_zn(:,:,i))
             ! Prepare for update of W (M-step)
-            self%W_1 = self%W_1+matmul(self%X,transpose(self%E_zn(i,:,:)))
+            self%W_1 = self%W_1+matmul(self%X,self%E_zn(:,:,i))
             self%W_2 = self%W_2+self%E_znzn
         end do
         ! M-STEP
         call matinv(self%W_2, self%W_3, self%Q, err)
         if( err == -1 ) return
         ! update W
-        self%W = matmul(self%W_1,self%W_3)
+        self%W   = matmul(self%W_1,self%W_3)
         ! update Wt
-        self%Wt = transpose(self%W)
+        self%Wt  = transpose(self%W)
         ! set W_2 to WtW
         self%W_2 = matmul(self%Wt,self%W)
         ! EVAL REC ERR
         p = 0.
         do i=1,self%N
             ! set data vec
-            self%X(:,1) = pcavecs(i,:)
-            tmp = matmul(self%W,self%E_zn(i,:,:))
-            p = p + sqrt(sum((self%X(:,1)-tmp(:,1))**2.))
+            self%X(:,1) = pcavecs(:,i)
+            tmp = matmul(self%W,transpose(self%E_zn(:,:,i)))
+            p   = p + sqrt(sum((self%X(:,1)-tmp(:,1))**2.))
         end do
     end subroutine em_opt
 
