80f25662438948cf18a1fcecae5d12b50eef7aa8
cong <vanc2@nih.gov>
2024-04-02 13:46:38 -0400

    adding extraction of PCA components, i.e. eigenvalues/vecs

--- production/tests/test_ppca_kpca/simple_test_ppca_kpca.f90
+++ production/tests/test_ppca_kpca/simple_test_ppca_kpca.f90
@@ -4,7 +4,7 @@ implicit none
 integer, parameter :: NP = 2, NS = 4, NC = 1, MAXPCAITS = 25
 type(ppca_inmem)   :: prob_pca
 integer :: j
-real    :: data_ori(NP, NS), avg(NP), tmpvec(NP), data_pca(NP, NS)
+real    :: data_ori(NP, NS), avg(NP), tmpvec(NP), data_pca(NP, NS), W(NP, NC), E_zn(NC, NS)
 data_ori(1,:) = [1, 2, 3, 4]
 data_ori(2,:) = [3, 1, 5, 8]
 print *, data_ori(1,:)
@@ -22,4 +22,8 @@ end do
 !$omp end parallel do
 print *, data_pca(1,:)
 print *, data_pca(2,:)
+call prob_pca%get_W(W)
+print *, W
+call prob_pca%get_E_zn(E_zn)
+print *, E_zn
 end program simple_test_ppca_kpca
