58cd6d40bce233c530c7a873acc38b83f525a050
cong <vanc2@nih.gov>
2024-04-03 12:11:52 -0400

    initial work on pca using svd and its unit test

--- production/tests/test_ppca_kpca/simple_test_ppca_kpca.f90
+++ production/tests/test_ppca_kpca/simple_test_ppca_kpca.f90
@@ -1,10 +1,13 @@
 program simple_test_ppca_kpca
+include 'simple_lib.f08'
 use simple_ppca_inmem, only: ppca_inmem
+use simple_pca,        only: pca
 implicit none
 integer, parameter :: NP = 3, NS = 4, NC = 2, MAXPCAITS = 15
 type(ppca_inmem)   :: prob_pca
+type(pca)          :: pca_obj
 integer :: j
-real    :: data_ori(NP, NS), avg(NP), tmpvec(NP), data_pca(NP, NS), E_zn(NC, NS)
+real    :: data_ori(NP, NS), avg(NP), tmpvec(NP), data_pca(NP, NS), E_zn(NC, NS), eig_vecs(NP, NS), eig_vals(NS), tmp(NS, NS)
 data_ori(1,:) = [ 1, 2, 3, 4]
 data_ori(2,:) = [ 3, 1, 5, 8]
 data_ori(3,:) = [-1, 0, 4, 10]
@@ -31,4 +34,12 @@ do j = 1, NC
     print *, E_zn(j, :)
     print *, prob_pca%get_var(j)
 enddo
+! SVD test
+eig_vecs = data_ori
+call svdcmp(eig_vecs,eig_vals,tmp)
+print *, eig_vals**2 / 3.
+print *, eig_vecs(:,1)
+! PCA test
+call pca_obj%new(NS, NP, NC)
+call pca_obj%master(data_ori)
 end program simple_test_ppca_kpca
