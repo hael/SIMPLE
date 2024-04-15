754c9fc5e22c13809499c173379017764b1cf79a
cong <vanc2@nih.gov>
2024-04-15 11:40:51 -0400

    renaming check_oris to check_states and testing new displaying check_states stats

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -174,7 +174,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('8a7042b7')
+call simple_print_git_version('da2e72dc')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
