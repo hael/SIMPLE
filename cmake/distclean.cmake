fed1116ca6b6b530b962b898842c43c7b64fe584
Ruben Meana-Paneda <rubenmepa@gmail.com>
2024-04-22 15:36:22 -0400

    more work formatting and cleaning cmake

--- cmake/distclean.cmake
+++ cmake/distclean.cmake
@@ -3,68 +3,68 @@
 
 # We want to start from the top of the source dir, so if we are in build
 # we want to start one directory up
-GET_FILENAME_COMPONENT(BASEDIR ${CMAKE_SOURCE_DIR} NAME)
+get_filename_component(BASEDIR ${CMAKE_SOURCE_DIR} NAME)
 #IF(${BASEDIR} STREQUAL "build")
 #    SET(TOPDIR "${CMAKE_SOURCE_DIR}/..")
 #ELSE()
-    SET(TOPDIR "${CMAKE_SOURCE_DIR}")
+set(TOPDIR "${CMAKE_SOURCE_DIR}")
 #ENDIF()
 message(STATUS "TOPDIR ${TOPDIR}")
-MACRO(GET_PARENT_DIRECTORIES search_string return_list grandparents)
-    FILE(GLOB_RECURSE new_list ${search_string})
-    SET(dir_list "")
-    FOREACH(file_path ${new_list})
-        GET_FILENAME_COMPONENT(dir_path ${file_path} PATH)
+macro(GET_PARENT_DIRECTORIES search_string return_list grandparents)
+    file(GLOB_RECURSE new_list ${search_string})
+    set(dir_list "")
+    foreach(file_path ${new_list})
+        get_filename_component(dir_path ${file_path} PATH)
         # Remove an extra directory component to return grandparent
-        IF(${grandparents})
+        if(${grandparents})
             # Tack on a fake extension to trick CMake into removing a second
             # path component
-            SET(dir_path "${dir_path}.tmp")
-            GET_FILENAME_COMPONENT(dir_path ${dir_path} PATH)
-        ENDIF(${grandparents})
-        SET(dir_list ${dir_list} ${dir_path})
-    ENDFOREACH()
-    LIST(REMOVE_DUPLICATES dir_list)
-    SET(${return_list} ${dir_list})
-ENDMACRO()
+            set(dir_path "${dir_path}.tmp")
+            get_filename_component(dir_path ${dir_path} PATH)
+        endif(${grandparents})
+        set(dir_list ${dir_list} ${dir_path})
+    endforeach()
+    list(REMOVE_DUPLICATES dir_list)
+    set(${return_list} ${dir_list})
+endmacro()
 
 # Find directories and files that we will want to remove
-FILE(GLOB_RECURSE CMAKECACHE "${TOPDIR}/*CMakeCache.txt")
-FILE(GLOB_RECURSE CMAKEINSTALL "${TOPDIR}/*cmake_install.cmake"
-                               "${TOPDIR}/*install_manifest.txt")
-FILE(GLOB_RECURSE MAKEFILE "${TOPDIR}/*Makefile")
-FILE(GLOB_RECURSE MODFILES RELATIVE "${TOPDIR}" "*.mod")
-FILE(GLOB_RECURSE CMAKETESTFILES "${TOPDIR}/*CTestTestfile.cmake")
-SET(TOPDIRECTORIES "${TOPDIR}/cmake" "${TOPDIR}/tests" "${TOPDIR}/bin/tests" "${TOPDIR}/bin/simple" 
-  )
+file(GLOB_RECURSE CMAKECACHE "${TOPDIR}/*CMakeCache.txt")
+file(GLOB_RECURSE CMAKEINSTALL "${TOPDIR}/*cmake_install.cmake"
+                               "${TOPDIR}/*install_manifest.txt"
+)
+file(GLOB_RECURSE MAKEFILE "${TOPDIR}/*Makefile")
+file(GLOB_RECURSE MODFILES RELATIVE "${TOPDIR}" "*.mod")
+file(GLOB_RECURSE CMAKETESTFILES "${TOPDIR}/*CTestTestfile.cmake")
+set(TOPDIRECTORIES "${TOPDIR}/cmake" "${TOPDIR}/tests" "${TOPDIR}/bin/tests" "${TOPDIR}/bin/simple")
 
 # CMake has trouble finding directories recursively, so locate these
 # files and then save the parent directory of the files
-GET_PARENT_DIRECTORIES(Makefile.cmake CMAKEFILES 0)
-GET_PARENT_DIRECTORIES(CMakeDirectoryInformation.cmake SUBDIRECTORIES 0) 
-GET_PARENT_DIRECTORIES(LastTest.log CMAKETESTING 1)
+get_parent_directories(Makefile.cmake CMAKEFILES 0)
+get_parent_directories(CMakeDirectoryInformation.cmake SUBDIRECTORIES 0) 
+get_parent_directories(LastTest.log CMAKETESTING 1)
 
 # Place these files and directories into a list
-SET(DEL ${CMAKECACHE}
+set(DEL ${CMAKECACHE}
         ${CMAKEINSTALL}
         ${MAKEFILE}
         ${CMAKEFILES}
         ${CMAKETESTING}
         ${CMAKETESTFILES}
         ${SUBDIRECTORIES}
-         ${TOPDIRECTORIES}
+        ${TOPDIRECTORIES}
 )
 
 # If we are not in the build dir, delete that as well
-IF(NOT (${BASEDIR} STREQUAL "build"))
-    FILE(GLOB BUILD "${TOPDIR}/build")
-    SET(DEL ${DEL} ${BUILD})
-ENDIF()
+if(NOT (${BASEDIR} STREQUAL "build"))
+    file(GLOB BUILD "${TOPDIR}/build")
+    set(DEL ${DEL} ${BUILD})
+endif()
 
 # Loop over the directories and delete each one
-FOREACH(D ${DEL})
-    IF(EXISTS ${D})
-message(STATUS "Removing ${D}")
-      FILE(REMOVE_RECURSE ${D})
-    ENDIF()
-ENDFOREACH()
+foreach(D ${DEL})
+    if(EXISTS ${D})
+        message(STATUS "Removing ${D}")
+        file(REMOVE_RECURSE ${D})
+    endif()
+endforeach()
