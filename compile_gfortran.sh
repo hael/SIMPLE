706580afb24cee55a1865f7ea8ab8b1494d2bd72
Ruben Meana-Paneda <rubenmepa@gmail.com>
2024-04-29 15:28:58 -0400

    Add new compilation formulas

--- compile_gfortran.sh
+++ compile_gfortran.sh
@@ -2,7 +2,7 @@
 rm -rf build
 mkdir build
 cd build
-cmake .. -DGUI=off -DUSE_LIBTIFF=off -DCMAKE_C_COMPILER=gcc -DCMAKE_Fortran_COMPILER=gfortran -DCMAKE_CXX_COMPILER=g++
+cmake -LAH .. -DGUI=off -DUSE_LIBTIFF=off -DCMAKE_C_COMPILER=gcc -DCMAKE_Fortran_COMPILER=gfortran -DCMAKE_CXX_COMPILER=g++
 make -j install
 #cmake . -B ./build -DGUI=off -DUSE_LIBTIFF=off -DCMAKE_C_COMPILER=icx -DCMAKE_Fortran_COMPILER=ifx -DCMAKE_CXX_COMPILER=icpx
-#cmake --build 
+#cmake --build --config Debug
