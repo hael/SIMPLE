062e73a73fbb7c1dec3d67f78f4b4de616c01d7d
Ruben Meana-Paneda <rubenmepa@gmail.com>
2024-05-06 16:25:42 -0400

    Add more tests to Ctest

--- cmake/SimpleTest.cmake
+++ cmake/SimpleTest.cmake
@@ -1,6 +1,27 @@
 # CTest - Simple Test Set
 # emulate GNU Autotools `make check`
 #add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND})
+
+#add_test(
+#NAME Test_2D_core_finder
+#COMMAND simple_2D_core_finder
+#)
+#add_test(
+#NAME Test_2D_opt_filt
+#COMMAND simple_2D_opt_filt
+#)
+#add_test(
+#NAME Test_3D_core_finder
+#COMMAND simple_3D_core_finder
+#)
+#add_test(
+#NAME Test_3D_lpbasic
+#COMMAND simple_3D_lpbasic
+#)
+#add_test(
+#NAME Test_3D_opt_filt
+#COMMAND simple_3D_opt_filt
+#)
 add_test(
 NAME Test_angres
 COMMAND simple_test_angres
@@ -22,36 +43,88 @@ NAME Test_bounds_from_mask3D
 COMMAND simple_test_bounds_from_mask3D
 )
 add_test(
-NAME Test_cont_inplane
-COMMAND simple_test_cont_inplane
+NAME Test_cartcorr_shifted
+COMMAND simple_test_cartcorr_shifted
 )
+add_test(
+NAME Test_cavg_kpca
+COMMAND simple_test_cavg_kpca
+)
+add_test(
+NAME Test_cc_gradient
+COMMAND simple_test_cc_gradient
+)
+#add_test(
+#NAME Test_cont_inplane
+#COMMAND simple_test_cont_inplane
+#)
 add_test(
 NAME Test_corrs2weights
 COMMAND simple_test_corrs2weights
 )
+#add_test(
+#NAME Test_corrweights
+#COMMAND simple_test_corrweights
+#)
 add_test(
 NAME Test_CTF
 COMMAND simple_test_ctf
 )
+#add_test(
+#NAME Test_eo_diff
+#COMMAND simple_eo_diff
+#)
 add_test(
 NAME Test_Euler
 COMMAND simple_test_euler
+)
+add_test(
+NAME Test_eval_polarftcc
+COMMAND simple_test_eval_polarftcc
 )
 add_test(
 NAME Test_extr_frac
 COMMAND simple_test_extr_frac
 )
 add_test(
+NAME Test_fast_corrcalc
+COMMAND simple_test_fast_corrcalc
+)
+add_test(
+NAME Test_fftw_plan_many_1D
+COMMAND simple_test_fftw_plan_many_1D
+)
+add_test(
 NAME Test_fileio
 COMMAND simple_test_fileio
 )
-add_test(
-NAME Test_io_parallel
-COMMAND simple_test_io_parallel
-)
+#add_test(
+#NAME Test_find_boundaries
+#COMMAND simple_find_boundaries
+#)
+#add_test(
+#NAME Test_fit_lattice
+#COMMAND simple_fit_lattice
+#)
 add_test(
 NAME Test_fplane_proj
 COMMAND simple_test_fplane_proj
+)
+#add_test(
+#NAME Test_ft_expanded
+#COMMAND simple_test_ft_expanded
+#)
+#add_test(
+#NAME Test_gauss2Dfit
+#COMMAND simple_test_gauss2Dfit
+#)
+add_test(
+NAME Test_gaussian1D
+COMMAND simple_test_gaussian1D
+)
+add_test(
+NAME Test_gencorrs_fft
+COMMAND simple_test_gencorrs_fft
 )
 add_test(
 NAME Test_graphene_mask
@@ -74,6 +147,10 @@ NAME Test_io
 COMMAND simple_test_io
 )
 add_test(
+NAME Test_io_parallel
+COMMAND simple_test_io_parallel
+)
+add_test(
 NAME Test_lplims
 COMMAND simple_test_lplims
 )
@@ -81,6 +158,10 @@ add_test(
 NAME Test_mask
 COMMAND simple_test_mask
 )
+#add_test(
+#NAME Test_masscen_nano
+#COMMAND simple_masscen_nano
+#)
 add_test(
 NAME Test_maxnloc
 COMMAND simple_test_maxnloc
@@ -89,10 +170,20 @@ add_test(
 NAME Test_memuse
 COMMAND simple_test_memuse
 )
+if(USE_MPI)
+   add_test(
+   NAME Test_MPI
+   COMMAND simple_test_mpi
+   )
 add_test(
 NAME Test_multinomal
 COMMAND simple_test_multinomal
 )
+endif()
+#add_test(
+#NAME Test_nano_detect_atoms
+#COMMAND simple_nano_detect_atoms
+#)
 add_test(
 NAME Test_neigh
 COMMAND simple_test_neigh
@@ -105,6 +196,14 @@ add_test(
 NAME Test_opt_genetic
 COMMAND simple_test_opt_genetic
 )
+#add_test(
+#NAME Test_opt_lp
+#COMMAND simple_test_opt_lp
+#)
+#add_test(
+#NAME Test_order2D_byresol
+#COMMAND simple_test_order2D_byresol
+#)
 add_test(
 NAME Test_order_corr
 COMMAND simple_test_order_corr
@@ -125,6 +224,22 @@ add_test(
 NAME Test_phaseplate_correct_FSC
 COMMAND simple_test_phaseplate_correct_fsc
 )
+#add_test(
+#NAME Test_picker_comp
+#COMMAND simple_test_picker_comp
+#)
+#add_test(
+#NAME Test_polar2cartesian
+#COMMAND simple_test_polar2cartesian
+#)
+add_test(
+NAME Test_ppca_kpca
+COMMAND simple_test_ppca_kpca
+)
+#add_test(
+#NAME Test_radial_cc
+#COMMAND simple_test_radial_cc_
+#)
 add_test(
 NAME Test_rad_med
 COMMAND simple_test_rad_med
@@ -133,17 +248,41 @@ add_test(
 NAME Test_rank_weights
 COMMAND simple_test_rank_weights
 )
+#add_test(
+#NAME Test_ref_assign
+#COMMAND simple_test_ref_assign
+#)
 add_test(
 NAME Test_replace_substr
 COMMAND simple_test_replace_substr
 )
 add_test(
+NAME Test_rotate_ref
+COMMAND simple_test_rotate_ref
+)
+#add_test(
+#NAME Test_segpicker
+#COMMAND simple_test_segpicker
+#)
+add_test(
 NAME Test_serialize
 COMMAND simple_test_serialize
 )
+#add_test(
+#NAME Test_shiftsrch
+#COMMAND simple_test_shiftsrch
+#)
 add_test(
 NAME Test_simd
 COMMAND simple_test_simd
+)
+#add_test(
+#NAME Test_speed
+#COMMAND simple_test_speed
+#)
+add_test(
+NAME Test_sp_project
+COMMAND simple_test_sp_project
 )
 add_test(
 NAME Test_srch
@@ -153,6 +292,10 @@ add_test(
 NAME Test_stack
 COMMAND simple_test_stack
 )
+#add_test(
+#NAME Test_stack_io
+#COMMAND simple_test_stack_io
+#)
 add_test(
 NAME Test_starfile
 COMMAND simple_test_starfile
@@ -170,17 +313,21 @@ NAME Test_tseries_neigh
 COMMAND simple_test_tseries_neigh
 )
 add_test(
+NAME Test_uniform_cluster
+COMMAND simple_test_uniform_cluster
+)
+add_test(
 NAME Test_uniform_euler
 COMMAND simple_test_uniform_euler
 )
-add_test(
-NAME Test_uniform_filter_2D
-COMMAND simple_test_uniform_filter_2D
-)
-add_test(
-NAME Test_uniform_filter_3D
-COMMAND simple_test_uniform_filter_3D
-)
+#add_test(
+#NAME Test_uniform_filter_2D
+#COMMAND simple_test_uniform_filter_2D
+#)
+#add_test(
+#NAME Test_uniform_filter_3D
+#COMMAND simple_test_uniform_filter_3D
+#)
 add_test(
 NAME Test_uniform_rot
 COMMAND simple_test_uniform_rot
