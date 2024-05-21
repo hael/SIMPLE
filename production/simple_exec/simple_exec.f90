69b7c4b89ccf78b3ec62e5c3736a15b5d59d7a4a
Hans Elmlund <hans.elmlund@nih.gov>
2024-05-21 11:23:46 -0400

    edits to ab initio wflow for tau testing

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -396,7 +396,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('321a648a')
+call simple_print_git_version('3237ff21')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
