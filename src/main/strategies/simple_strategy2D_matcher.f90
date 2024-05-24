e237be16ad2fc509b260b3ddd3ad8663a37f967d
Hans Elmlund <hans.elmlund@nih.gov>
2024-05-24 14:13:22 -0400

    ICM regularization in 2D only on towards the end, after shifts are turned on

--- src/main/strategies/simple_strategy2D_matcher.f90
+++ src/main/strategies/simple_strategy2D_matcher.f90
@@ -41,6 +41,7 @@ real(timer_int_kind)         :: rt_init, rt_prep_pftcc, rt_align, rt_cavg, rt_pr
 integer(timer_int_kind)      ::  t_init,  t_prep_pftcc,  t_align,  t_cavg,  t_projio,  t_tot
 character(len=STDLEN)        :: benchfname
 logical                      :: l_stream = .false.
+logical                      :: l_icm    = .false.
 logical                      :: l_ctf    = .false.
 
 contains
@@ -71,13 +72,14 @@ contains
         frac_srch_space = build_glob%spproj_field%get_avg('frac')
 
         ! SWITCHES
-        l_partial_sums     = .false.
-        l_snhc             = .false.
-        l_greedy           = .false.
-        l_greedy_smpl      = .false.
-        l_snhc_smpl        = .false.
-        l_frac_update      = .false.
-        l_stream           = trim(params_glob%stream).eq.'yes'
+        l_icm          = params_glob%l_icm .and. frac_srch_space > FRAC_SH_LIM
+        l_partial_sums = .false.
+        l_snhc         = .false.
+        l_greedy       = .false.
+        l_greedy_smpl  = .false.
+        l_snhc_smpl    = .false.
+        l_frac_update  = .false.
+        l_stream       = trim(params_glob%stream).eq.'yes'
         if( params_glob%extr_iter == 1 )then
             ! greedy start
             l_greedy = .true.
@@ -493,7 +495,7 @@ contains
                 call prep2Dref(cavgs_merged(icls), match_imgs(icls), icls, iseven=.false., center=do_center, xyz_out=xyz)
                 if( .not.params_glob%l_lpset )then
                     if( pop_even >= MINCLSPOPLIM .and. pop_odd >= MINCLSPOPLIM )then
-                        if( params_glob%l_icm ) call cavgs_even(icls)%ICM2D_eo(cavgs_odd(icls), params_glob%lambda, verbose=.false.)
+                        if( l_icm ) call cavgs_even(icls)%ICM2D_eo(cavgs_odd(icls), params_glob%lambda, verbose=.false.)
                         ! here we are passing in the shifts and do NOT map them back to classes
                         call prep2Dref(cavgs_even(icls), match_imgs(icls), icls, iseven=.true., center=do_center, xyz_in=xyz)
                         call build_glob%img_crop_polarizer%polarize(pftcc, match_imgs(icls), icls, isptcl=.false., iseven=.true., mask=build_glob%l_resmsk)  ! 2 polar coords
@@ -501,7 +503,7 @@ contains
                         call prep2Dref(cavgs_odd(icls), match_imgs(icls), icls, iseven=.false., center=do_center, xyz_in=xyz)
                         call build_glob%img_crop_polarizer%polarize(pftcc, match_imgs(icls), icls, isptcl=.false., iseven=.false., mask=build_glob%l_resmsk)  ! 2 polar coords
                     else
-                        if( params_glob%l_icm )then
+                        if( l_icm )then
                             call cavgs_merged(icls)%ICM2D(params_glob%lambda, verbose=.false.)
                             call prep2Dref(cavgs_merged(icls), match_imgs(icls), icls, iseven=.false., center=do_center, xyz_in=xyz)
                         endif
@@ -510,7 +512,7 @@ contains
                         call pftcc%cp_even2odd_ref(icls)
                     endif
                 else
-                    if( params_glob%l_icm ) call cavgs_merged(icls)%ICM2D(params_glob%lambda, verbose=.false.)
+                    if( l_icm ) call cavgs_merged(icls)%ICM2D(params_glob%lambda, verbose=.false.)
                     call prep2Dref(cavgs_merged(icls), match_imgs(icls), icls, iseven=.false., center=do_center, xyz_in=xyz)
                     call build_glob%img_crop_polarizer%polarize(pftcc, match_imgs(icls), icls, isptcl=.false., iseven=.true., mask=build_glob%l_resmsk)  ! 2 polar coords
                     call pftcc%cp_even2odd_ref(icls)
