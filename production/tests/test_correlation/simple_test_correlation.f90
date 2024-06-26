895989ae2038fe4e855aefa32b6245b1bc6d4ca8
Rilyn McKallip <rilynnmckallip@gmail.com>
2024-06-26 18:16:36 -0400

    reorganized detect atoms test and added correlation test

---
+++ production/tests/test_correlation/simple_test_correlation.f90
@@ -0,0 +1,35 @@
+program simple_test_correlation
+    include 'simple_lib.f08'
+    use simple_nano_detect_atoms
+    use simple_stat
+    use simple_image
+
+    ! want to compare atom image to essentially blank image
+    ! will compare to completely blank image (all zeroes) and outlier position in image
+    type(nano_picker) :: np 
+    type(image) :: blank, boximg
+    real :: pos(3), smpd, boximg_corr
+    real, allocatable :: rmat_blank(:,:,:)
+    character(len=100) :: filename_exp, pdbfile_ref
+    character(len=2) :: element
+    integer :: offset, peak_thres_level, ldim_box(3), center(3)
+
+    filename_exp     = 'rec_merged.mrc'
+    pdbfile_ref      = 'reference.pdb'
+    element          = 'PT'
+    smpd             = 0.358
+    peak_thres_level = 2
+    offset           = 2
+    ldim_box         = [8,8,8]
+    center           = [4,4,4]
+
+    allocate(rmat_blank(8,8,8), source=0.)
+    pos = [0., 0., 0.]
+    call np%new(smpd, element, filename_exp, peak_thres_level, offset, denoise=.false.)
+    call np%simulate_atom()
+    call np%extract_img_from_pos(pos, boximg)
+    call boximg%write('boximg.mrc')
+    call np%one_box_corr(pos, circle=.true., corr=boximg_corr)
+    print *, 'boximg_corr = ', boximg_corr
+
+end program simple_test_correlation
---
+++ src/utils/simple_nano_picker_utils.f90
@@ -0,0 +1,61 @@
+module simple_nano_picker_utils
+include 'simple_lib.f08'
+use simple_image
+use simple_atoms
+use simple_parameters
+use simple_srch_sort_loc, only : hpsort
+implicit none
+#include "simple_local_flags.inc"
+    
+    contains 
+    
+    subroutine find_closest( coords_1, coords_2, length_1, length_2, distances, filename )
+        integer,                    intent(in)  :: length_1, length_2
+        real,                       intent(in)  :: coords_1(3,length_1), coords_2(3,length_2)
+        real,                       intent(out) :: distances(length_1)
+        character(len=*), optional, intent(in)  :: filename
+        integer :: i, j, min_loc(1)
+        real :: closest_coord(3)
+        real, allocatable :: dists(:)
+        real :: min_dist
+    
+        if( present(filename) )then
+            open(unit=22, file=filename)
+        else
+            open(unit=22, file='combined_coords.csv')
+        endif
+    
+        do i = 1, length_1
+            allocate(dists(length_2))
+            do j = 1, length_2
+                dists(j) = euclid(real(coords_1(:,i)),real(coords_2(:,j)))
+            enddo
+            min_loc       = minloc(dists)
+            min_dist      = minval(dists)
+            distances(i)  = min_dist
+            closest_coord = coords_2(:,min_loc(1))
+            write(22,'(7(f8.3,a))') coords_1(1,i), ',', coords_1(2,i), ',', coords_1(3,i), ',', closest_coord(1), ',', closest_coord(2), ',', closest_coord(3), ',', min_dist
+            deallocate(dists)
+        enddo
+    
+        close(22)
+    end subroutine find_closest
+    
+    subroutine write_centers( fname, coords, smpd )
+        character(len=*),           intent(in)    :: fname
+        real,                       intent(in)    :: coords(:,:)
+        real,                       intent(in)    :: smpd
+        type(atoms) :: centers_pdb
+        integer     :: cc
+        call centers_pdb%new(size(coords, dim = 1), dummy=.true.)
+        do cc = 1, size(coords, dim = 1)
+            call centers_pdb%set_name(cc,params_glob%element)
+            call centers_pdb%set_element(cc,params_glob%element)
+            call centers_pdb%set_coord(cc,coords(cc,:)*smpd)
+            !call centers_pdb%set_beta(cc,nano%atominfo(cc)%valid_corr) ! use per atom valid corr
+            call centers_pdb%set_resnum(cc,cc)
+        enddo
+        call centers_pdb%writepdb(fname)
+    end subroutine write_centers
+    
+end module simple_nano_picker_utils
