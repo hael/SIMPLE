658b1938f611f4369a36193008d2fed1e1516525
cyrilr <cyril.reboul@gmail.com>
2024-08-06 15:59:08 +1000

    Tidy and minor edits

--- src/main/commanders/simple_commander_refine3D.f90
+++ src/main/commanders/simple_commander_refine3D.f90
@@ -944,11 +944,7 @@ contains
         ! make CTFs
         if( l_ctf ) call pftcc%create_polar_absctfmats(build%spproj, params%oritype)
         call pftcc%memoize_ptcls
-        if( trim(params%sh_inv).eq.'yes' )then
-            call eulprob_obj_part%fill_tab_shinv(pftcc)
-        else
-            call eulprob_obj_part%fill_tab(pftcc)
-        endif
+        call eulprob_obj_part%fill_tab(pftcc)
         fname = trim(DIST_FBODY)//int2str_pad(params%part,params%numlen)//'.dat'
         call eulprob_obj_part%write_tab(fname)
         call eulprob_obj_part%kill
