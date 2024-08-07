1d73c2f6859c7d65539222c9aade1a26a800d397
cyrilr <cyril.reboul@gmail.com>
2024-08-07 18:38:15 +1000

    More cleanup and abinitio shift search work

--- src/main/commanders/simple_commander_refine3D.f90
+++ src/main/commanders/simple_commander_refine3D.f90
@@ -944,7 +944,12 @@ contains
         ! make CTFs
         if( l_ctf ) call pftcc%create_polar_absctfmats(build%spproj, params%oritype)
         call pftcc%memoize_ptcls
-        call eulprob_obj_part%fill_tab(pftcc)
+        select case(trim(params%algorithm))
+        case('dev')
+            call eulprob_obj_part%fill_tab_dev(pftcc)
+        case DEFAULT
+            call eulprob_obj_part%fill_tab(pftcc)
+        end select
         fname = trim(DIST_FBODY)//int2str_pad(params%part,params%numlen)//'.dat'
         call eulprob_obj_part%write_tab(fname)
         call eulprob_obj_part%kill
