26948bfbef1194194a8e217beec1db0551ce4921
Hans Elmlund <hans.elmlund@nih.gov>
2024-08-17 20:44:09 -0400

    edits to ini3D2

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -179,7 +179,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('9a462b1d')
+call simple_print_git_version('c0b9c57d')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
