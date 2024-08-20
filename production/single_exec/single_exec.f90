d5bff056aa08e7c7949f62cd25873db2687eafab
Hans Elmlund <hans.elmlund@nih.gov>
2024-08-20 12:02:02 -0400

    possible to control trs in first stage ini3D2

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -179,7 +179,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('aeabbbd2')
+call simple_print_git_version('0057160b')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
