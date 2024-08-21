aeaa8bdc407f5c5b3f78718aae56df99e61a3995
Hans Elmlund <hans.elmlund@nih.gov>
2024-08-20 22:28:58 -0400

    another edit automated lp estimation ini3D2

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -414,7 +414,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('e3cd05eb')
+call simple_print_git_version('149a5476')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
