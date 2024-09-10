5479798d158d7c20ba2d3be5a58d18dd4966c0f0
cong <vanc2@nih.gov>
2024-09-10 10:30:28 -0400

    testing different kernel for kpca

--- production/tests/test_kpca/simple_test_kpca.f90
+++ production/tests/test_kpca/simple_test_kpca.f90
@@ -6,7 +6,7 @@ integer, parameter :: D = 2, Q = 4
 type(kpca_svd)     :: kpca_obj
 real, allocatable  :: XY(:,:), data_here(:,:)
 integer            :: N, funit, i
-real               :: avg(D)
+real               :: avg(D), A(3,2), B(3,2), ker(2,2)
 character(len=120) :: binfname
 binfname = '/home/vanc2/Downloads/output.txt'
 open(unit=funit,file=binfname)
@@ -25,4 +25,13 @@ binfname = '/home/vanc2/result.txt'
 open(unit=funit,file=binfname)
 write(funit,*) transpose(data_here)
 call fclose(funit)
+!
+call kpca_obj%kill
+call kpca_obj%new(2, 3, 2)
+A(:,1) = [0, 0, 0]
+A(:,2) = [1, 1, 1]
+B(:,1) = [1, 0, 0]
+B(:,2) = [1, 1, 0]
+call kpca_obj%cosine_kernel(A, B, ker)
+print *, ker
 end program simple_test_kpca
