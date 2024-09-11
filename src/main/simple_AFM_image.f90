b7321ca269266c54788a67b50c9876cbb23c5625
afanoa <afanatif1@gmail.com>
2024-09-11 11:34:15 -0400

    moved all procedures to module.

--- src/main/simple_AFM_image.f90
+++ src/main/simple_AFM_image.f90
@@ -14,10 +14,117 @@ type :: AFM_image
     type(image), allocatable :: img_array(:)
     character(len = 50), allocatable :: img_names(:)
 contains
+    procedure   :: read_ibw1
     procedure   :: pick_valid1
     procedure   :: get_AFM1
+    procedure   :: zero_padding1
+    procedure   :: align_avg1
 end type AFM_image 
 contains
+    subroutine read_ibw1(AFM, fn_in)
+        class(AFM_image), intent(out)       :: AFM
+        character(len=*),      intent(in)  :: fn_in 
+        integer         :: in, check, real_type
+        integer                          :: real_type1, data, total_bytes, bytes_read, iter_ind, prop_ind, img_ind, i
+        real(kind = 4), allocatable          :: Rank3_Data_4byte(:, :, :, :)
+        character(:), allocatable                            :: channel_info
+        character(len = 10)    :: iteration(2), properties(4)
+    
+        type    :: bin_header5
+            INTEGER(KIND=2) :: version 
+            INTEGER(KIND=2) :: checksum 
+            INTEGER(KIND=4) :: wfmSize 
+            INTEGER(KIND=4) :: formulaSize 
+            INTEGER(KIND=4) :: noteSize 
+            INTEGER(KIND=4) :: dataEUnitsSize 
+            INTEGER(KIND=4) :: dimEUnitsSize(4) 
+            INTEGER(KIND=4) :: dimLabelsSize(4)
+            INTEGER(KIND=4) :: sIndicesSize 
+            INTEGER(KIND=4) :: optionsSize1 
+            INTEGER(KIND=4) :: optionsSize2
+        end type
+
+        type    :: wave_header5
+            INTEGER(KIND=4)    :: Structure_Padding
+            INTEGER(C_INT32_T) :: creationDate
+            INTEGER(C_INT32_T) :: modDate
+            INTEGER(KIND=4)    :: npnts 
+            INTEGER(KIND=2)    :: type 
+            INTEGER(KIND=2)    :: dlock 
+            CHARACTER          :: whpad(6)
+            INTEGER(KIND=2)    :: whVersion 
+            CHARACTER          :: bname(32)
+            INTEGER(KIND=4)    :: whpad2
+            INTEGER(KIND=4)    :: DataFolder_Padding
+            INTEGER(KIND=4)    :: nDim(4)
+            REAL(KIND=8)       :: sfA(4)
+            REAL(KIND=8)       :: sfB(4)
+            CHARACTER          :: dataUnits(4)
+            CHARACTER          :: dimUnits(4,4)
+            INTEGER(KIND=2)    :: fsvalid
+            INTEGER(KIND=2)    :: whpad3
+            REAL(KIND=8)       :: topFullScale, botFullScale
+            INTEGER(KIND = 4)  :: dataEUnits, dimEUnits(4), dimLabels(4), waveNoteH
+            INTEGER(KIND=4)    :: whUnused(16)
+            INTEGER(KIND=2)    :: aModified
+            INTEGER(KIND=2)    :: wModified 
+            INTEGER(KIND=2)    :: sModified  
+            CHARACTER          :: useBits
+            CHARACTER          :: kindBits
+            INTEGER(KIND=4)    :: formula_pointer
+            INTEGER(KIND=4)    :: depID
+            INTEGER(KIND=2)    :: whpad4, srcFldr
+            CHARACTER(KIND=4)  :: fileName
+            INTEGER(KIND=4)    :: sIndices 
+        end type
+        type(bin_header5)   :: binheader
+        type(wave_header5)  :: waveheader
+        iteration = [character(len = 10) :: 'Trace', 'Retrace']
+        properties = [character(len = 10) :: 'Height', 'Amplitude', 'Phase', 'ZSensor' ]
+        if(index(fn_in, '.ibw') == 0) then 
+            print *, 'Error: only .ibw files are supported'
+            stop 
+        end if
+        open(newunit = check, file = fn_in, status = 'old', access='stream')
+        read(check) binheader%version
+        if(binheader%version > 5) then
+            open(newunit = in, file = fn_in, status = 'old', access='stream', convert='swap')
+        else
+            open(newunit = in, file = fn_in, status = 'old', access='stream')
+        end if
+        read(in)binheader, waveheader
+        if(binheader%version /= 5) then
+            print *, "Error: only version 5 files are supported"
+            stop 
+        end if 
+        allocate(Rank3_Data_4byte(waveheader%nDim(1) ,waveheader%nDim(2), 1, waveheader%nDim(3)))
+        open(newunit = data, file = fn_in, status = 'old', access='stream')
+        read(data, pos = 385) Rank3_Data_4byte
+        inquire(data, pos = bytes_read)
+        allocate(character(binheader%dimLabelsSize(3)) :: channel_info)
+        allocate(AFM%img_names(size(iteration)*size(properties)))
+        read(data, pos = binheader%noteSize + bytes_read) channel_info
+        ! getting image names 
+        do prop_ind = 1, size(properties)
+            do iter_ind = 1, size(iteration)
+                if( index(channel_info, trim(properties(prop_ind)) // trim(iteration(iter_ind)) ) /= 0 .AND. iter_ind == 2 ) then 
+                    AFM%img_names(iter_ind*prop_ind) = trim(properties(prop_ind)) // trim(iteration(iter_ind))
+                else if( index(channel_info, trim(properties(prop_ind)) // trim(iteration(iter_ind)) ) /= 0 .AND. iter_ind == 1) then
+                    AFM%img_names(2*prop_ind - iter_ind) = trim(properties(prop_ind)) // trim(iteration(iter_ind)) 
+                end if 
+            end do 
+        end do     
+        allocate(AFM%img_array(waveheader%nDim(3)))
+        do img_ind = 1, waveheader%nDim(3)
+            call AFM%img_array(img_ind)%new([waveheader%nDim(1), waveheader%nDim(2), 1], real(waveheader%sfA(1)) * 10.**10.)
+            call AFM%img_array(img_ind)%set_rmat(Rank3_Data_4byte(:, :, img_ind, :), .false.)
+            call AFM%img_array(img_ind)%norm_minmax()
+            call AFM%img_array(img_ind)%bin_inv
+        end do 
+        deallocate(Rank3_Data_4byte)
+        
+    end subroutine read_ibw1
+
     subroutine pick_valid1(AFM_in)
         class(AFM_image), intent(inout)    :: AFM_in 
         type(image)                        :: HeightTrace, HeightRetrace, AvgHeight
@@ -31,7 +138,6 @@ contains
         logical                            :: outs = .true. 
         integer                            :: neighbor(3, 8), nsiz, center(3)
         real                               :: corr_r, corr_t, val_score_t, val_score_r 
-        ! real, allocatable                  :: corr_final(:)   
         real, allocatable                  :: corr_final_t(:), corr_final_r(:)
         
 
@@ -59,7 +165,6 @@ contains
         call retrace_slim%new(ldim_box, smpd_box)
 
         box_count = 0 
-        ! allocate(corr_final(avg_p%get_nboxes()))
         allocate(val_centers(3, avg_p%get_nboxes()))
         call avg_p%get_positions(pickpos) 
         allocate(corr_final_r(avg_p%get_nboxes()))
@@ -126,5 +231,75 @@ contains
         type(image), intent(out)  :: image_at_key
         image_at_key = AFM_Hash%img_array(findloc(index(AFM_Hash%img_names, key),1, dim = 1))
     end subroutine get_AFM1
+
+    subroutine zero_padding1(AFM_pad)
+        class(AFM_image), intent(inout)  :: AFM_pad
+        integer        :: img_ind
+        integer :: dim(3)
+        do img_ind = 1, size(AFM_pad%img_array)
+            dim = AFM_pad%img_array(img_ind)%get_ldim()
+            if( dim(1) /= dim(2) ) then 
+                call AFM_pad%img_array(img_ind)%pad_inplace([maxval(dim), maxval(dim), 1])
+                call AFM_pad%img_array(img_ind)%vis()
+            end if 
+        end do 
+    end subroutine zero_padding1
+    
+    subroutine align_avg1(AFM_in, Align_AFM)
+        class(AFM_image), intent(in)     :: AFM_in 
+        class(AFM_image), intent(out)    :: Align_AFM
+        real, allocatable                  :: shifts(:, :)
+        integer                            :: num_avg, avg_ind, prop_ind, new_size, count, num_mic, tr_ind, retr_ind, i 
+        character(len = 50)                :: new_name 
+        num_mic = size(AFM_in%img_array)
+        new_size = int(num_mic*1.5)
+        allocate(Align_AFM%img_array(new_size))
+        allocate(Align_AFM%img_names(new_size))
+        num_avg = num_mic/2
+        allocate(shifts(2, num_avg))
+        if(modulo(size(shifts), 2) /= 0) then 
+            print *, 'One or more traces are not present'
+        end if 
+        do i = 1, num_mic
+            Align_AFM%img_array(i) = AFM_in%img_array(i)
+            Align_AFM%img_names(i) = AFM_in%img_names(i)
+        end do 
+        count = 0
+        do i = num_mic + 1, new_size
+            Align_AFM%img_array(i) = AFM_in%img_array(i - num_mic + count)
+            count = count + 1
+        end do 
+    
+        do avg_ind = 1, size(Align_AFM%img_array)
+            call Align_AFM%img_array(avg_ind)%fft()
+        end do
+
+        count = 0
+        do avg_ind = num_mic + 1, new_size
+            count = count + 1
+            call Align_AFM%img_array(avg_ind)%fcorr_shift(Align_AFM%img_array(avg_ind - num_mic + count), 20., shifts(:, count), .true.)
+        end do 
+        
+        do avg_ind = 1, num_avg
+            call Align_AFM%img_array(2*avg_ind)%fft()
+            call Align_AFM%img_array(2*avg_ind)%shift([shifts(1, avg_ind), shifts(2, avg_ind), 0.])
+            call Align_AFM%img_array(2*avg_ind)%ifft()
+        end do
+        
+        do avg_ind = 1, new_size 
+            if(Align_AFM%img_array(avg_ind)%is_ft()) then
+                call Align_AFM%img_array(avg_ind)%ifft()
+            end if    
+        end do 
+        count = 0 
+        do avg_ind = num_mic + 1, new_size 
+           tr_ind = avg_ind - num_mic + count
+           retr_ind = avg_ind - num_mic + 1 + count
+           Align_AFM%img_array(avg_ind) =  Align_AFM%img_array(tr_ind) * 0.5 + Align_AFM%img_array(retr_ind) * 0.5
+           new_name = AFM_in%img_names(tr_ind)
+           Align_AFM%img_names(avg_ind) = 'Avg' // new_name(1:len_trim(new_name)  - 5)
+           count = count + 1
+        end do 
+    end subroutine align_avg1
 
 end module simple_AFM_image 
