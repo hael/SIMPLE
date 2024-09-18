8fd2b8bce53e86dc45c23996e47ec75e02160102
cong <vanc2@nih.gov>
2024-09-18 15:47:19 -0400

    testing kpca with new eigvals/vecs function

--- production/tests/test_eigh/simple_test_eigh.f90
+++ production/tests/test_eigh/simple_test_eigh.f90
@@ -2,13 +2,18 @@ program simple_test_eigh
 include 'simple_lib.f08'
 integer, parameter :: N = 5, N_EIGS = 3
 integer  :: i
-real(dp) :: mat(N, N), eigvals(N_EIGS), eigvecs(N,N_EIGS)
-mat(:,1) = [ 0.67, 0.00, 0.00, 0.00, 0.00]
-mat(:,2) = [-0.20, 3.82, 0.00, 0.00, 0.00]
-mat(:,3) = [ 0.19,-0.13, 3.27, 0.00, 0.00]
-mat(:,4) = [-1.06, 1.06, 0.11, 5.86, 0.00]
+real(dp) :: mat(N, N), eigvals(N_EIGS), eigvecs(N,N_EIGS), tmp(N,N), eig_vals(N), mat_ori(N, N)
+mat(:,1) = [ 0.67,-0.20, 0.19,-1.06, 0.46]
+mat(:,2) = [-0.20, 3.82,-0.13, 1.06,-0.48]
+mat(:,3) = [ 0.19,-0.13, 3.27, 0.11, 1.10]
+mat(:,4) = [-1.06, 1.06, 0.11, 5.86,-0.98]
 mat(:,5) = [ 0.46,-0.48, 1.10,-0.98, 3.54]
-call eigh( N, mat, N_EIGS, eigvals, eigvecs )
+mat_ori  = mat
+print *, 'SVDCMP result:'
+call svdcmp(mat, eig_vals, tmp)
+print *, eig_vals
+print *, 'EIGH result:'
+call eigh( N, mat_ori, N_EIGS, eigvals, eigvecs )
 print *, 'Selected eigenvalues', eigvals
 print *, 'Selected eigenvectors (stored columnwise)'
 do i = 1, N
--- production/tests/test_kpca/simple_test_kpca.f90
+++ production/tests/test_kpca/simple_test_kpca.f90
@@ -1,13 +1,19 @@
 program simple_test_kpca
 include 'simple_lib.f08'
-use simple_kpca_svd, only: kpca_svd
+use simple_cmdline,    only: cmdline
+use simple_parameters, only: parameters
+use simple_kpca_svd,   only: kpca_svd
 implicit none
 integer, parameter :: D = 2, Q = 4
 type(kpca_svd)     :: kpca_obj
+type(cmdline)      :: cline
+type(parameters)   :: p
 real, allocatable  :: XY(:,:), data_here(:,:)
 integer            :: N, funit, i
 real               :: avg(D)
 character(len=120) :: binfname
+call cline%parse_oldschool
+call p%new(cline)
 binfname = '/home/vanc2/Downloads/output.txt'
 open(unit=funit,file=binfname)
 read(funit,*) N
