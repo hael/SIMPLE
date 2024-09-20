8b0284a11f58f86f24ebaf441ffaf9e3d84edb75
cong <vanc2@nih.gov>
2024-09-20 14:28:03 -0400

    changing eigh unit test

--- production/tests/test_eigh/simple_test_eigh.f90
+++ production/tests/test_eigh/simple_test_eigh.f90
@@ -1,8 +1,10 @@
 program simple_test_eigh
 include 'simple_lib.f08'
-integer, parameter :: N = 5, N_EIGS = 3
+integer, parameter :: N = 5, N_EIGS = 3, N_L = 15000, N_EIGS_L = 12
 integer  :: i
 real(dp) :: mat(N, N), eigvals(N_EIGS), eigvecs(N,N_EIGS), tmp(N,N), eig_vals(N), mat_ori(N, N)
+real(dp) :: A(N_L, N_L), A_vals(N_EIGS_L), A_eigvecs(1000, N_EIGS_L)
+integer(timer_int_kind) :: eigh_t
 mat(:,1) = [ 0.67,-0.20, 0.19,-1.06, 0.46]
 mat(:,2) = [-0.20, 3.82,-0.13, 1.06,-0.48]
 mat(:,3) = [ 0.19,-0.13, 3.27, 0.11, 1.10]
@@ -19,4 +21,8 @@ print *, 'Selected eigenvectors (stored columnwise)'
 do i = 1, N
     print *, eigvecs(i,:)
 enddo
+call random_number(A)
+eigh_t = tic()
+call eigh( N_L, A, N_EIGS_L, A_vals, A_eigvecs )
+print *, toc(eigh_t)
 end program simple_test_eigh
