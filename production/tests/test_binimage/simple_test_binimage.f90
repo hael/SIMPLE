a00bcef64a1707ab4fedb7554d18aee79bd2f47a
cong <vanc2@nih.gov>
2024-10-02 14:57:09 -0400

    testing much faster find_ccs algorithm

--- production/tests/test_binimage/simple_test_binimage.f90
+++ production/tests/test_binimage/simple_test_binimage.f90
@@ -3,17 +3,34 @@ program simple_test_binimage
     include 'simple_lib.f08'
     use simple_binimage
 
-    type(binimage) :: test_binimage, test_binimage_3D
-    integer :: imat(4,4,1), imat_3D(3,3,2)
+    type(binimage)       :: test_binimage, test_binimage_3D, ccimage
+    integer, allocatable :: imat_ccs(:,:,:)
+    integer :: imat(4,4,1), imat_3D(3,3,2), nccs, i
     real    :: dist, dist_3D
 
     !! 2D test
     print *, '2D TEST'
     call test_binimage%new_bimg(ldim=[4,4,1], smpd=1.)
-    imat = reshape( (/ 0,0,0,0,1,1,1,0,0,1,1,0,0,0,0,1 /), shape(imat) )
+    imat(1,:,1) = [1,0,0,0]
+    imat(2,:,1) = [1,0,1,0]
+    imat(3,:,1) = [0,0,0,0]
+    imat(4,:,1) = [0,0,0,1]
     call test_binimage%set_imat(imat)
     call test_binimage%max_dist(dist)
     print *, 'MAXIMUM DISTANCE FROM CENTER IS ', dist
+    call test_binimage%find_ccs(ccimage)
+    call ccimage%get_nccs(nccs)
+    print *, nccs
+    call ccimage%get_imat(imat_ccs)
+    do i = 1,4
+        print *, imat_ccs(i,:,1)
+    enddo
+    call test_binimage%find2_ccs(ccimage)
+    print *, nccs
+    call ccimage%get_imat(imat_ccs)
+    do i = 1,4
+        print *, imat_ccs(i,:,1)
+    enddo
 
     print *, ' '
 
