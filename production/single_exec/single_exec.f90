36a2de9efc0f3a8e5a18bbc38527c88c03c21f78
Hans Elmlund <hans.elmlund@nih.gov>
2024-10-18 12:40:43 -0400

    some changes to abinitio workflow based on results on export gate, for further testing

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -185,7 +185,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('cdd9ce33')
+call simple_print_git_version('3f07f560')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
