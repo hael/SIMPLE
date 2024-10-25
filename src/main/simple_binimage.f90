45987253fafa540c0d6a2aedd4fb732a899c1adc
cyrilr <cyril.reboul@gmail.com>
2024-10-25 13:35:23 +1100

    Minor bugfix binimage

--- src/main/simple_binimage.f90
+++ src/main/simple_binimage.f90
@@ -162,8 +162,13 @@ contains
         integer,           allocatable   :: dqueue(:,:)     ! queue data structure
         integer :: i, j, k, comp_ind, queue_first, queue_last
         logical :: visited(self%bldim(1),self%bldim(2),self%bldim(3))
-        if( present(update_imat) .and. update_imat ) call self%set_imat
-        if( present(black)       .and. black)        self%bimat = -1 * (self%bimat - 1)
+        logical :: l_black
+        if( present(update_imat) )then
+            if( update_imat ) call self%set_imat
+        endif
+        l_black = .false.
+        if( present(black) ) l_black = black
+        if( l_black ) self%bimat = -1 * (self%bimat - 1)
         call ccimage%new_bimg(self%bldim, self%bsmpd)
         visited  = .false.
         comp_ind = 0
@@ -181,7 +186,7 @@ contains
         ! update instance
         ccimage%bimat_is_set = .true.
         ccimage%nccs         = maxval(ccimage%bimat)
-        if( present(black) .and. black ) self%bimat = -1 * (self%bimat - 1)
+        if( l_black ) self%bimat = -1 * (self%bimat - 1)
         call ccimage%update_img_rmat
 
       contains
@@ -190,8 +195,8 @@ contains
             integer, intent(in) :: i,j,k,iVal
             if(  i < 1 .or. i > self%bldim(1) .or.&
                 &j < 1 .or. j > self%bldim(2) .or.&
-                &k < 1 .or. k > self%bldim(3) .or.&
-                &visited(i,j,k) .or. self%bimat(i,j,k) /= iVal ) return
+                &k < 1 .or. k > self%bldim(3) ) return
+            if( visited(i,j,k) .or. self%bimat(i,j,k) /= iVal ) return
             visited(i, j, k)     = .true.
             ccimage%bimat(i,j,k) = comp_ind
             queue_last           = queue_last + 1
