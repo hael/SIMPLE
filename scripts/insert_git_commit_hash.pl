1209d503fa4b441d840e373a01ed7c7b8a7fbb9b
cyrilr <cyril.reboul@gmail.com>
2024-10-29 16:55:10 +1100

    Added version hash to simple_stream

--- scripts/insert_git_commit_hash.pl
+++ scripts/insert_git_commit_hash.pl
@@ -7,9 +7,11 @@ my $dir = getcwd;
 chomp($dir);
 my $simple_exec     = `readlink -f ../../production/simple_exec/simple_exec.f90`;
 my $single_exec     = `readlink -f ../../production/single_exec/single_exec.f90`;
+my $stream_exec     = `readlink -f ../../production/simple_stream/simple_stream.f90`;
 my $git_commit_hash = `git rev-parse --short HEAD`;
 chomp($simple_exec);
 chomp($single_exec);
+chomp($stream_exec);
 chomp($git_commit_hash);
 my @simple_exec_doc;
 tie @simple_exec_doc, 'Tie::File', $simple_exec or die "Cannot tie to file: $simple_exec\n";
@@ -22,6 +24,14 @@ foreach my $line (@simple_exec_doc){
 my @single_exec_doc;
 tie @single_exec_doc, 'Tie::File', $single_exec or die "Cannot tie to file: $single_exec\n";
 foreach my $line (@single_exec_doc){
+    if( $line =~ /call simple_print_git_version\(\'(.+)\'\)/ ){
+        chomp($1);
+        $line =~ s/$1/$git_commit_hash/;
+    }
+}
+my @simple_stream_doc;
+tie @simple_stream_doc, 'Tie::File', $stream_exec or die "Cannot tie to file: $stream_exec\n";
+foreach my $line (@simple_stream_doc){
     if( $line =~ /call simple_print_git_version\(\'(.+)\'\)/ ){
         chomp($1);
         $line =~ s/$1/$git_commit_hash/;
