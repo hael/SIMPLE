d7aab3a44dba0fce5d4931166d01f4032c1f3bc3
Ruben Meana-Paneda <rubenmepa@gmail.com>
2024-11-05 13:50:59 -0500

    bugfix

--- src/main/commanders/simple_commander_volops.f90
+++ src/main/commanders/simple_commander_volops.f90
@@ -445,9 +445,10 @@ contains
         type(parameters)       :: params
         integer                :: i
         character(:), allocatable :: fn_vol_docked
-        if( .not. cline%defined('mkdir') ) call cline%set('mkdir', 'yes')
+        !if( .not. cline%defined('mkdir') ) call cline%set('mkdir', 'yes')
         call params%new(cline)
         fn_vol_docked = trim(get_fbody(params%vols(2),'mrc'))//'_docked.mrc'
+        !fn_vol_docked = trim(get_fbody(basename(params%vols(2)),'mrc'))//'_docked.mrc'
         select case( trim(params%dockmode) )
             case('shift')
                 call dvols%new(params%vols(1), params%vols(2), params%smpd, params%lpstop, params%lpstart, params%mskdiam, mag=.true.)
