2e7d89e3d1c69aaa1e2eb14011031bbc1233e1ad
Hans Elmlund <hans.elmlund@nih.gov>
2024-12-02 00:59:45 -0500

    speedup calc_start_rec in abinitio3D

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -420,7 +420,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('3deef4fc')
+call simple_print_git_version('4812c4e6')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
