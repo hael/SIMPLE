2b0a95fbedef75dc93e1c3bc6b85011b722576fc
Ruben Meana-Paneda <rubenmepa@gmail.com>
2024-12-13 14:31:56 -0500

    add mrc validation test for George

---
+++ production/tests/test_mrc_validation/simple_test_mrc_validation.f90
@@ -0,0 +1,27 @@
+program simple_test_mrc_validation
+include 'simple_lib.f08' 
+use simple_atoms, only: atoms
+use simple_image
+implicit none
+character(len=STDLEN)         :: vol_file
+character(len=:), allocatable :: smpd_char
+character(len=:), allocatable :: cmd
+type(image)                   :: vol 
+real                          :: smpd
+integer                       :: ldim(3), ifoo, slen
+if( command_argument_count() /= 2 )then
+    write(logfhandle,'(a)') 'ERROR! Usage: simple_test_mrc_validation vol.mrc smpd'
+    write(logfhandle,'(a)') 'vol.mrc : volume' 
+    write(logfhandle,'(a)') 'smpd    : SMPD value in Angstrom per voxel ' 
+else
+    call get_command_argument(1, vol_file)
+    allocate(character(slen) :: smpd_char)
+    call get_command_argument(2, smpd_char)
+    read(smpd_char, *) smpd
+endif
+call find_ldim_nptcls(vol_file,  ldim, ifoo)
+call vol%new(ldim, smpd)
+call vol%read(vol_file)
+call vol%write('vol_simple.mrc')
+call vol%kill
+end program simple_test_mrc_validation
