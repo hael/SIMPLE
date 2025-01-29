5d4dba4871841013dd81287c5d36bfd42d0e12d6
cong <vanc2@nih.gov>
2025-01-29 15:28:08 -0500

    testing removing state overlap in 3D convergence, due to particles bouncing between states, and more unit testing of invlinear and pca

--- production/tests/test_invlinear_vols/simple_test_invlinear_vols.f90
+++ production/tests/test_invlinear_vols/simple_test_invlinear_vols.f90
@@ -1,10 +1,13 @@
 program simple_test_invlinear_vols
 include 'simple_lib.f08'
 use simple_opt_filter, only: uni_inv_linear
+use simple_pca_svd,    only: pca_svd
 implicit none
-integer, parameter :: NSTATES = 3, NP = 10, LOW_IND = 1, HIGH_IND = 4
+integer, parameter :: NSTATES = 3, NP = 10, LOW_IND = 1, HIGH_IND = 4, NC = 2, MAXPCAITS = 10, NINDS = HIGH_IND - LOW_IND + 1
+type(pca_svd)      :: pca_obj
 integer :: ivol, j
-real    :: truths(NP,NSTATES), vols(NP,NSTATES), probs(NSTATES,NSTATES), truths_inv(NP,NSTATES)
+real    :: truths(NP,NSTATES), vols(NP,NSTATES), probs(NSTATES,NSTATES), truths_inv(NP,NSTATES), avg(NSTATES),&
+        &data_cen(NSTATES, NINDS), tmpvec(NSTATES), data_pca(NSTATES, NINDS)
 truths(:,1) = [5.,   4.,  3., 4., 5., 6., 7., 8.,  9., 10.]
 truths(:,2) = [1.1,  1.9, 3., 4., 3., 5., 7., 8., 11., 14.]
 truths(:,3) = [1.05, 2.1, 3., 4., 5., 6., 7., 8.,  9., 10.]
@@ -19,4 +22,21 @@ do ivol = 1, NSTATES
     print *, vols(:,ivol)
 enddo
 call uni_inv_linear(NSTATES, HIGH_IND - LOW_IND + 1, vols(LOW_IND:HIGH_IND,:), truths_inv(LOW_IND:HIGH_IND,:), verbose=.true.)
+! PCA
+data_cen = transpose(vols(LOW_IND:HIGH_IND,:))
+avg      = sum(data_cen, dim=2) / real(NSTATES)
+do j = 1, NSTATES
+    data_cen(j,:) = data_cen(j,:) - avg(j)
+enddo
+call pca_obj%new(NINDS, NSTATES, NC)
+call pca_obj%master(data_cen, MAXPCAITS)
+do j = 1, NINDS
+    call pca_obj%generate(j, avg, tmpvec)
+    data_pca(:,j) = tmpvec
+end do
+print *, 'Pre-imaged data using PCA:'
+do j = 1, NSTATES
+    print *, data_pca(j,:)
+enddo
+print *, sum(data_pca, dim=1)/real(NSTATES)
 end program simple_test_invlinear_vols
