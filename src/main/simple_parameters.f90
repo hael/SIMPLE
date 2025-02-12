db987786f8230853a4b7a1a52b1c34d7498651b7
cong <vanc2@nih.gov>
2025-02-12 14:58:16 -0500

    minor changes to latest push

--- src/main/simple_parameters.f90
+++ src/main/simple_parameters.f90
@@ -340,7 +340,6 @@ type :: parameters
     integer :: shift_stage=0
     integer :: split_stage=7       !< splitting stage when multivol_mode==docked
     integer :: startit=1           !< start iterating from here
-    integer :: endingit=1          !< ending iteration number
     integer :: state=1             !< state to extract
     integer :: stepsz=1            !< size of step{1}
     integer :: tofny=0
@@ -834,7 +833,6 @@ contains
         call check_iarg('shift_stage',    self%shift_stage)
         call check_iarg('split_stage',    self%split_stage)
         call check_iarg('startit',        self%startit)
-        call check_iarg('endingit',       self%endingit)
         call check_iarg('state',          self%state)
         call check_iarg('stepsz',         self%stepsz)
         call check_iarg('top',            self%top)
