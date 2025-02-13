6c8a93f4239a02daf4f7fa921f145e68e4209016
cong <vanc2@nih.gov>
2025-02-13 13:34:13 -0500

    minor improvement of point-clustering unit test and working on atom position matching

---
+++ production/tests/test_atom_posmatch/simple_test_atom_posmatch.f90
@@ -0,0 +1,4 @@
+program simple_test_atom_posmatch
+include 'simple_lib.f08'
+implicit none
+end program simple_test_atom_posmatch
--- production/tests/test_point_cluster/simple_test_point_cluster.f90
+++ production/tests/test_point_cluster/simple_test_point_cluster.f90
@@ -3,7 +3,7 @@ include 'simple_lib.f08'
 implicit none
 integer, parameter :: N_CLUSTER = 5, N_POINTS = 50
 real,    parameter :: EPS = 0.2
-integer :: i, j, iter, truth, cur_clusN, prev_clusN, inds(N_POINTS), cur_cluster(N_POINTS), prev_cluster(N_POINTS)
+integer :: i, j, iter, truth, cur_clusN, prev_clusN, inds(N_POINTS)
 real    :: points(N_POINTS), avg_points(N_POINTS), costs(N_POINTS)
 logical :: taken(N_POINTS)
 call seed_rnd
@@ -17,12 +17,11 @@ print *, points
 avg_points = points
 cur_clusN  = N_POINTS
 do iter = 1, 10
-    prev_clusN   = cur_clusN
-    points       = avg_points(1:prev_clusN)
-    cur_clusN    = 0
-    taken        = .false.
-    avg_points   = 0.
-    prev_cluster = (/(j,j=1,N_POINTS)/)
+    prev_clusN = cur_clusN
+    points     = avg_points(1:prev_clusN)
+    cur_clusN  = 0
+    taken      = .false.
+    avg_points = 0.
     do i = 1, prev_clusN
         if( taken(i) ) cycle
         taken(i)  = .true.
@@ -32,12 +31,11 @@ do iter = 1, 10
             if( taken(j) .or. j == i ) cycle
             costs(j) = abs(points(j) - points(i))
         enddo
-        inds = (/(j,j=1,prev_clusN)/)
-        call hpsort(costs, inds)
+        j = minloc(cost)
         ! check if the best cost is within EPS
-        if( costs(1) < EPS )then
-            taken(inds(1)) = .true.
-            avg_points(cur_clusN) = (points(i) + points(inds(1))) / 2.
+        if( costs(j) < EPS )then
+            taken(j) = .true.
+            avg_points(cur_clusN) = (points(i) + points(j)) / 2.
         else
             avg_points(cur_clusN) = points(i)
         endif
@@ -47,5 +45,4 @@ do iter = 1, 10
     print *, avg_points(1:cur_clusN)
     if( prev_clusN == cur_clusN ) exit
 enddo
-
 end program simple_test_point_cluster
