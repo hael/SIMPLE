bff7488ab4dd368e9fbb12d60a3aa8367791a147
Hans Elmlund <hans.elmlund@nih.gov>
2025-03-06 14:50:10 -0500

    bugfix

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -438,7 +438,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('91b17943')
+call simple_print_git_version('b1cc2ae1')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
