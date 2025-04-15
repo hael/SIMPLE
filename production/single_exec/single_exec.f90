291ca6e54f0b349476fd08b53c5b6030bc4283a4
Hans Elmlund <hans.elmlund@nih.gov>
2025-04-15 13:45:05 -0400

    initial refactoring to extend class sampling functionality

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -206,7 +206,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('ab874f19')
+call simple_print_git_version('350d0212')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
