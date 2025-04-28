b5bfff0bf2e8f06cd6ecf52d958abf6b15e341d5
Hans Elmlund <hans.elmlund@nih.gov>
2025-04-28 10:56:34 -0400

    autoselect3D_cavgs: reporting average local standard deviation of spectral groups

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -478,7 +478,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('cec07976')
+call simple_print_git_version('cab7ce45')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
