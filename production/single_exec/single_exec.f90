3090c2994866544deae7191ef41cba301b541a4a
Joseph Caesar <joseph.caesar@path.ox.ac.uk>
2025-05-14 23:46:07 +0100

    Nice 97a417a updates and bugfix Ubuntu compile

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -211,7 +211,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('41e46ba6')
+call simple_print_git_version('c70a45ee')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
