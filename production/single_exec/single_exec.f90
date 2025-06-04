2bba78e225bf3a4f3ceb05e4f0d19a6ed30fbb72
Hans Elmlund <hans.elmlund@nih.gov>
2025-06-03 22:26:23 -0400

    cluster_cavgs: use of l1 norm better for rank boundary estimation with common lines

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -211,7 +211,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('fb13461b')
+call simple_print_git_version('cbec4161')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
