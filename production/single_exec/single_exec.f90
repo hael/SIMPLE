0e6935c9562e356042fef385f096ff3cf9b6248c
Hans Elmlund <hans.elmlund@nih.gov>
2025-06-04 21:58:22 -0400

    cluster_cavgs: rank bound estimation using spectral power distance works, more testing needed

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -211,7 +211,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('5c0058d0')
+call simple_print_git_version('e58dfb8e')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
