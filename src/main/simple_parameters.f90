dcf2d544c0611c652bbb01c93e8973e08923df78
Hans Elmlund <hans.elmlund@nih.gov>
2025-06-16 11:27:27 -0400

    cluster_cavgs: good/bad assignment based on thresholding of joint score

--- src/main/simple_parameters.f90
+++ src/main/simple_parameters.f90
@@ -481,7 +481,6 @@ type :: parameters
     real    :: pool_threshold_factor=POOL_THRESHOLD_FACTOR  !< stream pool class rejection adjustment
     real    :: prob_athres=10.     !< angle threshold for prob distribution samplings
     real    :: res_target = 3.     !< resolution target in A
-    real    :: res_cutoff = 8.     !< resolution cutoff
     real    :: scale=1.            !< image scale factor{1}
     real    :: sherr=0.            !< shift error(in pixels){2}
     real    :: sigma=1.0           !< for gaussian function generation {1.}
@@ -1002,7 +1001,6 @@ contains
         call check_rarg('phranlp',        self%phranlp)
         call check_rarg('pool_threshold_factor', self%pool_threshold_factor)
         call check_rarg('prob_athres',    self%prob_athres)
-        call check_rarg('res_cutoff',     self%res_cutoff)
         call check_rarg('res_target',     self%res_target)
         call check_rarg('scale',          self%scale)
         call check_rarg('sherr',          self%sherr)
