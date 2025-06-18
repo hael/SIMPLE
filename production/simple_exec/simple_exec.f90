f6958743d04bca3422edebb4d2de0b5f64f6fee4
Hans Elmlund <hans.elmlund@nih.gov>
2025-06-18 09:32:17 -0400

    cluster_cavgs: changes to good/bad selection approach based on Cyrils tests

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -469,7 +469,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('fe93c2b9')
+call simple_print_git_version('8d12a20f')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
