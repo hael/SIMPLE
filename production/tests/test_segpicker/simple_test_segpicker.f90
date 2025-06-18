6609e2164d9ea16387475c3db64d181e521e6bc1
Ruben Meana-Paneda <rubenmepa@gmail.com>
2025-06-18 07:59:20 -0400

    add test to play with picker

--- production/tests/test_segpicker/simple_test_segpicker.f90
+++ production/tests/test_segpicker/simple_test_segpicker.f90
@@ -7,7 +7,8 @@ use simple_pickseg
 implicit none
 #include "simple_local_flags.inc"
 
-character(len=*), parameter :: mic       = '/home/elmlundho/cache/NanoX/NP_Minyoung_dry/intgs/FoilHole_6388490_Data_6383078_31_20240207_130359_EER_intg.mrc'
+!character(len=*), parameter :: mic       = '/home/elmlundho/cache/NanoX/NP_Minyoung_dry/intgs/FoilHole_6388490_Data_6383078_31_20240207_130359_EER_intg.mrc'
+character(len=*), parameter :: mic       = '/usr/local/data/NP_charm_grid2/2_motion_correct/FoilHole_4603854_Data_4599189_4599191_20240605_070652_EER_intg.mrc'
 type(parameters), target    :: params
 type(pickseg) :: picker
 
