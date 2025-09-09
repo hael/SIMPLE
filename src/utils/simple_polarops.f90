9ee16f0818674a56fefdc3da2c2520c62c3ea6a5
cyrilr <cyril.reboul@gmail.com>
2025-09-09 10:04:37 +1000

    Removed legacy convention in references orientations parameters and corresponding edits to polar symmetry

--- src/utils/simple_polarops.f90
+++ src/utils/simple_polarops.f90
@@ -222,15 +222,7 @@ contains
         integer     :: icls, eo_pop(2), pop, k, pops(ncls), npops, m
         real        :: res_fsc05, res_fsc0143, min_res_fsc0143, max_res_fsc0143, avg_res_fsc0143, avg_res_fsc05,&
                       &cavg_clin_frcs(kfromto(1):kfromto(2),ncls), dfrcs(kfromto(1):kfromto(2),ncls)
-        logical     :: ymirror
         if( l_comlin )then
-            ! handling of mirroring
-            ymirror = .false.
-            if( build_glob%pgrpsyms%is_circular() )then
-                ymirror = .true.
-            else
-                ymirror = build_glob%pgrpsyms%is_icosahedral()
-            endif
             ! 3D-related tasks
             if( .not. present(reforis) )THROW_HARD('Reference orientations need be inputted in polar_cavger_merge_eos_and_norm')
             ! Mirroring etc.
@@ -306,15 +298,9 @@ contains
                     call safe_norm(pfto, ctf2o, pfts_odd(:,:,icls))
                     ! mirroring the restored images
                     m = reforis%get_int(icls,'mirr')
-                    if( ymirror )then
-                        call mirror_pft(pfts_merg(:,:,icls), pfts_merg(:,:,m))
-                        call mirror_pft(pfts_even(:,:,icls), pfts_even(:,:,m))
-                        call mirror_pft(pfts_odd(:,:,icls),  pfts_odd(:,:,m))
-                    else
-                        call mirrorx_pft(pfts_merg(:,:,icls), pfts_merg(:,:,m))
-                        call mirrorx_pft(pfts_even(:,:,icls), pfts_even(:,:,m))
-                        call mirrorx_pft(pfts_odd(:,:,icls),  pfts_odd(:,:,m))
-                    endif
+                    call mirror_pft(pfts_merg(:,:,icls), pfts_merg(:,:,m))
+                    call mirror_pft(pfts_even(:,:,icls), pfts_even(:,:,m))
+                    call mirror_pft(pfts_odd(:,:,icls),  pfts_odd(:,:,m))
                 enddo
                 !$omp end parallel do
             case('vol')
@@ -334,15 +320,9 @@ contains
                     call safe_norm(pfto, ctf2o, pfts_odd(:,:,icls))
                     ! mirroring the restored images
                     m = reforis%get_int(icls,'mirr')
-                    if( ymirror )then
-                        call mirror_pft(pfts_merg(:,:,icls), pfts_merg(:,:,m))
-                        call mirror_pft(pfts_even(:,:,icls), pfts_even(:,:,m))
-                        call mirror_pft(pfts_odd(:,:,icls),  pfts_odd(:,:,m))
-                    else
-                        call mirrorx_pft(pfts_merg(:,:,icls), pfts_merg(:,:,m))
-                        call mirrorx_pft(pfts_even(:,:,icls), pfts_even(:,:,m))
-                        call mirrorx_pft(pfts_odd(:,:,icls),  pfts_odd(:,:,m))
-                    endif
+                    call mirror_pft(pfts_merg(:,:,icls), pfts_merg(:,:,m))
+                    call mirror_pft(pfts_even(:,:,icls), pfts_even(:,:,m))
+                    call mirror_pft(pfts_odd(:,:,icls),  pfts_odd(:,:,m))
                 enddo
                 !$omp end parallel do
             case DEFAULT
@@ -432,21 +412,14 @@ contains
             !$omp parallel do default(shared) proc_bind(close) private(iref,mref,pft,ctf2)
             do iref = 1,ncls/2
                 mref = ref_space%get_int(iref,'mirr')
-                if( ymirror )then
-                    call mirror_pft(pfts_even(:,:,mref), pft)
-                    pfts_even(:,:,iref) = pfts_even(:,:,iref) + pft
-                    call mirror_pft(pfts_odd(:,:,mref), pft)
-                    pfts_odd(:,:,iref)  = pfts_odd(:,:,iref)  + pft
-                    call mirror_pft( pfts_even(:,:,iref), pfts_even(:,:,mref))
-                    call mirror_pft( pfts_odd(:,:,iref),  pfts_odd(:,:,mref))
-                else
-                    call mirrorx_pft(pfts_even(:,:,mref), pft)
-                    pfts_even(:,:,iref) = pfts_even(:,:,iref) + pft
-                    call mirrorx_pft(pfts_odd(:,:,mref), pft)
-                    pfts_odd(:,:,iref)  = pfts_odd(:,:,iref)  + pft
-                    call mirrorx_pft( pfts_even(:,:,iref), pfts_even(:,:,mref))
-                    call mirrorx_pft( pfts_odd(:,:,iref),  pfts_odd(:,:,mref))
-                endif
+                ! Fourier components
+                call mirror_pft(pfts_even(:,:,mref), pft)
+                pfts_even(:,:,iref) = pfts_even(:,:,iref) + pft
+                call mirror_pft(pfts_odd(:,:,mref), pft)
+                pfts_odd(:,:,iref)  = pfts_odd(:,:,iref)  + pft
+                call mirror_pft( pfts_even(:,:,iref), pfts_even(:,:,mref))
+                call mirror_pft( pfts_odd(:,:,iref),  pfts_odd(:,:,mref))
+                ! CTF
                 call mirror_ctf2(ctf2_even(:,:,mref), ctf2)
                 ctf2_even(:,:,iref) = ctf2_even(:,:,iref) + ctf2
                 call mirror_ctf2(ctf2_odd(:,:,mref),  ctf2)
@@ -671,13 +644,8 @@ contains
             !$omp do schedule(static)
             do iref = 1,ncls/2
                 mref = ref_space%get_int(iref,'mirr')
-                if( ymirror )then
-                    call mirror_pft(pfts_clin_even(:,:,iref), pfts_clin_even(:,:,mref))
-                    call mirror_pft(pfts_clin_odd(:,:,iref),  pfts_clin_odd(:,:,mref))
-                else
-                    call mirrorx_pft(pfts_clin_even(:,:,iref), pfts_clin_even(:,:,mref))
-                    call mirrorx_pft(pfts_clin_odd(:,:,iref),  pfts_clin_odd(:,:,mref))
-                endif
+                call mirror_pft(pfts_clin_even(:,:,iref), pfts_clin_even(:,:,mref))
+                call mirror_pft(pfts_clin_odd(:,:,iref),  pfts_clin_odd(:,:,mref))
                 call mirror_ctf2(ctf2_clin_even(:,:,iref), ctf2_clin_even(:,:,mref))
                 call mirror_ctf2(ctf2_clin_odd(:,:,iref),  ctf2_clin_odd(:,:,mref))
             enddo
@@ -1191,14 +1159,6 @@ contains
         i = pftsz/2 + 1
         pftout(i,:) = pftin(i,:)
     end subroutine mirror_pft
-
-    ! produces x-mirror of complex matrix (=complex conjugate of y-mirror)
-    pure subroutine mirrorx_pft( pftin, pftout )
-        complex(dp), intent(in)    :: pftin(pftsz,kfromto(1):kfromto(2))
-        complex(dp), intent(inout) :: pftout(pftsz,kfromto(1):kfromto(2))
-        call mirror_pft(pftin, pftout)
-        pftout = conjg(pftout)
-    end subroutine mirrorx_pft
 
     ! Private utility
     pure subroutine safe_norm( Mnum, Mdenom, Mout )
