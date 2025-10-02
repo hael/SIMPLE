af337798947f1e0cedbe4d5f34dcac6f3a9b7810
Joseph Caesar <joseph.caesar@path.ox.ac.uk>
2025-10-02 11:44:10 +0100

    switched project history to use time rather than number of iterations to reduce memory usage

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -215,7 +215,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('7320586b')
+call simple_print_git_version('f016e825')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
