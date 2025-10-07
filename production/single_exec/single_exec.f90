a0fc62f3e95edc30392ae82d690dd9758ea58030
Joseph Caesar <joseph.caesar@path.ox.ac.uk>
2025-10-07 14:48:47 +0100

    switch gui from mod_wsgi to gunicorn

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -215,7 +215,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('30f60f96')
+call simple_print_git_version('a3144bc5')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
