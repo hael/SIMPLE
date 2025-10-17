1d509a9732413368a6e7a2807913ee2485d78c7a
Hans Elmlund <hans.elmlund@nih.gov>
2025-10-17 10:12:56 -0400

    peak detection through vector quantization to generate a hierarchy of thresholds and minimization of the standard mean difference to a reference distribution to determine the optimal threshold

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -489,7 +489,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('29242d0c')
+call simple_print_git_version('c0a17e04')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
