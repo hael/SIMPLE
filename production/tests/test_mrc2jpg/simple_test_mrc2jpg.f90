81b502caa898f21005831639502bf6e57b1f1ee8
Ruben Meana-Paneda <rubenmepa@gmail.com>
2025-10-21 10:52:10 -0400

    bugfix test mrc2jpg

--- production/tests/test_mrc2jpg/simple_test_mrc2jpg.f90
+++ production/tests/test_mrc2jpg/simple_test_mrc2jpg.f90
@@ -5,15 +5,13 @@ use simple_jpg
 use simple_cmdline,           only: cmdline
 use simple_parameters,        only: parameters
 implicit none
-type(image)                   :: microg 
-character(len=STDLEN)         :: outputfile, fbody, filetable
-!character(len=STDLEN), parameter :: filetable='filetab.txt'
-integer                       :: i, j, nfiles, ldim(3), ifoo
-!real                          :: smpd 
-real                          :: smpd 
 character(len=LONGSTRLEN), allocatable :: micname(:)
-type(cmdline)                 :: cline
-type(parameters)              :: p
+type(image)           :: microg 
+character(len=STDLEN) :: outputfile, fbody, filetable
+integer               :: i, j, nfiles, ldim(3), ifoo, ldim_refs(3)
+real                  :: smpd 
+type(cmdline)         :: cline
+type(parameters)      :: p
 
 if( command_argument_count() < 2 )then
     write(logfhandle,'(a)') 'Usage: simple_test_mrc2jpg filetab=filetab.txt smpd=0.3'
@@ -28,21 +26,21 @@ call p%new(cline)
 call read_filetable(p%filetab, micname)
 nfiles=size(micname)
 do i=1,nfiles
-    fbody      = get_fbody(basename(micname(i)),'mrc')
+    fbody = get_fbody(basename(micname(i)),'mrc')
     call find_ldim_nptcls(micname(i), ldim, ifoo)
-    print *, 'ldim ', ldim
+    ldim_refs = [ldim(1), ldim(2), 1]
     if(ldim(3)==1)then
-        call microg%new(ldim, p%smpd)
+        call microg%new(ldim_refs, p%smpd)
         call microg%read(micname(i))
         outputfile = trim(fbody)//'.jpeg'
         call microg%write_jpg(outputfile)
         call microg%kill()
     else
-        call microg%new(ldim, p%smpd)
+        call microg%new(ldim_refs, p%smpd)
         do j=1,ldim(3)
             call microg%read(micname(i),j)
             outputfile = trim(fbody)//int2str_pad(j,3)//'.jpeg'
-            print *, j, outputfile
+            write(logfhandle,'(a)') '>>> WRITING '//trim(adjustl(outputfile))
             call microg%write_jpg(outputfile)
         enddo
         call microg%kill()
