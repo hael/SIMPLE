edb39185e0f9683467b50b97e15c4fe18f960a03
Ruben Meana-Paneda <rubenmepa@gmail.com>
2025-10-21 13:07:47 -0400

    minor edit

--- production/tests/test_mrc2jpeg/simple_test_mrc2jpeg.f90
+++ production/tests/test_mrc2jpeg/simple_test_mrc2jpeg.f90
@@ -1,4 +1,4 @@
-program simple_test_mrc2jpg
+program simple_test_mrc2jpeg
 include 'simple_lib.f08' 
 use simple_image
 use simple_jpg
@@ -12,17 +12,15 @@ integer               :: i, j, nfiles, ldim(3), ifoo, ldim_refs(3)
 real                  :: smpd 
 type(cmdline)         :: cline
 type(parameters)      :: p
-
 if( command_argument_count() < 2 )then
     write(logfhandle,'(a)') 'Usage: simple_test_mrc2jpg filetab=filetab.txt smpd=0.3'
     stop
 endif
 call cline%parse_oldschool
-call cline%checkvar('filetab',     1)
-call cline%checkvar('smpd',      2)
+call cline%checkvar('filetab', 1)
+call cline%checkvar('smpd',    2)
 call cline%check
 call p%new(cline)
-
 call read_filetable(p%filetab, micname)
 nfiles=size(micname)
 do i=1,nfiles
@@ -30,13 +28,14 @@ do i=1,nfiles
     call find_ldim_nptcls(micname(i), ldim, ifoo)
     ldim_refs = [ldim(1), ldim(2), 1]
     if(ldim(3)==1)then
-        call microg%new(ldim_refs, p%smpd)
+        call microg%new([ldim(1), ldim(2), 1], p%smpd)
         call microg%read(micname(i))
         outputfile = trim(fbody)//'.jpeg'
+        write(logfhandle,'(a)') '>>> WRITING '//trim(adjustl(outputfile))
         call microg%write_jpg(outputfile)
         call microg%kill()
     else
-        call microg%new(ldim_refs, p%smpd)
+        call microg%new([ldim(1), ldim(2), 1], p%smpd)
         do j=1,ldim(3)
             call microg%read(micname(i),j)
             outputfile = trim(fbody)//int2str_pad(j,3)//'.jpeg'
@@ -46,4 +45,4 @@ do i=1,nfiles
         call microg%kill()
     endif
 enddo
-end program simple_test_mrc2jpg
+end program simple_test_mrc2jpeg
