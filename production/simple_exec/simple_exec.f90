74aabdbb19588fb1ea2896415cd022cdcb4f94bd
hael <hans.elmlund@nih.gov>
2025-10-30 12:10:28 -0500

    tidying

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -500,7 +500,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('796fdac9')
+    call simple_print_git_version('60c7c486')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
