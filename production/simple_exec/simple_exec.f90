046e993f6aed4f62012182f4ecb8fd79413fa56c
Susan Lea <susan.lea@stjude.org>
2025-10-31 16:39:36 -0500

    bugfix, Ruben please compile before you push

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -497,7 +497,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('0f4176e0')
+    call simple_print_git_version('bea1aca5')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
