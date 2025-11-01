8d6c6b18e44a483368cf7954917e6c6a4545ec56
hael <hans.elmlund@nih.gov>
2025-11-01 14:19:43 -0500

    modified peak detection algorithm when particle_density is set to low

--- production/simple_stream/simple_stream.f90
+++ production/simple_stream/simple_stream.f90
@@ -67,7 +67,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('e39d7e7c')
+call simple_print_git_version('2730beea')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
