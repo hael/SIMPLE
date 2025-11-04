e69590f9367dd056823a014a2eefa01e59f8ce8a
Hans Elmlund <hans.elmlund@nih.gov>
2025-11-04 10:04:44 -0500

    dev code switch for cluster_cavgs testing

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -497,7 +497,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('656af1eb')
+    call simple_print_git_version('3827d59e')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
--- production/simple_stream/simple_stream.f90
+++ production/simple_stream/simple_stream.f90
@@ -67,7 +67,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('656af1eb')
+call simple_print_git_version('3827d59e')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
