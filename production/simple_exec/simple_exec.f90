f4b2b6e6bcd2663f6a25ddb06549e1fed40ca5ad
hael <hans.elmlund@nih.gov>
2025-11-05 15:16:25 -0500

    work on getting clustering based on pairwise resolution estimation going, needs OpenMP

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -497,7 +497,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('210cc2b3')
+    call simple_print_git_version('3677654e')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
