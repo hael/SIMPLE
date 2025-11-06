e5c19a1c393e334e0fce53abbe52985cc674fd73
Joseph Caesar <joseph.caesar@path.ox.ac.uk>
2025-11-06 16:44:45 +0000

    Bugfix state=0 micrographs in segdiampick_mics

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -497,7 +497,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('f4b2b6e6')
+    call simple_print_git_version('964a1550')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
