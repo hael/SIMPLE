461301968dca543894961fde87437bf4c3cdefbb
Hans Elmlund <hans.elmlund@nih.gov>
2025-11-06 20:36:09 -0500

    minor tidy

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -497,7 +497,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('e5c19a1c')
+    call simple_print_git_version('caf1dc37')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
