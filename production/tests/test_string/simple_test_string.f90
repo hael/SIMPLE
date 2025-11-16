631f51b8089048b3b627206360dc26414dc9c75d
cyrilr <cyril.reboul@gmail.com>
2025-11-17 10:15:04 +1100

    Circumventing 'internal compiler error' on FRCE & ubuntu gcc11.5

--- production/tests/test_string/simple_test_string.f90
+++ production/tests/test_string/simple_test_string.f90
@@ -2,14 +2,18 @@ program simple_test_string
 use simple_string
 implicit none
 
-type(string) :: a, b, c
+type(string) :: a, b, c, d, e
 
 a = "HELLO"
 b = " WORLD"
+d = ""
 
 c = a // b
 
-print *, "c:", c%to_char()
-print *, "Length:", c%strlen()
-    
+print *, "c:|", c%to_char(), "|"
+print *, "Length:", c%strlen(), " vs. ",len_trim(c%to_char())
+print *, "d:|", d%to_char(), "|"
+print *, "Empty length:", d%strlen()
+print *, "e:|", e%to_char(), "|"
+print *, "Unallocated length:", d%strlen()
 end program simple_test_string
