df2047ebdc08e2e10f5e0fdfa7a6ff7455aa1066
hael <hans.elmlund@nih.gov>
2025-11-16 20:08:11 -0500

    tidying module simple_strings

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -497,7 +497,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('574a8890')
+    call simple_print_git_version('ab3ba6d2')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
