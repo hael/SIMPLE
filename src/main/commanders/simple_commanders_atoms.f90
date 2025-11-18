f6da4bb25356937d19ec4897d15444fae3f9bf3e
cong <vanc2@nih.gov>
2025-11-18 11:01:15 -0500

    adding crystal score commander

--- src/main/commanders/simple_commanders_atoms.f90
+++ src/main/commanders/simple_commanders_atoms.f90
@@ -21,6 +21,11 @@ type, extends(commander_base) :: commander_atoms_register
     procedure :: execute      => exec_atoms_register
 end type commander_atoms_register
 
+type, extends(commander_base) :: commander_crys_score
+  contains
+    procedure :: execute      => exec_crys_score
+end type commander_crys_score
+
 type, extends(commander_base) :: commander_conv_atom_denoise
   contains
     procedure :: execute      => exec_conv_atom_denoise
@@ -178,6 +183,170 @@ contains
         ! end gracefully
         call simple_end('**** SIMPLE_ATOMS_REGISTER NORMAL STOP ****')
     end subroutine exec_atoms_register
+
+    subroutine exec_crys_score( self, cline )
+        class(commander_crys_score), intent(inout) :: self
+        class(cmdline),              intent(inout) :: cline !< command line input
+        character(len=LONGSTRLEN),   allocatable   :: pdbfnames(:), cmd, corenames(:)
+        real,                        allocatable   :: dists(:), mat(:,:), dists_cen(:), vars(:), ref_stats(:), cur_mat(:,:), cur_stats(:)
+        integer,                     allocatable   :: cnts(:)
+        logical,                     allocatable   :: l_dists(:)
+        character(len=LONGSTRLEN) :: t_name
+        type(parameters)          :: p
+        integer                   :: npdbs, ipdb, icore, ncores, Natoms, cnt, i, j, nclus
+        real                      :: min_dist, dist, prob, d, max_dist, moldiam
+        call p%new(cline)
+        call read_filetable(p%fname, pdbfnames)
+        npdbs = size(pdbfnames)
+        do ipdb = 1, npdbs
+            ! finding max_dist for moldiam
+            call read_pdb2matrix( trim(pdbfnames(ipdb)) // 'startvol_ATMS.pdb', mat )
+            Natoms   = size(mat,2)
+            max_dist = 0.
+            do i = 1, Natoms-1
+                do j = i+1, Natoms
+                    dist = sqrt(sum((mat(:,i) - mat(:,j))**2))
+                    if( dist > max_dist ) max_dist = dist
+                enddo
+            enddo
+            moldiam = max_dist
+            cmd     = 'nohup single_exec prg=simulate_atoms outvol=tmp.mrc box=160 smpd='//real2str(p%smpd)//' moldiam='//real2str(moldiam)//' nthr='//int2str(p%nthr)//' element=Pt'
+            call execute_command_line(cmd)
+            cmd     = 'nohup single_exec prg=detect_atoms vol1=./tmp.mrc smpd='//real2str(p%smpd)//' element=Pt nthr='//int2str(p%nthr)//' mskdiam=40'
+            call execute_command_line(cmd)
+            ! generating stats for the reference lattice
+            call read_pdb2matrix( 'tmp_ATMS.pdb', mat )
+            Natoms = size(mat,2)
+            if( allocated(dists)   )deallocate(dists)
+            if( allocated(l_dists) )deallocate(l_dists)
+            allocate(  dists(Natoms**2), source=0.)
+            allocate(l_dists(Natoms**2), source=.false.)
+            ! finding all dists and min_dist
+            cnt      = 0
+            min_dist = huge(min_dist)
+            do i = 1, Natoms-1
+                do j = i+1, Natoms
+                    cnt          = cnt + 1
+                    dists(cnt)   = sqrt(sum((mat(:,i) - mat(:,j))**2))
+                    l_dists(cnt) = .true.
+                    if( dists(cnt) < min_dist .and. dists(cnt) > TINY ) min_dist = dists(cnt)
+                enddo
+            enddo
+            dists = pack(dists, mask=l_dists)
+            call sort_cluster(dists, nclus, dists_cen, cnts, vars, delta=min_dist / 10.)
+            call hpsort(dists_cen)
+            if( allocated(ref_stats) )deallocate(ref_stats)
+            if( allocated(cur_stats) )deallocate(cur_stats)
+            allocate(ref_stats(nclus), cur_stats(nclus), source=0.)
+            call compute_stats(mat, dists_cen(1:nclus), ref_stats)
+            ! generating stats for the core
+            t_name = trim(pdbfnames(ipdb)) // 'segvols/1_tseries_core_atoms_analysis/'
+            print *, trim(t_name)
+            cmd = 'ls '// trim(t_name) // '*_core.pdb | xargs -n 1 basename > output_'//int2str(ipdb)//'.log'
+            call execute_command_line(cmd)
+            call read_filetable('output_'//int2str(ipdb)//'.log', corenames)
+            ncores = size(corenames)
+            do icore = 1, ncores
+                call read_pdb2matrix( trim(t_name) // trim(corenames(icore)), cur_mat )
+                call compute_stats(cur_mat, dists_cen(1:nclus), cur_stats)
+                call kstwo( ref_stats, nclus, cur_stats, nclus, d, prob )
+                print *, trim(corenames(icore)), prob
+            enddo
+            call execute_command_line('rm nohup.out largest_cc.mrc split_ccs.mrc binarized.mrc tmp* output_'//int2str(ipdb)//'.log')
+        enddo
+        ! end gracefully
+        call simple_end('**** SIMPLE_CRYS_SCORE NORMAL STOP ****')
+
+      contains
+
+        ! compute the distribution of the dists around the reference distance points
+        subroutine compute_stats(pos_mat, ref_dists, out_stats, msk)
+            real,    allocatable, intent(in)    :: pos_mat(:,:)
+            real,                 intent(in)    :: ref_dists(:)
+            real,    allocatable, intent(inout) :: out_stats(:)
+            logical, optional,    intent(in)    :: msk(:)
+            integer :: Na, Nr, i, j, k
+            real    :: eps_g, eps_l
+            Na        = size(pos_mat,2)
+            Nr        = size(ref_dists)
+            out_stats = 0.
+            do i = 1, Nr
+                if( i > 1 .and. i < Nr )then
+                    eps_g = (ref_dists(i+1) - ref_dists(i))/2.
+                    eps_l = (ref_dists(i)   - ref_dists(i-1))/2.
+                elseif( i == 1 )then
+                    eps_g = (ref_dists(i+1) - ref_dists(i))/2.
+                    eps_l =  huge(eps_l)
+                elseif( i == Nr )then
+                    eps_g =  huge(eps_g)
+                    eps_l = (ref_dists(i)   - ref_dists(i-1))/2.
+                endif
+                do j = 1, Na-1
+                    do k = j+1, Na
+                        if( present(msk) )then
+                            if( .not.msk(j) .or. .not.msk(k) )cycle
+                        endif
+                        dist = sqrt(sum((pos_mat(:,j) - pos_mat(:,k))**2))
+                        if( (dist <= ref_dists(i) .and. abs(dist - ref_dists(i)) < eps_l ) .or. &
+                            (dist >= ref_dists(i) .and. abs(dist - ref_dists(i)) < eps_g ) )then
+                            out_stats(i) = out_stats(i) + 1.
+                        endif
+                    enddo
+                enddo
+            enddo
+            out_stats = out_stats * 100. / sum(out_stats)
+        end subroutine compute_stats
+
+        subroutine sort_cluster( points_in, cur_clusN, out_points, out_cnts, out_vars, delta )
+            real,                 intent(in)    :: points_in(:)
+            integer,              intent(inout) :: cur_clusN
+            real,    allocatable, intent(inout) :: out_points(:)
+            integer, allocatable, intent(inout) :: out_cnts(:)
+            real,    allocatable, intent(inout) :: out_vars(:)
+            real,                 intent(in)    :: delta
+            real,    allocatable :: points(:), avg_points(:), vars(:)
+            integer, allocatable :: cnts(:)
+            integer :: cur_ind, n_points, i, j
+            real    :: cur_avg
+            n_points = size(points_in)
+            allocate(points(n_points), source=points_in)
+            allocate(avg_points(n_points),cnts(n_points),vars(n_points))
+            call hpsort(points)
+            cur_clusN = 0
+            cur_ind   = 1
+            cur_avg   = 0.
+            do i = 1, n_points - 1
+                cur_avg = cur_avg+points(i)
+                if( abs(points(i+1) - cur_avg/real(i-cur_ind+1)) > delta )then
+                    cur_clusN = cur_clusN + 1
+                    avg_points(cur_clusN) = cur_avg/real(i-cur_ind+1)
+                    cnts(cur_clusN)       = i-cur_ind+1
+                    vars(cur_clusN)       = 0.
+                    do j = cur_ind, i
+                        vars(cur_clusN) = vars(cur_clusN) + (points(i) - avg_points(cur_clusN))**2
+                    enddo
+                    vars(cur_clusN) = vars(cur_clusN)/real(i-cur_ind+1)
+                    cur_ind = i + 1
+                    cur_avg = 0.
+                endif
+            enddo
+            ! last cluster
+            cur_clusN = cur_clusN + 1
+            avg_points(cur_clusN) = sum(points(cur_ind:i))/real(i-cur_ind+1)
+            cnts(cur_clusN)       = i-cur_ind+1
+            vars(cur_clusN)       = 0.
+            do j = cur_ind, i
+                vars(cur_clusN) = vars(cur_clusN) + (points(i) - avg_points(cur_clusN))**2
+            enddo
+            vars(cur_clusN) = vars(cur_clusN)/real(i-cur_ind+1)
+            if( allocated(out_points) )deallocate(out_points)
+            if( allocated(out_cnts)   )deallocate(out_cnts)
+            if( allocated(out_vars)   )deallocate(out_vars)
+            allocate(out_points(1:cur_clusN), source=avg_points(1:cur_clusN))
+            allocate(out_cnts(1:cur_clusN),   source=cnts(1:cur_clusN))
+            allocate(out_vars(1:cur_clusN),   source=vars(1:cur_clusN))
+        end subroutine sort_cluster
+    end subroutine exec_crys_score
 
     subroutine exec_conv_atom_denoise( self, cline )
         class(commander_conv_atom_denoise), intent(inout) :: self
