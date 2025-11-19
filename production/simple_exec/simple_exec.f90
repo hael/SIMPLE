39a4ebc28695c51b4a3115e3e63587d6fcc9fecf
Joseph Caesar <joseph.caesar@path.ox.ac.uk>
2025-11-19 22:29:22 +0000

    Bugfix passed over selection in sieving

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -497,7 +497,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('e77b3db8')
+    call simple_print_git_version('264491bb')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
