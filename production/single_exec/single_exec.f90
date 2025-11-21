72d3f78e5221104ae1c63ea5492607e6b6459c42
Hans Elmlund <hans.elmlund@nih.gov>
2025-11-21 10:08:06 -0500

    changed default fudge factor (tau) to 1

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -223,7 +223,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('093ce025')
+    call simple_print_git_version('b7566bf1')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
