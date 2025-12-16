6addd010312222b403ab06a73f876b3bb1805e94
Hans Elmlund <hans.elmlund@nih.gov>
2025-12-16 13:18:34 -0500

    match_cavgs: revisions to make cluster2D_subsets run as intended. All fields must be included in merge_chunk_projfiles, also the state=0 ones. The only output from match_cavgs is the merged project file, no labelling information is propagated to the target project file

--- production/simple_stream/simple_stream.f90
+++ production/simple_stream/simple_stream.f90
@@ -70,7 +70,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('2f52e7c4')
+call simple_print_git_version('19927814')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
