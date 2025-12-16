da1407bdeec471bb7e43388c02ccefcff73a2ae0
Ruben Meana-Paneda <rubenmepa@gmail.com>
2025-12-16 10:40:12 -0500

    build with static libraries for faster compilation

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -395,7 +395,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('2030a3d0')
+    call simple_print_git_version('2f52e7c4')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
