6615ef2639b4a105668bac2d7af0d87426d6e9d6
Hans Elmlund <hans.elmlund@nih.gov>
2025-12-16 06:45:18 -0500

    Cmake edits: removed install directory, all executabes now in build/bin folder including nice_lite. Removed stop in simple_test_units. Edited template bash environment scripts to rmove LD_LIB path nonsense

--- cmake/PostInstall/PostInstallMessage.cmake
+++ cmake/PostInstall/PostInstallMessage.cmake
@@ -1,6 +1,3 @@
-##
-# Print some post install messages for the user
-##
 message("\n\n")
 message("Installation complete.")
 message("==========================================================================")
@@ -11,9 +8,5 @@ message(" bash$ cat add2.bashrc >> ~/.bashrc                                    
 message(" tcsh$ cat add2.tcshrc >> ~/.tcshrc                                       ")
 message("==========================================================================")
 message("For minimal installation to work correctly add:\n${CMAKE_INSTALL_PREFIX}/bin and\n${CMAKE_INSTALL_PREFIX}/scripts\n to your PATH environment variable.")
-#if(BUILD_SHARED_LIBS)
-# message("For shared or dynamic builds: ")
-#message("SHARED LIB: ${CMAKE_INSTALL_PREFIX}/lib appended LD_LIBRARY_PATH")
-#endif()
 message("==========================================================================")
 message("\n\n")
