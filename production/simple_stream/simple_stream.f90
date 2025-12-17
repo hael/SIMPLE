6bfb7817c920a7c061ccf8df919b42745b249517
Hans Elmlund <hans.elmlund@nih.gov>
2025-12-17 05:19:52 -0500

    GUI bugfix. Corrected typoin json addition in opening2D: selected_refs -> selected_references

--- production/simple_stream/simple_stream.f90
+++ production/simple_stream/simple_stream.f90
@@ -70,7 +70,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('19927814')
+call simple_print_git_version('0b3ff85b')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
