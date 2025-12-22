f087c7ba75e667cdf17a9417c86990d136e11f8e
Susan Lea <susan.lea@stjude.org>
2025-12-22 11:56:42 -0600

    leaving the chunks for testing & draft of policy for sieving

--- production/simple_stream/simple_stream.f90
+++ production/simple_stream/simple_stream.f90
@@ -70,7 +70,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('ff09e0ad')
+call simple_print_git_version('64c42bc1')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
