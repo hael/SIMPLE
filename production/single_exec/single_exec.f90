2c2e1ae70233fdb63fe9eb0a0a3fd0fe739e1330
Hans Elmlund <hans.elmlund@nih.gov>
2026-01-06 11:41:12 -0500

    edits to match_cavgs

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -200,7 +200,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('c6f5700a')
+    call simple_print_git_version('7e14cd82')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
