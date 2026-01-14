e5d3de2e8a65b621f682e3656b3539131908c794
hael <hans.elmlund@nih.gov>
2026-01-14 08:02:22 -0500

    submodule refactoring of polarops

---
+++ src/main/pftc/simple_polarops.f90
@@ -0,0 +1,354 @@
+module simple_polarops
+use simple_core_module_api
+!$ use omp_lib
+!$ use omp_lib_kinds
+use simple_builder,          only: builder, build_glob
+use simple_image,            only: image
+use simple_parameters,       only: params_glob
+use simple_polarft_calc,     only: polarft_calc, pftc_glob
+use simple_sp_project,       only: sp_project
+use simple_strategy2D_utils, only: calc_cavg_offset
+use simple_imgarr_utils,     only: alloc_imgarr, write_imgarr, dealloc_imgarr
+use simple_cmdline,          only: cmdline
+implicit none
+
+! State
+public :: polar_cavger_new
+public :: polar_cavger_zero_pft_refs
+public :: polar_cavger_set_ref_pftc
+public :: polar_cavger_calc_pops
+public :: polar_cavger_update_sums
+public :: polar_cavger_kill
+public :: center_3Dpolar_refs
+! Restore
+public :: polar_cavger_merge_eos_and_norm2D
+public :: polar_cavger_merge_eos_and_norm
+public :: polar_cavger_calc_and_write_frcs_and_eoavg
+public :: polar_prep2Dref
+public :: polar_prep3Dref
+public :: polar_cavger_gen2Dclassdoc
+! I/O
+public :: polar_cavger_refs2cartesian
+public :: polar_cavger_read
+public :: polar_cavger_write
+public :: polar_cavger_writeall
+public :: polar_cavger_writeall_pftcrefs
+public :: polar_cavger_write_cartrefs
+public :: polar_cavger_read_all
+public :: polar_cavger_readwrite_partial_sums
+public :: polar_cavger_assemble_sums_from_parts
+public :: polar_cavger_dims_from_header 
+private
+#include "simple_local_flags.inc"
+
+complex(dp), allocatable :: pfts_even(:,:,:), pfts_odd(:,:,:), pfts_merg(:,:,:)
+real(dp),    allocatable :: ctf2_even(:,:,:), ctf2_odd(:,:,:)
+integer,     allocatable :: prev_eo_pops(:,:), eo_pops(:,:)
+real                     :: smpd       = 0.
+integer                  :: ncls       = 0
+integer                  :: kfromto(2) = 0
+integer                  :: pftsz      = 0
+integer                  :: nrots      = 0
+logical                  :: l_comlin   = .false.
+
+!-----------------------------
+! Interfaces implemented in submodules
+!-----------------------------
+interface
+
+    ! ===== STATE: simple_polarops_state.f90
+
+    module subroutine polar_cavger_new(pftc, comlin, nrefs)
+        class(polarft_calc),     intent(in) :: pftc
+        logical,                 intent(in) :: comlin
+        integer,       optional, intent(in) :: nrefs
+    end subroutine
+
+    module subroutine polar_cavger_zero_pft_refs()
+    end subroutine
+
+    module subroutine polar_cavger_set_ref_pftc(icls, which, pftc)
+        integer,             intent(in)    :: icls
+        character(len=*),    intent(in)    :: which
+        class(polarft_calc), intent(inout) :: pftc
+    end subroutine
+
+    module subroutine polar_cavger_calc_pops(spproj)
+        class(sp_project), target, intent(in) :: spproj
+    end subroutine
+
+    module subroutine polar_cavger_update_sums(nptcls, pinds, spproj, pftc, incr_shifts, is3D)
+        integer,                         intent(in)    :: nptcls
+        integer,                         intent(in)    :: pinds(nptcls)
+        class(sp_project),               intent(inout) :: spproj
+        class(polarft_calc), target,     intent(inout) :: pftc
+        real,                  optional, intent(in)    :: incr_shifts(2,nptcls)
+        logical,               optional, intent(in)    :: is3D
+    end subroutine
+
+    module subroutine polar_cavger_kill()
+    end subroutine
+
+    module subroutine center_3Dpolar_refs(pftc, algndoc, algnrefs)
+        class(polarft_calc), intent(inout) :: pftc
+        class(oris),         intent(inout) :: algndoc
+        class(oris),         intent(in)    :: algnrefs
+    end subroutine
+    
+    ! ===== RESTORE: simple_polarops_restore.f90
+
+    module subroutine polar_cavger_merge_eos_and_norm2D()
+    end subroutine
+
+    module subroutine polar_cavger_merge_eos_and_norm(reforis, cl_weight)
+        type(oris),           intent(in) :: reforis
+        real,       optional, intent(in) :: cl_weight
+    end subroutine
+
+    module subroutine polar_cavger_calc_and_write_frcs_and_eoavg(fname, cline)
+        class(string), intent(in) :: fname
+        type(cmdline), intent(in) :: cline
+    end subroutine
+
+    module subroutine polar_prep2Dref(icls, cavg, center, xyz)
+        integer,                intent(in)    :: icls
+        class(image), optional, intent(inout) :: cavg
+        logical,      optional, intent(in)    :: center
+        real,         optional, intent(out)   :: xyz(3)
+    end subroutine
+
+    module subroutine polar_prep3Dref(icls)
+        integer, intent(in) :: icls
+    end subroutine
+
+    module subroutine polar_cavger_gen2Dclassdoc(spproj)
+        class(sp_project), target, intent(inout) :: spproj
+    end subroutine
+
+    ! ===== I/O: simple_polarops_io.f90
+
+    module subroutine polar_cavger_refs2cartesian(pftc, cavgs, which, pfts_in)
+        class(polarft_calc),     intent(in)    :: pftc
+        type(image),             intent(inout) :: cavgs(ncls)
+        character(len=*),        intent(in)    :: which
+        complex(dp),   optional, intent(in)    :: pfts_in(1:pftsz,kfromto(1):kfromto(2),1:ncls)
+    end subroutine
+
+    module subroutine polar_cavger_read( fname, which )
+        class(string),    intent(in) :: fname
+        character(len=*), intent(in) :: which
+    end subroutine
+
+    module subroutine polar_cavger_write(fname, which)
+        class(string),    intent(in) :: fname
+        character(len=*), intent(in) :: which
+    end subroutine
+
+    module subroutine polar_cavger_writeall(tmpl_fname)
+        class(string), intent(in) :: tmpl_fname
+    end subroutine
+
+    module subroutine polar_cavger_writeall_pftcrefs(tmpl_fname)
+        class(string), intent(in) :: tmpl_fname
+    end subroutine
+
+    module subroutine polar_cavger_write_cartrefs(pftc, tmpl_fname, which)
+        class(polarft_calc), intent(in) :: pftc
+        class(string),       intent(in) :: tmpl_fname
+        character(len=*),    intent(in) :: which
+    end subroutine
+
+    module subroutine polar_cavger_read_all(fname)
+        class(string), intent(in) :: fname
+    end subroutine
+
+    module subroutine polar_cavger_readwrite_partial_sums(which)
+        character(len=*), intent(in) :: which
+    end subroutine
+
+    module subroutine polar_cavger_assemble_sums_from_parts(reforis, clin_anneal)
+        type(oris), optional, intent(in) :: reforis
+        real,       optional, intent(in) :: clin_anneal
+    end subroutine
+
+    module subroutine polar_cavger_dims_from_header(fname, pftsz_here, kfromto_here, ncls_here)
+        class(string), intent(in)    :: fname
+        integer,       intent(inout) :: pftsz_here, kfromto_here(2), ncls_here
+    end subroutine
+
+    module subroutine open_pft_array_for_write( fname, funit )
+        class(string), intent(in)  :: fname
+        integer,       intent(out) :: funit
+    end subroutine open_pft_array_for_write
+
+    module subroutine write_pft_array_local( funit, array )
+        integer,     intent(in) :: funit
+        complex(dp), intent(in) :: array(pftsz,kfromto(1):kfromto(2),ncls)
+    end subroutine write_pft_array_local
+
+    module subroutine write_pft_array( array, fname )
+        complex(dp),   intent(in) :: array(pftsz,kfromto(1):kfromto(2),ncls)
+        class(string), intent(in) :: fname
+    end subroutine write_pft_array
+
+    module subroutine open_ctf2_array_for_write( fname, funit )
+        class(string), intent(in)  :: fname
+        integer,       intent(out) :: funit
+    end subroutine open_ctf2_array_for_write
+
+    module subroutine write_ctf2_array_local( funit, array )
+        integer,  intent(in) :: funit
+        real(dp), intent(in) :: array(pftsz,kfromto(1):kfromto(2),ncls)
+    end subroutine write_ctf2_array_local
+
+    module subroutine write_ctf2_array( array, fname )
+        real(dp),      intent(in) :: array(pftsz,kfromto(1):kfromto(2),ncls)
+        class(string), intent(in) :: fname
+    end subroutine write_ctf2_array
+
+    module subroutine open_pft_array_for_read( fname, array, funit, dims, buffer )
+        class(string),            intent(in)    :: fname
+        complex(dp), allocatable, intent(inout) :: array(:,:,:)
+        integer,                  intent(out)   :: funit, dims(4)
+        complex(sp), allocatable, intent(inout) :: buffer(:,:,:)
+    end subroutine open_pft_array_for_read
+
+    module subroutine transfer_pft_array_buffer( array, funit, dims, buffer )
+        complex(dp), intent(inout) :: array(pftsz,kfromto(1):kfromto(2),ncls)
+        integer,     intent(in)    :: funit, dims(4)
+        complex(sp), intent(inout) :: buffer(dims(1),dims(2):dims(3),dims(4))
+    end subroutine transfer_pft_array_buffer
+
+    module subroutine read_pft_array( fname, array)
+        class(string),            intent(in)    :: fname
+        complex(dp), allocatable, intent(inout) :: array(:,:,:)
+    end subroutine read_pft_array
+
+    module subroutine open_ctf2_array_for_read( fname, array, funit, dims, buffer )
+        class(string),         intent(in)    :: fname
+        real(dp), allocatable, intent(inout) :: array(:,:,:)
+        integer,               intent(out)   :: funit, dims(4)
+        real(sp), allocatable, intent(inout) :: buffer(:,:,:)
+    end subroutine open_ctf2_array_for_read
+
+    module subroutine transfer_ctf2_array_buffer( array, funit, dims, buffer )
+        real(dp), intent(inout) :: array(pftsz,kfromto(1):kfromto(2),ncls)
+        integer,  intent(in)    :: funit, dims(4)
+        real(sp), intent(inout) :: buffer(dims(1),dims(2):dims(3),dims(4))
+    end subroutine transfer_ctf2_array_buffer
+
+    module subroutine read_ctf2_array( fname, array )
+        class(string),         intent(in)    :: fname
+        real(dp), allocatable, intent(inout) :: array(:,:,:)
+    end subroutine read_ctf2_array
+
+    module subroutine pft2img()
+    end subroutine pft2img
+
+end interface
+
+contains
+
+    ! TEST UNIT
+
+    subroutine test_polarops
+        use simple_cmdline,    only: cmdline
+        use simple_parameters, only: parameters
+        integer,     parameter :: N=128
+        integer,     parameter :: NIMGS=200
+        integer,     parameter :: NCLS=5
+        type(image)            :: tmpl_img, img, cavgs(NCLS)
+        type(cmdline)          :: cline
+        type(polarft_calc)     :: pftc
+        type(parameters)       :: p
+        type(builder)          :: b
+        real    :: ang, shift(2), shifts(2,NIMGS)
+        integer :: pinds(NIMGS), i, eo, icls
+        ! dummy structure
+        call tmpl_img%soft_ring([N,N,1], 1., 8.)
+        call tmpl_img%fft
+        call tmpl_img%shift2Dserial([ 8.,-16.])
+        call img%soft_ring([N,N,1], 1., 12.)
+        call img%fft
+        call img%shift2Dserial([ 32., 0.])
+        call tmpl_img%add(img)
+        call img%soft_ring([N,N,1], 1., 16.)
+        call img%fft
+        call img%shift2Dserial([ -16., 8.])
+        call tmpl_img%add(img)
+        call img%soft_ring([N,N,1], 1., 32.)
+        call img%fft
+        call tmpl_img%add(img)
+        call tmpl_img%ifft
+        call tmpl_img%write(string('template.mrc'))
+        ! init of options & parameters
+        call cline%set('prg',    'xxx')
+        call cline%set('objfun', 'cc')
+        call cline%set('smpd',   1.0)
+        call cline%set('box',    N)
+        call cline%set('ctf',    'no')
+        call cline%set('oritype','ptcl2D')
+        call cline%set('ncls',    NCLS)
+        call cline%set('nptcls',  NIMGs)
+        call cline%set('lp',      3.)
+        call cline%set('nthr',    8)
+        call cline%set('mskdiam', real(N)/2-10.)
+        ! Calculators
+        call b%init_params_and_build_strategy2D_tbox(cline, p)
+        call pftc%new(NCLS, [1,NIMGS], p%kfromto)
+        pinds = (/(i,i=1,NIMGS)/)
+        call b%img_crop_polarizer%init_polarizer(pftc, p%alpha)
+        do i = 1,NIMGS
+            shift = 10.*[ran3(), ran3()] - 5.
+            ! ang   = 360. * ran3()
+            ang   = 0.
+            eo    = 0
+            if( .not.is_even(i) ) eo = 1
+            icls  = ceiling(ran3()*4.)
+            call img%copy_fast(tmpl_img)
+            call img%fft
+            call img%shift2Dserial(-shift)
+            call img%ifft
+            call img%rtsq(ang, 0.,0.)
+            call img%add_gauran(2.)
+            call img%write(string('rotimgs.mrc'), i)
+            call img%fft
+            call b%spproj_field%set_euler(i, [0.,0.,ang])
+            call b%spproj_field%set_shift(i, shift)
+            call b%spproj_field%set(i,'w',1.0)
+            call b%spproj_field%set(i,'state',1)
+            call b%spproj_field%set(i,'class', icls)
+            call b%spproj_field%set(i,'eo',eo)
+            shifts(:,i) = -shift
+            call b%img_crop_polarizer%polarize(pftc, img, i, isptcl=.true., iseven=eo==0, mask=b%l_resmsk)
+        enddo
+        call polar_cavger_new(pftc,.false.)
+        call polar_cavger_update_sums(NIMGS, pinds, b%spproj, pftc, shifts)
+        call polar_cavger_merge_eos_and_norm2D
+        call polar_cavger_calc_and_write_frcs_and_eoavg(string(FRCS_FILE), cline)
+        ! write
+        call polar_cavger_write(string('cavgs_even.bin'), 'even')
+        call polar_cavger_write(string('cavgs_odd.bin'),  'odd')
+        call polar_cavger_write(string('cavgs.bin'),      'merged')
+        call polar_cavger_refs2cartesian(pftc, cavgs, 'even')
+        call write_imgarr(cavgs, string('cavgs_even.mrc'))
+        call polar_cavger_refs2cartesian(pftc, cavgs, 'odd')
+        call write_imgarr(cavgs, string('cavgs_odd.mrc'))
+        call polar_cavger_refs2cartesian(pftc, cavgs, 'merged')
+        call write_imgarr(cavgs, string('cavgs_merged.mrc'))
+        call polar_cavger_kill
+        ! read & write again
+        call polar_cavger_new(pftc,.false.)
+        call polar_cavger_read(string('cavgs_even.bin'), 'even')
+        call polar_cavger_read(string('cavgs_odd.bin'),  'odd')
+        call polar_cavger_read(string('cavgs.bin'),      'merged')
+        call polar_cavger_refs2cartesian(pftc, cavgs, 'even')
+        call write_imgarr(cavgs, string('cavgs2_even.mrc'))
+        call polar_cavger_refs2cartesian(pftc, cavgs, 'odd')
+        call write_imgarr(cavgs, string('cavgs2_odd.mrc'))
+        call polar_cavger_refs2cartesian(pftc, cavgs, 'merged')
+        call write_imgarr(cavgs, string('cavgs2_merged.mrc'))
+        call polar_cavger_kill
+    end subroutine test_polarops
+
+end module simple_polarops
---
+++ src/main/pftc/simple_polarops_state.f90
@@ -0,0 +1,268 @@
+submodule (simple_polarops) simple_polarops_state
+implicit none
+#include "simple_local_flags.inc"
+contains
+
+    !> Module initialization
+    module subroutine polar_cavger_new( pftc, comlin, nrefs )
+        class(polarft_calc),     intent(in) :: pftc
+        logical,                 intent(in) :: comlin
+        integer,       optional, intent(in) :: nrefs
+        call polar_cavger_kill
+        nrots    = pftc%get_nrots()
+        pftsz    = pftc%get_pftsz()
+        kfromto  = pftc%get_kfromto()
+        l_comlin = comlin
+        smpd     = params_glob%smpd
+        if( present(nrefs) )then
+            ncls = nrefs
+        else
+            ncls = pftc%get_nrefs()
+        endif
+        allocate(prev_eo_pops(2,ncls), eo_pops(2,ncls), source=0)
+        allocate(pfts_even(pftsz,kfromto(1):kfromto(2),ncls),pfts_odd(pftsz,kfromto(1):kfromto(2),ncls),&
+                &ctf2_even(pftsz,kfromto(1):kfromto(2),ncls),ctf2_odd(pftsz,kfromto(1):kfromto(2),ncls),&
+                &pfts_merg(pftsz,kfromto(1):kfromto(2),ncls))
+        call polar_cavger_zero_pft_refs
+        pfts_merg = DCMPLX_ZERO
+    end subroutine polar_cavger_new
+
+    module subroutine polar_cavger_zero_pft_refs
+        pfts_even = DCMPLX_ZERO
+        pfts_odd  = DCMPLX_ZERO
+        ctf2_even = 0.d0
+        ctf2_odd  = 0.d0
+    end subroutine polar_cavger_zero_pft_refs
+
+    module subroutine polar_cavger_set_ref_pftc( icls, which, pftc )
+        integer,             intent(in)    :: icls
+        character(len=*),    intent(in)    :: which
+        class(polarft_calc), intent(inout) :: pftc
+        select case(trim(which))
+        case('merged')
+            call pftc%set_ref_pft(icls, cmplx(pfts_merg(:,:,icls),kind=sp), .true.)
+        case('even')
+            call pftc%set_ref_pft(icls, cmplx(pfts_even(:,:,icls),kind=sp), .true.)
+        case('odd')
+            call pftc%set_ref_pft(icls, cmplx(pfts_odd(:,:,icls),kind=sp), .false.)
+        end select
+    end subroutine polar_cavger_set_ref_pftc
+
+    module subroutine polar_cavger_calc_pops( spproj )
+        class(sp_project), target, intent(in) :: spproj
+        class(oris), pointer :: ptcl_field, cls_field
+        integer :: i, icls, iptcl, eo
+        logical :: l_3D
+        l_3D = .false.
+        select case(trim(params_glob%oritype))
+        case('ptcl2D')
+            ptcl_field => spproj%os_ptcl2D
+            cls_field  => spproj%os_cls2D
+        case('ptcl3D')
+            ptcl_field => spproj%os_ptcl3D
+            cls_field  => spproj%os_cls3D
+            l_3D       = .true.
+        case DEFAULT
+            THROW_HARD('Unsupported ORITYPE: '//trim(params_glob%oritype))
+        end select
+        eo_pops = 0
+        !$omp parallel do schedule(guided) proc_bind(close) default(shared)&
+        !$omp private(iptcl,eo,icls)&
+        !$omp reduction(+:eo_pops)
+        do iptcl = 1,ptcl_field%get_noris()
+            if( ptcl_field%get_state(iptcl) == 0  ) cycle
+            if( ptcl_field%get(iptcl,'w') < SMALL ) cycle
+            eo = ptcl_field%get_eo(iptcl)+1
+            if( l_3D )then
+                icls = ptcl_field%get_proj(iptcl)
+            else
+                icls = ptcl_field%get_class(iptcl)
+            endif
+            eo_pops(eo,icls) = eo_pops(eo,icls) + 1
+        enddo
+        !$omp end parallel do
+        prev_eo_pops = 0
+        if( cls_field%get_noris() == ncls )then
+            do i = 1,ncls
+                if( l_3D )then
+                    icls = ptcl_field%get_proj(i)
+                else
+                    icls = ptcl_field%get_class(i)
+                endif
+                if( .not.cls_field%isthere(i,'prev_pop_even') ) cycle
+                prev_eo_pops(1,icls) = cls_field%get_int(i,'prev_pop_even')
+                prev_eo_pops(2,icls) = cls_field%get_int(i,'prev_pop_odd')
+            enddo
+        endif
+        eo_pops = eo_pops + prev_eo_pops
+    end subroutine polar_cavger_calc_pops
+
+    !>  \brief  Updates Fourier components and normalization matrices with new particles
+    module subroutine polar_cavger_update_sums( nptcls, pinds, spproj, pftc, incr_shifts, is3D )
+        use simple_euclid_sigma2, only: eucl_sigma2_glob
+        integer,                         intent(in)    :: nptcls
+        integer,                         intent(in)    :: pinds(nptcls)
+        class(sp_project),               intent(inout) :: spproj
+        class(polarft_calc), target,     intent(inout) :: pftc
+        real,                  optional, intent(in)    :: incr_shifts(2,nptcls)
+        logical,               optional, intent(in)    :: is3d
+        class(oris), pointer :: spproj_field
+        complex(sp), pointer :: pptcls(:,:,:), rptcl(:,:)
+        real(sp),    pointer :: pctfmats(:,:,:), rctf(:,:)
+        real(dp),    pointer :: rctf2(:,:)
+        real(dp) :: w
+        real     :: sigma2(kfromto(1):kfromto(2)), incr_shift(2)
+        integer  :: eopops(2,ncls), i, icls, iptcl, irot, k
+        logical  :: l_ctf, l_even, l_3D, l_shift
+        l_3D = .false.
+        if( present(is3D) ) l_3D = is3D
+        l_shift = .false.
+        if( present(incr_shifts) ) l_shift = .true.
+        ! retrieve particle info & pointers
+        call spproj%ptr2oritype(params_glob%oritype, spproj_field)
+        call pftc%get_ptcls_ptr(pptcls)
+        l_ctf = pftc%is_with_ctf()
+        if( l_ctf ) call pftc%get_ctfmats_ptr(pctfmats)
+        ! update classes
+        eopops = 0
+        !$omp parallel do default(shared) proc_bind(close) schedule(static) reduction(+:eopops)&
+        !$omp private(i,iptcl,w,l_even,icls,irot,incr_shift,rptcl,rctf,rctf2,k,sigma2)
+        do i = 1,nptcls
+            ! particles parameters
+            iptcl = pinds(i)
+            if( spproj_field%get_state(iptcl) == 0  ) cycle
+            w = real(spproj_field%get(iptcl,'w'),dp)
+            if( w < DSMALL ) cycle
+            l_even = spproj_field%get_eo(iptcl)==0
+            if( l_3D )then
+                icls = spproj_field%get_proj(iptcl)
+            else
+                icls = spproj_field%get_class(iptcl)
+            endif
+            irot = pftc%get_roind_fast(spproj_field%e3get(iptcl))
+            if( l_shift )then
+                incr_shift = incr_shifts(:,i)
+                ! weighted restoration
+                if( any(abs(incr_shift) > 1.e-6) ) call pftc%shift_ptcl(iptcl, -incr_shift)
+            endif
+            call pftc%get_work_pft_ptr(rptcl)
+            ! Particle rotation
+            call pftc%rotate_pft(pptcls(:,:,i), irot, rptcl)
+            ! Particle weight
+            rptcl = real(w) * rptcl
+            ! Particle ML regularization
+            if( params_glob%l_ml_reg )then
+                sigma2 = eucl_sigma2_glob%sigma2_noise(kfromto(1):kfromto(2),iptcl)
+                do k = kfromto(1),kfromto(2)
+                    rptcl(:,k) = rptcl(:,k) / sigma2(k)
+                enddo
+            endif
+            ! Array updates
+            if( l_ctf )then
+                call pftc%get_work_rpft_ptr(rctf)
+                call pftc%get_work_rpft8_ptr(rctf2)
+                ! weighted CTF2
+                call pftc%rotate_pft(pctfmats(:,:,i), irot, rctf)
+                rctf2 = w * real(rctf,kind=dp)**2
+                rptcl = rptcl * rctf    ! PhFlip(X).|CTF|
+                ! CTF2 ML regularization
+                if( params_glob%l_ml_reg )then
+                    do k = kfromto(1),kfromto(2)
+                        rctf2(:,k) = rctf2(:,k) / real(sigma2(k),dp)
+                    enddo
+                endif
+                if( l_even )then
+                    !$omp critical
+                    pfts_even(:,:,icls) = pfts_even(:,:,icls) + cmplx(rptcl,kind=dp)
+                    ctf2_even(:,:,icls) = ctf2_even(:,:,icls) + rctf2
+                    !$omp end critical
+                else
+                    !$omp critical
+                    pfts_odd(:,:,icls)  = pfts_odd(:,:,icls)  + cmplx(rptcl,kind=dp)
+                    ctf2_odd(:,:,icls)  = ctf2_odd(:,:,icls)  + rctf2
+                    !$omp end critical
+                endif
+            else
+                if( params_glob%l_ml_reg )then
+                    ! CTF2=1 & ML regularization
+                    call pftc%get_work_rpft8_ptr(rctf2)
+                    do k = kfromto(1),kfromto(2)
+                        rctf2(:,k) = w / real(sigma2(k),dp)
+                    enddo
+                    if( l_even )then
+                        !$omp critical
+                        pfts_even(:,:,icls) = pfts_even(:,:,icls) + cmplx(rptcl,kind=dp)
+                        ctf2_even(:,:,icls) = ctf2_even(:,:,icls) + rctf2
+                        !$omp end critical
+                    else
+                        !$omp critical
+                        pfts_odd(:,:,icls)  = pfts_odd(:,:,icls)  + cmplx(rptcl,kind=dp)
+                        ctf2_odd(:,:,icls)  = ctf2_odd(:,:,icls)  + rctf2
+                        !$omp end critical
+                    endif
+                else
+                    if( l_even )then
+                        !$omp critical
+                        pfts_even(:,:,icls) = pfts_even(:,:,icls) + cmplx(rptcl,kind=dp)
+                        ctf2_even(:,:,icls) = ctf2_even(:,:,icls) + w
+                        !$omp end critical
+                    else
+                        !$omp critical
+                        pfts_odd(:,:,icls)  = pfts_odd(:,:,icls)  + cmplx(rptcl,kind=dp)
+                        ctf2_odd(:,:,icls)  = ctf2_odd(:,:,icls)  + w
+                        !$omp end critical
+                    endif
+                endif
+            endif
+            ! total population
+            if( l_even )then
+                eopops(1,icls) = eopops(1,icls) + 1
+            else
+                eopops(2,icls) = eopops(2,icls) + 1
+            endif
+        enddo
+        !$omp end parallel do
+        eo_pops = eo_pops + eopops
+        ! cleanup
+        nullify(spproj_field,rptcl,rctf,pptcls,pctfmats)
+    end subroutine polar_cavger_update_sums
+
+    module subroutine polar_cavger_kill
+        if( allocated(pfts_even) )then
+            deallocate(pfts_even,pfts_odd,ctf2_even,ctf2_odd,pfts_merg,eo_pops,prev_eo_pops)
+        endif
+        smpd       = 0.
+        ncls       = 0
+        nrots      = 0
+        kfromto(2) = 0
+        pftsz      = 0
+        l_comlin   = .false.
+    end subroutine polar_cavger_kill
+
+    ! Estimates the center of the volume based on the distribution of
+    ! the individual particles in-plane offsets and map the shifts to both
+    ! the particles and the references stored in the pftc
+    module subroutine center_3Dpolar_refs( pftc, algndoc, algnrefs )
+        class(polarft_calc), intent(inout) :: pftc
+        class(oris),         intent(inout) :: algndoc
+        class(oris),         intent(in)    :: algnrefs
+        real    :: R(3,3),offset3D(3), offset2D(3)
+        integer :: iref
+        ! estimate 3D offset from particle alignement parameters
+        call algndoc%calc_avg_offset3D(offset3D, state=1)
+        ! report 3D offset to particles 2D offsets
+        call algndoc%map3dshift22d(-offset3D, state=1)
+        ! report 3D offset to alignment references
+        !$omp parallel do proc_bind(close) default(shared) private(iref,R,offset2D)
+        do iref = 1,ncls
+            ! Projection direction rotation matrix
+            R = euler2m([algnrefs%e1get(iref), algnrefs%e2get(iref), 0.0])
+            ! 3D Shift rotated with respect to projection direction
+            offset2D = matmul(R, offset3D)
+            ! Apply offset to e/o references
+            call pftc%shift_ref(iref, offset2D(1:2))
+        enddo
+        !$omp end parallel do
+    end subroutine center_3Dpolar_refs
+
+end submodule simple_polarops_state
