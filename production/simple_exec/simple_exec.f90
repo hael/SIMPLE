8a30dfa63276a59524f15fd8d0fb2bfe91d3fbf0
Hans Elmlund <hans.elmlund@nih.gov>
2026-01-16 16:09:05 -0500

    forgot to put in the CTF memoization for 3D applications

--- production/simple_exec/simple_exec.f90
+++ production/simple_exec/simple_exec.f90
@@ -466,7 +466,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('f8b46010')
+    call simple_print_git_version('83176d71')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
