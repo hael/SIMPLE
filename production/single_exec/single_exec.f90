97723fccbf41b764aab13198d1ea5bf119ddfd1c
Hans Elmlund <hans.elmlund@nih.gov>
2026-01-21 13:12:25 -0500

    fixed git cockup

--- production/single_exec/single_exec.f90
+++ production/single_exec/single_exec.f90
@@ -200,11 +200,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-<<<<<<< Updated upstream
-    call simple_print_git_version('383e6b22')
-=======
-    call simple_print_git_version('24ca98e1')
->>>>>>> Stashed changes
+    call simple_print_git_version('99278953')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
