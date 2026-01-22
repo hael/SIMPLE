ee16bcbe37e226d13ce13f220cbbe5791920f7e1
Hans Elmlund <hans.elmlund@nih.gov>
2026-01-22 13:18:12 -0500

    first draft of directory info file for automatic documentation

--- production/simple_stream.f90
+++ production/simple_stream.f90
@@ -70,7 +70,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('b650bef7')
+call simple_print_git_version('f4400f06')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
