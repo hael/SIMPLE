31017498b4b987c94815194c0579c3708f81d426
hael <hans.elmlund@nih.gov>
2026-01-25 11:45:18 -0500

    updates to documentation

--- production/simple_exec.f90
+++ production/simple_exec.f90
@@ -466,7 +466,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('1cb38c5e')
+    call simple_print_git_version('6f9ccf5e')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
