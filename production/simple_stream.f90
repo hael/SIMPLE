928e4dde95541ada7466e8e4bbbed79b5be8f67d
hael <hans.elmlund@nih.gov>
2026-01-27 00:46:36 -0800

    with pointers removed for comaprative testing

--- production/simple_stream.f90
+++ production/simple_stream.f90
@@ -70,7 +70,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('553a376a')
+call simple_print_git_version('e12a8b0e')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
--- production/single_exec.f90
+++ production/single_exec.f90
@@ -201,7 +201,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('553a376a')
+    call simple_print_git_version('e12a8b0e')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
