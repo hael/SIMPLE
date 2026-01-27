7c06e26013de863e8843a76716bf1870139d71c4
hael <hans.elmlund@nih.gov>
2026-01-27 00:40:55 -0800

    put back the pointer in apply_serial as well, since that was how I ran my original tests

--- production/simple_stream.f90
+++ production/simple_stream.f90
@@ -70,7 +70,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('d058df85')
+call simple_print_git_version('553a376a')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
