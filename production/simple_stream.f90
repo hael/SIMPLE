2f272faf69bce2e77a5b909a92ad017b048a5dda
hael <hans.elmlund@nih.gov>
2026-01-28 22:34:11 -0800

    normalization for ML compatibility by default in gen_instrfun_img

--- production/simple_stream.f90
+++ production/simple_stream.f90
@@ -70,7 +70,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('ca68b64c')
+call simple_print_git_version('dc074cf5')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
