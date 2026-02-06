0d463fa3f0c6ec9e239f6306aec4b7ed26cc1896
Ruben Meana-Paneda <rubenmepa@gmail.com>
2026-02-05 19:11:19 -0500

    make gfortran first candidate in cmake compilers autodetect - add simple_test_units to pre- hook scripts

--- production/single_exec.f90
+++ production/single_exec.f90
@@ -201,7 +201,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('b7f09ed9')
+    call simple_print_git_version('18f4c078')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
