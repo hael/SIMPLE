08e88c2907e9999a9e46f81f2a5b71887db5d31e
Ruben Meana-Paneda <rubenmepa@gmail.com>
2026-02-10 19:26:16 -0500

    add simple_test_exec to production - organized test suite for SIMPLE e.g. simple_test_exec test=list display the test suite

--- production/simple_exec.f90
+++ production/simple_exec.f90
@@ -469,7 +469,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('3dd3c4d8')
+    call simple_print_git_version('7b8f21ce')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
