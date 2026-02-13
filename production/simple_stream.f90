358229083aa86938443f032f442f14d0d451d39a
hael <hans.elmlund@nih.gov>
2026-02-13 08:53:25 -0500

    singly linked list refactoring of parameter fields in ui_program, still many TODOs in simple_user_interface before the split can be done

--- production/simple_stream.f90
+++ production/simple_stream.f90
@@ -70,7 +70,7 @@ call update_job_descriptions_in_project( cline )
 if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
-call simple_print_git_version('c3a40c44')
+call simple_print_git_version('68d2b233')
 ! end timer and print
 rt_exec = toc(t0)
 call simple_print_timer(rt_exec)
---
+++ src/main/ui/simple_ui_param.f90
@@ -0,0 +1,82 @@
+module simple_ui_param
+use simple_string, only: string
+implicit none
+
+! common input parameter type
+type ui_param
+    type(string) :: key
+    type(string) :: keytype
+    type(string) :: descr_short
+    type(string) :: descr_long
+    type(string) :: descr_placeholder
+    type(string) :: gui_submenu
+    type(string) :: active_flags
+    type(string) :: exclusive_group
+    type(string) :: cval_default
+    real         :: rval_default = 0.
+    logical      :: required = .true.
+    logical      :: advanced = .true.
+    logical      :: online   = .false.
+contains
+    procedure, private :: set_param_1
+    procedure, private :: set_param_2
+    generic            :: set_param => set_param_1, set_param_2
+    procedure          :: apply_gui_overrides
+    final              :: finalize
+end type ui_param
+
+contains
+
+    subroutine set_param_1( self, key, keytype, descr_short, descr_long, descr_placeholder, required, default_value )
+        class(ui_param), intent(inout) :: self
+        character(len=*),       intent(in)    :: key, keytype, descr_short, descr_long, descr_placeholder
+        logical,                intent(in)    :: required
+        real,                   intent(in)    :: default_value
+        self%key               = trim(key)
+        self%keytype           = trim(keytype)
+        self%descr_short       = trim(descr_short)
+        self%descr_long        = trim(descr_long)
+        self%descr_placeholder = trim(descr_placeholder)
+        self%required = required
+        if( .not. self%required ) self%rval_default = default_value
+    end subroutine set_param_1
+
+    subroutine set_param_2( self, key, keytype, descr_short, descr_long, descr_placeholder, required, default_value )
+        class(ui_param), intent(inout) :: self
+        character(len=*),       intent(in)    :: key, keytype, descr_short, descr_long, descr_placeholder
+        logical,                intent(in)    :: required
+        character(len=*),       intent(in)    :: default_value
+        self%key               = trim(key)
+        self%keytype           = trim(keytype)
+        self%descr_short       = trim(descr_short)
+        self%descr_long        = trim(descr_long)
+        self%descr_placeholder = trim(descr_placeholder)
+        self%required = required
+        if( .not. self%required ) self%cval_default = trim(default_value)
+    end subroutine set_param_2
+
+    subroutine apply_gui_overrides(p, gui_submenu, gui_exclusive_group, gui_active_flags, gui_advanced, gui_online)
+        class(ui_param),            intent(inout) :: p
+        character(len=*), optional, intent(in)    :: gui_submenu, gui_exclusive_group, gui_active_flags
+        logical,          optional, intent(in)    :: gui_advanced, gui_online
+        if( present(gui_submenu))         p%gui_submenu     = trim(gui_submenu)
+        if( present(gui_exclusive_group)) p%exclusive_group = trim(gui_exclusive_group)
+        if( present(gui_active_flags))    p%active_flags    = trim(gui_active_flags)
+        if( present(gui_online))          p%online          = gui_online
+        if( present(gui_advanced))        p%advanced        = gui_advanced
+    end subroutine apply_gui_overrides
+
+    subroutine finalize(self)
+        type(ui_param), intent(inout) :: self
+        call self%key%kill()
+        call self%keytype%kill()
+        call self%descr_short%kill()
+        call self%descr_long%kill()
+        call self%descr_placeholder%kill()
+        call self%gui_submenu%kill()
+        call self%active_flags%kill()
+        call self%exclusive_group%kill()
+        call self%cval_default%kill()
+    end subroutine finalize
+
+end module simple_ui_param
