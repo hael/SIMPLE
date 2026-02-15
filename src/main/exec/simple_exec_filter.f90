71e0b9c68b7e087bb5cd3ad11b109031bb2342ee
hael <hans.elmlund@nih.gov>
2026-02-15 07:33:25 -0500

    forgotten files

---
+++ src/main/exec/simple_exec_filter.f90
@@ -0,0 +1,35 @@
+module simple_exec_filter
+use simple_cmdline,             only: cmdline
+use simple_commanders_resolest, only: commander_uniform_filter2D, commander_uniform_filter3D
+use simple_commanders_imgops,   only: commander_filter
+implicit none
+
+public :: exec_filter_commander
+private
+
+type(commander_filter)           :: xfilter
+type(commander_uniform_filter2D) :: xuniform_filter2D
+type(commander_uniform_filter3D) :: xuniform_filter3D
+
+contains
+
+    subroutine exec_filter_commander(which, cline, l_silent, l_did_execute)
+        character(len=*),    intent(in)    :: which
+        class(cmdline),      intent(inout) :: cline
+        logical,             intent(out)   :: l_silent
+        logical,             intent(out)   :: l_did_execute
+        l_silent      = .false.
+        l_did_execute = .true.
+        select case(trim(which))
+            case( 'filter' )
+                call xfilter%execute(cline)
+            case( 'uniform_filter2D' )
+                call xuniform_filter2D%execute(cline)
+            case( 'uniform_filter3D' )
+                call xuniform_filter3D%execute(cline)
+            case default
+                l_did_execute = .false.
+        end select
+    end subroutine exec_filter_commander
+
+end module simple_exec_filter
---
+++ src/main/exec/simple_exec_ori.f90
@@ -0,0 +1,37 @@
+module simple_exec_ori
+use simple_cmdline, only: cmdline
+use simple_commanders_ori, only: commander_make_oris, commander_oriops, commander_oristats, commander_vizoris
+implicit none
+
+public :: exec_ori_commander
+private
+
+type(commander_make_oris) :: xmake_oris
+type(commander_oriops)    :: xoriops
+type(commander_oristats)  :: xoristats
+type(commander_vizoris)   :: xvizoris   
+
+contains
+
+    subroutine exec_ori_commander(which, cline, l_silent, l_did_execute)
+        character(len=*),    intent(in)    :: which
+        class(cmdline),      intent(inout) :: cline
+        logical,             intent(out)   :: l_silent
+        logical,             intent(out)   :: l_did_execute
+        l_silent      = .false.
+        l_did_execute = .true.
+        select case(trim(which))
+            case( 'make_oris' )
+                call xmake_oris%execute(cline)
+            case( 'oriops' )
+                call xoriops%execute(cline)
+            case( 'oristats' )
+                call xoristats%execute(cline)
+            case( 'vizoris' )
+                call xvizoris%execute(cline)
+            case default
+                l_did_execute = .false.
+        end select
+    end subroutine exec_ori_commander
+
+end module simple_exec_ori
