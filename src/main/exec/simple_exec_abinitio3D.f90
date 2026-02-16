41c59e890db400dbd4993603c619104a7442f0ae
Joseph Caesar <joseph.caesar@path.ox.ac.uk>
2026-02-16 12:34:57 +0000

    Major bugfix: simple_ui - all programs exited immediately plus simple_stream programs were uninitialised. Correction particle_sieving

--- src/main/exec/simple_exec_abinitio3D.f90
+++ src/main/exec/simple_exec_abinitio3D.f90
@@ -22,8 +22,9 @@ contains
     subroutine exec_abinitio3D_commander(which, cline, l_silent, l_did_execute)
         character(len=*),    intent(in)    :: which
         class(cmdline),      intent(inout) :: cline
+        logical,             intent(inout) :: l_did_execute
         logical,             intent(out)   :: l_silent
-        logical,             intent(out)   :: l_did_execute
+        
         if( l_did_execute )return
         l_silent      = .false.
         l_did_execute = .true.
--- src/main/exec/simple_exec_image.f90
+++ src/main/exec/simple_exec_image.f90
@@ -23,8 +23,8 @@ contains
     subroutine exec_image_commander(which, cline, l_silent, l_did_execute)
         character(len=*),    intent(in)    :: which
         class(cmdline),      intent(inout) :: cline
+        logical,             intent(inout) :: l_did_execute
         logical,             intent(out)   :: l_silent
-        logical,             intent(out)   :: l_did_execute
         if( l_did_execute )return
         l_silent      = .false.
         l_did_execute = .true.
