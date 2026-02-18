f9046b2d2b39e0f7d39f5c8dc92314880dbcc3bf
Afan Atif <afanatif1@gmail.com>
2026-02-18 13:28:26 -0500

    separating multi dendro and binary tree

--- production/single_exec.f90
+++ production/single_exec.f90
@@ -48,7 +48,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('d3fa2ff0')
+    call simple_print_git_version('68efd3e9')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
---
+++ src/utils/structs/simple_multi_dendro.f90
@@ -0,0 +1,136 @@
+module simple_multi_dendro
+use simple_core_module_api
+use simple_srch_sort_loc, only: mask2inds
+use simple_hclust,        only: hclust
+use simple_binary_tree,   only: binary_tree, bt_node
+implicit none
+
+public :: multi_dendro
+private
+#include "simple_local_flags.inc"
+
+type :: multi_dendro
+   private
+   type(binary_tree), allocatable :: trees(:)
+   real,              allocatable :: dist_mat(:,:)
+   integer,           allocatable :: cls_pops(:)
+   logical,           allocatable :: cls_map(:,:)
+   integer,           allocatable :: medoids(:)
+   integer :: n_trees = 0
+   integer :: n_refs  = 0
+   logical :: exists  = .false.
+contains
+   procedure          :: new
+   procedure          :: build_multi_dendro
+   procedure          :: get_left_right_idxs
+   procedure, private :: get_tree_indx
+   procedure          :: kill
+end type multi_dendro
+
+contains
+
+   subroutine new(self, dist_mat, labels)
+      class(multi_dendro), intent(inout) :: self
+      real,                intent(in)    :: dist_mat(:,:)
+      integer,             intent(in)    :: labels(:)
+      integer :: i, j
+      call self%kill()
+      if (any(labels == 0)) THROW_HARD('0 labels not allowed')
+      self%n_trees = maxval(labels)
+      self%n_refs  = size(labels)
+      allocate(self%dist_mat(self%n_refs,self%n_refs), source=dist_mat)
+      allocate(self%cls_pops(self%n_trees), self%medoids(self%n_trees), source=0)
+      do i = 1, self%n_trees
+         self%cls_pops(i) = count(labels == i)
+      end do
+      allocate(self%cls_map(self%n_trees,self%n_refs))
+      do i = 1, self%n_trees
+         do j = 1, self%n_refs
+            self%cls_map(i,j) = (labels(j) == i)
+         end do
+      end do
+      allocate(self%trees(self%n_trees))
+      self%exists = .true.
+   end subroutine new
+
+   subroutine build_multi_dendro(self, linkage)
+      class(multi_dendro), intent(inout) :: self
+      integer,             intent(in)    :: linkage
+      real,    allocatable :: sub_distmat(:,:), height(:)
+      integer, allocatable :: refs(:), merge_mat(:,:)
+      integer :: itree, i, j, nref_sub, root_idx
+      type(hclust)  :: hc
+      type(bt_node) :: node
+      do itree = 1, self%n_trees
+         nref_sub = count(self%cls_map(itree,:))
+         refs     = mask2inds(self%cls_map(itree,:))
+         if (nref_sub == 1) then
+            ! degenerate tree: single node
+            call self%trees(itree)%build_from_hclust(reshape([1,1],[2,1]), refs, self%dist_mat)
+            self%medoids(itree) = refs(1)
+            deallocate(refs)
+            cycle
+         end if
+         allocate(sub_distmat(nref_sub, nref_sub))
+         sub_distmat = 1.0
+         do i = 1, nref_sub
+            do j = i + 1, nref_sub
+               sub_distmat(i,j) = self%dist_mat(refs(i), refs(j))
+               sub_distmat(j,i) = sub_distmat(i,j)
+            end do
+         end do
+         allocate(merge_mat(2, nref_sub-1))
+         call hc%new(nref_sub, sub_distmat, linkage)
+         call hc%cluster(merge_mat, height)
+         call hc%kill()
+         call self%trees(itree)%build_from_hclust(merge_mat, refs, self%dist_mat)
+         root_idx = self%trees(itree)%get_root_idx()
+         node     = self%trees(itree)%get_node(root_idx)
+         self%medoids(itree) = node%ref_idx
+         deallocate(sub_distmat, refs, merge_mat)
+      end do
+   end subroutine build_multi_dendro
+
+   pure subroutine get_left_right_idxs(self, ref_idx, left_ref_idx, right_ref_idx)
+      class(multi_dendro), intent(in)  :: self
+      integer,             intent(in)  :: ref_idx
+      integer,             intent(out) :: left_ref_idx, right_ref_idx
+      integer :: itree
+      itree = self%get_tree_indx(ref_idx)
+      call self%trees(itree)%get_left_right_ref(ref_idx, left_ref_idx, right_ref_idx)
+   end subroutine get_left_right_idxs
+
+   pure integer function get_tree_indx(self, ref_idx) result(tree_idx)
+      class(multi_dendro), intent(in) :: self
+      integer,             intent(in) :: ref_idx
+      integer :: itree
+      tree_idx = 1
+      if (self%n_trees == 1) return
+      do itree = 1, self%n_trees
+         if (self%trees(itree)%find_node_by_ref(ref_idx) /= 0) then
+            tree_idx = itree
+            return
+         end if
+      end do
+   end function get_tree_indx
+
+   subroutine kill(self)
+      class(multi_dendro), intent(inout) :: self
+      integer :: i
+      if (.not. self%exists) return
+      if (allocated(self%trees)) then
+         do i = 1, size(self%trees)
+            call self%trees(i)%kill()
+         end do
+         deallocate(self%trees)
+      end if
+      if (allocated(self%dist_mat))  deallocate(self%dist_mat)
+      if (allocated(self%cls_pops))  deallocate(self%cls_pops)
+      if (allocated(self%cls_map))   deallocate(self%cls_map)
+      if (allocated(self%medoids))   deallocate(self%medoids)
+      self%n_trees = 0
+      self%n_refs  = 0
+      self%exists  = .false.
+   end subroutine kill
+
+end module simple_multi_dendro
