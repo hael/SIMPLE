cab2db100bada1257024ab9cd3019f13a10fda98
Afan Atif <afanatif1@gmail.com>
2026-02-20 12:57:31 -0500

    change from greedy descent on block tree to best first and stochastic version

--- production/tests/simple_test_block_tree.f90
+++ production/tests/simple_test_block_tree.f90
@@ -1,82 +1,121 @@
 !@descr: test for block-tree search space decomposition and mapping
 program simple_test_block_tree
+    ! ANGUALR RESOLUTION TEST
+    !      500   9.95311069    
+    !     1000   7.01425362    
+    !     1500   5.79369545    
+    !     2000   4.97815180    
+    !     2500   4.49270058    
+    !     3000   4.07986355    
+    !     3500   3.78387618    
+    !     4000   3.54364777    
+    !     4500   3.33894396    
+    !     5000   3.17138481    
+    !     5500   3.00895309    
+    !     6000   2.88649368    
+    !     6500   2.76534128    
+    !     7000   2.68634987    
+    !     7500   2.58823109    
+    !     8000   2.50811410    
+    !     8500   2.43517947    
+    !     9000   2.36347413    
+    !     9500   2.30154777    
+    !    10000   2.24602771    
+    !    10500   2.18775916    
+    !    11000   2.13184810    
+    !    11500   2.09256053    
+    !    12000   2.05061388    
+    !    12500   2.00759602    
+    !    13000   1.96523058    
+    !    13500   1.93482089    
+    !    14000   1.89599454    
+    !    14500   1.86424625    
+    !    15000   1.83344340    
+    !    15500   1.80385077    
+    !    16000   1.77486765    
+    !    16500   1.74663663    
+    !    17000   1.71862495    
+    !    17500   1.69673645    
+    !    18000   1.67374372    
+    !    18500   1.64686966    
+    !    19000   1.62810659    
+    !    19500   1.60912454    
+    !    20000   1.58461475 
 use simple_core_module_api
 use simple_multi_dendro,  only: multi_dendro
-use simple_block_tree,    only: gen_eulspace_block_tree
+use simple_block_tree     ! use all in there
+use simple_timer
 implicit none
 
 character(len=*), parameter :: PGRP       = 'c1'
 integer,          parameter :: NSPACE     = 5000
-integer,          parameter :: NSPACE_SUB = 500
+integer,          parameter :: NSPACE_SUB = 300
 integer,          parameter :: NSAMPLE    = 1000
 
-type(sym)            :: pgrpsym 
-type(multi_dendro)   :: block_tree
-type(oris)           :: eulspace, eulspace_sub
-
-integer   :: i, j, ind_min, irnd, imed, left_ref_idx, right_ref_idx
-type(ori) :: osmp, o, osym
-real      :: inplrotdist, dist, dist_min, dist_left, dist_right, dist_subspace
+type(sym)          :: pgrpsym
+type(multi_dendro) :: block_tree
+type(oris)         :: eulspace, eulspace_sub
+integer            :: i, j, ind_min, irnd
+integer            :: itree, best_ref
+type(ori)          :: osmp, o, osym
+real               :: inplrotdist, dist, dist_min, dist_subspace
+real               :: dist_min_tot, dist_subspace_tot, speedup
+integer(timer_int_kind) :: total_time
+real(timer_int_kind)    :: elapsed_tot = 0.0, exhaustive_time = 0.0
 
 call pgrpsym%new(PGRP)
 call eulspace    %new(NSPACE,     is_ptcl=.false.)
 call eulspace_sub%new(NSPACE_SUB, is_ptcl=.false.)
 call pgrpsym%build_refspiral(eulspace)
 call pgrpsym%build_refspiral(eulspace_sub)
-
 block_tree = gen_eulspace_block_tree(eulspace, eulspace_sub, pgrpsym)
-
+total_time = tic()
+dist_min_tot = 0.0
+dist_subspace_tot = 0.0
 do i = 1, NSAMPLE
     irnd = irnd_uni(NSPACE)
     call eulspace%get_ori(irnd, osmp)
-    ! find closest sub-space point
+    ! ---- (1) coarse: find closest sub-space point ----
     dist_min = huge(1.0)
+    ind_min  = 1
     do j = 1, NSPACE_SUB
         call eulspace_sub%get_ori(j, o)
         call pgrpsym%sym_dists(osmp, o, osym, dist, inplrotdist)
-
-        ! print *, 'SAMPLE ', irnd, ': sub-space point ', j, ' dist = ', dist
-
-        if( dist < dist_min) then
+        if (dist < dist_min) then
             dist_min = dist
             ind_min  = j
         end if
     end do
     dist_subspace = dist_min
-
-    ! print *, 'to here: closest sub-space point = ', ind_min, ' dist = ', dist_min
-    imed = block_tree%get_medoid(ind_min)
-
-    ! print *, 'SAMPLE ', irnd, ': closest sub-space point = ', ind_min, ' medoid = ', imed, ' dist = ', dist_min
-
-    call eulspace%get_ori(imed, o)
-    call pgrpsym%sym_dists(osmp, o, osym, dist_min, inplrotdist)
-    do
-        call block_tree%get_left_right_idxs(imed, left_ref_idx, right_ref_idx)
-        dist_left  = huge(1.0)
-        if( left_ref_idx /= 0 )then
-            call eulspace%get_ori(left_ref_idx, o)
-            call pgrpsym%sym_dists(osmp, o, osym, dist_left, inplrotdist)
-        endif
-        dist_right = huge(1.0)
-        if( right_ref_idx /= 0 )then
-            call eulspace%get_ori(right_ref_idx, o)
-            call pgrpsym%sym_dists(osmp, o, osym, dist_right, inplrotdist)
-        endif
-        if( any([dist_left, dist_right] < dist_min) ) then
-             if( dist_left < dist_right) then
-                dist_min = dist_left
-                imed     = left_ref_idx
-             else
-                dist_min = dist_right
-                imed     = right_ref_idx
-             end if
-        else
-            exit
+    itree = ind_min
+    call srch_eul_bl_tree_best_first_greedy(osmp, eulspace, pgrpsym, block_tree, itree, best_ref, dist_min)
+    !  call srch_eul_bl_tree_best_first_stoch(osmp, eulspace, pgrpsym, block_tree, itree, best_ref, dist_min)
+    dist_min_tot      = dist_min_tot + dist_min
+    dist_subspace_tot = dist_subspace_tot + dist_subspace
+    print *, 'SAMPLE ', irnd, ': itree=', itree, ' dist=', dist_min, ' dist_subspace=', dist_subspace
+end do
+elapsed_tot = toc(total_time)
+total_time  = tic()
+do i = 1, NSAMPLE
+    irnd = irnd_uni(NSPACE)
+    call eulspace%get_ori(irnd, osmp)
+    ! ---- (1) coarse: find closest sub-space point ----
+    dist_min = huge(1.0)
+    ind_min  = 1
+    do j = 1, NSPACE
+        call eulspace%get_ori(j, o)
+        call pgrpsym%sym_dists(osmp, o, osym, dist, inplrotdist)
+        if (dist < dist_min) then
+            dist_min = dist
+            ind_min  = j
         end if
     end do
-    print *, 'SAMPLE ', irnd, ': closest sub-space point = ', ind_min, ' medoid = ', imed, ' dist = ', dist_min, ' dist_subspace = ', dist_subspace
 end do
-
-
+exhaustive_time = toc(total_time)
+speedup = exhaustive_time / elapsed_tot
+print *, 'AVERAGE DISTANCE TO CLOSEST SUB-SPACE POINT = ', dist_subspace_tot / NSAMPLE
+print *, 'AVERAGE SEARCH TIME (ms)                    = ', elapsed_tot / NSAMPLE * 1e3
+print *, 'AVERAGE EXHAUSTIVE SEARCH TIME (ms)         = ', exhaustive_time / NSAMPLE * 1e3
+print *, 'SPEEDUP OVER EXHAUSTIVE SEARCH              = ', speedup
+print *, '*****FINAL AVERAGE DISTANCE*****            = ', dist_min_tot / NSAMPLE
 end program simple_test_block_tree
