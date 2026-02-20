b3d5d4c6faa66c527a1f466481d09c2d61223197
hael <hans.elmlund@nih.gov>
2026-02-20 05:31:53 -0500

    more build_glob cleanups

--- production/simple_exec.f90
+++ production/simple_exec.f90
@@ -57,7 +57,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('2624614c')
+    call simple_print_git_version('4e9c95a3')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
