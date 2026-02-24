178aa0d032cc043954df1c5f04f485beda4d45b0
Ruben Meana-Paneda <rubenmepa@gmail.com>
2026-02-24 14:20:41 -0500

    add sars_cov2_spkgp_6vxx molecule for testing purposes

--- production/tests/simple_test_pdb2mrc.f90
+++ production/tests/simple_test_pdb2mrc.f90
@@ -2,14 +2,18 @@ program simple_test_pdb2mrc
 use simple_core_module_api
 use simple_image, only : image
 use simple_atoms, only : atoms
-use simple_molecule_data, only : molecule_data, betagal_1jyx_molecule
+use simple_molecule_data, only : molecule_data, betagal_1jyx, sars_cov2_spkgp_6vxx
 implicit none
 type(string)        :: pdb_file, vol_file
-type(atoms)         :: betagal
+type(atoms)         :: molecule
 type(molecule_data) :: mol
-real, parameter     :: smpd = 1.
-mol = betagal_1jyx_molecule()
+real, parameter     :: smpd = 1.3
+mol = sars_cov2_spkgp_6vxx()
+pdb_file = '6VXX.pdb'
+vol_file = '6VXX.mrc'
+call molecule%pdb2mrc( pdb_file, vol_file, smpd, mol=mol )
+mol = betagal_1jyx()
 pdb_file = '1JYX.pdb'
 vol_file = '1JYX.mrc'
-call betagal%pdb2mrc( pdb_file, vol_file, smpd, mol=mol )
+call molecule%pdb2mrc( pdb_file, vol_file, smpd, mol=mol )
 end program simple_test_pdb2mrc
