be0c019040a1e90bf3a656dcdd8ca7ee23f9031a
hael <hans.elmlund@nih.gov>
2026-02-25 22:43:12 -0500

    abinitio2D: regularization scheme trying to mirror abinitio3D, testing underway

--- production/single_exec.f90
+++ production/single_exec.f90
@@ -49,7 +49,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('4a42c4f2')
+    call simple_print_git_version('94cd2efb')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
