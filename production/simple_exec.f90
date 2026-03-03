72292b97ac3f6d85f5ece2b280a111a4fde97cd5
hael <hans.elmlund@nih.gov>
2026-03-03 07:08:10 -0500

    minor edits before re-testing convergence behavior of polar=yes mode on streptav

--- production/simple_exec.f90
+++ production/simple_exec.f90
@@ -58,7 +58,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('cad0b085')
+    call simple_print_git_version('8ab1a57a')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
