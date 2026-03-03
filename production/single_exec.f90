fc66ba39a82d29a53a1c655b102df0f288e9806d
Hans Elmlund <hans.elmlund@nih.gov>
2026-03-03 11:06:56 -0500

    slight modification of early stopping criterion for abinitio3D with polar=yes

--- production/single_exec.f90
+++ production/single_exec.f90
@@ -49,7 +49,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('8ab1a57a')
+    call simple_print_git_version('72292b97')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
