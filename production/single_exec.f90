3f250a3061f69e2aad9366b03fe7d9655209a0ac
hael <hans.elmlund@nih.gov>
2026-03-07 14:17:07 -0500

    simplified fill-in sampling

--- production/single_exec.f90
+++ production/single_exec.f90
@@ -49,7 +49,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('2c40a2f8')
+    call simple_print_git_version('c3ce5726')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
