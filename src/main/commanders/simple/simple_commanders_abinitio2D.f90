965cb16563d472cbaa4d09e772b31ad9bd538d34
Hans Elmlund <hans.elmlund@nih.gov>
2026-03-26 10:11:28 -0400

    separated out a cluster2D control module from simple_commanders_abinitio2D to make changes to stage-dependent control parameters more convenien

--- src/main/commanders/simple/simple_commanders_abinitio2D.f90
+++ src/main/commanders/simple/simple_commanders_abinitio2D.f90
@@ -3,6 +3,7 @@ module simple_commanders_abinitio2D
 use simple_commanders_api
 use simple_commanders_cavgs
 use simple_commanders_cluster2D
+use simple_abinitio2D_utils_cluster2D 
 implicit none
 
 public :: commander_abinitio2D
@@ -14,24 +15,6 @@ type, extends(commander_base) :: commander_abinitio2D
     procedure :: execute => exec_abinitio2D
 end type commander_abinitio2D
 
-! Dimensions
-real,    parameter :: SMPD_TARGET     = 2.67
-integer, parameter :: MINBOXSZ        = 88
-! Stages
-integer, parameter :: NSTAGES_CLS     = 6
-integer, parameter :: NSTAGES_SINGLE  = 1
-integer, parameter :: ITS_INCR_SINGLE = 4
-integer, parameter :: ITS_INCR        = 5
-integer, parameter :: PHASES(2)       = [4, 6]
-integer, parameter :: EXTR_LIM_LOCAL  = 20
-
-! convenience type
-type stage_params
-    real    :: lp=0., smpd_crop=0., trslim=0.
-    integer :: box_crop = 0, max_cls_pop=0, nptcls=0
-    logical :: l_lpset=.false.
-end type stage_params
-
 ! class variables
 type(stage_params), allocatable :: stage_parms(:)
 
@@ -42,14 +25,14 @@ contains
         class(commander_abinitio2D), intent(inout) :: self
         class(cmdline),              intent(inout) :: cline
         ! commanders
-        type(commander_cluster2D)        :: xcluster2D
-        type(commander_calc_pspec)       :: xcalc_pspec
+        type(commander_cluster2D)  :: xcluster2D
+        type(commander_calc_pspec) :: xcalc_pspec
         ! command lines
-        type(cmdline)                    :: cline_cluster2D, cline_calc_pspec
+        type(cmdline)              :: cline_cluster2D, cline_calc_pspec
         ! other
-        type(parameters)                 :: params
-        type(sp_project)                 :: spproj
-        class(oris),             pointer :: spproj_field
+        type(parameters)           :: params
+        type(sp_project)           :: spproj
+        class(oris),       pointer :: spproj_field
         integer :: maxits, istage, last_iter, nptcls_eff, nstages
         logical :: l_shmem, l_inpl
         call cline%set('oritype',   'ptcl2D')
@@ -76,21 +59,7 @@ contains
         maxits = params%extr_lim
         call cline%delete('stats')
         ! check refinement flag and set stages
-        l_inpl = .false.
-        select case(trim(params%refine))
-            case('inpl','inpl_smpl')
-                ! in-plane refinement, one stage only
-                nstages = NSTAGES_SINGLE
-                l_inpl  = .true.
-            case('prob')
-                ! probabilistic 2D refinement
-                nstages = NSTAGES_CLS
-            case('snhc','snhc_smpl')
-                ! usual suspects
-                nstages = NSTAGES_CLS
-            case DEFAULT
-                THROW_HARD('Unsupported REFINE argument: '//trim(params%refine))
-        end select
+        call determine_abinitio2D_stages(trim(params%refine), nstages, l_inpl)
         ! override # stages
         if( cline%defined('nstages') ) nstages = min(params%nstages,NSTAGES_CLS)
         allocate(stage_parms(nstages))
@@ -122,7 +91,7 @@ contains
                 write(logfhandle,'(A,I3,A)')'>>> STAGE ', istage,' WITH GOLD STANDARD E/O'
             endif
             ! parameters update
-            call set_cline_cluster2D(istage)
+            call set_cline_cluster2D_stage(cline_cluster2D, cline, params, stage_parms, nstages, maxits, istage)
             ! classify
             call execute_cluster2D
         enddo
@@ -253,7 +222,7 @@ contains
             real         :: lpstart, lpstop, cenlp
             integer      :: istage
             ! Resolution limits
-            call mskdiam2lplimits_here(params%mskdiam, lpstart, lpstop, cenlp)
+            call mskdiam2lplimits_cluster2D(params%mskdiam, lpstart, lpstop, cenlp)
             lpstart = max(lpstart, 2.*params%smpd_crop)
             lpstop  = max(lpstop,  2.*params%smpd_crop)
             cenlp   = max(cenlp,   2.*params%smpd_crop)
@@ -326,142 +295,6 @@ contains
             call cline_cluster2D%set('chunk',     'no')
             call set_automask2D_defaults( cline_cluster2D )
         end subroutine prep_command_lines
-
-        subroutine set_cline_cluster2D( istage )
-            integer, intent(in) :: istage
-            type(string) :: refine, center, objfun, refs, gauref, ml_reg
-            integer      :: iphase, iter, imaxits, minits, extr_iter
-            real         :: trs, gaufreq
-            logical      :: l_gauref, l_gaufreq_input
-            refine = trim(params%refine)
-            ! Gaussian filter
-            l_gauref        = trim(params%gauref).eq.'yes'
-            l_gaufreq_input = cline%defined('gaufreq')
-            ! objective function
-            if( params%cc_objfun == OBJFUN_CC )then
-                objfun   = 'cc'
-            else
-                objfun   = 'euclid'
-            endif
-            ! iteration number book-keeping
-            iter = 0
-            if( cline_cluster2D%defined('endit') ) iter = cline_cluster2D%get_iarg('endit')
-            iter = iter + 1
-            call cline_cluster2D%delete('which_iter')
-            ! Phase & stages
-            if( nstages == 1 )then
-                ! Single stage refinement
-                minits    = ITS_INCR_SINGLE
-                imaxits   = ITS_INCR_SINGLE+2
-                extr_iter = 999
-                trs       = stage_parms(1)%trslim
-                center    = trim(params%center)
-                objfun    = 'euclid'
-                ! Filters deactivated
-                gauref    = 'no'
-                ml_reg    = params%ml_reg
-                ! continue from previous references
-                refs      = params%refs
-                ! resolution limit
-                call cline_cluster2D%set('lpstart', params%lpstart)
-                call cline_cluster2D%set('lpstop',  params%lpstop)
-                if( stage_parms(1)%l_lpset ) call cline_cluster2D%set('lp', params%lp)
-            else
-                ! phase logics
-                if(      istage <= PHASES(1) )then
-                    iphase = 1
-                else if( istage <= PHASES(2) )then
-                    iphase = 2
-                else
-                    iphase = 0
-                    THROW_HARD('Invalid istage index')
-                endif
-                ! stages
-                select case(iphase)
-                    case(1)
-                        ! phase constants
-                        extr_iter = 0
-                        ! phase variables
-                        imaxits = nint(real(istage)*real(maxits)/real(PHASES(1)))
-                        minits  = imaxits
-                        select case(istage)
-                            case(1)
-                                trs      = 0.
-                                center   = 'no'
-                                if( cline%defined('refs') )then
-                                    refs     = params%refs
-                                else
-                                    refs     = NIL
-                                endif
-                                ml_reg   = 'no'
-                                gauref   = 'yes'
-                                if( l_gaufreq_input )then
-                                    gaufreq  = params%gaufreq
-                                else
-                                    gaufreq  = stage_parms(istage)%lp
-                                endif
-                            case(2)
-                                trs      = stage_parms(istage)%trslim
-                                center   = trim(params%center)
-                                refs     = CAVGS_ITER_FBODY//int2str_pad(iter-1,3)//params%ext%to_char()
-                                ml_reg   = params%ml_reg
-                                gauref   = 'no'
-                            case(3)
-                                trs      = stage_parms(istage)%trslim
-                                center   = trim(params%center)
-                                refs     = CAVGS_ITER_FBODY//int2str_pad(iter-1,3)//params%ext%to_char()
-                                ml_reg   = params%ml_reg
-                                gauref   = 'no'
-                            case(4)
-                                trs      = stage_parms(istage)%trslim
-                                center   = trim(params%center)
-                                refs     = CAVGS_ITER_FBODY//int2str_pad(iter-1,3)//params%ext%to_char()
-                                ml_reg   = params%ml_reg
-                                gauref   = 'no'
-                        end select
-                    case(2)
-                        ! phase constants
-                        imaxits   = iter+ITS_INCR-1
-                        trs       = stage_parms(istage)%trslim
-                        center    = trim(params%center)
-                        extr_iter = params%extr_lim+1
-                        refs      = CAVGS_ITER_FBODY//int2str_pad(iter-1,3)//params%ext%to_char()
-                        ml_reg    = params%ml_reg
-                        gauref    = 'no'
-                        minits    = iter+1
-                end select
-            endif
-            ! command line update
-            if( stage_parms(istage)%l_lpset )then
-                call cline_cluster2D%set('lp',    stage_parms(istage)%lp)
-            else
-                call cline_cluster2D%delete('lp')
-            endif
-            if( refs .ne. NIL ) call cline_cluster2D%set('refs', refs)
-            if( extr_iter > 0 )then
-                call cline_cluster2D%set('extr_iter', extr_iter)
-            else
-                call cline_cluster2D%delete('extr_iter')
-            endif
-            call cline_cluster2D%set('ml_reg', ml_reg)
-            call cline_cluster2D%set('gauref', gauref)
-            if( gauref.eq.'yes' )then
-                call cline_cluster2D%set('gaufreq', gaufreq)
-            else
-                call cline_cluster2D%delete('gaufreq')
-            endif
-            call cline_cluster2D%set('minits',    minits)
-            call cline_cluster2D%set('maxits',    imaxits)
-            call cline_cluster2D%set('startit',   iter)
-            call cline_cluster2D%set('refine',    refine)
-            call cline_cluster2D%set('objfun',    objfun)
-            call cline_cluster2D%set('trs',       trs)
-            call cline_cluster2D%set('center',    center)
-            call cline_cluster2D%set('box_crop',  stage_parms(istage)%box_crop)
-            call cline_cluster2D%set('smpd_crop', stage_parms(istage)%smpd_crop)
-            call cline_cluster2D%delete('update_frac')
-            call cline_cluster2D%delete('endit')
-        end subroutine set_cline_cluster2D
 
         subroutine execute_cluster2D
             call del_file(CLUSTER2D_FINISHED)
@@ -544,15 +377,6 @@ contains
             call cline_rank_cavgs%set('outstk',   finalcavgs_ranked)
             call xrank_cavgs%execute( cline_rank_cavgs )
         end subroutine gen_final_cavgs
-
-        ! attempt at defining resolution limits
-        subroutine mskdiam2lplimits_here( mskdiam, lpstart,lpstop, lpcen )
-            real, intent(in)    :: mskdiam
-            real, intent(inout) :: lpstart,lpstop, lpcen
-            lpstart = min(max(mskdiam/12., 15.), 20.)
-            lpstop  = min(max(mskdiam/22.,  6.),  8.)
-            lpcen   = min(max(mskdiam/6.,  20.), 30.)
-        end subroutine mskdiam2lplimits_here
 
     end subroutine exec_abinitio2D
 
