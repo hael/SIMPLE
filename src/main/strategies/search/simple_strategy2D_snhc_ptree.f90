70b315dfe3ac89982319777a9610a14b1699d997
Hans Elmlund <hans.elmlund@nih.gov>
2026-03-27 13:28:53 -0400

    forgotten flies added

---
+++ src/main/strategies/search/simple_strategy2D_snhc_ptree.f90
@@ -0,0 +1,95 @@
+!@descr: 2D hybrid strategy: SNHC coarse class selection followed by probabilistic tree descent
+module simple_strategy2D_snhc_ptree
+use simple_pftc_srch_api
+use simple_strategy2D_alloc
+use simple_strategy2D_tree_utils, only: descend_tree_prob, get_tree_for_ref
+use simple_strategy2D,            only: strategy2D
+use simple_strategy2D_srch,       only: strategy2D_spec
+use simple_builder,               only: builder
+implicit none
+
+public :: strategy2D_snhc_ptree
+private
+#include "simple_local_flags.inc"
+
+logical, parameter :: DEBUG = .false.
+
+type, extends(strategy2D) :: strategy2D_snhc_ptree
+contains
+    procedure :: new  => new_snhc_ptree
+    procedure :: srch => srch_snhc_ptree
+    procedure :: kill => kill_snhc_ptree
+end type strategy2D_snhc_ptree
+
+contains
+
+    subroutine new_snhc_ptree( self, params, spec, build )
+        class(strategy2D_snhc_ptree), intent(inout) :: self
+        class(parameters),             intent(in)    :: params
+        class(strategy2D_spec),        intent(inout) :: spec
+        class(builder),                intent(in)    :: build
+        call self%s%new(params, spec, build)
+        self%spec = spec
+    end subroutine new_snhc_ptree
+
+    subroutine srch_snhc_ptree( self, os )
+        class(strategy2D_snhc_ptree), intent(inout) :: self
+        class(oris),                  intent(inout) :: os
+        integer :: iref, isample, inpl_ind, itree, ntrees, order_ind
+        integer :: nrefs_coarse, nrefs_tree, iref_best_coarse
+        real    :: corrs(self%s%nrots), inpl_corr, corr_best_coarse
+        integer :: vec_nrots(self%s%nrots)
+        if( os%get_state(self%s%iptcl) <= 0 )then
+            call os%reject(self%s%iptcl)
+            return
+        endif
+        call self%s%prep4srch(os)
+        call self%s%inpl_srch_first
+        if( .not. allocated(self%s%b_ptr%subspace_full2sub_map) )then
+            THROW_HARD('snhc_ptree search requires subspace_full2sub_map. Check builder construction.')
+        endif
+        ntrees = self%s%b_ptr%block_tree%get_n_trees()
+        if( ntrees <= 0 )then
+            THROW_HARD('snhc_ptree search requires at least one block tree.')
+        endif
+        nrefs_coarse     = 0
+        nrefs_tree       = 0
+        corr_best_coarse = -huge(1.0)
+        iref_best_coarse = self%s%prev_class
+        do isample = 1, self%s%nrefs
+            iref = s2D%srch_order(self%s%iptcl_batch, isample)
+            self%s%nrefs_eval = self%s%nrefs_eval + 1
+            if( self%s%nrefs_eval > s2D%snhc_nrefs_bound ) exit
+            if( s2D%cls_pops(iref) == 0 ) cycle
+            if( self%s%l_sh_first )then
+                call self%s%b_ptr%pftc%gen_objfun_vals(iref, self%s%iptcl, self%s%xy_first, corrs)
+            else
+                call self%s%b_ptr%pftc%gen_objfun_vals(iref, self%s%iptcl, [0.,0.],         corrs)
+            endif
+            call power_sampling( s2D%power, self%s%nrots, corrs, vec_nrots, &
+                                &s2D%snhc_smpl_ninpl, inpl_ind, order_ind, inpl_corr )
+            nrefs_coarse = nrefs_coarse + 1
+            if( inpl_corr > corr_best_coarse )then
+                corr_best_coarse  = inpl_corr
+                iref_best_coarse  = iref
+                self%s%best_class = iref
+                self%s%best_corr  = inpl_corr
+                self%s%best_rot   = inpl_ind
+            endif
+        end do
+        if( nrefs_coarse > 0 )then
+            itree = get_tree_for_ref(self%s, iref_best_coarse, ntrees)
+            if( itree > 0 ) call descend_tree_prob(self%s, itree, nrefs_tree)
+        endif
+        self%s%nrefs_eval = nrefs_coarse
+        call self%s%inpl_srch
+        call self%s%store_solution(os, nrefs=min(self%s%nrefs, s2D%snhc_nrefs_bound+1))
+        if( DEBUG ) write(logfhandle,*) '>>> strategy2D_snhc_ptree::FINISHED SNHC+PTREE SEARCH'
+    end subroutine srch_snhc_ptree
+
+    subroutine kill_snhc_ptree( self )
+        class(strategy2D_snhc_ptree), intent(inout) :: self
+        call self%s%kill
+    end subroutine kill_snhc_ptree
+
+end module simple_strategy2D_snhc_ptree
---
+++ src/main/strategies/search/simple_strategy2D_tree_utils.f90
@@ -0,0 +1,161 @@
+!@descr: helper routines shared by tree-guided 2D search strategies
+module simple_strategy2D_tree_utils
+use simple_core_module_api
+use simple_stat, only: power_sampling
+use simple_strategy_tree_helpers
+use simple_strategy2D_alloc
+use simple_strategy2D_srch, only: strategy2D_srch
+implicit none
+
+public :: peak_tree_selection, init_peak_tree_selection, select_peak_trees, &
+&descend_tree_prob, descend_tree_greedy, get_tree_for_ref
+private
+#include "simple_local_flags.inc"
+
+type :: peak_tree_selection
+    integer :: ntrees       = 0
+    integer :: npeak_target = 0
+    integer :: npeak_trees  = 0
+    integer :: peak_trees(MAX_NPEAKS)
+    real    :: tree_best_corrs(MAX_NTREES)
+    real    :: peak_tree_corrs(MAX_NPEAKS)
+end type peak_tree_selection
+
+contains
+
+    subroutine init_peak_tree_selection( sel, ntrees, npeak_target )
+        type(peak_tree_selection), intent(inout) :: sel
+        integer,                   intent(in)    :: ntrees, npeak_target
+        sel%npeak_trees  = 0
+        sel%ntrees       = ntrees
+        sel%npeak_target = npeak_target
+        if( ntrees > 0 )then
+            sel%tree_best_corrs(1:ntrees) = INVALID_CORR
+        endif
+        if( npeak_target > 0 )then
+            sel%peak_trees(1:npeak_target)      = 0
+            sel%peak_tree_corrs(1:npeak_target) = INVALID_CORR
+        endif
+    end subroutine init_peak_tree_selection
+
+    subroutine select_peak_trees( sel )
+        type(peak_tree_selection), intent(inout) :: sel
+        integer :: loc(1)
+        real    :: work_corrs(MAX_NTREES)
+        sel%npeak_trees                         = 0
+        sel%peak_trees(1:sel%npeak_target)      = 0
+        sel%peak_tree_corrs(1:sel%npeak_target) = INVALID_CORR
+        work_corrs(1:sel%ntrees) = sel%tree_best_corrs(1:sel%ntrees)
+        do while( sel%npeak_trees < sel%npeak_target )
+            loc = maxloc(work_corrs(1:sel%ntrees))
+            if( is_invalid_corr(work_corrs(loc(1))) ) exit
+            sel%npeak_trees                      = sel%npeak_trees + 1
+            sel%peak_trees(sel%npeak_trees)      = loc(1)
+            sel%peak_tree_corrs(sel%npeak_trees) = work_corrs(loc(1))
+            work_corrs(loc(1)) = INVALID_CORR
+        end do
+    end subroutine select_peak_trees
+
+    subroutine descend_tree_prob( s, itree, nrefs_tree )
+        use simple_binary_tree, only: bt_node
+        class(strategy2D_srch), intent(inout) :: s
+        integer,                intent(in)    :: itree
+        integer,                intent(inout) :: nrefs_tree
+        type(bt_node) :: node_cur, node_root
+        integer       :: inode, inode_next
+        real          :: best_corr_L, best_corr_R, tree_best_corr
+        node_root = s%b_ptr%block_tree%get_root_node(itree)
+        call eval_tree_ref(s, node_root%ref_idx, tree_best_corr, nrefs_tree)
+        inode = node_root%node_idx
+        do
+            if( inode == 0 ) exit
+            if( s%b_ptr%block_tree%is_leaf(itree, inode) ) exit
+            node_cur = s%b_ptr%block_tree%get_node(itree, inode)
+            if( node_cur%left_idx == 0 .and. node_cur%right_idx == 0 ) exit
+            call eval_child_best(s, itree, node_cur%left_idx,  best_corr_L, nrefs_tree)
+            call eval_child_best(s, itree, node_cur%right_idx, best_corr_R, nrefs_tree)
+            tree_best_corr = max(tree_best_corr, best_corr_L, best_corr_R)
+            inode_next = choose_next_child_prob(node_cur%left_idx, node_cur%right_idx, best_corr_L, best_corr_R)
+            if( inode_next == 0 ) exit
+            inode = inode_next
+        end do
+    end subroutine descend_tree_prob
+
+    subroutine descend_tree_greedy( s, itree, nrefs_tree )
+        use simple_binary_tree, only: bt_node
+        class(strategy2D_srch), intent(inout) :: s
+        integer,                intent(in)    :: itree
+        integer,                intent(inout) :: nrefs_tree
+        type(bt_node) :: node_cur, node_root
+        integer       :: inode, inode_next
+        real          :: best_corr_L, best_corr_R, tree_best_corr
+        node_root = s%b_ptr%block_tree%get_root_node(itree)
+        call eval_tree_ref(s, node_root%ref_idx, tree_best_corr, nrefs_tree)
+        inode = node_root%node_idx
+        do
+            if( inode == 0 ) exit
+            if( s%b_ptr%block_tree%is_leaf(itree, inode) ) exit
+            node_cur = s%b_ptr%block_tree%get_node(itree, inode)
+            if( node_cur%left_idx == 0 .and. node_cur%right_idx == 0 ) exit
+            call eval_child_best(s, itree, node_cur%left_idx,  best_corr_L, nrefs_tree)
+            call eval_child_best(s, itree, node_cur%right_idx, best_corr_R, nrefs_tree)
+            tree_best_corr = max(tree_best_corr, best_corr_L, best_corr_R)
+            inode_next = choose_next_child_greedy(node_cur%left_idx, node_cur%right_idx, best_corr_L, best_corr_R)
+            if( inode_next == 0 ) exit
+            inode = inode_next
+        end do
+    end subroutine descend_tree_greedy
+
+    integer function get_tree_for_ref( s, iref, ntrees ) result(itree)
+        class(strategy2D_srch), intent(in) :: s
+        integer,                intent(in) :: iref, ntrees
+        itree = 0
+        if( iref < 1 ) return
+        itree = s%b_ptr%subspace_full2sub_map(iref)
+        if( ntrees > 0 )then
+            if( itree < 1 .or. itree > ntrees ) itree = 0
+        endif
+    end function get_tree_for_ref
+
+    subroutine eval_child_best( s, itree, child_idx, best_corr, nrefs_tree )
+        use simple_binary_tree, only: bt_node
+        class(strategy2D_srch), intent(inout) :: s
+        integer,                intent(in)    :: itree
+        integer,                intent(in)    :: child_idx
+        real,                   intent(out)   :: best_corr
+        integer,                intent(inout) :: nrefs_tree
+        type(bt_node) :: node_child
+        best_corr = INVALID_CORR
+        if( child_idx == 0 ) return
+        node_child = s%b_ptr%block_tree%get_node(itree, child_idx)
+        if( node_child%ref_idx == 0 ) return
+        call eval_tree_ref(s, node_child%ref_idx, best_corr, nrefs_tree)
+    end subroutine eval_child_best
+
+    subroutine eval_tree_ref( s, ref_idx, best_corr, nrefs_tree )
+        class(strategy2D_srch), intent(inout) :: s
+        integer,                intent(in)    :: ref_idx
+        real,                   intent(out)   :: best_corr
+        integer,                intent(inout) :: nrefs_tree
+        integer :: vec_nrots(s%nrots), inpl_ind, order_ind
+        real    :: corr_tmp, inpl_corrs(s%nrots)
+        best_corr = INVALID_CORR
+        if( ref_idx < 1 ) return
+        if( s2D%cls_pops(ref_idx) == 0 ) return
+        if( s%l_sh_first )then
+            call s%b_ptr%pftc%gen_objfun_vals(ref_idx, s%iptcl, s%xy_first, inpl_corrs)
+        else
+            call s%b_ptr%pftc%gen_objfun_vals(ref_idx, s%iptcl, [0.,0.], inpl_corrs)
+        endif
+        call power_sampling( s2D%power, s%nrots, inpl_corrs, vec_nrots, &
+                            &s2D%snhc_smpl_ninpl, inpl_ind, order_ind, corr_tmp )
+        nrefs_tree = nrefs_tree + 1
+        best_corr  = corr_tmp
+        if( corr_tmp > s%best_corr )then
+            s%best_class = ref_idx
+            s%best_rot   = inpl_ind
+            s%best_corr  = corr_tmp
+        endif
+    end subroutine eval_tree_ref
+
+end module simple_strategy2D_tree_utils
