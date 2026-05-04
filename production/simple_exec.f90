d59e62524b9f3856bede019da3f2b129b0557f2f
Hans Elmlund <hans.elmlund@nih.gov>
2026-05-03 21:07:56 -0400

    bugfix abinitio3D_cavgs

--- production/simple_exec.f90
+++ production/simple_exec.f90
@@ -70,7 +70,7 @@ if( logfhandle .ne. OUTPUT_UNIT )then
     if( is_open(logfhandle) ) call fclose(logfhandle)
 endif
 if( .not. l_silent )then
-    call simple_print_git_version('e6bd8ed3')
+    call simple_print_git_version('e8e225b2')
     ! end timer and print
     rt_exec = toc(t0)
     call simple_print_timer(rt_exec)
