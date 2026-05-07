84710fe62e3353c4a3d0db785a989098fb132b78
Ruben Meana-Paneda <rubenmepa@gmail.com>
2026-05-07 12:48:52 -0400

    update perl scripts to generate movies or mrc lists in the current directory or one passed as argument

--- scripts/filetab_movs.pl
+++ scripts/filetab_movs.pl
@@ -1,20 +1,48 @@
 #!/usr/bin/env perl
-# Create filetab.txt file - List of all mrc files in the current directory
+# Create movies.txt file from movie files in a directory (MRC/EER/TIFF)
 use strict;
 use warnings;
 use Cwd 'abs_path';
 
-my $outfile = 'filetab.txt';
+if (@ARGV > 1) {
+    die "Usage: $0 [directory]\n";
+}
 
-opendir(my $dh, '.') or die "Cannot open current directory: $!";
-my @mrc_files = sort grep { /\.mrc$/i && -f $_ } readdir($dh);
+my $input_dir = @ARGV ? $ARGV[0] : '.';
+die "Input is not a directory: $input_dir\n" unless -d $input_dir;
+
+my $base_dir = abs_path($input_dir);
+my $outfile = 'movies.txt';
+
+opendir(my $dh, $base_dir) or die "Cannot open directory $base_dir: $!\n";
+my @entries = readdir($dh);
 closedir($dh);
 
+my @mrc_files  = sort grep { /\.mrc$/i          && -f "$base_dir/$_" } @entries;
+my @eer_files  = sort grep { /\.eer$/i          && -f "$base_dir/$_" } @entries;
+my @tiff_files = sort grep { /\.(?:tif|tiff)$/i && -f "$base_dir/$_" } @entries;
+
+my @selected;
+my $kind;
+if (@mrc_files) {
+    @selected = @mrc_files;
+    $kind = 'MRC';
+} elsif (@eer_files) {
+    @selected = @eer_files;
+    $kind = 'EER';
+} elsif (@tiff_files) {
+    @selected = @tiff_files;
+    $kind = 'TIFF';
+} else {
+    die "No .mrc, .eer, .tif, or .tiff files found in $base_dir\n";
+}
+
 open(my $fh, '>', $outfile) or die "Cannot open $outfile: $!";
-for my $file (@mrc_files) {
-    my $abs = abs_path($file);
+for my $file (@selected) {
+    my $abs = abs_path("$base_dir/$file");
     print $fh "$abs\n";
 }
 close($fh);
 
-print "Found " . scalar(@mrc_files) . " MRC file(s). Written to $outfile\n";
+print "Searched directory: $base_dir\n";
+print "Using $kind entries: " . scalar(@selected) . " file(s). Written to $outfile\n";
--- scripts/filetab_mrc.pl
+++ scripts/filetab_mrc.pl
@@ -1,20 +1,32 @@
 #!/usr/bin/env perl
-# Create filetab.txt file - List of all mrc files in the current directory
+# Create filetab.txt file - List of all mrc files in a directory
 use strict;
 use warnings;
 use Cwd 'abs_path';
 
+if (@ARGV > 1) {
+    die "Usage: $0 [directory]\n";
+}
+
+my $input_dir = @ARGV ? $ARGV[0] : '.';
+die "Input is not a directory: $input_dir\n" unless -d $input_dir;
+
+my $base_dir = abs_path($input_dir);
+
 my $outfile = 'filetab.txt';
 
-opendir(my $dh, '.') or die "Cannot open current directory: $!";
-my @mrc_files = sort grep { /\.mrc$/i && -f $_ } readdir($dh);
+opendir(my $dh, $base_dir) or die "Cannot open directory $base_dir: $!\n";
+my @entries = readdir($dh);
 closedir($dh);
+
+my @mrc_files = sort grep { /\.mrc$/i && -f "$base_dir/$_" } @entries;
 
 open(my $fh, '>', $outfile) or die "Cannot open $outfile: $!";
 for my $file (@mrc_files) {
-    my $abs = abs_path($file);
+    my $abs = abs_path("$base_dir/$file");
     print $fh "$abs\n";
 }
 close($fh);
 
+print "Searched directory: $base_dir\n";
 print "Found " . scalar(@mrc_files) . " MRC file(s). Written to $outfile\n";
