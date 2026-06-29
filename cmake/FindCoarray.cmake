50fb5d8d5971eb9d1d3af0ccc834fb3562481622
Ruben Meana-Paneda <ruben.meana-paneda@nih.gov>
2026-06-29 16:54:39 -0400

    add support of coarrays find libraries in Ubuntu

--- cmake/FindCoarray.cmake
+++ cmake/FindCoarray.cmake
@@ -7,71 +7,57 @@
 find_package(MPI QUIET COMPONENTS Fortran)
 
 find_program(Coarray_CAF_EXECUTABLE NAMES caf)
-find_program(Coarray_CAFRUN_EXECUTABLE NAMES cafrun)
 
 set(_Coarray_LIBRARY_HINTS)
-set(_Coarray_CAF_MPI_LIBRARY_CANDIDATE)
+set(_Coarray_LIBRARY_FROM_CAF)
+
 if(Coarray_CAF_EXECUTABLE)
     foreach(_caf_show_arg IN ITEMS --show -show)
         execute_process(
             COMMAND "${Coarray_CAF_EXECUTABLE}" "${_caf_show_arg}"
             OUTPUT_VARIABLE _caf_show
-            ERROR_VARIABLE _caf_show_error
+            ERROR_QUIET
             RESULT_VARIABLE _caf_show_result
             OUTPUT_STRIP_TRAILING_WHITESPACE
-            ERROR_STRIP_TRAILING_WHITESPACE
         )
-        string(STRIP "${_caf_show} ${_caf_show_error}" _caf_show)
         if(_caf_show_result EQUAL 0 AND _caf_show)
             break()
         endif()
+        unset(_caf_show)
     endforeach()
+
     if(_caf_show)
         separate_arguments(_caf_show_args UNIX_COMMAND "${_caf_show}")
-        set(_Coarray_NEXT_IS_LIBRARY_DIR OFF)
         foreach(_caf_arg IN LISTS _caf_show_args)
-            if(_Coarray_NEXT_IS_LIBRARY_DIR)
-                list(APPEND _Coarray_LIBRARY_HINTS "${_caf_arg}")
-                set(_Coarray_NEXT_IS_LIBRARY_DIR OFF)
-            elseif(_caf_arg STREQUAL "-L")
-                set(_Coarray_NEXT_IS_LIBRARY_DIR ON)
-            elseif(_caf_arg MATCHES "^-L(.+)")
+            if(_caf_arg MATCHES "^-L(.+)")
                 list(APPEND _Coarray_LIBRARY_HINTS "${CMAKE_MATCH_1}")
-            elseif(_caf_arg MATCHES "libcaf_mpi\\.(a|so|dylib)(\\.[0-9.]+)?$" AND EXISTS "${_caf_arg}")
-                set(_Coarray_CAF_MPI_LIBRARY_CANDIDATE "${_caf_arg}")
+            elseif(_caf_arg MATCHES "^/.*/libcaf_[^/]+\\.(a|so|dylib)(\\.[0-9.]+)?$" AND EXISTS "${_caf_arg}")
+                get_filename_component(_caf_lib_dir "${_caf_arg}" DIRECTORY)
+                list(APPEND _Coarray_LIBRARY_HINTS "${_caf_lib_dir}")
+                set(_Coarray_LIBRARY_FROM_CAF "${_caf_arg}")
             endif()
         endforeach()
     endif()
 endif()
 
-if(_Coarray_CAF_MPI_LIBRARY_CANDIDATE AND NOT Coarray_CAF_MPI_LIBRARY)
-    set(Coarray_CAF_MPI_LIBRARY "${_Coarray_CAF_MPI_LIBRARY_CANDIDATE}" CACHE FILEPATH "OpenCoarrays caf_mpi library" FORCE)
+if(_Coarray_LIBRARY_FROM_CAF AND NOT Coarray_CAF_LIBRARY)
+    set(Coarray_CAF_LIBRARY "${_Coarray_LIBRARY_FROM_CAF}" CACHE FILEPATH "OpenCoarrays runtime library" FORCE)
 endif()
 
-find_library(Coarray_CAF_MPI_LIBRARY
-    NAMES caf_mpi libcaf_mpi
+find_library(Coarray_CAF_LIBRARY
+    NAMES caf_mpi caf_openmpi caf_mpich libcaf_mpi libcaf_openmpi libcaf_mpich
     HINTS ${_Coarray_LIBRARY_HINTS}
 )
 
 set(Coarray_FOUND FALSE)
-if(Coarray_CAF_MPI_LIBRARY AND MPI_Fortran_FOUND)
+if(Coarray_CAF_LIBRARY AND MPI_Fortran_FOUND)
     set(Coarray_FOUND TRUE)
-elseif(NOT Coarray_FIND_QUIETLY)
-    if(NOT MPI_Fortran_FOUND)
-        message(STATUS "Could NOT find Coarray: missing MPI Fortran")
-    elseif(NOT Coarray_CAF_MPI_LIBRARY)
-        message(STATUS "Could NOT find Coarray: missing caf_mpi/libcaf_mpi")
-    endif()
-endif()
-
-if(Coarray_FIND_REQUIRED AND NOT Coarray_FOUND)
-    message(FATAL_ERROR "Could NOT find Coarray (requires caf_mpi/libcaf_mpi and MPI Fortran)")
 endif()
 
 if(Coarray_FOUND AND NOT TARGET Coarray::Coarray)
     add_library(Coarray::Coarray INTERFACE IMPORTED)
     target_link_libraries(Coarray::Coarray INTERFACE
-        "${Coarray_CAF_MPI_LIBRARY}"
+        "${Coarray_CAF_LIBRARY}"
         MPI::MPI_Fortran
     )
     target_compile_options(Coarray::Coarray INTERFACE
@@ -84,6 +70,5 @@ endif()
 
 mark_as_advanced(
     Coarray_CAF_EXECUTABLE
-    Coarray_CAFRUN_EXECUTABLE
-    Coarray_CAF_MPI_LIBRARY
+    Coarray_CAF_LIBRARY
 )
